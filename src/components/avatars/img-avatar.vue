e755974ebcae632674978911af084655151e6438
drsoft28 <dif.rabie@gmail.com>
2022-12-06 19:24:34 +0100

    create image avatar component

---
+++ src/components/avatars/img-avatar.vue
@@ -0,0 +1,24 @@
+<template>
+<img  class="rounded-circle" :src="img" 
+v-bind="$attrs"
+
+:width="`${size}px`" :height="`${size}px`">
+</template>
+
+<script>
+export default {
+ props:{
+    img:{
+        type:String
+    },
+    size:{
+        type:[Number,String],
+        default:50
+    }
+ }
+}
+</script>
+
+<style>
+
+</style>
