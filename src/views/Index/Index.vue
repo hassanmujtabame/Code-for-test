34fe8021a054c22126585d4d482563f0cc2d5792
drsoft28 <dif.rabie@gmail.com>
2022-12-06 20:12:46 +0100

    convert sect 1 to vue file

--- src/views/Index/Index.vue
+++ src/views/Index/Index.vue
@@ -2,30 +2,9 @@
   <div>
     
     
-      <div class="sec-color p-0 m-0 sec-one">
-        <div class="img-sec-one">
-          <img class="img-fluid" src="/assets/img/Group 14510.png" alt="" />
-        </div>
-        <div class="container">
-          <div class="row align-items-center pt-5 pb-5">
-            <div class="col-md-7 text ">
-              <h1 class="fs-1 text-js"> </h1>
-              <p class="fs-4 text-p">
-               
-              </p>
-              <div class="">
-                <button class="btn-custmer">انضمي الان</button>
-                <button class="btn-custmer-w">سجلي دخولك</button>
-              </div>
-            </div>
-            <div class="col-md-5 circle-img m-auto">
-              <div>
-                <img class="landing" src="/assets/img/image.png" alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <!-- section-1 -->
+      <SectMainSlider />
+      <!-- #section-1 -->
       <div class="container">
         <!-- services -->
         <ServicesPart/>
@@ -356,6 +335,7 @@
 </template>
 
 <script>
+import SectMainSlider from './parts/section-1/main-slider.vue'
 import ServicesPart from './parts/services.vue'
 import SectTrainingCourses from './parts/section-5/training-courses.vue'
 import SectPartnerRiadiat from './parts/section-6/partners-riadiat.vue'
@@ -365,6 +345,7 @@ import sectSubscripe from './parts/section-subscripe/sect-subscripe.vue'
 export default {
   name: 'IndexView',
   components: {
+    SectMainSlider,
     ServicesPart,
     SectTrainingCourses,
     SectPartnerRiadiat,
---
+++ src/views/Index/parts/section-1/main-slider.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="sec-color p-0 m-0 sec-one">
+        <div class="img-sec-one">
+          <img class="img-fluid" src="/assets/img/Group 14510.png" alt="" />
+        </div>
+        <div class="container">
+          <div class="row align-items-center pt-5 pb-5">
+            <div class="col-md-7 text ">
+              <h1 class="fs-1 text-js"> </h1>
+              <p class="fs-4 text-p">
+               
+              </p>
+              <div class="">
+                <button class="btn-custmer">انضمي الان</button>
+                <button class="btn-custmer-w">سجلي دخولك</button>
+              </div>
+            </div>
+            <div class="col-md-5 circle-img m-auto">
+              <div>
+                <img class="landing" src="/assets/img/image.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
