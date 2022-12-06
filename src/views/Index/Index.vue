3bbebaa5f9d2002c6b3fceb74aea8f96714f69ae
drsoft28 <dif.rabie@gmail.com>
2022-12-06 19:35:41 +0100

    move section 8 to vue file

--- src/views/Index/Index.vue
+++ src/views/Index/Index.vue
@@ -346,90 +346,9 @@
         </div>
 
       </div>
-      <div style="overflow: hidden;" class="sec-nighn mt-5">
-        <div class="container">
-
-          <div
-          class="d-flex justify-content-between align-items-center container my-2"
-        >
-          <h1>مدونة رياديات    </h1>
-          <div>
-            <button class="more">المزيد</button>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4 " data-aos="fade-right" data-aos-duration="1000">
-            <div class="box rounded-3 border">
-              <div class="image">
-                <img class="w-100" src="/assets/img/Rectangle 1768.png" alt="" height="184">
-              </div>
-              <div class="text p-3">
-                <h6>عنوان المدونة </h6>
-                <p>
-                  نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي
-                </p>
-              </div>
-              <div class="date text-end mx-1 p-3">
-                <p>
-                  10  sep, 2021
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z" fill="#979797"/>
-                    </svg>
-                    
-                </p>
-  
-              </div>
-  
-            </div>
-            
-  
-          </div>
-          <div class="col-md-4 " data-aos="fade-right" data-aos-duration="2000">
-            <div class="box rounded-3 border">
-              <div class="image">
-                <img class="w-100" src="/assets/img/Rectangle 2.png" alt="" height="184">
-              </div>
-              <div class="text p-3">
-                <h6>عنوان المدونة </h6>
-                <p>
-                  نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي
-                </p>
-              </div>
-              <div class="date text-end mx-1 p-3">
-                <p>
-                  10  sep, 2021
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z" fill="#979797"/>
-                    </svg>
-                    
-                </p>
-              </div>
-            </div>            
-          </div>
-          <div class="col-md-4 " data-aos="fade-right" data-aos-duration="3000">
-            <div class="box rounded-3 border">
-              <div class="image">
-                <img class="w-100" src="/assets/img/Rectangle 3.png" alt="" height="184">
-              </div>
-              <div class="text p-3">
-                <h6>عنوان المدونة </h6>
-                <p>
-                  نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي
-                </p>
-              </div>
-              <div class="date text-end mx-1 p-3">
-                <p>
-                  10  sep, 2021
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z" fill="#979797"/>
-                    </svg>        
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        </div>
-      </div>
+     <!-- section 8-->
+     <SectBlog/>
+     <!-- #section 8-->
       <div class="sec-subscripe mt-5">
         <div class="container">
           <div class="row align-items-center justify-content-center p-4">
@@ -460,13 +379,15 @@ import ServicesPart from './parts/services.vue'
 import SectTrainingCourses from './parts/section-5/training-courses.vue'
 import SectPartnerRiadiat from './parts/section-6/partners-riadiat.vue'
 import SectAboutUs from './parts/section-7/about-us.vue'
+import SectBlog from './parts/section-8/set-blog.vue'
 export default {
   name: 'IndexView',
   components: {
     ServicesPart,
     SectTrainingCourses,
     SectPartnerRiadiat,
-    SectAboutUs
+    SectAboutUs,
+    SectBlog
   },
   data:()=>({
     
