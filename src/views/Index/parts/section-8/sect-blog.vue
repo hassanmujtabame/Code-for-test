d85149c74dc5b246c7c26ed5eb4984ecb36fa670
drsoft28 <dif.rabie@gmail.com>
2022-12-06 20:06:49 +0100

    make list dynamic and use blog info card

--- src/views/Index/parts/section-8/sect-blog.vue
+++ src/views/Index/parts/section-8/sect-blog.vue
@@ -10,69 +10,13 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-4 " data-aos="fade-right" data-aos-duration="1000">
-            <div class="box rounded-3 border">
-              <div class="image">
-                <img class="w-100" src="/assets/img/Rectangle 1768.png" alt="" height="184">
-              </div>
-              <div class="text p-3">
-                <h6>عنوان المدونة </h6>
-                <p>
-                  نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي
-                </p>
-              </div>
-              <div class="date text-end mx-1 p-3">
-                <p>
-                  10  sep, 2021
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z" fill="#979797"/>
-                    </svg>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4 " data-aos="fade-right" data-aos-duration="2000">
-            <div class="box rounded-3 border">
-              <div class="image">
-                <img class="w-100" src="/assets/img/Rectangle 2.png" alt="" height="184">
-              </div>
-              <div class="text p-3">
-                <h6>عنوان المدونة </h6>
-                <p>
-                  نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي
-                </p>
-              </div>
-              <div class="date text-end mx-1 p-3">
-                <p>
-                  10  sep, 2021
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z" fill="#979797"/>
-                    </svg>
-                    
-                </p>
-              </div>
-            </div>            
-          </div>
-          <div class="col-md-4 " data-aos="fade-right" data-aos-duration="3000">
-            <div class="box rounded-3 border">
-              <div class="image">
-                <img class="w-100" src="/assets/img/Rectangle 3.png" alt="" height="184">
-              </div>
-              <div class="text p-3">
-                <h6>عنوان المدونة </h6>
-                <p>
-                  نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي
-                </p>
-              </div>
-              <div class="date text-end mx-1 p-3">
-                <p>
-                  10  sep, 2021
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z" fill="#979797"/>
-                    </svg>        
-                </p>
-              </div>
-            </div>
+          <div v-for="(blog,i) in blogs" :key="i" class="col-md-4 " data-aos="fade-right" data-aos-duration="1000">
+           <BlogInfoCard 
+            :img="blog.img"
+            :title="(blog.title+' '+i)"
+            :description="blog.description"
+            :date="blog.date"
+           />
           </div>
         </div>
         </div>
@@ -80,8 +24,19 @@
 </template>
 
 <script>
+import BlogInfoCard from '@/components/cards/blog-info.vue';
 export default {
+ components:{
+    BlogInfoCard
+ },
+ data:()=>({
+    blogs:[
+        {title:'عنوان المدونة',img:'/assets/img/Rectangle 3.png',date:'10  sep, 2021',description:'نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي'},
+        {title:'عنوان المدونة',img:'/assets/img/Rectangle 3.png',date:'10  sep, 2021',description:'نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي'},
+        {title:'عنوان المدونة',img:'/assets/img/Rectangle 3.png',date:'10  sep, 2021',description:'نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي نص  تعريفي'},
 
+    ]
+ })
 }
 </script>
 
