dd2232c93a23ba15b37fff07dd9fc494614dcf94
drsoft28 <dif.rabie@gmail.com>
2022-12-06 19:25:23 +0100

    convert list partner from static to dynamic list

--- src/views/Index/parts/section-6/partners-riadiat.vue
+++ src/views/Index/parts/section-6/partners-riadiat.vue
@@ -2,50 +2,33 @@
     <div class="sec-six">
         <div class="container p-3">
             <h2>شركائنا في رياديات </h2>
-
             <div class="row justify-content-center">
-                <div class=" col-12 col-md-3 text-center" data-aos="fade-up" data-aos-duration="1000">
-                 <img  class="rounded-circle" src="/assets/img/Ellipse 24.png" alt="" width="180px" height="180px">
-
+                <div v-for="(partner,i) in partners" :key="i" class=" col-12 col-md-3 text-center" data-aos="fade-up" data-aos-duration="2000">
+                    <ImgAvatar :img="partner.img" :alt="partner.name" size=180 />
                 </div>
-                <div class=" col-12 col-md-3 text-center" data-aos="fade-up" data-aos-duration="2000">
-                    <img sr class="rounded-circle" c="/assets/img/Ellipse 25.png" alt="" width="180px" height="180px">
-   
-                </div>
-                <div class=" col-12 col-md-3 text-center" data-aos="fade-up" data-aos-duration="3000">
-                    <img class="rounded-circle" src="/assets/img/Ellipse 24.png" alt="" width="180px" height="180px">
-   
-                   </div>
-                   <div class=" col-12 col-md-3 text-center" data-aos="fade-up" data-aos-duration="3000">
-                    <img sr class="rounded-circle" c="/assets/img/Ellipse 25.png" alt="" width="180px" height="180px">
-   
-                </div>
-                <div class=" col-12 col-md-3 text-center" data-aos="fade-up" data-aos-duration="1000">
-                    <img class="rounded-circle" src="/assets/img/Ellipse 24.png" alt="" width="180px" height="180px">
-   
-                   </div>
-                   <div class=" col-12 col-md-3 text-center" data-aos="fade-up" data-aos-duration="2000">
-                       <img class="rounded-circle"  src="/assets/img/Ellipse 25.png" alt="" width="180px" height="180px">
-      
-                   </div>
-                   <div class=" col-12 col-md-3 text-center" data-aos="fade-up" data-aos-duration="3000">
-                       <img class="rounded-circle" src="/assets/img/Ellipse 24.png" alt="" width="180px" height="180px">
-      
-                      </div>
-                      <div class=" col-12 col-md-3 text-center" data-aos="fade-up" data-aos-duration="3000">
-                       <img class="rounded-circle"  src="/assets/img/Ellipse 25.png" alt="" width="180px" height="180px">
-      
-                   </div>
-    
             </div>
         </div>
-        
       </div>
 </template>
 
 <script>
+import ImgAvatar from '@/components/avatars/img-avatar.vue';
 export default {
-
+    components:{
+        ImgAvatar
+    },
+ data:()=>({
+    partners:[
+        {img:'/assets/img/Ellipse 24.png',name:'partner 01'},
+        {img:'/assets/img/Ellipse 25.png',name:'partner 02'},
+        {img:'/assets/img/Ellipse 24.png',name:'partner 03'},
+        {img:'/assets/img/Ellipse 25.png',name:'partner 04'},
+        {img:'/assets/img/Ellipse 24.png',name:'partner 05'},
+        {img:'/assets/img/Ellipse 25.png',name:'partner 06'},
+        {img:'/assets/img/Ellipse 24.png',name:'partner 07'},
+        {img:'/assets/img/Ellipse 25.png',name:'partner 08'},
+    ]
+ })
 }
 </script>
 
