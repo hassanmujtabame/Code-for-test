34b78229f827d7ef031e3aa12681cad708c27f7b
drsoft28 <dif.rabie@gmail.com>
2022-12-08 11:10:56 +0100

    decompose network home page to sections

---
+++ src/views/network/home/parts/section-1/index.vue
@@ -0,0 +1,49 @@
+<template>
+   <div class="consulting p-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1 class="fs-1 text-white">
+                            شبكة رياديات 
+                                                </h1>
+                        <p class="text-white">
+                            طموحة و تسعين لتحقيق حلمك و شغفك ؟
+                            شبكة رياديات بتساعدك في مشروعك من الالف الى الياء
+                         </p>
+                         <div class="btn-main-style">
+                          <a href="subscribe.html" class="btn-custmer shadow mt-2">انضمي الان</a>
+                          <a class="btn-custmer-w bg-transparent text-white mt-2">سجلي دخولك</a>
+                         </div>
+                    </div>
+                    <div class="col-md-6 m-auto">
+                        <div class="img-cons m-auto ">
+                            <img style="    border-radius: 50%;
+                            position: absolute;
+                            width: 300px;
+                            height: 300px;" src="/assets/img/1664179507602 aefwG.png" alt="" >
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="star-cons">
+                    <img src="/assets/img/starrrr.png" alt="">
+                </div>
+                <div class="star-cons stars">
+                    <img src="/assets/img/starsss.png" alt="">
+                </div>
+
+            </div>
+
+        </div>
+</template>
+
+<script>
+export default {
+    name:'section-1'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/network/home/parts/section-5/index.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="sec-five mt-5 p-3">
+            <div class="">
+              <div
+                class="d-flex justify-content-between align-items-center container"
+              >
+                <h1>أعضاء مميزات    </h1>
+                <div>
+                  <button class="more">المزيد</button>
+                </div>
+              </div>
+              <div
+                style="overflow-x: hidden"
+                swiperRef=""
+                class="swiper-container mySwiper main-swiper"
+              >
+                <div
+                 
+                  class="swiper-wrapper"
+                >
+                  <div class="swiper-slide rounded-3">
+                    <div class="box ">
+                        <div class="">
+                            <img  src="/assets/img/Rectangle 1775qa.png" alt="" width="259" height="192">
+                        </div>
+                        <div class="text bg-white ">
+                            <h6> العنود محمد </h6>
+                            <p class="m-c"> التصوير  </p>
+
+                        </div>
+
+                    </div>
+                 
+                  </div>
+                  <div class="swiper-slide rounded-3">
+                    <div class="box ">
+                        <div class="">
+                            <img  src="/assets/img/Rectangle 1775qa.png" alt="" width="259" height="192">
+                        </div>
+                        <div class="text bg-white ">
+                            <h6> العنود محمد </h6>
+                            <p class="m-c"> التصوير  </p>
+
+                        </div>
+
+                    </div>
+                 
+                  </div>
+                  <div class="swiper-slide rounded-3">
+                    <div class="box ">
+                        <div class="">
+                            <img  src="/assets/img/Rectangle 1775qa.png" alt="" width="259" height="192">
+                        </div>
+                        <div class="text bg-white ">
+                            <h6> العنود محمد </h6>
+                            <p class="m-c"> التصوير  </p>
+
+                        </div>
+
+                    </div>
+                 
+                  </div>
+                  <div class="swiper-slide rounded-3">
+                    <div class="box ">
+                        <div class="">
+                            <img  src="/assets/img/Rectangle 1775qa.png" alt="" width="259" height="192">
+                        </div>
+                        <div class="text bg-white ">
+                            <h6> العنود محمد </h6>
+                            <p class="m-c"> التصوير  </p>
+
+                        </div>
+
+                    </div>
+                 
+                  </div>
+                  <div class="swiper-slide rounded-3">
+                    <div class="box ">
+                        <div class="">
+                            <img  src="/assets/img/Rectangle 1775qa.png" alt="" width="259" height="192">
+                        </div>
+                        <div class="text bg-white ">
+                            <h6> العنود محمد </h6>
+                            <p class="m-c"> التصوير  </p>
+
+                        </div>
+
+                    </div>
+                 
+                  </div>
+                  <div class="swiper-slide rounded-3">
+                    <div class="box ">
+                        <div class="">
+                            <img  src="/assets/img/Rectangle 1775qa.png" alt="" width="259" height="192">
+                        </div>
+                        <div class="text bg-white ">
+                            <h6> العنود محمد </h6>
+                            <p class="m-c"> التصوير  </p>
+
+                        </div>
+
+                    </div>
+                 
+                  </div>
+                  <div class="swiper-slide rounded-3">
+                    <div class="box ">
+                        <div class="">
+                            <img  src="/assets/img/Rectangle 1775qa.png" alt="" width="259" height="192">
+                        </div>
+                        <div class="text bg-white ">
+                            <h6> العنود محمد </h6>
+                            <p class="m-c"> التصوير  </p>
+
+                        </div>
+
+                    </div>
+                 
+                  </div>
+                  <div class="swiper-slide rounded-3">
+                    <div class="box ">
+                        <div class="">
+                            <img  src="/assets/img/Rectangle 1775qa.png" alt="" width="259" height="192">
+                        </div>
+                        <div class="text bg-white ">
+                            <h6> العنود محمد </h6>
+                            <p class="m-c"> التصوير  </p>
+
+                        </div>
+
+                    </div>
+                 
+                  </div>
+           
+                </div>
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+              </div>
+            </div>
+          </div>
+</template>
+
+<script>
+export default {
+ name:'section-5'
+}
+</script>
+
+<style>
+
+</style>
