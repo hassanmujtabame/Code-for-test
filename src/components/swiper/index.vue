875b7ac2c948ce737ecf098150ddfd61902f24bb
drsoft28 <dif.rabie@gmail.com>
2022-12-08 18:31:36 +0100

    create swiper local component

---
+++ src/components/swiper/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <swiper
+             v-bind="$attrs"
+            >
+       
+            <SwiperSlide v-for="(item,i) in items"  :key="i" >
+               <slot :item="item">
+               </slot>
+                </SwiperSlide>
+            
+                </swiper>
+</template>
+
+<script>
+ import { Navigation, Pagination } from 'swiper'
+
+import { SwiperCore, Swiper, SwiperSlide } from 'swiper-vue2'
+// Import Swiper styles
+import 'swiper/swiper-bundle.css'
+SwiperCore.use([Navigation, Pagination])
+export default {
+ name:'d-swiper',
+ props:{
+    items:{
+        type:Array,
+        require:true
+    }
+ },
+ components:{
+    Swiper, SwiperSlide
+ }
+}
+</script>
+
+<style>
+
+</style>
