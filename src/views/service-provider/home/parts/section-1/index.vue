3c1104d020316e1c297a3628a7e86366e29f2506
drsoft28 <dif.rabie@gmail.com>
2022-12-08 20:15:37 +0100

    decompose service provider page to section

---
+++ src/views/service-provider/home/parts/section-1/index.vue
@@ -0,0 +1,50 @@
+<template>
+ <div class="consulting p-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1 class="fs-1 text-white">
+                            مقدمين الخدمات
+                                                                        </h1>
+                        <p class="text-white">
+                            منصة تجمع أهم وألمع  رائدين و رائدات الأعمال في السعودية والخليج في مكان واحد
+                                                </p>
+                                                <div class="btn-main-style">
+                                                    <a href="subscribe.html" class="btn-custmer shadow mt-2">انضمي الان</a>
+                                                    <a class="btn-custmer-w bg-transparent text-white mt-2">سجلي دخولك</a>
+                                                </div>
+                    </div>
+                    <div class="col-md-6 m-auto">
+                        <div style="overflow: inherit;" class="img-cons m-auto ">
+                            <img style="  
+                            position: absolute;
+                              width: 341px;
+                            height: 341px;
+                            top: 7px;
+                            left: -36px;" src="/assets/img/1664280407882 1.png" alt="" >
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="star-cons">
+                    <img src="/assets/img/starrrr.png" alt="">
+                </div>
+                <div class="star-cons stars">
+                    <img src="/assets/img/starsss.png" alt="">
+                </div>
+
+            </div>
+
+        </div>
+</template>
+
+<script>
+export default {
+ name:'section-1'
+}
+</script>
+
+<style>
+
+</style>
