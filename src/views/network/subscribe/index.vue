d2ca61d36d007f35dfbecb19eb75689f611b58ba
drsoft28 <dif.rabie@gmail.com>
2022-12-08 16:59:09 +0100

    uses modal component in subscribe

--- src/views/network/subscribe/index.vue
+++ src/views/network/subscribe/index.vue
@@ -11,11 +11,7 @@
                     <div class="col-md-6">
                         <img class="img-fluid" src="/assets/img/_x36__1_.png" alt="">
                     </div>
-                    <div style="left: 2px;
-                    bottom: 14px;
-                    width: fit-content;
-                    transform: rotate(127deg);
-                " class="position-absolute">
+                    <div style="left: 2px; bottom: 14px;width: fit-content;transform: rotate(127deg)" class="position-absolute">
                         <img src="/assets/img/starrrr.png" alt="">
                     </div>
 
@@ -25,171 +21,88 @@
             <div class="shadow bg-white rounded-bottom">
                 <div class="row p-3">
                     <div class="col-md-4  mt-2 s">
-                        <div class="box-subscribe  border shadow p-5 text-center rounded-2 bg-white d-flex flex-column justify-content-between">
+                        <SubscribeCard
+                        title="الاشتراك العادي"
+                        price="مجاناً"
+                        :features="['تواصلي مع أعضاء الشبكة','شاركي في اللقاءات المستمرة لرواد الشبكة','تواصلي مع أعضاء الشبكة']"
+                        type-subscribe="normal"
+                        @selected="choose"
+                        ></SubscribeCard>
                      
-
-                                <div>
-    
-                                    <h2 class="fw-bolder">
-                                        الاشتراك العادي
-                                    </h2>
-                                    <h1 class="y-c fw-bolder fs-1 py-4">
-                                        مجاناً
-                                    </h1>
-                                    <div class="text-start">
-        
-                                        <p>
-                                            <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
-                                            </svg>
-                                            تواصلي مع أعضاء الشبكة
-                                                
-                                        </p>
-                                        <p>
-                                            <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
-                                            </svg>
-                                            شاركي في اللقاءات المستمرة لرواد الشبكة                                    
-                                        </p>
-                                        <p>
-                                            <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
-                                            </svg>
-                                            تواصلي مع أعضاء الشبكة                                    
-                                        </p>
-                                    </div>
-                                </div>
-                                <div >
-                                    <a href="../network/subscribe-finish.html" class="btn-main px-4 py-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                        اشتركي الآن
-                                    </a>
-                                </div>
-                         
-                            </div>
                     </div>
                     <div class="col-md-4 mt-2">
-                        <div class="box-subscribe  border shadow p-5 text-center rounded-2 bg-white d-flex flex-column justify-content-between">
-                     
-
-                                <div>
-    
-                                    <h2 class="fw-bolder">
-                                        الاشتراك الشهري
-                                    </h2>
-                                    <h1 class="y-c fw-bolder fs-1 py-4">
-                                        <span class="y-c">
-                                            1500
-                                        </span>
-                                        ريال
-                                    </h1>
-                                    <div class="text-start">
-        
-                                        <p>
-                                            <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
-                                            </svg>
-                                            تواصلي مع أعضاء الشبكة
-                                                
-                                        </p>
-                                        <p>
-                                            <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
-                                            </svg>
-                                            شاركي في اللقاءات المستمرة لرواد الشبكة                                    
-                                        </p>
-                                        <p>
-                                            <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
-                                            </svg>
-                                            تواصلي مع أعضاء الشبكة                                    
-                                        </p>
-                                        <p>
-                                            <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
-                                            </svg>
-                                            شاركي في اللقاءات المستمرة لرواد الشبكة                                        </p>
-                                    </div>
-                                </div>
-                                <div >
-                                    <a href="../network/subscribe-finish.html" class="btn-main px-4 py-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                        اشتركي الآن
-                                    </a>
-                                </div>
-                         
-                            </div>
+                        <SubscribeCard
+                        title="الاشتراك الشهري"
+                        price="500 يال"
+                        :features="['تواصلي مع أعضاء الشبكة','شاركي في اللقاءات المستمرة لرواد الشبكة','تواصلي مع أعضاء الشبكة']"
+                        type-subscribe="monthly"
+                        @selected="choose"
+                        ></SubscribeCard>
                     </div>
                     <div class="col-md-4 mt-2">
-                        <div class="box-subscribe  border shadow p-5 text-center rounded-2 bg-white d-flex flex-column justify-content-between">
-                     
-
-                                <div>
-    
-                                    <h2 class="fw-bolder">
-                                        الاشتراك العادي
-                                    </h2>
-                                    <h1 class="y-c fw-bolder fs-1 py-4">
-                                        <span class="y-c">
-                                            5500
-                                        </span>
-                                        ريال
-                                    </h1>
-                                    <div class="text-start">
-        
-                                        <p>
-                                            <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
-                                            </svg>
-                                            تواصلي مع أعضاء الشبكة
-                                                
-                                        </p>
-                                        <p>
-                                            <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
-                                            </svg>
-                                            شاركي في اللقاءات المستمرة لرواد الشبكة                                    
-                                        </p>
-                                        <p>
-                                            <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
-                                            </svg>
-                                            تواصلي مع أعضاء الشبكة                                    
-                                        </p>
-                                        <p>
-                                            <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
-                                            </svg>
-                                            شاركي في اللقاءات المستمرة لرواد الشبكة                                  
-                                        </p>          <p>
-                                            <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
-                                            </svg>
-                                            تواصلي مع أعضاء الشبكة                                    
-                                        </p>          <p>
-                                            <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
-                                            </svg>
-                                            شاركي في اللقاءات المستمرة لرواد الشبكة                                        </p>
-                                    </div>
-                                </div>
-                                <div >
-                                    <a href="../network/subscribe-finish.html" class="btn-main px-4 py-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                        اشتركي الآن
-                                    </a>
-                                </div>
-                         
-                            </div>
+                        <SubscribeCard
+                        title="الاشتراك السنوي"
+                        price="1500 ريال"
+                        :features="['تواصلي مع أعضاء الشبكة','شاركي في اللقاءات المستمرة لرواد الشبكة','تواصلي مع أعضاء الشبكة']"
+                        type-subscribe="annual"
+                        @selected="choose"
+                        ></SubscribeCard>
                     </div>
                 </div>
             </div>
 
         </div>
+        <DialogSubscribe :show.sync="show">
+                <template>
+          <p class="px-3">
+            حتى تتمكن من الانضمام الى باقي خدمتنا
+            (
+            <a href="home.html" class="m-c">
+                الاكاديمية
+            </a>
+            او
+            <a href="../Incubator/home.html" class="m-c">
+                الحاضنة
+            </a>
+            او
+            <a href="../service-provider/home.html" class="m-c">
+                مقدمي الخدمة
+            </a>
+            )
+            يجب ان تكون عضو في شبكة رياديات  أولا
+            زر الصفحة التعريفية
+            <a href="../index.html" class="m-c">
+                لشبكة رياديات
 
+            </a>
+       
+          </p>
+          </template>
+        <template v-slot:actions>
+            <a href="home.html" type="button" class="btn btn-main py-2 h-100 w-50" >الاستمرار</a>
+          <button type="button" class="btn  border-danger text-danger w-50" data-bs-dismiss="modal">تراجع عن التسجيل</button>
+        </template>
+                </DialogSubscribe>
     </div>
 </template>
 
 <script>
+import SubscribeCard from './subscribe-card.vue';
+import DialogSubscribe from '@/components/modals/simple.vue'
 export default {
-name:'network-subscribe'
+name:'network-subscribe',
+components:{
+    SubscribeCard,
+    DialogSubscribe
+},
+  data:()=>({
+    show:false,
+  }),
+  methods:{
+    choose(type){
+        this.show=true
+    }
+  }
 }
 </script>
 
---
+++ src/views/network/subscribe/subscribe-card.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="box-subscribe  border shadow p-5 text-center rounded-2 bg-white d-flex flex-column justify-content-between">
+                     
+
+                     <div>
+
+                         <h2 class="fw-bolder">{{title}}</h2>
+                         <h1 class="y-c fw-bolder fs-1 py-4">
+                             {{price}}
+                         </h1>
+                         <div class="text-start">
+
+                             <p v-for="(feature,i) in features" :key="i">
+                                 <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                     <path d="M4.53915 7.60486L1.5259 4.59162L0.521484 5.59604L4.53915 9.6137L13.1484 1.00442L12.144 0L4.53915 7.60486Z" fill="#1FB9B3"/>
+                                 </svg>
+                                {{feature}}
+                                </p>
+                         </div>
+                     </div>
+                     <div >
+                         <a @click="selected" href="#" class="btn-main px-4 py-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                             اشتركي الآن
+                         </a>
+                     </div>
+            
+                 </div>
+</template>
+
+<script>
+
+export default {
+  props:{
+    title:{
+        type:String,
+    },
+    price:{
+        type:String
+    },
+    typeSubscribe:{
+        type:String
+    },
+    features:{
+        type:[Array,Object],
+        
+    }
+  },
+  methods:{
+    selected(){
+        this.$emit('selected',this.typeSubscribe)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
