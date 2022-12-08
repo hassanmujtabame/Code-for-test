cccf0573e58514e8804ee4d6917f68255e3726e0
drsoft28 <dif.rabie@gmail.com>
2022-12-08 16:58:25 +0100

    add subscribe finish page

--- src/router/network.js
+++ src/router/network.js
@@ -11,6 +11,12 @@ const routers =[
         meta:{layout:LayoutDefault},
         name: 'network-subscribe',
         component: () => import(/* webpackChunkName: "network-subscribe" */ '../views/network/subscribe/index.vue') 
+    },
+    {
+        path: '/network/subscribe/finish',
+        meta:{layout:LayoutDefault},
+        name: 'network-subscribe-finish',
+        component: () => import(/* webpackChunkName: "network-subscribe-finish" */ '../views/network/SubscribeFinish.vue') 
     }
 ]
 export default routers;
