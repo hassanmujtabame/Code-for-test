299713a2fb4bf15b96730aec47883f9626c0c831
drsoft28 <dif.rabie@gmail.com>
2022-12-08 17:25:12 +0100

    add incubator subscribe page

--- src/router/incubator.js
+++ src/router/incubator.js
@@ -5,6 +5,12 @@ const routers =[
         meta:{layout:LayoutDefault},
         name: 'incubator-home',
         component: () => import(/* webpackChunkName: "incubator-home" */ '../views/incubator/home/index.vue') 
+    },
+    {
+        path: '/incubator/subscribe',
+        meta:{layout:LayoutDefault},
+        name: 'incubator-subscribe',
+        component: () => import(/* webpackChunkName: "incubator-subscribe" */ '../views/incubator/subscribe/index.vue') 
     }
 ]
 export default routers;
