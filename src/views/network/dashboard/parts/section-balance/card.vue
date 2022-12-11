2c0bb3ae24c1d816321afcf0b0d2796e2847fbdf
drsoft28 <dif.rabie@gmail.com>
2022-12-11 18:01:25 +0100

    فصل جزء خاص بالرصيد عن الصفحة

---
+++ src/views/network/dashboard/parts/section-balance/card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p>
+              <img :src="img">
+                {{title}}
+            </p>
+            <h3 class=" fw-bolder" :class="classValue">
+                0 ر.س
+            </h3>
+
+  </div>
+</template>
+
+<script>
+export default {
+name:'card-vue',
+props:{
+    title:{
+        type:String,
+        require:true
+    },
+    img:{
+        type:String,
+        require:true
+    },
+    classValue:{
+        type:String,
+        default:'m-c'
+    },
+    value:{
+        default:''
+    },
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/network/dashboard/parts/section-balance/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="row border mt-5">
+        <div class="col-md-4 border-end  text-center p-2 ">
+    <balanceCard 
+    title="الرصيد الكلي" 
+    :value="0" 
+    img="/assets/svg/balance-total.svg" 
+    />
+    </div>
+    <div class="col-md-4 border-end  text-center p-2 ">
+    <balanceCard 
+    title="الرصيد المعلق" 
+    :value="0" 
+    img="/assets/svg/balance-outstanding.svg"
+    class-value="text-danger" 
+    />
+    </div>
+    <div class="col-md-4 border-end  text-center p-2 ">
+    <balanceCard 
+    title="الرصيد المتاح" 
+    :value="0" 
+    class-value="text-warning"
+    img="/assets/svg/balance-available.svg" 
+    />
+    </div>
+  </div>
+</template>
+
+<script>
+import balanceCard from './card.vue';
+export default {
+ name:'section-balance',
+ components:{
+  balanceCard
+ }
+}
+</script>
+
+<style>
+
+</style>
