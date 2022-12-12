dde8153ac042112ea2a1da85a4978a3ed69f6e54
drsoft28 <dif.rabie@gmail.com>
2022-12-12 20:59:33 +0100

    load list from backend

--- src/views/network/exhibitions/parts/section-filter-exhibitions/index.vue
+++ src/views/network/exhibitions/parts/section-filter-exhibitions/index.vue
@@ -228,9 +228,9 @@
                     <button @click="changePage(p)" class="page-link border-0" href="#">{{p}}</button>
                 </li>
                 
-                <li class="page-item">
+                <li class="page-item" :class="{disabled:(metaInfo.current_page==metaInfo.total_page)}">
                     <button @click="changePage(metaInfo.total_page)" class="page-link" href="#"
-                    :class="{disabled:(metaInfo.current_page==metaInfo.total_page)}"
+                    
                     >
                         <svg width="10" height="15" viewBox="0 0 10 15"
                             fill="none" xmlns="http://www.w3.org/2000/svg">
