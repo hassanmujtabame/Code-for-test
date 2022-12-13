5838975fe8e9a2bfd70e35d61e251aa122cf7940
drsoft28 <dif.rabie@gmail.com>
2022-12-13 13:13:43 +0100

    install js cookie

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
+import Cookies from 'js-cookie';
 import VueI18n from 'vue-i18n'
-
+let lang = Cookies.get('i18n_lang') || process.env.VUE_APP_I18N_LOCALE || 'ar';
 Vue.use(VueI18n)
 
 function loadLocaleMessages () {
-  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+  const locales = require.context('@/locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
   const messages = {}
   locales.keys().forEach(key => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i)
@@ -17,7 +18,7 @@ function loadLocaleMessages () {
 }
 
 export default new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'ar',
+  locale:lang,
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'ar',
   messages: loadLocaleMessages()
 })
