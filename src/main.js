b32f5869e3ac21ffee02fca57ae6425914aaa837
drsoft28 <dif.rabie@gmail.com>
2022-12-13 13:10:53 +0100

    move i18n to plugin folder

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ window.AOS=AOS
 //import installI18n from './lang/index'
 import router from './router'
 
-import i18n from './i18n'
+import i18n from './plugins/i18n'
 import store from './store'
 
 Vue.config.productionTip = false
