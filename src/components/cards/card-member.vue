69b2ab95f90f9a01cf310ff8f5215d30f70a8fb0
drsoft28 <dif.rabie@gmail.com>
2022-12-14 11:51:05 +0100

    add text-center to footer

--- src/components/cards/card-member.vue
+++ src/components/cards/card-member.vue
@@ -3,7 +3,7 @@
                         <div class="">
                             <img  :src="img" alt="" width="259" height="192">
                         </div>
-                        <div class="text bg-white " :class="{'p-3':!description}">
+                        <div class="text-center bg-white " :class="{'p-3':!description}">
                             <h6>{{name}}</h6>
                             <p v-if="description" class="m-c">{{description}}</p>
 
