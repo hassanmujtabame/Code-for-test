47bdae018d2d9bf37df857c65fee17b67bad9ce8
drsoft28 <dif.rabie@gmail.com>
2022-12-14 11:40:42 +0100

    fix issue carousel

--- src/views/network/home/parts/section-hear/index.vue
+++ src/views/network/home/parts/section-hear/index.vue
@@ -73,7 +73,26 @@
 
 <script>
 export default {
-    name:'hear-about-us'
+    name:'hear-about-us',
+ mounted(){
+    $('.feature-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    rtl:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+ }
 }
 </script>
 
