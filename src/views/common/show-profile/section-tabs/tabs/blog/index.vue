91d346cc4e3cbde132d4a53c1b17e2f3069b0885
drsoft28 <dif.rabie@gmail.com>
2022-12-15 17:13:09 +0100

    تصحيح صفحة عرض بروفيل

---
+++ src/views/common/show-profile/section-tabs/tabs/blog/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+   <ItemCard />
+   <ItemCard />
+   <ItemCard />
+  </div>
+</template>
+
+<script>
+import ItemCard from './card.vue'
+export default {
+ name:'blog-tab',
+ components:{
+    ItemCard
+ }
+}
+</script>
+
+<style>
+
+</style>
