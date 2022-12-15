eaa70777b9d6ab7cee22c271745cba44d02d18ad
drsoft28 <dif.rabie@gmail.com>
2022-12-15 15:46:35 +0100

    create calendar as component

---
+++ src/components/calendar/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div style="margin: -51px 0;" :id="id" class=" e "></div>
+</template>
+
+<script>
+export default {
+ name:'calendar-simple',
+ data:()=>({
+  id:'calendar-'+Math.random().toString().substring(2,16)
+ }),
+ mounted(){
+    $("#"+this.id).simpleCalendar({
+  displayEvent: true,
+  events: [
+      // generate new event after tomorrow for one hour
+      {
+          startDate: new Date(new Date().setHours(new Date().getHours() + 24)).toDateString(),
+          endDate: new Date(new Date().setHours(new Date().getHours() + 25)).toISOString(),
+          summary: 'Visit of the Eiffel Tower'
+      },
+      // generate new event for yesterday at noon
+      {
+          startDate: new Date(new Date().setHours(new Date().getHours() - new Date().getHours() - 12, 0)).toISOString(),
+          endDate: new Date(new Date().setHours(new Date().getHours() - new Date().getHours() - 11)).getTime(),
+          summary: 'Restaurant'
+      },
+      // generate new event for the last two days
+      {
+          startDate: new Date(new Date().setHours(new Date().getHours() - 48)).toISOString(),
+          endDate: new Date(new Date().setHours(new Date().getHours() - 24)).getTime(),
+          summary: 'Visit of the Louvre'
+      }
+  ],
+  months: ['يناير', 'فبراير', 'مارس', 'ابريل', 'مايو', 'يونيو', 'يوليو', 'اغسطس', 'سبتمبر', 'اكتوبر', 'نوفمبر', 'ديسمبر'],
+})
+
+ }
+}
+</script>
+
+<style>
+
+</style>
--- src/plugins/components.js
+++ src/plugins/components.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
 
 import DSwiper from '@/components/swiper/index.vue'
+import DCalendar from '@/components/calendar/index.vue'
 Vue.component('d-swiper', DSwiper)
+Vue.component('d-calendar', DCalendar)
