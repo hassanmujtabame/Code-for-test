7d5dc757bb8a22d236ae69eabfef500a39f1582b
drsoft28 <dif.rabie@gmail.com>
2022-12-19 17:57:57 +0100

    add section auth header

---
+++ src/views/academy/home/parts/section-auth-header/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="consulting ">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-md-6">
+                    <h1 class="fs-1 text-white">
+                        اكاديمية رياديات </h1>
+                    <p class="text-white">
+                        طوري مهاراتك و أنطلقي في عالم ريادة الاعمال والعمل الحر , أكاديمية رياديات ستكون معكِ خطوة بخطوة
+
+                        ويمكن من خلالها الحصول على المساعدة والتشجيع للدخول في مجال ريادة الأعمال من خلال مجموعة من
+                        الخدمات والمزايا
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <div style="overflow: inherit;" class=" m-auto text-end">
+                        <img class="img-fluid" src="/assets/img/Group 1171274931s.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+ name:'section-auth-header'
+}
+</script>
+
+<style>
+
+</style>
