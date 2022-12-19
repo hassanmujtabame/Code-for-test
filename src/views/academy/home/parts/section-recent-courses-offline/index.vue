55b2a4285d393aa3e58a00c9323d17d969498d26
drsoft28 <dif.rabie@gmail.com>
2022-12-19 18:09:19 +0100

    add section recent course offline

---
+++ src/views/academy/home/parts/section-recent-courses-offline/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class=" mt-5 p-3">
+            <div class="">
+              <div
+                class="d-flex justify-content-between align-items-center container"
+              >
+              <h1> أحدث الدورات المسجلة    </h1>
+                <div>
+                    <button class="more">
+                      <a href="" class="text-dark">
+                          المزيد
+                      </a>
+                    </button>
+                  </div>
+              </div>
+              <d-swiper
+              style="overflow-x: hidden"
+            :slides-per-view="4"
+            :space-between="10"
+              :items="items"
+            >
+       
+            <template  v-slot:default="{item}" >
+                <CardVue :title="item.title" :img="item.img"/>
+                </template>
+              </d-swiper>
+            </div>
+          </div>
+</template>
+
+<script>
+import CardVue from './card.vue'
+
+export default {
+    name:'section-recent-courses',
+    components:{
+        CardVue
+    },
+    data:()=>({
+        items:[
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {},
+        ]
+    })
+}
+</script>
+
+<style>
+
+</style>
