1ba84375c94822fad48e62598de67538a2eb5938
drsoft28 <dif.rabie@gmail.com>
2022-12-19 21:10:53 +0100

    make some components as global components

--- src/plugins/components.js
+++ src/plugins/components.js
@@ -4,7 +4,11 @@ import DSwiper from '@/components/swiper/index.vue'
 import DCalendar from '@/components/calendar/index.vue'
 import DCarousel from '@/components/carousel/index.vue'
 import DCarouselSlide from '@/components/carousel/slide.vue'
+import DFilterList from '@/components/filter-list/index.vue'
+import RSliderInput from '@/components/inputs/r-slider/index.vue'
 Vue.component('d-swiper', DSwiper)
 Vue.component('d-calendar', DCalendar)
 Vue.component('d-carousel', DCarousel)
 Vue.component('d-carousel-slide', DCarouselSlide)
+Vue.component('d-filter-list', DFilterList)
+Vue.component('rslider-input', RSliderInput)
