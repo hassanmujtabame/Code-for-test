89660effb98f68a8b3f08f6aefda3f3e6d4647a4
drsoft28 <dif.rabie@gmail.com>
2022-12-19 12:52:38 +0100

    create overlay components

---
+++ src/components/overlays/simple.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="overlay">
+    <i class="fas fa-spinner fa-fw fa-2x fa-spin" style="color:white"></i>
+  </div>
+</template>
+
+<script>
+export default {
+ name:'over-lay'
+}
+</script>
+
+<style>
+.overlay{
+    position: absolute;
+    width:100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: rgba(0, 0, 0, 0.377);
+    z-index: 999999;
+}
+</style>
