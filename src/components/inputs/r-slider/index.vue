fd6f9d3e9a714360be9d3415090a8425d7484133
drsoft28 <dif.rabie@gmail.com>
2022-12-19 21:09:37 +0100

    create r-slider input component

---
+++ src/components/inputs/r-slider/index.vue
@@ -0,0 +1,66 @@
+<template>
+   <input type="text" :id="id" class="slider" />
+</template>
+
+<script>
+export default {
+ name:'rslider-input',
+ props:{
+  min:{
+    type:[Number,String],
+    default:0
+  },
+  max:{
+    type:[Number,String],
+    default:100
+  },
+  lmin:{
+    type:[Number,String],
+    default:0
+  },
+  lmax:{
+    type:[Number,String],
+    default:100
+  },
+},
+ data:(vm)=>{
+    return {id:`d-r-slider-input-${Math.random().toString().substring(2,8)}`,
+    contentMin:vm.min,
+    contentMax:vm.max,
+    slider:null,
+    loaded:false
+}
+
+ },
+ mounted(){
+    if(!this.loaded)
+    this.initialize()
+ },
+ methods:{
+    onchange(vals){
+        let [mn,mx] =vals.split(',')
+               this.contentMax=mx;
+               this.contentMin=mn;
+              this.$emit('update:min', this.contentMin)
+              this.$emit('update:max', this.contentMax)
+    },
+    initialize(){
+       this.slider= new rSlider({
+            target: "#"+this.id,
+            values: { min: this.lmin, max: this.lmax },
+            step: 10,
+            range: true,
+            set: [this.contentMin,  this.contentMax],
+            scale: true,
+            labels: false,
+            onChange: this.onchange,
+          });
+          this.loaded=true
+    }
+ }
+}
+</script>
+
+<style>
+  @import "@/assets/css/rSlider.min.css";
+</style>
