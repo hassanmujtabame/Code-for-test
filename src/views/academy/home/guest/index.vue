7d9541c99f6d427bc1a7a3be81117ca1995eb1f6
drsoft28 <dif.rabie@gmail.com>
2022-12-19 16:45:34 +0100

    make as guest page

---
+++ src/views/academy/home/guest/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+        <!-- section 1-->
+        <SectionOne />
+        <!--#section 1-->
+       <!-- section 2-->
+       <SectionTwo/>
+        <!--#section 2-->
+        <!-- section 3-->
+        <SectionThree />
+        <!--#section 3-->
+       <!-- section 4-->
+       <SectionFour />
+        <!--#section 4-->
+        <!-- section 5-->
+        <SectionFive/>
+        <!--#section 5-->
+        <!-- section 6-->
+        <SectionSix />
+        <!--#section 6-->
+        <!-- section 7-->
+        <SectionSeven />
+        <!--#section 7-->
+         <!-- section 8-->
+         <SectionEight />
+        <!--#section 8--> 
+         <!-- section 9-->
+         <SectionNine />
+        <!--#section 9-->   
+        <!-- section 10-->
+        <SectionTen />
+        <!--#section 10-->  
+        <!-- section 11-->
+        <SectionEleven />
+        <!--#section 11--> 
+        <!-- section 12-->
+        <SectionTwelve/>
+        <!--#section 12--> 
+        <!-- section 13-->
+        <SectionThreeteen />
+        <!--#section 13-->   
+        <!-- section 14-->
+      <SectionForteen />
+        <!--#section 14--> 
+      <!-- section 15-->
+      <SectionFifteen />
+        <!--#section 15-->
+    
+
+    </div>
+</template>
+
+<script>
+import SectionOne from './parts/section-1/index.vue'
+import SectionTwo from './parts/section-2/index.vue'
+import SectionThree from './parts/section-3/index.vue'
+import SectionFour from './parts/section-4/index.vue'
+import SectionFive from './parts/section-5/index.vue'
+import SectionSix from './parts/section-6/index.vue'
+import SectionSeven from './parts/section-7/index.vue'
+import SectionEight from './parts/section-8/index.vue'
+import SectionNine from './parts/section-9/index.vue'
+import SectionTen from './parts/section-10/index.vue'
+import SectionEleven from './parts/section-11/index.vue'
+import SectionTwelve from './parts/section-12/index.vue'
+import SectionThreeteen from './parts/section-13/index.vue'
+import SectionForteen from './parts/section-14/index.vue'
+import SectionFifteen from './parts/section-15/index.vue'
+export default {
+name:"academy-home",
+components:{
+  SectionOne,
+  SectionTwo,
+  SectionThree,
+  SectionFour,
+  SectionFive,
+  SectionSix,
+  SectionSeven,
+  SectionEight,
+  SectionNine,
+  SectionTen,
+  SectionEleven,
+  SectionTwelve,
+  SectionThreeteen,
+  SectionForteen,
+  SectionFifteen
+}
+}
+</script>
+
+<style>
+
+</style>
--- src/views/academy/home/index.vue
+++ src/views/academy/home/index.vue
@@ -1,89 +1,16 @@
 <template>
   <div style="margin-top: 85px " class="consult">
-        <!-- section 1-->
-        <SectionOne />
-        <!--#section 1-->
-       <!-- section 2-->
-       <SectionTwo/>
-        <!--#section 2-->
-        <!-- section 3-->
-        <SectionThree />
-        <!--#section 3-->
-       <!-- section 4-->
-       <SectionFour />
-        <!--#section 4-->
-        <!-- section 5-->
-        <SectionFive/>
-        <!--#section 5-->
-        <!-- section 6-->
-        <SectionSix />
-        <!--#section 6-->
-        <!-- section 7-->
-        <SectionSeven />
-        <!--#section 7-->
-         <!-- section 8-->
-         <SectionEight />
-        <!--#section 8--> 
-         <!-- section 9-->
-         <SectionNine />
-        <!--#section 9-->   
-        <!-- section 10-->
-        <SectionTen />
-        <!--#section 10-->  
-        <!-- section 11-->
-        <SectionEleven />
-        <!--#section 11--> 
-        <!-- section 12-->
-        <SectionTwelve/>
-        <!--#section 12--> 
-        <!-- section 13-->
-        <SectionThreeteen />
-        <!--#section 13-->   
-        <!-- section 14-->
-      <SectionForteen />
-        <!--#section 14--> 
-      <!-- section 15-->
-      <SectionFifteen />
-        <!--#section 15-->
-    
-
+      
+     <GuestPage />
     </div>
 </template>
 
 <script>
-import SectionOne from './guest/parts/section-1/index.vue'
-import SectionTwo from './guest/parts/section-2/index.vue'
-import SectionThree from './guest/parts/section-3/index.vue'
-import SectionFour from './guest/parts/section-4/index.vue'
-import SectionFive from './guest/parts/section-5/index.vue'
-import SectionSix from './guest/parts/section-6/index.vue'
-import SectionSeven from './guest/parts/section-7/index.vue'
-import SectionEight from './guest/parts/section-8/index.vue'
-import SectionNine from './guest/parts/section-9/index.vue'
-import SectionTen from './guest/parts/section-10/index.vue'
-import SectionEleven from './guest/parts/section-11/index.vue'
-import SectionTwelve from './guest/parts/section-12/index.vue'
-import SectionThreeteen from './guest/parts/section-13/index.vue'
-import SectionForteen from './guest/parts/section-14/index.vue'
-import SectionFifteen from './guest/parts/section-15/index.vue'
+import GuestPage from './guest/index'
 export default {
 name:"academy-home",
 components:{
-  SectionOne,
-  SectionTwo,
-  SectionThree,
-  SectionFour,
-  SectionFive,
-  SectionSix,
-  SectionSeven,
-  SectionEight,
-  SectionNine,
-  SectionTen,
-  SectionEleven,
-  SectionTwelve,
-  SectionThreeteen,
-  SectionForteen,
-  SectionFifteen
+  GuestPage
 }
 }
 </script>
