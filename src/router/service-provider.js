df492122b1c3f0b7c955ddd5686d3a3c8426c8c1
drsoft28 <dif.rabie@gmail.com>
2022-12-20 11:42:17 +0100

    change layout

--- src/router/service-provider.js
+++ src/router/service-provider.js
@@ -1,8 +1,8 @@
-import LayoutDefault from '@/layouts/default/index.vue'
+import LayoutService from '@/layouts/service-provider'
 const routers =[
     {
         path: '/service-provider/home',
-        meta:{layout:LayoutDefault},
+        meta:{layout:LayoutService},
         name: 'service-provider-home',
         component: () => import(/* webpackChunkName: "service-provider-home" */ '../views/service-provider/home/index.vue') 
     }
