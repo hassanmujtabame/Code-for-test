7a3c01b4bd13dd94417bef75c5e384d20230c58e
drsoft28 <dif.rabie@gmail.com>
2022-12-20 11:37:43 +0100

    use getRouteLocale

--- src/layouts/network/header.vue
+++ src/layouts/network/header.vue
@@ -29,27 +29,27 @@
               <!-- <a class="nav-link active" aria-current="page" href="../index.html"
                 >الرئيسية</a
               > -->
-              <router-link :to="{name:'network-home',params:{lang:$i18n.locale}}"  class="nav-link">الرئيسية</router-link>
+              <router-link :to="getRouteLocale('network-home')"  class="nav-link">الرئيسية</router-link>
             </li>
             <li class="nav-item px-2">
            
-              <router-link :to="{name:'network-investment-project',params:{lang:$i18n.locale}}" class="nav-link">مشاريع للإستثمار</router-link>
+              <router-link :to="getRouteLocale('network-investment-project')" class="nav-link">مشاريع للإستثمار</router-link>
             </li>
             <li class="nav-item px-2">
-              <router-link :to="{name:'network-exhibitions',params:{lang:$i18n.locale}}" class="nav-link">المعارض</router-link>
+              <router-link :to="getRouteLocale('network-exhibitions')" class="nav-link">المعارض</router-link>
             </li>
            
             <li class="nav-item px-2">
-              <router-link :to="{name:'network-offers',params:{lang:$i18n.locale}}" class="nav-link">العروض</router-link>
+              <router-link :to="getRouteLocale('network-offers')" class="nav-link">العروض</router-link>
             </li>
             <li class="nav-item px-2">
-              <router-link :to="{name:'network-models',params:{lang:$i18n.locale}}" class="nav-link">النماذج</router-link>
+              <router-link :to="getRouteLocale('network-models')" class="nav-link">النماذج</router-link>
             </li>
             <li class="nav-item px-2">
-              <router-link :to="{name:'network-blog',params:{lang:$i18n.locale}}" class="nav-link">المدونة</router-link>
+              <router-link :to="getRouteLocale('network-blog')" class="nav-link">المدونة</router-link>
             </li>
             <li class="nav-item px-2">
-              <router-link to="/contact-us" class="nav-link">تواصل معنا</router-link>
+              <router-link :to="getRouteLocale('contact-us')" class="nav-link">تواصل معنا</router-link>
             </li>
             <div  v-if="false" style="line-height: 2.5; height: 40px;" class="btn-main btn-nav text-center">
                         <a href="" class="text-white " data-bs-toggle="modal"
