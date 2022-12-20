6569225b26c6c4492e507376d00090bb4e9dbf2f
drsoft28 <dif.rabie@gmail.com>
2022-12-20 12:15:36 +0100

    add link to my profile

--- src/components/cards/user-dash-info.vue
+++ src/components/cards/user-dash-info.vue
@@ -8,9 +8,12 @@
 
             <h5 class="m-c">{{ member.name }}</h5>
             <p>{{ member.job }}</p>
-            <button class="border px-3 py-2 rounded-2 bg-transparent">
+            <router-link custom :to="getRouteLocale('my-profile')" v-slot="{navigate}">
+            <button @click="navigate" role="link" class="border px-3 py-2 rounded-2 bg-transparent">
                 صفحتك الشخصية
             </button>
+            </router-link>
+            
         </div>
 
 
