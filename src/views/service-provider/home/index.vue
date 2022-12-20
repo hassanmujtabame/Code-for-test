b7471c9aa3e794c7df14b555bc0d9e3ac2f0d2fd
drsoft28 <dif.rabie@gmail.com>
2022-12-20 10:23:04 +0100

    rename section 1 to secion header

--- src/views/service-provider/home/index.vue
+++ src/views/service-provider/home/index.vue
@@ -1,38 +1,37 @@
 <template>
     <div style="margin-top: 85px " class="consult">
-          <!-- section 1-->
+          <!-- section Header-->
           <SectionOne />
-          <!--#section 1-->
+
          <!-- section 2-->
          <SectionTwo/>
-          <!--#section 2-->
+
           <!-- section 3-->
           <SectionThree />
-          <!--#section 3-->
+
          <!-- section 4-->
          <SectionFour />
-          <!--#section 4-->
+
           <!-- section 5-->
           <SectionFive/>
-          <!--#section 5-->
+
           <!-- section 6-->
           <SectionSix />
-          <!--#section 6-->
           <!-- section 7-->
           <SectionSeven />
-          <!--#section 7-->
+
            <!-- section 8-->
            <SectionEight />
-          <!--#section 8--> 
+
            <!-- section 9-->
            <SectionNine />
-          <!--#section 9-->   
+ 
           <!-- section 10-->
           <SectionTen />
-          <!--#section 10-->  
+  
           <!-- section 11-->
           <SectionEleven />
-          <!--#section 11--> 
+
           <!-- section 12-->
           <SectionTwelve/>
           <!--#section 12--> 
@@ -43,7 +42,7 @@
   </template>
   
   <script>
-  import SectionOne from './parts/section-1/index.vue'
+  import SectionOne from './parts/section-header/index.vue'
   import SectionTwo from './parts/section-2/index.vue'
   import SectionThree from './parts/section-3/index.vue'
   import SectionFour from './parts/section-4/index.vue'
--- src/views/service-provider/home/parts/section-header/index.vue
+++ src/views/service-provider/home/parts/section-header/index.vue
@@ -41,7 +41,7 @@
 
 <script>
 export default {
- name:'section-1'
+ name:'section-header'
 }
 </script>
 
