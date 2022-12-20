d0086891bebd173193f06399fbeafccbe3575686
drsoft28 <dif.rabie@gmail.com>
2022-12-20 12:29:38 +0100

    add page ready services

---
+++ src/views/service-provider/ready-services/index.vue
@@ -0,0 +1,188 @@
+<template>
+    <div style="margin-top: 85px " class="consult">
+        <div class="consulting ">
+            <div class="container">
+                <div class="row justify-content-between align-items-center">
+                    <div class="col-md-6">
+                        <h1 class="fs-1 text-white"> خدماتك الجاهزة </h1>
+                        <p class="text-white">
+يمكنك أضافة خدماتك و ما يمكنك تقديمه الى العملاء كخدمات جاهزة يمكنهم طلب شرائها منك</p>
+                    </div>
+                    <div class="col-md-6 pt-4">
+                        <div style="overflow: inherit;" class=" m-auto text-end">
+                            <img src="/assets/img/Group1.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="star-cons">
+                    <img src="/assets/img/starrrr.png" alt="">
+                </div>
+                <div class="star-cons stars">
+                    <img src="/assets/img/starsss.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row justify-content-between align-items-center">
+                <div style="  line-height: 2.5; height: 40px;"
+                    class=" col-12 col-md-5 mt-3 btn-main btn-nav text-center">
+                    <a href="" class="text-white " data-bs-toggle="modal" data-bs-target="#addModal">أضف خدمة جديدة</a>
+                </div>
+                <div class=" col-12 col-md-5 mt-3">
+                    <label for="" class="position-relative">
+                        <input class="form-control py-3 px-5" type="text" placeholder="ابحث عن خدمة ">
+                        <p style="top: 25%;right: 7px;" class="position-absolute">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M11.5 21.75C5.85 21.75 1.25 17.15 1.25 11.5C1.25 5.85 5.85 1.25 11.5 1.25C17.15 1.25 21.75 5.85 21.75 11.5C21.75 17.15 17.15 21.75 11.5 21.75ZM11.5 2.75C6.67 2.75 2.75 6.68 2.75 11.5C2.75 16.32 6.67 20.25 11.5 20.25C16.33 20.25 20.25 16.32 20.25 11.5C20.25 6.68 16.33 2.75 11.5 2.75Z"
+                                    fill="#979797" />
+                                <path
+                                    d="M22.0004 22.7499C21.8104 22.7499 21.6204 22.6799 21.4704 22.5299L19.4704 20.5299C19.1804 20.2399 19.1804 19.7599 19.4704 19.4699C19.7604 19.1799 20.2404 19.1799 20.5304 19.4699L22.5304 21.4699C22.8204 21.7599 22.8204 22.2399 22.5304 22.5299C22.3804 22.6799 22.1904 22.7499 22.0004 22.7499Z"
+                                    fill="#979797" />
+                            </svg>
+
+                        </p>
+                    </label>
+                </div>
+                <div class=" col-12 col-md-3 mt-3 position-relative">
+                    <select class="form-select form-select-lg mb-3  py-3" aria-label=".form-select-lg example">
+                        <option selected>الاقل سعرا </option>
+                        <option value="1">الاعلى سعرا</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                    <p style="top: -13px;right: 24px;background: white;" class="position-absolute">
+                        ترتيب حسب
+                    </p>
+                </div>
+            </div>
+            <div class="row mt-5 order">
+                <div class=" col-12 col-md-6  col-lg-4 mt-3">
+                    <div class="box">
+                        <div>
+                            <img class="w-100" src="/assets/img/Rectangle 1798.png" alt="" height="164px">
+                        </div>
+                        <div class="p-3">
+                            <div class="d-flex align-items-center justify-content-between">
+
+                                <h6>تجهيز كوشة فرح</h6>
+                                <p class="m-c"> الافراح</p>
+                            </div>
+                            <p>عبارة عن تجهيز كوشة فرح وتجهيز القاعة بالكامل.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-12 col-md-6  col-lg-4 mt-3">
+                    <div class="box">
+                        <div>
+                            <img class="w-100" src="/assets/img/Rectangle 1798.png" alt="" height="164px">
+                        </div>
+                        <div class="p-3">
+                            <div class="d-flex align-items-center justify-content-between">
+                                <h6>تجهيز كوشة فرح</h6>
+                                <p class="m-c">
+                                    الافراح
+                                </p>
+                            </div>
+                            <p>عبارة عن تجهيز كوشة فرح وتجهيز القاعة بالكامل.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-12 col-md-6  col-lg-4 mt-3">
+                    <div class="box">
+                        <div>
+                            <img class="w-100" src="/assets/img/Rectangle 1798.png" alt="" height="164px">
+                        </div>
+                        <div class="p-3">
+                            <div class="d-flex align-items-center justify-content-between">
+
+                                <h6>تجهيز كوشة فرح</h6>
+                                <p class="m-c">
+                                    الافراح
+                                </p>
+                            </div>
+                            <p>عبارة عن تجهيز كوشة فرح وتجهيز القاعة بالكامل.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-12 col-md-6  col-lg-4 mt-3">
+                    <div class="box">
+                        <div>
+                            <img class="w-100" src="/assets/img/Rectangle 1798.png" alt="" height="164px">
+                        </div>
+                        <div class="p-3">
+                            <div class="d-flex align-items-center justify-content-between">
+
+                                <h6>تجهيز كوشة فرح</h6>
+                                <p class="m-c"> الافراح</p>
+                            </div>
+                            <p>عبارة عن تجهيز كوشة فرح وتجهيز القاعة بالكامل.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-12 col-md-6  col-lg-4 mt-3">
+                    <div class="box">
+                        <div>
+                            <img class="w-100" src="/assets/img/Rectangle 1798.png" alt="" height="164px">
+                        </div>
+                        <div class="p-3">
+                            <div class="d-flex align-items-center justify-content-between">
+
+                                <h6>
+                                    تجهيز كوشة فرح
+                                </h6>
+                                <p class="m-c">
+                                    الافراح
+                                </p>
+                            </div>
+                            <p>عبارة عن تجهيز كوشة فرح وتجهيز القاعة بالكامل.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-12 col-md-6  col-lg-4 mt-3 service-stoped">
+                    <div class="box">
+                        <div>
+                            <img class="w-100" src="/assets/img/Rectangle 1798.png" alt="" height="164px">
+                        </div>
+                        <div class="p-3">
+                            <div class="d-flex align-items-center justify-content-between">
+                                <h6>تجهيز كوشة فرح</h6>
+                                <p class="m-c">
+                                    الافراح
+                                </p>
+                            </div>
+                            <p>عبارة عن تجهيز كوشة فرح وتجهيز القاعة بالكامل.</p>
+                        </div>
+                        <div class="box-stoped">
+                            <p class="p-1 m-0">
+                                خدمة معلقة
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+   
+        <SectionLearning />
+       <SectionHear />
+    </div>
+</template>
+
+<script>
+import SectionHear from '../home/parts/section-hear/index.vue'
+import SectionLearning from '../home/parts/section-continue-learning/index.vue'
+
+export default {
+ name:'ready-services',
+ components:{
+    SectionHear,
+    SectionLearning
+ }
+}
+</script>
+
+<style>
+
+</style>
