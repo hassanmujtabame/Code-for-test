0d687ae71b02cae8ab4c6fc8933bc5ee096600f1
drsoft28 <dif.rabie@gmail.com>
2022-12-20 12:56:37 +0100

    add class order

--- src/components/filter-list/index.vue
+++ src/components/filter-list/index.vue
@@ -190,7 +190,7 @@
                 </div>
             </div>
             <div class="col-md-9">
-                <div class="row ">
+                <div class="row order">
                     <div v-for="(item,i) in items" :key="i" class="col-12 col-md-6 mt-2">
                         <div class="swiper-slide rounded-3">
                          <slot :item="item"></slot>
