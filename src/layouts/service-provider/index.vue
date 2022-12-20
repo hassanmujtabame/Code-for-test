a0d054586d87ff1adf929c5cf6e5fa0892f23d5d
drsoft28 <dif.rabie@gmail.com>
2022-12-20 11:42:38 +0100

    create layout for service provider

---
+++ src/layouts/service-provider/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <DefaultHeader/>
+      <MyDrawer v-if="user"/>
+      <main style="margin-top: 96px">
+          <slot></slot>
+          </main>
+          <SectSubscribe/>
+          <DefaultFooter/>
+    </div>
+  </template>
+  
+  <script>
+  import MyDrawer from '@/layouts/common/drawer.vue'
+  import DefaultFooter from '@/layouts/common/footer.vue'
+  import DefaultHeader from './header.vue'
+  import SectSubscribe from '@/layouts/common/section-subscribe/sect-subscribe.vue';
+  export default {
+      name:'network-layout',
+      components:{
+          DefaultFooter,
+          DefaultHeader,
+          SectSubscribe,
+          MyDrawer
+      }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
