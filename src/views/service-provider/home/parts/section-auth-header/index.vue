cbc4af19386e0030cdcb8dbd93089fe2749718a4
drsoft28 <dif.rabie@gmail.com>
2022-12-20 11:24:00 +0100

    add section auth header

---
+++ src/views/service-provider/home/parts/section-auth-header/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="">
+        <div class="over-lay">
+            <img class="img-fluid" src="" alt="">
+            <div style="padding: 184px 0;" class="text-center ">
+
+                <h1 style="font-size: 50px;" class="text-white">
+                    أهلاً و مرحباً بك في مقدمي الخدمات
+                </h1>
+                <h6 class="text-white">
+                    منصة تجمع أهم وألمع رائدين و رائدات الأعمال في السعودية والخليج في مكان واحد
+                </h6>
+                <div>
+                    <input class="p-3 rounded-2 border-0 bg-white w-50" type="text"
+                        placeholder="ابحث عن خدمات او طلبات من هنا">
+                    <button style="height: 51px;" class="btn-main">
+                        ابحث
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+ name:'section-auth-header'
+}
+</script>
+
+<style>
+
+</style>
