b73e9fb5487201aa0577e6d543621c91fbb84f7c
drsoft28 <dif.rabie@gmail.com>
2022-12-21 10:44:17 +0100

    تصحيح بداية الرابط

--- src/router/service-provider.js
+++ src/router/service-provider.js
@@ -1,49 +1,49 @@
 import LayoutService from '@/layouts/service-provider'
 const routers =[
     {
-        path: '/service-provider/home',
+        path: 'service-provider/home',
         meta:{layout:LayoutService},
         name: 'service-provider-home',
         component: () => import(/* webpackChunkName: "service-provider-home" */ '../views/service-provider/home/index.vue') 
     },
     {
-        path: '/service-provider/dashboard',
+        path: 'service-provider/dashboard',
         meta:{layout:LayoutService},
         name: 'service-provider-dashboard',
         component: () => import(/* webpackChunkName: "service-provider-dashboard" */ '../views/service-provider/dashboard/index.vue') 
     },
     {
-        path: '/service-provider/ready-services',
+        path: 'service-provider/ready-services',
         meta:{layout:LayoutService},
         name: 'service-provider-ready-services',
         component: () => import(/* webpackChunkName: "service-provider-ready-services" */ '../views/service-provider/ready-services/index.vue') 
     },
     {
-        path: '/service-provider/ready-services/:id',
+        path: 'service-provider/ready-services/:id',
         meta:{layout:LayoutService},
         name: 'service-provider-ready-service',
         component: () => import(/* webpackChunkName: "service-provider-page-service-ready" */ '../views/service-provider/page-service-ready/index.vue') 
     },
     {
-        path: '/service-provider/show-services',
+        path: 'service-provider/show-services',
         meta:{layout:LayoutService},
         name: 'service-provider-show-services',
         component: () => import(/* webpackChunkName: "service-provider-show-services" */ '../views/service-provider/show-services/index.vue') 
     },
     {
-        path: '/service-provider/show-services/:id',
+        path: 'service-provider/show-services/:id',
         meta:{layout:LayoutService},
         name: 'service-provider-page-service',
         component: () => import(/* webpackChunkName: "service-provider-page-service" */ '../views/service-provider/page-service/index.vue') 
     },
     {
-        path: '/service-provider/services-online',
+        path: 'service-provider/services-online',
         meta:{layout:LayoutService},
         name: 'service-provider-services-online',
         component: () => import(/* webpackChunkName: "service-provider-services-online" */ '../views/service-provider/services-online/index.vue') 
     },
     {
-        path: '/service-provider/services-offline',
+        path: 'service-provider/services-offline',
         meta:{layout:LayoutService},
         name: 'service-provider-services-offline',
         component: () => import(/* webpackChunkName: "service-provider-services-offline" */ '../views/service-provider/services-offline/index.vue') 
