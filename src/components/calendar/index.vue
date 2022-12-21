3877828e2930789f3f831ef565b546102bf9b8e7
drsoft28 <dif.rabie@gmail.com>
2022-12-21 18:15:10 +0100

    improve simple calendar component

--- src/components/calendar/index.vue
+++ src/components/calendar/index.vue
@@ -5,14 +5,10 @@
 <script>
 export default {
  name:'calendar-simple',
- data:()=>({
-  id:'calendar-'+Math.random().toString().substring(2,16)
- }),
- mounted(){
-    $("#"+this.id).simpleCalendar({
-  displayEvent: true,
-  events: [
-      // generate new event after tomorrow for one hour
+ props:{
+  events:{
+    type:[Array,Object],
+    default:()=>[ // generate new event after tomorrow for one hour
       {
           startDate: new Date(new Date().setHours(new Date().getHours() + 24)).toDateString(),
           endDate: new Date(new Date().setHours(new Date().getHours() + 25)).toISOString(),
@@ -29,9 +25,46 @@ export default {
           startDate: new Date(new Date().setHours(new Date().getHours() - 48)).toISOString(),
           endDate: new Date(new Date().setHours(new Date().getHours() - 24)).getTime(),
           summary: 'Visit of the Louvre'
-      }
-  ],
+      }]
+  },
+  displayEvent:{
+    type:Boolean,
+    default:false,
+  },
+  disableEventDetails:{
+    type:Boolean,
+    default:true,
+  },
+  disableEmptyDetails:{
+    type:Boolean,
+    default:true,
+  }
+ },
+ data:()=>({
+  id:'calendar-'+Math.random().toString().substring(2,16)
+ }),
+ methods:{
+ onSelectDate(date,events){
+  console.log('onSelectDate',date,events)
+  return false;
+ }
+ },
+ mounted(){
+    $("#"+this.id).simpleCalendar({
+      displayYear: true,              // Display year in header
+        fixedStartDay: true,            // Week begin always by monday or by day set by number 0 = sunday, 7 = saturday, false = month always begin by first day of the month
+        displayEvent: this.displayEvent,             // Display existing event
+        disableEventDetails: this.disableEventDetails, // disable showing event details
+        disableEmptyDetails: this.disableEmptyDetails, // disable showing empty date details
+        events: [],
+        onInit: function (calendar) {}, // Callback after first initialization
+        onMonthChange: function (month, year) {}, // Callback on month change
+        onDateSelect: this.onSelectDate, // Callback on date selection
+        onEventSelect: function() {}, // Callback on event selection - use $(this).data('event') to access the event
+        onEventCreate: function( $el ) {},          // Callback fired when an HTML event is created - see $(this).data('event')
+        onDayCreate:   function( $el, d, m, y ) {},  // Callback fired when an HTML day is created   - see $(this).data('today'), .data('todayEvents')
   months: ['يناير', 'فبراير', 'مارس', 'ابريل', 'مايو', 'يونيو', 'يوليو', 'اغسطس', 'سبتمبر', 'اكتوبر', 'نوفمبر', 'ديسمبر'],
+  days: ['الاحد','الإثنين','الثلاثاء','الأربعاء','الخميس','الجمعة','السبت'],
 })
 
  }
