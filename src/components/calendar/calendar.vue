8df29d9dca053d280f72aa139a2379ec28be020d
drsoft28 <dif.rabie@gmail.com>
2022-12-21 17:42:17 +0100

    calendar component

---
+++ src/components/calendar/calendar.vue
@@ -0,0 +1,26 @@
+<template>
+<vc-calendar  :attributes="attributes" :locale="$i18n.locale" style="direction:ltr" v-bind="$attrs"/>
+</template>
+
+<script>
+export default {
+ name:'d-calendar',
+ data:()=>({
+  attributes: [
+      {
+        key: 'today',
+        highlight: true,
+        dates: {
+    start: new Date(), 
+    //end:null  
+  }
+      }
+    ],
+  id:'d-calendar-'+Math.random().toString().substring(2,16)
+ })
+}
+</script>
+
+<style>
+
+</style>
