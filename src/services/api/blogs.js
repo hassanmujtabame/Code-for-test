9ad3fe5ea2d99002f5fff47078e31e2c9e006366
drsoft28 <dif.rabie@gmail.com>
2022-12-24 16:35:54 +0100

    Add blogs apis service

---
+++ src/services/api/blogs.js
@@ -0,0 +1,26 @@
+
+class BlogsApi {
+    //Get all Blogs
+    getAll(params={}){
+        let s="";
+        Object.keys(params).forEach((key)=>{
+            if(s!="") s+=`&`
+             s+=`${key}=${params[key]}`
+        })
+        return window.axios.get(`network/blogs?${s}`);
+    }
+    getItem(id){
+        return window.axios.get(`network/blogs/${id}`);
+    }
+    getCategories(){
+        return window.axios.get(`network/blogs-categories`);
+    }
+    getRecent(){
+        return window.axios.get(`network/blogs`);
+    }
+    getTags(){
+        return window.axios.get(`network/blogs-tags`);
+    }
+}
+
+export default new BlogsApi();
