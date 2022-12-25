7767520d8c443015fb2182696f161b2ccac8cb66
drsoft28 <dif.rabie@gmail.com>
2022-12-25 20:22:12 +0100

    create exhibitions APi service

---
+++ src/services/api/exhibitions.js
@@ -0,0 +1,26 @@
+
+import BaseApi from "./base-service";
+class ExhibitionsApi extends BaseApi{
+    //Get all Exhibitions
+    getAll(params={}){
+        let s=this.generateQueryUrl(params)
+        return window.axios.get(`network/exhibitions?${s}`);
+    }
+    getItem(id){
+        return window.axios.get(`network/exhibitions/${id}`);
+    }
+    getCategories(){
+        return window.axios.get(`network/exhibitions-categories`);
+    }
+    getRecent(){
+        return window.axios.get(`network/latest-exhibitions`);
+    }
+    getTags(){
+        return window.axios.get(`network/exhibitions-tags`);
+    }
+    addExhibition(data){
+        return window.axios.post(`network/exhibitions`,data);  
+    }
+}
+
+export default new ExhibitionsApi();
