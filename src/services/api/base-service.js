e6a8b65173e6914199d540f187cdc908394d23fc
drsoft28 <dif.rabie@gmail.com>
2022-12-25 20:16:33 +0100

    create base class for Apis

---
+++ src/services/api/base-service.js
@@ -0,0 +1,19 @@
+class BaseApi {
+    generateQueryUrl(params){
+        let s="";
+        Object.keys(params).forEach((key)=>{
+            if(params[key]===null) return;
+            if(s!="") s+=`&`
+            if( Array.isArray(params[key]))
+            params[key].forEach((element,i) => {
+                if(i!==0) s+=`&`
+                s+=`${key}[]=${element}`
+            });
+            else
+             s+=`${key}=${params[key]}`
+        })
+        return s
+      }
+}
+
+export default BaseApi;
