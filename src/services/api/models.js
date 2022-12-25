655a461f77970386340a9a002388a5a5eb0e67f9
drsoft28 <dif.rabie@gmail.com>
2022-12-25 17:50:08 +0100

    update models APIs

--- src/services/api/models.js
+++ src/services/api/models.js
@@ -1,11 +1,22 @@
 
 class ModelsApi {
     //Get all Models
-    getAll(){
-        return window.axios.get(`network/models`);
+    getAll(params={}){
+        let s="";
+        Object.keys(params).forEach((key)=>{
+            if(s!="") s+=`&`
+             s+=`${key}=${params[key]}`
+        })
+        return window.axios.get(`network/models?${s}`);
+    }
+    getCategories(){
+        return window.axios.get(`network/blogs-categories`);//إستعمال مؤقت 
     }
     getItem(id){
         return window.axios.get(`network/models/${id}`);
+    }
+    getRecent(){
+        return window.axios.get(`network/models`);
     }
 }
 
