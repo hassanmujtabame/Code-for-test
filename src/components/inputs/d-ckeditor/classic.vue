e1d469437c510098985b203a22bf2295d01694f2
drsoft28 <dif.rabie@gmail.com>
2022-12-27 17:37:18 +0100

    create ckeditor classic

---
+++ src/components/inputs/d-ckeditor/classic.vue
@@ -0,0 +1,36 @@
+<template>
+        <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+</template>
+
+<script>
+    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+    export default {
+        name: 'd-ckeditor-classic',
+        props:{
+            value:{
+                type:String
+            }
+        },
+        data() {
+            return {
+                editor: ClassicEditor,
+                editorData: '<p>Content of the editor.</p>',
+                editorConfig: {
+                    // The configuration of the editor.
+                }
+            };
+        },
+        watch:{
+            value:{
+                immediate:true,
+                handler(val){
+                    this.editorData=val
+                }
+            },
+            editorData(){
+                this.$emit('input',this.editorData)
+            }
+        }
+    }
+</script>
--- src/plugins/components.js
+++ src/plugins/components.js
@@ -8,6 +8,7 @@ import DCarousel from '@/components/carousel/index.vue'
 import DCarouselSlide from '@/components/carousel/slide.vue'
 import DFilterList from '@/components/filter-list/index.vue'
 import RSliderInput from '@/components/inputs/r-slider/index.vue'
+import DCkeditorClassicInput from '@/components/inputs/d-ckeditor/classic.vue'
 import DUserInfo from '@/components/cards/user-info.vue'
 import DUserInfoLi from '@/components/cards/user-info-left-img.vue'
 import DStoryCard from '@/components/cards/story.vue'
@@ -20,6 +21,7 @@ import SectionContinueLearning from '@/components/sections/section-continue-lear
 import RequestPurchaseCard from '@/components/cards/request-purchase-service.vue'
 
 
+Vue.component('d-ckeditor-classic', DCkeditorClassicInput)
 Vue.component('section-continue-learning', SectionContinueLearning)
 Vue.component('d-job-card', DJobCard)
 Vue.component('d-blog-info-card', DBlogInfoCard)
