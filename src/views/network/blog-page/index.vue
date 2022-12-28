20fd157eebf408ec03799287f15a9d72fef855ac
drsoft28 <dif.rabie@gmail.com>
2022-12-28 13:49:26 +0100

    improve page code

--- src/views/network/blog-page/index.vue
+++ src/views/network/blog-page/index.vue
@@ -6,9 +6,9 @@
     </div>
     <div v-else class="container blog-page">
       <div class=" position-relative">
-        <img class="w-100" :src="blog.image" :alt="blog.title" height="432">
+        <img class="w-100" :src="itemPage.image" :alt="itemPage.title" height="432">
         <div class="d-flex gap-2 position-absolute bottom-0 data">
-          <p v-for="(cat,c) in blog.categories" :key="c" :style="{'background-color':colors[c%3]}" class="p-1 px-2 rounded-2 text-white m-0">
+          <p v-for="(cat,c) in itemPage.categories" :key="c" :style="{'background-color':colors[c%3]}" class="p-1 px-2 rounded-2 text-white m-0">
             {{ cat.name }}
           </p>
         </div>
@@ -16,9 +16,9 @@
 
       <div class="row mt-5">
         <div class=" col-12 col-md-8">
-          <h3 class="m-c">{{ blog.title }}
+          <h3 class="m-c">{{ itemPage.title }}
           </h3>
-          <p class="pargrapg" v-html="blog.description">
+          <p class="pargrapg" v-html="itemPage.description">
            </p>
         </div>
         <div class="col-12 col-md-4">
@@ -38,37 +38,7 @@
                   
           </div>
           </div>
-          <div v-else class="box border rounded-3 p-2">
-            <div class="row justify-content-center">
-              <div class="col-md-4 img-service text-center">
-                <img class="rounded-circle " :src="blog.user_info.image" :alt="blog.user_info.name"
-                  width="100" height="100">
-              </div>
-              <div class="col-md-8 t-c">
-                <h4 class="fw-bold">
-                 {{ blog.user_info.name}}
-                </h4>
-                <p>
-                  {{ blog.user_info.job}}
-                </p>
-                <div class="message-service">
-                  <button class="text-white border-0 p-2 px-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M17 21.25H7C3.35 21.25 1.25 19.15 1.25 15.5V8.5C1.25 4.85 3.35 2.75 7 2.75H17C20.65 2.75 22.75 4.85 22.75 8.5V15.5C22.75 19.15 20.65 21.25 17 21.25ZM7 4.25C4.14 4.25 2.75 5.64 2.75 8.5V15.5C2.75 18.36 4.14 19.75 7 19.75H17C19.86 19.75 21.25 18.36 21.25 15.5V8.5C21.25 5.64 19.86 4.25 17 4.25H7Z"
-                        fill="white" />
-                      <path
-                        d="M11.9998 12.87C11.1598 12.87 10.3098 12.61 9.65978 12.08L6.52978 9.57997C6.20978 9.31997 6.14978 8.84997 6.40978 8.52997C6.66978 8.20997 7.13978 8.14997 7.45978 8.40997L10.5898 10.91C11.3498 11.52 12.6398 11.52 13.3998 10.91L16.5298 8.40997C16.8498 8.14997 17.3298 8.19997 17.5798 8.52997C17.8398 8.84997 17.7898 9.32997 17.4598 9.57997L14.3298 12.08C13.6898 12.61 12.8398 12.87 11.9998 12.87Z"
-                        fill="white" />
-                    </svg>
-                    رسالة
-                  </button>
-                </div>
-              </div>
-
-
-            </div>
-          </div>
+          <d-user-info-li v-else sizeImage="100" :member="itemPage.user_info" />
           <div class="box border rounded-3 p-2 mt-3">
             <h3>
               تدوينات أخرى
@@ -208,15 +178,15 @@ export default {
       isOwner:true,
       loading:true,
       hasError:false,
-      blog:{},
+      itemPage:{},
       colors:['#F2631C','#FFBC00','#2C98B3']
   }},
   methods:{
     openEditDialog(){
-      this.fireOpenDialog('update-blog',this.blog)
+      this.fireOpenDialog('update-blog',this.itemPage)
     },
     openDeleteDialog(){
-      this.fireOpenDialog('delete-blog',this.blog)
+      this.fireOpenDialog('delete-blog',this.itemPage)
     },
     async initializing() {
       this.loading = true;
@@ -224,8 +194,8 @@ export default {
             try {
                 let { data } = await BlogsAPI.getItem(this.$route.params.id)
                 if (data.success) {
-                   this.blog = data.data;
-                   this.isOwner = this.blog.user_info.id==this.user.id
+                   this.itemPage = data.data;
+                   this.isOwner = this.itemPage.user_info.id==this.user.id
                 }else{
                   this.hasError = true;
                 }
