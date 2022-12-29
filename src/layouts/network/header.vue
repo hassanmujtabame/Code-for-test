e5325228d985c211a1a24d2f144ce5f302934ef1
drsoft28 <dif.rabie@gmail.com>
2022-12-29 16:20:37 +0100

    translate some words

--- src/layouts/network/header.vue
+++ src/layouts/network/header.vue
@@ -4,31 +4,31 @@
               <!-- <a class="nav-link active" aria-current="page" href="../index.html"
                 >الرئيسية</a
               > -->
-              <router-link :to="getRouteLocale('network-home')"  class="nav-link">الرئيسية</router-link>
+              <router-link :to="getRouteLocale('network-home')"  class="nav-link">{{ $t('Home-page') }}</router-link>
             </li>
             <li class="nav-item px-2">
            
-              <router-link :to="getRouteLocale('network-investment-project')" class="nav-link">مشاريع للإستثمار</router-link>
+              <router-link :to="getRouteLocale('network-investment-project')" class="nav-link">{{ $t('Investment-projects') }}</router-link>
             </li>
             <li class="nav-item px-2">
-              <router-link :to="getRouteLocale('network-exhibitions')" class="nav-link">المعارض</router-link>
+              <router-link :to="getRouteLocale('network-exhibitions')" class="nav-link">{{ $t('Exhibitions') }}</router-link>
             </li>
            
             <li class="nav-item px-2">
-              <router-link :to="getRouteLocale('network-offers')" class="nav-link">العروض</router-link>
+              <router-link :to="getRouteLocale('network-offers')" class="nav-link">{{ $t('Offers') }}</router-link>
             </li>
             <li class="nav-item px-2">
-              <router-link :to="getRouteLocale('network-models')" class="nav-link">النماذج</router-link>
+              <router-link :to="getRouteLocale('network-models')" class="nav-link">{{ $t('Models') }}</router-link>
             </li>
             <li class="nav-item px-2">
-              <router-link :to="getRouteLocale('network-blogs')" class="nav-link">المدونة</router-link>
+              <router-link :to="getRouteLocale('network-blogs')" class="nav-link">{{ $t('Blogs') }}</router-link>
             </li>
             <li class="nav-item px-2">
-              <router-link :to="getRouteLocale('contact-us')" class="nav-link">تواصل معنا</router-link>
+              <router-link :to="getRouteLocale('contact-us')" class="nav-link">{{ $t('contact-us') }}</router-link>
             </li>
             <div  v-if="false" style="line-height: 2.5; height: 40px;" class="btn-main btn-nav text-center">
                         <a href="" class="text-white " data-bs-toggle="modal"
-                        data-bs-target="#addModal">أضف خدمة جديدة</a>
+                        data-bs-target="#addModal">{{ $t('add-new-service') }}</a>
                     </div>
       
     </TemplateHeader>
--- src/layouts/service-provider/header.vue
+++ src/layouts/service-provider/header.vue
@@ -4,20 +4,20 @@
               <!-- <a class="nav-link active" aria-current="page" href="../index.html"
                 >الرئيسية</a
               > -->
-              <router-link :to="getRouteLocale('service-provider-home')"  class="nav-link">الرئيسية</router-link>
+              <router-link :to="getRouteLocale('service-provider-home')"  class="nav-link">{{ $t('Home-page') }}</router-link>
             </li>
             <li class="nav-item px-2">
            
-              <router-link :to="getRouteLocale('service-provider-show-services')" class="nav-link">أعرض خدماتك </router-link>
+              <router-link :to="getRouteLocale('service-provider-show-services')" class="nav-link">{{ $t('show-your-services') }} </router-link>
             </li>
             <li class="nav-item px-2">
-              <router-link :to="getRouteLocale('service-provider-ready-services')" class="nav-link">خدماتك الجاهزة</router-link>
+              <router-link :to="getRouteLocale('service-provider-ready-services')" class="nav-link">{{ $t('your-ready-service') }}</router-link>
             </li>
             <li class="nav-item px-2">
-              <router-link :to="getRouteLocale('contact-us')" class="nav-link">تواصل معنا</router-link>
+              <router-link :to="getRouteLocale('contact-us')" class="nav-link">{{ $t('contact-us') }}</router-link>
             </li>
             <button @click="openAddService" style="line-height: 2.5; height: 40px;" class="btn-main btn-nav text-center text-white">
-              أضف خدمة جديدة
+              {{ $t('add-new-service') }}
                     </button>
        
     </TemplateHeader>
