b37a3dca64d36a07aa1696968641bd9593ec0cfd
drsoft28 <dif.rabie@gmail.com>
2022-12-29 19:11:04 +0100

    add some APIs

--- src/services/api/service-provider.js
+++ src/services/api/service-provider.js
@@ -4,9 +4,16 @@ class ServiceProviderApi extends BaseApi{
     //Get all project-categories
     getAll(params={}){
         let s=this.generateQueryUrl(params)
+        return window.axios.get(`service-provider/provider/projects?${s}`);
+    }
+    getReadyServicesAll(params={}){
+        let s=this.generateQueryUrl(params)
+        return window.axios.get(`service-provider/provider/ready-services?${s}`);
+    }
+    getCategories(params={}){
+        let s=this.generateQueryUrl(params)
         return window.axios.get(`service-provider/provider/project-categories?${s}`);
     }
-
     getBestProvider(params={}){
         let s=this.generateQueryUrl(params)
         return window.axios.get(`service-provider/user/distinguished-providers?${s}`);
