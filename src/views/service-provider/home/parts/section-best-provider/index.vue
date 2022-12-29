dc0b276e52dd6c7adab8ccdfb55201afc37933b9
drsoft28 <dif.rabie@gmail.com>
2022-12-29 17:55:07 +0100

    load list best provider from backend

--- src/views/service-provider/home/parts/section-best-provider/index.vue
+++ src/views/service-provider/home/parts/section-best-provider/index.vue
@@ -8,6 +8,7 @@
             
               </div>
               <d-swiper
+              v-if="!loading"
               style="overflow-x: hidden"
             :slides-per-view="5"
             :space-between="10"
@@ -19,8 +20,8 @@
               <router-link class="router-link" :to="getRouteLocale('service-provider-show-profile',{id:item.id})">
                 <CardMember 
                 :name="item.name" 
-                :description="item.description" 
-                :img="item.img"/>
+                :description="item.job" 
+                :img="item.image"/>
               </router-link>
                 </template>
             </d-swiper>
@@ -32,14 +33,16 @@
 <script>
 
 import CardMember from '@/components/cards/card-member.vue'
-
+import serviceProviderAPIs from '@/services/api/service-provider';
 export default {
     name:'section-best-provider',
     components:{
         CardMember
     },
     data:()=>({
+      loading:true,
         items:[
+           /* {id:1,name:'العنود محمد',description:'التصوير',img:'/assets/img/Rectangle 1775qa.png'},
             {id:1,name:'العنود محمد',description:'التصوير',img:'/assets/img/Rectangle 1775qa.png'},
             {id:1,name:'العنود محمد',description:'التصوير',img:'/assets/img/Rectangle 1775qa.png'},
             {id:1,name:'العنود محمد',description:'التصوير',img:'/assets/img/Rectangle 1775qa.png'},
@@ -48,9 +51,26 @@ export default {
             {id:1,name:'العنود محمد',description:'التصوير',img:'/assets/img/Rectangle 1775qa.png'},
             {id:1,name:'العنود محمد',description:'التصوير',img:'/assets/img/Rectangle 1775qa.png'},
             {id:1,name:'العنود محمد',description:'التصوير',img:'/assets/img/Rectangle 1775qa.png'},
-            {id:1,name:'العنود محمد',description:'التصوير',img:'/assets/img/Rectangle 1775qa.png'},
-        ]
-    })
+    */]
+    }),
+    methods:{
+      async loadList(){
+          this.loading = true;
+          try {
+            let { data } = await serviceProviderAPIs.getBestProvider()
+            if(data.success){
+              this.items = data.data
+            }
+          } catch (error) {
+            console.log('error',error)
+            console.log('error response',error.response)
+          }
+          this.loading = false;
+      }
+    },
+    mounted(){
+      this.loadList()
+    }
 }
 </script>
 
