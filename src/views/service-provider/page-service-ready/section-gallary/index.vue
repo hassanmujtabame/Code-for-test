88602a26d4fd54700aecc36ef1783096288f7bee
drsoft28 <dif.rabie@gmail.com>
2022-12-31 20:10:59 +0100

    update gallary image

--- src/views/service-provider/page-service-ready/section-gallary/index.vue
+++ src/views/service-provider/page-service-ready/section-gallary/index.vue
@@ -1,68 +1,93 @@
 <template>
-  <div>
-    <div class="box">
-        <!--mySwiper2-->
-                            <d-swiper 
-                            class
-                            :items="items" 
-                            :spaceBetween="10"
-                            :slidesPerView="1"
-                            :navigation='{
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",}'
-                            style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff"
-                                >
-                                <template v-slot="{item}">
-                                        <img style="height:300px"  :src="item.image" />
-                                    </template>
-                            </d-swiper>
-
-                        </div>
-                        <!--mySwiper service-provider-->
-                        <d-swiper class="mySwiper service-provider" thumbsSlider="" 
-                        :items="items"
-                        :loop="true"
-                        :spaceBetween=10
-                            :slidesPerView=4
+    <div>
+      <div class="box">
+          <!--mySwiper2-->
+                              <d-swiper 
+                              v-if="controlledSwiper"
+                              :controller="{ control: controlledSwiper }"
+                              :items="items" 
+                              :spaceBetween="10"
+                              :slidesPerView="1"
+                              :loop="true"
+                              :navigation='{
+                              nextEl: ".swiper-button-next",
+                              prevEl: ".swiper-button-prev",}'
+                              style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff"
+                                  >
+                                  <template v-slot="{item}">
+                                          <img style="height:300px"  :src="item.image_path" />
+                                      </template>
+                              </d-swiper>
+  
+                          </div>
+                          <!--mySwiper service-provider-->
+                          <d-swiper
+  
+                          class="mySwiper service-provider" 
+                          :items="items"
+                          :loop="true"
+                          :spaceBetween=10
+                              :slidesPerView=4
                             :freeMode="true"
-                            :watchSlidesProgress="true"
-                        >
-                            <template v-slot="{item}">
-                                        <img style="height:115px" :src="item.image" />
-                                    </template>
-                            </d-swiper>
-                   
-  </div>
-</template>
-
-<script>
-export default {
- name:'section-gallary',
- data:()=>({
-    items:[
-        {image:'/assets/img/serv-1.png'},
-        {image:'/assets/img/serv-2.png'},
-        {image:'/assets/img/serv-3.png'},
-    ]
- })
-}
-</script>
-
-<style scoped>
-@media (max-width: 496px) {
-    .img-cons img {
-        width: 100% !important;
-        left: 0 !important;
-    }
-
-    .img-cons {
-        background-color: white;
-        border-radius: 50%;
-        width: 100%;
-    }
-}
-.box{
-    height: 300px;
-}
-
-</style>
+                              @swiper="setThumbsSwiper"
+                              watch-slides-progress
+                          >
+                              <template v-slot="{item}">
+                                          <img style="height:115px" :src="item.image_path" />
+                                      </template>
+                              </d-swiper>
+                     
+    </div>
+  </template>
+  
+  <script>
+  let itemsTest=[
+          {id:1,image_path:'/assets/img/serv-1.png'},
+          {id:2,image_path:'/assets/img/serv-2.png'},
+          {id:3,image_path:'/assets/img/serv-3.png'},
+      ]
+  export default {
+   name:'section-gallary',
+   props:{
+      item:{
+          type:[Object,Array],
+          require:true
+      }
+   },
+   data:(vm)=>({
+      thumbsSwiper: null,
+      controlledSwiper:null,
+      items:itemsTest
+      //items:vm.item.medias??itemsTest
+   }),
+      methods: {
+       
+        setThumbsSwiper(swiper) {
+          console.log('swiper')
+          this.controlledSwiper = swiper;
+         //this.thumbsSwiper = swiper;
+        },
+      },
+      mounted(){
+      }
+  }
+  </script>
+  
+  <style scoped>
+  @media (max-width: 496px) {
+      .img-cons img {
+          width: 100% !important;
+          left: 0 !important;
+      }
+  
+      .img-cons {
+          background-color: white;
+          border-radius: 50%;
+          width: 100%;
+      }
+  }
+  .box{
+      height: 300px;
+  }
+  
+  </style>
