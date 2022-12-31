914ad00c27108f1ebaa77cb80184c317488dc680
drsoft28 <dif.rabie@gmail.com>
2022-12-31 20:12:15 +0100

    add swiper v2 for test

---
+++ src/components/swiper/v2/index.vue
@@ -0,0 +1,91 @@
+<template>
+   <div v-if="!loaded"></div>
+  <swiper
+  v-else
+      :scrollbar="scrollbar"
+      :navigation="navigation"
+      :pagination="pagination"
+      :watch-slides-visibility="watchSlidesVisibility"
+      :watch-slides-progress="watchSlidesProgress"
+      :slides-per-view="slidesPerView"
+      :space-between="spaceBetween"
+             v-bind="$attrs"
+             v-on="$listeners"
+            >
+               <slot></slot>
+
+                </swiper>
+</template>
+
+<script>
+import { Navigation, Pagination ,  Thumbs } from 'swiper'
+import {  SwiperCore,Swiper  } from 'swiper-vue2'
+// Import Swiper styles
+import 'swiper/swiper-bundle.css'
+SwiperCore.use([Navigation, Pagination,Thumbs])
+import SwiperSlide from './slider.vue'
+export default {
+ name:'d-swiper',
+ props:{
+   pagination:{
+      type:[Boolean,Object],
+      default:false,
+   },
+   watchSlidesVisibility:{
+      type:[Boolean,Object],
+      default:false,
+   },
+   watchSlidesProgress:{
+      type:[Boolean,Object],
+      default:false,
+   },
+   navigation:{
+      type:[Boolean,Object],
+      default:true,
+   },
+   scrollbar:{
+      type:[Boolean,Object],
+      default:false,
+   },
+   slidesPerView:{
+      type:[Number,String],
+      default:3,
+   },
+   spaceBetween:{
+      type:[Number],
+      default:3,
+   },
+    items:{
+        type:Array,
+        require:true
+    }
+ },
+ components:{
+    Swiper
+ },
+ data:()=>{
+   return {
+   loaded:false,
+   modules: [],
+ }
+},
+ mounted(){
+   /*if(this.pagination)
+   this.modules.push(Pagination)
+   if(this.navigation)
+   this.modules.push(Navigation)
+   if(Object.hasOwnProperty.call(this.$attrs,'thumbs')){
+      console.log('has thumb')
+      this.modules.push(Thumbs)
+   }*/
+   this.loaded = true;
+ }
+}
+</script>
+
+<style scoped>
+.swiper-container-thumbs .swiper-slide.swiper-slide-visible{
+   opacity: 1;
+}
+
+</style>
---
+++ src/components/swiper/v2/slider.vue
@@ -0,0 +1,30 @@
+<template>
+  <SwiperSlide  >
+    <slot ></slot>
+  </SwiperSlide>
+</template>
+
+<script>
+import {   SwiperSlide  } from 'swiper-vue2'
+export default {
+    name:'swiper-slide',
+ components:{
+    SwiperSlide
+ },
+methods:{
+   test(t,b){
+      console.log('tesddt',t,b)
+      /**
+       *  updateClasses(swiper,el,classNames)
+       */
+   }
+},
+mounted(){
+   console.log('swiper-slider',this)
+ }
+}
+</script>
+
+<style>
+
+</style>
