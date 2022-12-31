105da12bcb6a61077fe53bd925f81c212d068abb
drsoft28 <dif.rabie@gmail.com>
2022-12-31 21:27:33 +0100

    ربط صفحة طلباتي بالباك انك

--- src/views/service-provider/show-services/parts/section-filter/index.vue
+++ src/views/service-provider/show-services/parts/section-filter/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <d-filter-list :fakeItems="items">
+  <d-filter-list :call-list="loadList">
 
 <template v-slot="{item}">
   <router-link :to="getRouteLocale('service-provider-page-service',{id:item.id})">
@@ -9,38 +9,93 @@
         :department="item.department"
         :category="item.category"
         :price="item.price"
-        :during="item.during"
-        :place="item.place"
-        :offers="item.offers"
-        :datePublish="item.datePublish"
+        :during="item.rest_days"
+        :place="item.city"
+        :offers="item.count_offer"
+        :datePublish="item.start_date"
      />
     </router-link>
 </template>
-
+<template v-slot:order>
+                <select v-model="filterItem.price" class="form-select form-select-lg mb-3 py-3" aria-label=".form-select-lg example">  
+                    <option v-for="(item,i) in orderList" :key="i" :value="item.id">{{ item.name }}</option>
+                </select>
+                <p style="top: -13px; right: 24px; background: white" class="position-absolute">
+                    ترتيب حسب
+                </p>
+            </template>
+            <template v-slot:side>
+              <SidebarBox :filterItem="filterItem"  @change="filterItem=$event"/>
+            </template>
 </d-filter-list>
 </template>
 
 <script>
+import myRequestsAPIs from '@/services/api/service-provider/user/my-requests.js'
+import SidebarBox from './sidebar.vue'
 import showRequest from '@/components/cards/show-request.vue'
 export default {
  name:'section-filter',
   components:{
-    showRequest
+    showRequest,
+    SidebarBox
   },
-  data:()=>({
+  data:(vm)=>{
+  
+    return {
+    orderList:[
+        {id:'asc',name:vm.$t('lower-price')},
+        {id:'desc',name:vm.$t('higher-price')},
+    ],
+    filterItem:{
+      price:'asc',
+      state:null,
+      category_id:null,
+      valueMinDuring:0,
+      valueMaxDuring:100,
+      priceMin:null,
+      priceMax:null
+    },
     items:[
-        {id:1,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',during:'6 أيام',offers:'3',datePublish:'نشر منذ ساعة',price:1500},
-        {id:2,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',during:'6 أيام',offers:'3',datePublish:'نشر منذ ساعة',price:1500},
-        {id:3,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',during:'6 أيام',offers:'3',datePublish:'نشر منذ ساعة',price:1500},
-        {id:4,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',during:'6 أيام',offers:'3',datePublish:'نشر منذ ساعة',price:1500},
-        {id:5,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',during:'6 أيام',offers:'3',datePublish:'نشر منذ ساعة',price:1500},
-        {id:6,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',during:'6 أيام',offers:'3',datePublish:'نشر منذ ساعة',price:1500},
-        {id:7,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',during:'6 أيام',offers:'3',datePublish:'نشر منذ ساعة',price:1500},
-        {id:8,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',during:'6 أيام',offers:'3',datePublish:'نشر منذ ساعة',price:1500},
-        {id:9,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',during:'6 أيام',offers:'3',datePublish:'نشر منذ ساعة',price:1500},
-        {id:10,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',during:'6 أيام',offers:'3',datePublish:'نشر منذ ساعة',price:1500}
+        {id:1,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',rest_days:'6 أيام',count_offer:'3',start_date:'نشر منذ ساعة',price:1500},
+        {id:2,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',rest_days:'6 أيام',count_offer:'3',start_date:'نشر منذ ساعة',price:1500},
+        {id:3,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',rest_days:'6 أيام',count_offer:'3',start_date:'نشر منذ ساعة',price:1500},
+        {id:4,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',rest_days:'6 أيام',count_offer:'3',start_date:'نشر منذ ساعة',price:1500},
+        {id:5,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',rest_days:'6 أيام',count_offer:'3',start_date:'نشر منذ ساعة',price:1500},
+        {id:6,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',rest_days:'6 أيام',count_offer:'3',start_date:'نشر منذ ساعة',price:1500},
+        {id:7,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',rest_days:'6 أيام',count_offer:'3',start_date:'نشر منذ ساعة',price:1500},
+        {id:8,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',rest_days:'6 أيام',count_offer:'3',start_date:'نشر منذ ساعة',price:1500},
+        {id:9,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',rest_days:'6 أيام',count_offer:'3',start_date:'نشر منذ ساعة',price:1500},
+        {id:10,title:'تصميم فستان لحلفة تخرج',description:'أحتاج مصممة ازياء على وجهه السرعة تصمم لي فستان بسيط لحفلة تخرجي',department:'قسم الازياء',category:'الخياطة',place:'جدة',rest_days:'6 أيام',count_offer:'3',start_date:'نشر منذ ساعة',price:1500}
     ]
-  })
+  }},
+  watch:{
+  filterItem:{
+    deep:true,
+    handler(){
+      this.fireEvent('d-filter-list-refresh')
+    }
+  }
+  },
+    methods: {
+        openAddService(evt){
+        evt.preventDefault();
+        this.fireOpenDialog('add-my-request-dialog')
+      },
+      closeAddService(evt){
+        evt.preventDefault();
+        this.fireCloseDialog('add-my-request-dialog')
+      },
+        async loadList(metaInfo) {
+            let params = {
+                page: metaInfo.current_page,
+                category_id: this.category_id,
+                ...this.filterItem
+            }
+            return await myRequestsAPIs.getAll(params)
+        }
+      
+    }
 }
 </script>
 
---
+++ src/views/service-provider/show-services/parts/section-filter/sidebar.vue
@@ -0,0 +1,155 @@
+<template>
+    <div class="box border p-3 rounded-3">
+        <h4>
+            فلتر البحث
+        </h4>
+        <div class="accordion" id="accordionPanelsStayOpenExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
+                        aria-controls="panelsStayOpen-collapseOne">
+                        تصنيف الخدمة
+                    </button>
+                </h2>
+                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
+                    aria-labelledby="panelsStayOpen-headingOne">
+                    <div class="accordion-body">
+                        <div>
+                            <div v-for="(item,i) in states" :key="i" class="form-check">
+                                <input class="form-check-input" type="radio" v-model="filter.state" :value="item.id" name="flexRadioDefault"
+                                    >
+                                <label class="form-check-label" >
+                                  {{  item.name }}
+                                </label>
+                            </div>
+                           
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false"
+                        aria-controls="panelsStayOpen-collapseTwo">
+                        مجالات الاختصاص
+
+                    </button>
+                </h2>
+                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse"
+                    aria-labelledby="panelsStayOpen-headingTwo">
+                    <div class="accordion-body">
+                        <div v-for="(cat,i) in categories" :key="i" class="form-check">
+                            <input v-model="filter.category_id" :value="cat.id" class="form-check-input" type="radio">
+                            <label class="form-check-label" for="defaultCheck1">
+                                {{cat.name}}
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false"
+                        aria-controls="panelsStayOpen-collapseThree">
+                        المدة
+                    </button>
+                </h2>
+                <div id="panelsStayOpen-collapseThree" class="accordion-collapse "
+                    aria-labelledby="panelsStayOpen-headingThree">
+                    <div style="margin: 20px 0px 0 0" class="a">
+                                    <div class="slider-container">
+                                        <rslider-input
+                                         :min.sync="filter.valueMinDuring" 
+                                         :max.sync="filter.valueMaxDuring"
+                                         :lmin="0"
+                                         :lmax="100"
+                                         />
+                                    </div>
+                                </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-headingFour">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false"
+                        aria-controls="panelsStayOpen-collapseFour">
+                        التكلفة
+                    </button>
+                </h2>
+                <div id="panelsStayOpen-collapseFour" class="accordion-collapse "
+                    aria-labelledby="panelsStayOpen-headingFour">
+                    <div style="margin: 20px 0px 0 0" class="a">
+                                    <div class="slider-container">
+                                        <rslider-input
+                                         :min.sync="filter.priceMin" 
+                                         :max.sync="filter.priceMax"
+                                         :lmin="0"
+                                         :lmax="100"
+                                         />
+                                    </div>
+                                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import myRequestsAPIs from '@/services/api/service-provider/user/my-requests.js'
+
+export default {
+ name:'sidebar-box',
+ props:{
+    filterItem:{
+        type:[Object,Array],//defaults values
+        require:true
+    }
+ },
+ data:(vm)=>{
+    return{
+        states:[
+            {id:null,name:'الكل'},
+            {id:'online',name:'خدمات اونلاين'},
+            {id:'offline',name:'خدمات اوفلاين'},
+        ],
+        categories:[],
+    filter:vm.filterItem
+ }},
+ watch:{
+    filter:{
+        deep:true,
+        handler(val){
+            this.$emit('change',val)
+        }
+    }
+ },
+ methods:{
+
+ async getCategories() {
+            try {
+                let { data } = await myRequestsAPIs.getCategories()
+                if (data.success) {
+
+                    let categories = data.data;
+                    categories.unshift({ id: null, name: 'الكل' })
+                    this.categories=categories
+                }
+            } catch (error) {
+                console.log('error', error)
+                console.log('error response', error.response)
+            }
+        }
+    },
+    mounted() {
+        this.getCategories();
+    }
+}
+</script>
+
+<style>
+
+</style>
