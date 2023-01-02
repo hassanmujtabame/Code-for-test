d3f75a7ec57fa598d1ca658208519c3cd6c97a78
drsoft28 <dif.rabie@gmail.com>
2023-01-02 14:40:44 +0100

    fix issue align text

--- src/views/auth/register/RegisterView.vue
+++ src/views/auth/register/RegisterView.vue
@@ -3,7 +3,7 @@
         
                 <div class="row align-items-center p-0 position-relative ">
                     <div class="col-12 col-md-6 p-4">
-                        <div style="top: 17px;left: 53%;" class="position-absolute form-loging">
+                        <div :style="{top: '17px',left: $i18n.locale=='ar'?'auto':'0',right: $i18n.locale!=='ar'?'auto':'0'}" class="position-absolute form-loging">
                             <h6>لديك حساب بالفعل؟  
                                 <router-link :to="getRouteLocale('login')" class="m-c">
                                   سجل دخولك الان
