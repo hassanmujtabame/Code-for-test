0aa77fb1e4c2cfc5b08248c4abd3f9820b70a520
drsoft28 <dif.rabie@gmail.com>
2023-01-02 14:56:14 +0100

    seperate section to parts

---
+++ src/views/network/home/guest/parts/section-2/parts/grow-your-network/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="row align-items-center m-auto">
+        <div class="col-md-6">
+            <div>
+                <div class="bg-back position-relative">
+                    <img class="img-fluid" src="/assets/img/pana.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 text">
+            <h2 class="fw-bolder">
+                شبكة رياديات: وسع دائرة معارفك
+            </h2>
+            <p>
+                رياديات شبكة تضم نخبة من رائدات الأعمال والمستثمرات والشركاء، وتهدف إلى إلهام وتمكين وجمع رائدات الأعمال
+                معًا على منصة واحدة ليساعد بعضهن البعض الآخر على النجاح في أعمالهن. ويمكن من خلالها الحصول على المساعدة
+                والتشجيع للدخول في مجال ريادة الأعمال من خلال مجموعة من الخدمات والمزايا، وقد صممت لمساعدة رائدات
+                الأعمال على التعلم وزيادة نمو أعمالهن والتواصل مع الرياديات
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+ name:'grow-your-network'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/network/home/guest/parts/section-2/parts/network-goals.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="mt-5">
+        <h2 class="text-center text-bold"> أهداف شبكة رياديات</h2>
+        <div class="row min-riadiat network">
+            <div class="col-md-3 mt-2 ">
+                <div class="box border d-flex align-items-center rounded-2">
+                    <div>
+                        <img src="/assets/img/a1.svg" alt="">
+                    </div>
+                    <div>
+                        جمع والهــــــام وتمكين النســاء </div>
+                </div>
+            </div>
+            <div class="col-md-3 mt-2 ">
+                <div class="box border d-flex align-items-center rounded-2">
+
+                    <div>
+                        <img src="/assets/img/a2.svg" alt="">
+                    </div>
+                    <div>
+                        بناء شبكــــــــــة علاقــــــــــــــات </div>
+                </div>
+            </div>
+            <div class="col-md-3 mt-2 ">
+                <div class="box border d-flex align-items-center rounded-2">
+
+                    <div>
+                        <img src="/assets/img/a3.png" alt="">
+                    </div>
+                    <div>
+                        شراكات مـــــــع
+                        مختلف الجهـات
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mt-2 ">
+                <div class="box border d-flex align-items-center rounded-2">
+                    <div>
+                        <img src="/assets/img/a4.png" alt="">
+                    </div>
+                    <div>
+                        تطوير رائـــــدات الاعمــــــــــــــال
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'network-goals'
+}
+</script>
+
+<style>
+
+</style>
