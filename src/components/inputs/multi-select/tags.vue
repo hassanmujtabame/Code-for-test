b1149130ab0ffad16357cbc64a934f26bbeb7beb
drsoft28 <dif.rabie@gmail.com>
2023-01-02 10:51:10 +0100

    إضافة مكون خاص ب اختار كلمات متعدد

---
+++ src/components/inputs/multi-select/tags.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+  <label v-if="label" class="typo__label">{{label}}</label>
+  <multiselect v-model="value_" 
+  :tag-placeholder="tagPlaceholder" 
+  :placeholder="placeholder" 
+  @remove="removeTag"
+  :label="trackText"
+   :track-by="trackBy" 
+   :options="options_" 
+   :multiple="true" 
+   :taggable="true" 
+   @tag="addTag"
+ 
+  ></multiselect>
+</div>
+</template>
+<script>
+
+import Multiselect from 'vue-multiselect'
+
+export default {
+    name:'d-multi-select-tag',
+    props:{
+        value:{
+            type:String,
+            default:''
+        },
+        label:{
+            type:String,
+        },
+        trackText:{
+            type:String,
+            default:'name'
+        },
+        trackBy:{
+            type:String,
+            default:'name'
+        },
+        tagPlaceholder:{
+            type:String,
+            default:'Add this as new tag'
+        },
+        placeholder:{
+            type:String,
+            default:'Search or add a tag'
+        },
+        options:{
+            type:[Array,Object],
+            default:()=> { return []}
+        }
+    },
+  components: {
+    Multiselect
+  },
+  data (vm) {
+    return {
+      value_:[],
+      options_:vm.options
+    }
+  },
+  watch:{
+    value:{
+
+        immediate:true,
+        handler(val){
+             if(!val)
+             this.value_ = [];
+             else{
+                this.value_ = this.value.split(',').map(x=>{return {name:x}})
+             }
+        }
+    }
+  },    
+  methods: {
+    updateEvent(){
+        this.$emit('input',this.value_.map(v=>v.name).join(','))
+    }
+    ,
+    removeTag(tag){
+    this.value_ = this.value_.filter(x=>x.name!==tag.name)
+    this.updateEvent()
+    },
+    addTag (newTag) {
+      const tag ={name:newTag} 
+      this.options_.push(tag)
+      this.value_.push(tag)
+      this.updateEvent()
+    }
+  }
+}
+</script>
+<style>
+
+</style>
--- src/plugins/components.js
+++ src/plugins/components.js
@@ -9,6 +9,7 @@ import DDatePicker from '@/components/calendar/d-date-picker.vue'
 import DCarousel from '@/components/carousel/index.vue'
 import DCarouselSlide from '@/components/carousel/slide.vue'
 import DFilterList from '@/components/filter-list/index.vue'
+import DMultiSelecetTagInput from '@/components/inputs/multi-select/tags.vue'
 import RSliderInput from '@/components/inputs/r-slider/index.vue'
 import DatePickerInput from '@/components/inputs/date-picker/index.vue'
 import DatePickerRange from '@/components/inputs/date-picker/range.vue'
@@ -27,6 +28,7 @@ import ErrorInput from '@/components/error-input/index.vue'
 
 
 Vue.component('d-error-input', ErrorInput)
+Vue.component('d-multi-select-tag', DMultiSelecetTagInput)
 Vue.component('date-picker-input', DatePickerInput)
 Vue.component('date-picker-range', DatePickerRange)
 Vue.component('d-ckeditor-classic', DCkeditorClassicInput)
