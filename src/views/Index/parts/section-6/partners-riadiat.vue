8dfc1bbf2d88797f13a8ee707b3f7cdc1af85a25
drsoft28 <dif.rabie@gmail.com>
2023-01-04 13:04:07 +0100

    ربط قسم شركائنل بالباك اند

--- src/views/Index/parts/section-6/partners-riadiat.vue
+++ src/views/Index/parts/section-6/partners-riadiat.vue
@@ -3,8 +3,8 @@
         <div class="container p-3">
             <h2>شركائنا في رياديات </h2>
             <div class="row justify-content-center">
-                <div v-for="(partner,i) in partners" :key="i" class=" col-12 col-md-3 text-center" data-aos="fade-up" data-aos-duration="2000">
-                    <ImgAvatar :img="partner.img" :alt="partner.name" size=180 />
+                <div v-for="(partner,i) in items" :key="i" class=" col-12 col-md-3 text-center" data-aos="fade-up" data-aos-duration="2000">
+                    <ImgAvatar :img="partner.image_path"  size=180 />
                 </div>
             </div>
         </div>
@@ -12,6 +12,7 @@
 </template>
 
 <script>
+import commonAPI from '@/services/api/common.js'
 import ImgAvatar from '@/components/avatars/img-avatar.vue';
 export default {
     components:{
@@ -19,16 +20,35 @@ export default {
     },
  data:()=>({
     partners:[
-        {img:'/assets/img/Ellipse 24.png',name:'partner 01'},
-        {img:'/assets/img/Ellipse 25.png',name:'partner 02'},
-        {img:'/assets/img/Ellipse 24.png',name:'partner 03'},
-        {img:'/assets/img/Ellipse 25.png',name:'partner 04'},
-        {img:'/assets/img/Ellipse 24.png',name:'partner 05'},
-        {img:'/assets/img/Ellipse 25.png',name:'partner 06'},
-        {img:'/assets/img/Ellipse 24.png',name:'partner 07'},
-        {img:'/assets/img/Ellipse 25.png',name:'partner 08'},
+        {image_path:'/assets/img/Ellipse 24.png',name:'partner 01'},
+        {image_path:'/assets/img/Ellipse 25.png',name:'partner 02'},
+        {image_path:'/assets/img/Ellipse 24.png',name:'partner 03'},
+        {image_path:'/assets/img/Ellipse 25.png',name:'partner 04'},
+        {image_path:'/assets/img/Ellipse 24.png',name:'partner 05'},
+        {image_path:'/assets/img/Ellipse 25.png',name:'partner 06'},
+        {image_path:'/assets/img/Ellipse 24.png',name:'partner 07'},
+        {image_path:'/assets/img/Ellipse 25.png',name:'partner 08'},
     ]
- })
+    ,
+    loading:true,
+    items:[]
+ }) ,methods:{
+    async initlizing(){
+      this.loading = true;
+        try {
+          let { data } =  await commonAPI.getPartnerHome();
+          if(data.success){
+            this.items = data.data
+          }
+        } catch (error) {
+          console.log('error',error)
+        }
+      this.loading = false;
+    }
+  },
+  mounted(){
+    this.initlizing()
+ }
 }
 </script>
 
