2588cc8668e9be25372bcba263841b122a627953
drsoft28 <dif.rabie@gmail.com>
2023-01-05 17:58:15 +0100

    create success add project dialog

---
+++ src/views/network/investment-project-request/dialogs/success-add-project.vue
@@ -0,0 +1,56 @@
+<template>
+<d-dialog-large :xl="false"
+:group="group"
+:closeDialog="closeDialog"
+:openDialog="openDialog"
+>
+<template v-slot:default>
+  <div class="text-center">
+    <div>
+                        <img class="img-fluid" src="/assets/img/cuate-2.png" alt="">
+                    </div>
+                    <div>
+                        <h3>
+                            تم ارسال طلبك بنجاح
+                        </h3>
+                        <p>
+                            طلبك الان قد وثل الى مرسل الاعلان اذا كان المكان الذي عرضته عليه مناسب بنسبه له سيتواصل معك
+                        </p>
+                    </div>
+            </div>
+</template>
+<template v-slot:actions>
+  <button @click="closeMe" class="btn bg-main text-white" >{{ $t('Ok') }}</button>
+</template>
+</d-dialog-large>
+</template>
+<script>
+export default {
+  name:"success-add-project",
+  props:{
+    group:{
+        type:String,
+        default:'success-add-project'
+    }
+  },
+  data:()=>({
+    showDialog:false,
+  }),
+  methods:{
+    closeMe(){
+    this.fireCloseDialog(this.group)
+  },
+    openDialog(){
+
+        this.showDialog = true;
+        return true;
+    },
+    closeDialog(){
+        this.showDialog = false
+
+        return true;
+    },
+  }
+}
+
+</script>
