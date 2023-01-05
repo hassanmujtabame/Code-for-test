5e21e680669a72b55935a60c7d16df2112fee54d
drsoft28 <dif.rabie@gmail.com>
2023-01-05 21:24:37 +0100

    number networks

---
+++ src/views/network/home/guest/parts/section-numbers/card-number.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="box">
+    <div class="box-wrapper">
+    <h5  class="m-c fw-bolder box-value">
+        {{value}}
+    </h5>
+    <p class="box-title"> {{title}}</p>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+ name:'card-number',
+ props:{
+    title:{
+        type:String
+    },
+    value:{
+        type:[String,Number],
+        default:'N/A'
+    }
+ },
+ watch:{
+    value(){
+
+    }
+ }
+}
+</script>
+
+<style scoped>
+.box{
+    width: 158px;
+    max-width: 158px;
+    height: 145px;
+    margin: 10px;
+    background: #FFFFFF;
+    padding: 5px;
+    box-shadow: 0px 0px 4px rgb(0 0 0 / 12%);
+    border-radius: 8px;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+}
+.box-wrapper{ 
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column ;
+}
+.box-wrapper .box-title,.box-wrapper .box-value{
+    flex: 1;
+}
+</style>
--- src/views/network/home/guest/parts/section-numbers/index.vue
+++ src/views/network/home/guest/parts/section-numbers/index.vue
@@ -1,81 +1,83 @@
 <template>
    <div class="container">
           <div class="mt-5">
-            <div class="riadiat-of-number">
+            <div class="">
                 <h2 class="text-center">الشبكة  في ارقام  </h2>
-                <div class="row justify-content-center ">
-                    <div class="col-6 col-md-auto mt-3">
-                        <div class="box">
-
-                            <h5 class="m-c fw-bolder">
-                                60000+
-                            </h5>
-                            <p>
-                                اشتراك في برامجنا
-
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-auto mt-3">
-                        <div class="box">
-
-                            <h5 class="m-c fw-bolder">
-                                5222+
-                            </h5>
-                            <p>
-                                اشتراك في برامجنا
-
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-auto mt-3">
-                        <div class="box">
-
-                            <h5 class="m-c fw-bolder">
-                                5222+
-                            </h5>
-                            <p>
-                                اشتراك في برامجنا
-
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-auto mt-3">
-                        <div class="box">
-
-                            <h5 class="m-c fw-bolder">
-                                9+
-                            </h5>
-                            <p>
-                                اشتراك في برامجنا
-
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-auto mt-3">
-                        <div class="box">
-
-                            <h5 class="m-c fw-bolder">
-                                876+
-                            </h5>
-                            <p>
-                                اشتراك في برامجنا
-
-                            </p>
-                        </div>
-                    </div>
-
-                </div>
-
+                <d-swiper  v-if="!loading" 
+                :items="categories"
+                classColCard="col-6 col-md-auto mt-3"
+                :slides-per-view="7"
+                >
+                    <template v-slot="{item}" >
+                        <cardNumber
+                        :title="item.name"
+                        :value="statics[item.id]"
+                        />
+                    </template>
+                </d-swiper>
             </div>
-
         </div>
       </div>
 </template>
 
 <script>
+import userAPI from '@/services/api/user';
+import cardNumber from './card-number.vue';
 export default {
-name:'section-numbers'
+name:'section-numbers-networks',
+components:{
+    cardNumber
+},
+data:()=>({
+    loading:true,
+    categories : [
+        {name:'لقاءات تعليمية',id:"count_meetings"},
+        {name:'نماذخ',id:"count_models"},
+        {name:'مدونات',id:"count_blogs"},
+        {name:'عروض',id:"count_offers"},
+        {name:'قصص نجاح',id:"count_stories"},
+        {name:'معارض',id:"count_exhibitions"},
+        {name:'مشاريع لاسثمار',id:"count_invest_projects"},
+        {name:'مستثمرون',id:"invested"},
+        {name:'شركائنا',id:"count_partners"},
+    ],
+  statics:{
+    count_meetings: 'N/A',
+        count_models: 'N/A',
+        count_blogs: 'N/A',
+        count_offers: 'N/A',
+        count_stories:'N/A',
+        count_exhibitions:'N/A',
+        count_invest_projects: 'N/A',
+        invested: 'N/A',
+        count_partners: 'N/A',
+  }
+}) ,
+methods:{
+  async initializing(){
+    this.loading = true;
+      try {
+         let { data } = await  userAPI.statisticsNetwork()
+         if(data.success){
+            this.statics = data.data;
+         }
+      } catch (error) {
+         console.log('error',error)
+         console.log('error response',error.response)
+         if(error.response){
+            let response =error.response
+            if(response.status==401){
+               //
+           }
+         }
+         
+      }
+      this.loading = false;
+   }
+ },
+ mounted(){
+  this.initializing()
+ }
 }
 </script>
 
