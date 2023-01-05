7cc9c07284dbf44b36c97f070441a108f6d3aab2
drsoft28 <dif.rabie@gmail.com>
2023-01-05 18:25:39 +0100

    create confirm make offer

---
+++ src/views/network/investment-project-page/dialogs/confirm-make-offer.vue
@@ -0,0 +1,64 @@
+<template>
+<d-dialog-large :xl="false"
+:group="group"
+:closeDialog="closeDialog"
+:openDialog="openDialog"
+>
+<template v-slot:default>
+    <div class="text-center">
+                <!-- icon-->
+                    <AlertRedIcon />
+              </div>
+    <h3>هل لديك مكان للأيجار لهذا المشروع</h3>
+              
+              <p class="m-c fs-5 m-3">  
+                اذا كنت ترغب في تأجير مكان بالمواصفات التي طلبها العميل فقدم طلبك الان
+              </p>
+</template>
+<template v-slot:actions>
+    <button
+        @click="openOfferDialog"
+               class="btn bg-main text-white"
+               >  تقديم طلب </button>
+</template>
+</d-dialog-large>
+</template>
+<script>
+import AlertRedIcon from '@/components/icon-svg/alert-red-icon.vue'
+export default {
+  name:"confirm-make-offer",
+  components:{
+    AlertRedIcon
+  },
+  props:{
+    group:{
+        type:String,
+        default:'confirm-make-offer'
+    }
+  },
+  data:()=>({
+    project:{},
+    showDialog:false,
+  }),
+  methods:{
+    openOfferDialog(){
+    this.fireOpenDialog('make-offer');
+    this.closeMe()
+  },
+  closeMe(){
+    this.fireCloseDialog(this.group)
+  },
+    openDialog(data){
+        this.project =  data;
+        this.showDialog = true;
+        return true;
+    },
+    closeDialog(){
+        this.showDialog = false
+
+        return true;
+    },
+  }
+}
+
+</script>
