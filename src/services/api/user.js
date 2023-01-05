be5a0bc7f0351ad90bd35319d04fde6a6a8b9fa7
drsoft28 <dif.rabie@gmail.com>
2023-01-05 19:58:44 +0100

    add API for statistics network

--- src/services/api/user.js
+++ src/services/api/user.js
@@ -5,7 +5,9 @@ class UserApi extends BaseApi {
     me(){
         return window.axios.post(`user/me`);
     }
-   
+    statisticsNetwork(){
+        return window.axios.get(`user/statistics-network`);
+    }
 }
 
 export default new UserApi();
