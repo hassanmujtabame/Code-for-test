66ae4f2c8380ff6d5aaf09e720f587b98f66958a
drsoft28 <dif.rabie@gmail.com>
2023-01-08 22:43:16 +0100

    translate نماذج رياديات

--- src/views/network/home/auth/parts/section-models/index.vue
+++ src/views/network/home/auth/parts/section-models/index.vue
@@ -4,7 +4,7 @@
               <div
                 class="d-flex justify-content-between align-items-center container"
               >
-                <h1> نماذج رياديات      </h1>
+                <h1> {{ $t('riadiat-models') }} </h1>
                 <div>
                 <router-link class="router-link" custom 
                 :to="getRouteLocale('network-models')"
