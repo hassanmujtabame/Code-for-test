0bb3f2fed6ef0c0f7bcf68525af2ecf387e55117
drsoft28 <dif.rabie@gmail.com>
2023-01-08 21:37:34 +0100

    translate some words

--- src/layouts/common/section-subscribe/sect-subscribe.vue
+++ src/layouts/common/section-subscribe/sect-subscribe.vue
@@ -3,16 +3,17 @@
         <div class="container">
           <div class="row align-items-center justify-content-center p-4">
             <div class="col-md-6">
-              <h1 class="text-white">اشتركي بالقائمة البريدية
+              <h1 class="text-white">
+              {{ $t('subscribe-newsletter') }}
               </h1>
               <p class="text-white">
-                ليصلك كل العروض والاخبار الجديدة
+                {{ $t('subscribe-newsletter-text') }}
               </p>
             </div>
             <div class="col-md-6">
               <div class="input-group input-group-lg w-75 m-auto"  >
-                <input type="text" class="form-control p-3" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="اكتبي بريدك الالكتروني">
-                <span class="input-group-text bg-yal text-white" id="inputGroup-sizing-lg">اشترك</span>
+                <input type="text" class="form-control p-3" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" :placeholder="$t('write-your-email')">
+                <span class="input-group-text bg-yal text-white" id="inputGroup-sizing-lg">{{ $t('Subscribe') }}</span>
               </div>
             </div>
           </div>
