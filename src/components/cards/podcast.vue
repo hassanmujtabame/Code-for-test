aaa1fcbf7cbec6271c5dd69310f405bb72619a28
drsoft28 <dif.rabie@gmail.com>
2023-01-09 21:56:47 +0100

    create podcast card

---
+++ src/components/cards/podcast.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="box podcast-card ">
+                        <div class="podcast-card-wrapper">
+                            <div class="podcast-card-image position-relative">
+                                <div class="podcast-card-play">
+                                    <PlayIcon />
+                                </div>
+                            <img  :src="image" :alt="name"  >
+                        </div>
+                        </div>
+                        <div class="text-center bg-white box-deatils" :class="{'p-3':!description}">
+                            <h6 class="network-podcast-name">{{name}}</h6>
+                            <p v-if="description" class="network-podcast-description">{{description}}</p>
+
+                        </div>
+
+                    </div>
+</template>
+
+<script>
+import PlayIcon from '@/components/icon-svg/play.vue'
+export default {
+ name:'card-podcast',
+ components:{
+    PlayIcon
+ },
+ props:{
+    image:{
+        type:String,
+        default:'/assets/img/podcast.png',
+    },
+    name:{
+        type:String,
+        default:'رائدات الأعمال'
+    },
+    description:{
+        type:String,
+        default:'55 دقيقة'
+    }
+ }
+}
+</script>
+
+<style scoped>
+.box{
+    width: 210px;
+    height:260px;
+    padding: 10px 15px;
+    max-width: 210px;
+    overflow: hidden;
+    background: #000;
+    border-radius: 8px;
+}
+.podcast-card-play{
+    position: absolute;
+    top:0;
+    left:0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.box-deatils{
+    padding-top: 5px;
+    max-width: 100%;
+    height: calc(260px - 180px);
+    background-color: transparent !important;
+    color:white;
+}
+.network-podcast-name,.network-podcast-description{
+    font-size: 16px;
+    font-weight: bold;
+    color:white
+}
+.podcast-card-image{
+    width: 100%;
+    height: 180px;
+    
+}
+.podcast-card-image>img{
+    width: 100%;
+    height: 100%;
+    border-radius: 8px;
+}
+</style>
---
+++ src/components/icon-svg/play.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_1129_64537)">
+<circle cx="17" cy="17" r="17" fill="white"/>
+<path d="M25 17L13 24V10L25 17Z" fill="#1FB9B3"/>
+</g>
+<defs>
+<clipPath id="clip0_1129_64537">
+<rect width="34" height="34" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+</template>
+
+<script>
+export default {
+ name:'play-icon'
+}
+</script>
+
+<style>
+
+</style>
