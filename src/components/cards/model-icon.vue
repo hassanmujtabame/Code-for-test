9bed3e0d7ed0c1d713d38685e979f9a040557df7
drsoft28 <dif.rabie@gmail.com>
2023-01-09 20:57:04 +0100

    use icon download in card

---
+++ src/components/cards/model-icon.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="box rounded-3 border">
+                      <div class="box ">
+                          <div class="">
+                              <img class="w-100"  :src="img" :alt="title"  height="186">
+                          </div>
+                          <div class="text-start bg-white  p-2">
+                              <h6>{{title}}</h6>
+                              <div>
+                                <DownloadIcon />
+                                <small class="box-download">
+                                    عدد التحميلات: <strong>{{value}} مرة</strong>
+                                </small>
+                                
+                            </div>
+              
+  
+                          </div>
+  
+                      </div>
+                    </div> 
+</template>
+
+<script>
+import DownloadIcon from '@/components/icon-svg/arrow-down-circle-outline.vue'
+export default {
+    name:'card-vue',
+    components:{
+        DownloadIcon
+    },
+props:{
+    img:{
+        type:String
+    },
+    title:{
+        type:String
+    },
+    value:{
+        type:[String,Number]
+    }
+}
+}
+</script>
+
+<style scoped>
+.box-download{
+    color: var(--b-color);
+    font-size: 12px;
+}
+</style>
