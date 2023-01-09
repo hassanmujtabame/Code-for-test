ab41704def41ff2b43b1f8d03e6d5a2e029a66a5
drsoft28 <dif.rabie@gmail.com>
2023-01-09 15:35:10 +0100

    add btn rate exeprience and dialog

---
+++ src/layouts/common/dialogs/rate-experience/index.vue
@@ -0,0 +1,113 @@
+<template>
+ <d-dialog-large :xl="false" 
+    :group="group" 
+    :closeDialog="closeDialog"
+    :openDialog="openDialog"
+    >
+    <template v-slot:header>
+      {{ title }}
+    </template>
+    <template v-slot:default>
+      <div class="container">
+        <ValidationObserver v-if="showDialog" ref="form" >
+          <ValidationProvider
+                      :name="$t('rate')"
+                      vid="rate"
+                      rules="required"
+                        v-slot="{errors}">
+                        
+      <div class="starts-rate">
+        <startItem v-for="id in stars" :key="id" 
+        :itemId="id" :isSelected="itemForm.rate>=id" 
+       v-model="itemForm.rate" 
+        :size="32"
+        />
+      </div>
+      <div v-if="errors.length!==0" class="col-12 text-input-error">
+                                {{errors[0]}}
+                                </div>
+          </ValidationProvider>
+      <div class="mt-1">
+        <ValidationProvider
+                      :name="$t('comment')"
+                      vid="comment"
+                      rules="required"
+                        v-slot="{errors}">
+        <textarea class="form-control" rows="5"
+        v-model="itemForm.comment"
+        :placeholder="$t('write-your-comment')"
+        >
+
+        </textarea>
+        <div v-if="errors.length!==0" class="col-12 text-input-error">
+                                {{errors[0]}}
+        </div>
+          </ValidationProvider>
+      </div>
+    </ValidationObserver>
+    </div>
+    </template>
+    <template v-slot:actions>
+        <button @click="save" type="button" class="btn btn-main">
+           {{ $t('send-rate') }}
+        </button>
+    </template>
+    </d-dialog-large>
+</template>
+
+<script>
+import startItem from './star-item.vue'
+export default {
+  name:'rate-your-experience-network',
+  props:{
+    group:{
+        type:String,
+        default:'rate-your-experience-network'
+    },
+    title:{
+      type:String,
+      require:true
+    }
+  },
+  components:{
+    startItem
+  },
+  data:()=>({
+    stars:[5,4,3,2,1],
+    itemForm:{
+      rate:null,
+      comment:''
+    },
+    showDialog:false,
+  }),
+  methods:{
+   async save(){
+      let valid = await this.$refs.form.validate();
+    if(!valid){
+        console.log('form invalid');
+        return ;
+    }
+    },
+    openDialog(){
+      this.itemForm.rate = null;
+      this.itemForm.comment = '';
+    this.showDialog = true;
+    return true;
+},
+closeDialog(){
+    this.showDialog = false
+    return true;
+},
+closeEvent(){
+   this.fireEvent(this.group+'-close-dialog')
+}
+  }
+}
+</script>
+
+<style>
+.starts-rate{
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/layouts/common/dialogs/rate-experience/star-item.vue
@@ -0,0 +1,57 @@
+<template>
+  <svg :width="size" :height="size" 
+  @click="click_"
+  viewBox="0 0 70 66" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M35 0L46.6381 19.9815L69.238 24.8754L53.8309 42.1185L56.1603 65.1246L35 55.8L13.8397 65.1246L16.1691 42.1185L0.761966 24.8754L23.3619 19.9815L35 0Z" 
+:fill="isSelected?selectedColor:unselectedColor"
+/>
+</svg>
+</template>
+
+<script>
+export default {
+ name:'rate-item',
+ props:{
+    itemId:{
+        type:[String,Number],
+        require:true,
+    },
+    size:{
+        type:[String,Number],
+        default:24,
+    },
+    isSelected:{
+        type:Boolean,
+        default:false,
+    },
+    selectedColor:{
+        type:String,
+        default:'orange'
+    },
+    unselectedColor:{
+        type:String,
+        default:'#CDD7D8'
+    }
+ },
+ data:(vm)=>{
+    return {
+        selected_:vm.isSelected
+    }
+ },
+ watch:{
+    isSelected(){}
+ },
+ methods:{
+    click_(){
+        this.$emit('input',this.itemId)
+    }
+ }
+}
+</script>
+
+<style scoped>
+svg{
+    cursor: pointer;
+}
+
+</style>
