e5734031b306bb9a906f74a07c9d38f24fe697c3
drsoft28 <dif.rabie@gmail.com>
2023-01-10 21:47:46 +0100

    fix issue mobile

--- src/layouts/common/section-subscribe/sect-subscribe.vue
+++ src/layouts/common/section-subscribe/sect-subscribe.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sec-subscripe mt-5">
         <div class="container">
-          <div class="row align-items-center justify-content-center p-4">
+          <div class="row align-items-center justify-content-center " :class="{'p-4':!isMobile,'py-4':isMobile}">
             <div class="col-md-6">
               <h1 class="text-white">
               {{ $t('subscribe-newsletter') }}
@@ -11,9 +11,9 @@
               </p>
             </div>
             <div class="col-md-6">
-              <div class="input-group input-group-lg w-75 m-auto"  >
-                <input type="text" class="form-control p-3" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" :placeholder="$t('write-your-email')">
-                <span class="input-group-text bg-yal text-white" id="inputGroup-sizing-lg">{{ $t('Subscribe') }}</span>
+              <div class="input-group input-group-lg  m-auto"  :class="{'w-75':!isMobile,'w-100':isMobile}">
+                <input type="text" class="form-control p-3" :class="{'fs-6':isMobile}" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" :placeholder="$t('write-your-email')">
+                <span class="input-group-text bg-yal text-white" :class="{'fs-6':isMobile}" id="inputGroup-sizing-lg">{{ $t('Subscribe') }}</span>
               </div>
             </div>
           </div>
