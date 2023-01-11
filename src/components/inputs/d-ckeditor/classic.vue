22025c2486a66927b06a0a9584443cec9de331a9
drsoft28 <dif.rabie@gmail.com>
2023-01-11 09:06:32 +0100

    Prettier format

--- src/components/inputs/d-ckeditor/classic.vue
+++ src/components/inputs/d-ckeditor/classic.vue
@@ -1,51 +1,51 @@
 <template>
-        <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"
-        @ready="ckEditorReady"
-        ></ckeditor>
+    <ckeditor :editor="editor" v-model="editorData" :config="editorConfig" @ready="ckEditorReady"></ckeditor>
 </template>
 
 <script>
-    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-    export default {
-        name: 'd-ckeditor-classic',
-        props:{
-            value:{
-                type:String
-            },
-            editorConfig:{
-                type:[Object,Array],
+export default {
+    name: 'd-ckeditor-classic',
+    props: {
+        value: {
+            type: String
+        },
+        editorConfig: {
+            type: [Object, Array],
 
-                default:()=>{return {
+            default: () => {
+                return {
                     // The configuration of the editor.
-                }}
-            }
-        },
-        data() {
-            return {
-                editor: ClassicEditor,
-                editorData: '<p></p>',
-            };
-        },
-        watch:{
-            value:{
-                immediate:true,
-                handler(val){
-                    this.editorData=val
                 }
-            },
-            editorData(){
-                this.$emit('input',this.editorData)
+            }
+        }
+    },
+    data() {
+        return {
+            editor: ClassicEditor,
+            editorData: '<p></p>',
+        };
+    },
+    watch: {
+        value: {
+            immediate: true,
+            handler(val) {
+                this.editorData = val
             }
         },
-        methods:{
-            
-ckEditorReady(editor) {
-    if(this.editorConfig.minHeight)
-      editor.ui.view.editable.element.style.minHeight = this.editorConfig.minHeight
-}
-
-
+        editorData() {
+            this.$emit('input', this.editorData)
         }
+    },
+    methods: {
+
+        ckEditorReady(editor) {
+            if (this.editorConfig.minHeight)
+                editor.ui.view.editable.element.style.minHeight = this.editorConfig.minHeight
+        }
+
+
     }
+}
 </script>
