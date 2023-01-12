30875ff3495bd62a378f88730a5375bdfaccd121
drsoft28 <dif.rabie@gmail.com>
2023-01-12 21:36:21 +0100

    load event in calendar

--- src/views/network/home/auth/parts/section-schedule-network/index.vue
+++ src/views/network/home/auth/parts/section-schedule-network/index.vue
@@ -27,7 +27,23 @@
                         <div class="col-md-4">
                             <div class="position-relative">
                                 <!--calendar-->
-                                <d-calendar class="calendar-schedule" :class="{'mobile':isMobile}"/>
+                                <d-calendar v-if="!loading" class="calendar-schedule" 
+                                :class="{'mobile':isMobile}"
+                                :attributes="attributes"
+                                @dayclick="dayclick"
+                                >
+                                <template #day-popover="{ day, format, masks }">
+                                    <div class="text-xs text-gray-300 font-semibold text-center">
+                                    {{ format(day.date, masks.dayPopover) }}
+                                    <hr class="mt-1"/>
+                                    <div :class="{'text-right':$i18n.locale=='ar','text-left':$i18n.locale!=='ar'}">
+                                    <p   class=" pb-1 mb-1">{{ day.attributes[0].customData.categoryName }}</p>
+                                    <p  class=" pb-1 mb-1">{{ $t('titled') }} :  {{ day.attributes[0].customData.title }}</p>
+                                    <p class=" pb-1 mb-1">{{ $t('at-time') }} :  {{ timeFormatAMPM(day.attributes[0].customData.time) }}</p>
+                                    </div>    
+                                </div>
+                                </template>
+                                </d-calendar>
                             </div>
                         </div>
     
@@ -48,19 +64,44 @@ export default {
  },
  data:()=>{
     return {
+        loading:false,
         items:[],
     }
  },
+ computed:{
+ attributes() {
+      return [
+        // Attributes for todos
+        ...this.items.map(event => ({
+          dates: event.date,
+          dot: {
+            color: 'red',
+            //class: todo.isComplete ? 'opacity-75' : '',
+          },
+          popover: true,
+          customData: event,
+        })),
+      ];
+    },
+},
  methods:{
+    dayclick(day){
+        console.log('day',day)
+    },
     async loadList(){
+        this.loading = true;
         try {
          let { data } = await schedulesAPI.getHomeNetwork() 
          if(data.success){
             this.items = data.data
+            if(process.env.NODE_ENV !== 'production')
+            this.items.push({id: -1, title: "تجريب", categoryName: "ويبنار تججريب",
+date:new Date(),image:null,time:"02:37"})
          }
         } catch (error) {
             console.log('error',error)
         }
+        this.loading = false;
     }
  },
  mounted(){
