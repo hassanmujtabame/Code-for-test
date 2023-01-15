759735924358d5d7fb6d7541e5ff37ab4df55c39
drsoft28 <dif.rabie@gmail.com>
2023-01-15 19:03:34 +0100

    add props for color icon

--- src/components/icon-svg/play.vue
+++ src/components/icon-svg/play.vue
@@ -1,12 +1,12 @@
 <template>
   <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_1129_64537)">
-<circle cx="17" cy="17" r="17" fill="white"/>
-<path d="M25 17L13 24V10L25 17Z" fill="#1FB9B3"/>
+<circle cx="17" cy="17" r="17" :fill="bgColor"/>
+<path d="M25 17L13 24V10L25 17Z" :fill="playColor"/>
 </g>
 <defs>
 <clipPath id="clip0_1129_64537">
-<rect width="34" height="34" fill="white"/>
+<rect width="34" height="34" :fill="playColor"/>
 </clipPath>
 </defs>
 </svg>
@@ -14,7 +14,21 @@
 
 <script>
 export default {
- name:'play-icon'
+ name:'play-icon',
+ props:{
+  size:{
+    type:Number,
+    default:34
+  },  
+  bgColor:{
+    type:String,
+    default:'#FFFFFF'
+  },
+  playColor:{
+    type:String,
+    default:'var(--m-color)'
+  }
+ }
 }
 </script>
 
