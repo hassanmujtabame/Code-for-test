d24d50217f9700a9c497b589e4a4f94f3e2a8018
drsoft28 <dif.rabie@gmail.com>
2023-01-23 09:53:45 +0100

    fix issue user card

--- src/views/common/preview-profile/index.vue
+++ src/views/common/preview-profile/index.vue
@@ -4,7 +4,7 @@
 
            <div class="row">
                <div class="col-md-4  mt-3">
-                   <userInfoCard />
+                   <userInfoCard :member="user" />
                </div>
                <div class="col-md-8 mt-3 ">
                    <div v-if="false" class="row border px-2">
