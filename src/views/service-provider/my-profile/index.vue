7d50d9140f2f284b86bea8117981b552478f1d44
drsoft28 <dif.rabie@gmail.com>
2023-01-25 23:38:50 +0100

    improve my profile in service provider

--- src/views/service-provider/my-profile/index.vue
+++ src/views/service-provider/my-profile/index.vue
@@ -1,109 +1,35 @@
 <template>
   <div style="margin-top: 96px;">
-    <d-overlays-simple v-if="loading" />
-    <div v-else-if="hasError">
-     {{ hasError }}
+        <div class="container my-profile">
+            <div class="row">
+                <div class="col-md-3 mt-3">
+                    
+                    <SectionUser />
+                </div>
+                <div class="col-md-9 mt-3">
+                    <!--my service-->
+                    <SectionMyService />
+                    <!--section gallery-->
+                    <SectionGallery />
+    
+                </div>
+            </div>
+        </div>
+
     </div>
-    <div v-else class="container">
-
-    <div class="row  profile-services ">
-        <div class="col-12 col-md-4 mt-3">
-          <!--sidebar-->
-          <Sidebar />
-        </div>
-        <div class="col-12 col-md-8 mt-3">
-          <div data-bs-spy="scroll" data-bs-target="#list-example" data-bs-smooth-scroll="true" class="scrollspy-example" tabindex="0">
-                    <!-- معومات العرض-->
-                    <SectionDisplay  :currentUser="currentUser"/>
-                    <!--معومات شخصية  -->
-                    <SectionPersonal  :currentUser="currentUser"/>
-                    <!-- بطاقات الدفع -->
-                    <SectionPaymentCards />
-                    <!--معلومات الدراسة -->
-                    <SectionEducation  :currentUser="currentUser" />
-                    <!--معلومات مهنية -->
-                    <SectionProfessional   :currentUser="currentUser"/>
-                    <!--معلومات الاقامة -->
-                    <SectionResidence   :currentUser="currentUser"/>
-                    <!-- معلومات التواصل -->
-                    <SectionCommunication   :currentUser="currentUser"/>
-                    <!-- اعدادات الاشعارات -->
-                    <SectionSettingsNotif   :currentUser="currentUser"/>
-                    <!--قائمة الحظر -->
-                    <SectionListBlocks   :currentUser="currentUser"/>
-                    <!--معرض الاعمال -->
-                    <SectionShowerProject   :currentUser="currentUser"/>
-                 
-          </div>
-        </div>
-      </div>
-
-</div>
-</div>
 </template>
 
 <script>
-import userAPI from '@/services/api/user.js'
-import Sidebar from './parts/sidebar/index.vue'
-import SectionPaymentCards from './parts/sections/payment-cards/index.vue'
-import SectionDisplay from './parts/sections/information-display/index.vue'
-import SectionPersonal from './parts/sections/information-personal/index.vue'
-import SectionEducation from './parts/sections/information-education/index.vue'
-import SectionProfessional from './parts/sections/information-professional/index.vue'
-import SectionCommunication from './parts/sections/information-communication/index.vue'
-import SectionResidence from './parts/sections/information-residence/index.vue'
-import SectionSettingsNotif from './parts/sections/settings-notif/index.vue'
-import SectionListBlocks from './parts/sections/list-blocks/index.vue'
-import SectionShowerProject from './parts/sections/shower-project/index.vue'
+//import readyServiceAPI from '@/services/api/service-provider/provider/ready-service.js'
+import SectionMyService from './parts/section-my-services/index.vue';
+import SectionGallery from './parts/section-gallery/index.vue';
+import SectionUser from './parts/section-user/index.vue';
 export default {
- name:'show-profile',
+ name:'show-profile-service-provider',
  components:{
-    Sidebar,
-    SectionDisplay,
-    SectionPersonal,
-    SectionEducation,
-    SectionCommunication,
-    SectionProfessional,
-    SectionResidence,
-    SectionSettingsNotif,
-    SectionListBlocks,
-    SectionShowerProject,
-    SectionPaymentCards
- },
- data:()=>{
-  return {
-    currentUser:{},
-    loading:true,
-    hasError:false,
-  }
- },
- methods:{
-  async loadCurrentUser(){
-    this.loading  = true;
-    this.hasError  = false;
-      try {
-         let { data } = await  userAPI.me()
-         if(data.success){
-           this.currentUser = data.data
-         }else{
-          this.hasError = data.message
-         }
-      } catch (error) {
-         if(error.response){
-            let response =error.response
-            if(response.status==401){
-               this.logout()
-           }else{
-            this.hasError = ' هناك خطأ غير معروف يرجي تحديث الصفحة'
-           }
-         }
-         
-      }
-      this.loading  = false;
-   }
- },
- mounted(){
-  this.loadCurrentUser()
+    SectionMyService,
+    SectionGallery,
+    SectionUser
  }
 }
 </script>
