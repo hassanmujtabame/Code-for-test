387deb739f74367d593f9cd01524e75c702a3c1f
drsoft28 <dif.rabie@gmail.com>
2023-01-28 21:42:58 +0100

    id to rate_id

--- src/views/service-provider/page-rate-my-services/abuse-comment.vue
+++ src/views/service-provider/page-rate-my-services/abuse-comment.vue
@@ -61,7 +61,7 @@ export default {
                 return;
             }
         try {
-            let {data} = await userAPI.sendAbuseCommentOnRate({comment:this.comment,id:this.itemDialog.id})
+            let {data} = await userAPI.sendAbuseCommentOnRate({comment:this.comment,rate_id:this.itemDialog.id})
             if(data.success){
               this.$emit('success',this.itemDialog)
                 this.closeEvent()
