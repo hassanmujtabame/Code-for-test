4b1078e914eb994ac5d39c4ce296997b45d6d65f
drsoft28 <dif.rabie@gmail.com>
2023-01-30 20:10:30 +0100

    chechoutPackage

--- src/services/api/network.js
+++ src/services/api/network.js
@@ -10,6 +10,25 @@ class NetworkApi extends BaseApi {
         let s=this.generateQueryUrl(params)
         return window.axios.get(`network/packages?${s}`);
     }
+    chechoutPackage(data){
+        /*
+          type if 0
+            paymentBrand
+            card_holder
+            expiryMonth
+            expiryYear
+            cvv
+            card_number
+            package_id
+
+            type if 1
+            payment_id
+            package_id
+        */
+        return window.axios.get(`network/user-checkout`,data);
+    }
+   
+
 }
 
 export default new NetworkApi();
