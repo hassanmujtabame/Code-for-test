2b39ea351f6e44ab3dd87301080800d7d30fc055
drsoft28 <dif.rabie@gmail.com>
2023-02-04 09:25:28 +0100

    add sections

--- src/views/service-provider/graduate-riadiat/index.vue
+++ src/views/service-provider/graduate-riadiat/index.vue
@@ -2,17 +2,24 @@
   <div style="margin-top: 85px" class="consult">
     <SectionHeader />
     <SectionFilterList />
+    <SectionContinueLearning />
+    <SectionHear />
   </div>
 </template>
 
 <script>
 import SectionHeader from './sections/section-header/index'
 import SectionFilterList from './sections/section-filter-list/index'
+import SectionContinueLearning from '@/views/service-provider/common-components/section-continue-learning/index'
+import SectionHear from '@/views/service-provider/common-components/section-hear/index'
+
 export default {
  name:'graduate-riadiate',
  components:{
   SectionHeader,
-  SectionFilterList
+  SectionFilterList,
+  SectionContinueLearning,
+  SectionHear
  }
 }
 </script>
