7c922a2702cc4fddbd61a690c263a1460a79bf34
drsoft28 <dif.rabie@gmail.com>
2023-02-04 13:43:18 +0100

    fix iusse draw arrow

--- src/views/network/affiliate-marketing/index.vue
+++ src/views/network/affiliate-marketing/index.vue
@@ -4,18 +4,21 @@
          <SectionHeader />
          <SectionIntro />
          <SectionFeatures />
+         <SectionHowWorkProgram />
     </div>
 </template>
 
 <script>
 import SectionHeader from './parts/section-header/index.vue'
 import SectionIntro from './parts/section-intro/index.vue'
+import SectionHowWorkProgram from './parts/section-how-work-program/index.vue'
 import SectionFeatures from './parts/section-features/index.vue'
 export default {
     name:'offers-page',
  components:{
     SectionHeader,
     SectionIntro,
+    SectionHowWorkProgram,
     SectionFeatures,
  }
 }
---
+++ src/views/network/affiliate-marketing/parts/section-how-work-program/index.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="py-4">
+        <h2 class="text-center">كيفية عمل البرنامج</h2>
+
+        <div class="container mt-4 ">
+           
+            <div class="row">
+                <div v-for="(item, i) in items" :key="i" class="col-4 col-md-4 position-relative">
+                    <hr class="line-dd" v-if="i!==(items.length-1)" />
+                    <cardShow :position="(i % 2) == 0 ? 'top' : 'bottom'" 
+                    :isFirst="i==0"
+                    :image="item.image" :title="item.title"
+                        :description="item.description" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import cardShow from './card'
+export default {
+    name: 'how-work',
+    components: {
+        cardShow
+    },
+    data: () => {
+        return {
+            items: [
+                { title: 'إنضمي الينا', image: '/assets/svg/hand-click.svg', description: 'سجلى كمسوقة بالعمولة و ابداى رحلتك معانا' },
+                { title: 'تواصلى مع الاخرين', image: '/assets/svg/contact-networking.svg', description: 'تواصلى مع الاخرين من خلال  لوحة تحكم خاصة بكى تسهل الترويج لمنتجاتك' },
+                { title: 'أربحى المال', image: '/assets/svg/money-bills-2.svg', description: 'تلقى عمولتك للتسويق لمنتجاتك من خلال منصة رياديات' },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+.line-dd{
+    position: absolute;
+    width: 100%;
+    height: 0px;
+    background: var(--m-color);
+    border: 2px solid var(--m-color);
+    top: calc(50% + 2px);
+    z-index: -2;
+    padding:0;
+    margin:0;
+    opacity: 1;
+}
+</style>
