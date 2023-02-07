320213218eb4828ac9d11d4e7f2f408823850221
drsoft28 <dif.rabie@gmail.com>
2023-02-07 12:08:38 +0100

    update dashboard academy

--- src/views/academy/dashboard/index.vue
+++ src/views/academy/dashboard/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <intructorsDash />
+  <div style="margin-top:85px">
+    <div class="container dashbord">
+  <SectionHeader />
+  <SectionBalance />
+</div>
+
+</div>
 </template>
 
 <script>
-import intructorsDash from './intructor-dash.vue'
+import SectionHeader from './parts/section-header/index.vue'
+import SectionBalance from './parts/section-balance/index.vue'
 export default {
  name:'your-courses-page',
  components:{
-  intructorsDash
+  SectionHeader,
+  SectionBalance
  }
 }
 </script>
