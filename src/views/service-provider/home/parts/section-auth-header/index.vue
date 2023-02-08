b98d59e489ae41b705b18dd3281f6501e762e32e
drsoft28 <dif.rabie@gmail.com>
2023-02-08 12:34:15 +0100

    improve for mobile

--- src/views/service-provider/home/parts/section-auth-header/index.vue
+++ src/views/service-provider/home/parts/section-auth-header/index.vue
@@ -1,21 +1,24 @@
 <template>
     <div class="">
         <div class="over-lay">
-            <img class="img-fluid" src="" alt="">
-            <div style="padding: 184px 0;" class="text-center ">
+            <div :style="{height:isMobile?'400px':'600px'}" class="d-flex flex-column justify-content-center align-items-center py-5">
 
-                <h1 style="font-size: 50px;" class="text-white">
+                <h1  class="service-provider-header_title" :class="{'mobile':isMobile}">
                     أهلاً و مرحباً بك في مقدمي الخدمات
                 </h1>
-                <h6 class="text-white">
+                <h6 class="service-provider-header_subtitle" :class="{'mobile':isMobile}">
                     منصة تجمع أهم وألمع رائدين و رائدات الأعمال في السعودية والخليج في مكان واحد
                 </h6>
-                <div>
-                    <input class="p-3 rounded-2 border-0 bg-white w-50" type="text"
+                <div class="row" :class="{'mx-5':!isMobile,'mx-1':isMobile}">
+                    <div class="col-12 col-md-9">
+                    <input class=" p-3 w-100 rounded-2 border-0 bg-white" type="text"
                         placeholder="ابحث عن خدمات او طلبات من هنا">
-                    <button style="height: 51px;" class="btn-main">
+                    </div>
+                    <div class="col-12 col-md-3" :class="{'mt-2 mb-3':isMobile}">
+                    <button style="height: 51px;" class="btn-main" :class="{'w-100':isMobile}">
                         ابحث
                     </button>
+                </div>
                 </div>
             </div>
         </div>
@@ -28,6 +31,45 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.service-provider-header_subtitle{
+    font-style: normal;
+font-weight: 400;
+font-size: 24px;
+line-height: 32px;
+/* identical to box height, or 133% */
 
+text-align: center;
+text-transform: capitalize;
+
+color: #FFFFFF;
+
+/* T */
+
+text-shadow: 0px 4px 16px rgba(0, 0, 0, 0.27);
+}
+.service-provider-header_subtitle.mobile{
+    font-size: 12px;
+    line-height: 16px;
+}
+.service-provider-header_title{
+    font-style: normal;
+font-weight: 600;
+font-size: 56px;
+line-height: 72px;
+/* identical to box height, or 129% */
+
+text-align: center;
+text-transform: capitalize;
+
+color: #FFFFFF;
+
+/* T */
+
+text-shadow: 0px 4px 16px rgba(0, 0, 0, 0.27);
+}
+.service-provider-header_title.mobile{
+    font-size: 30px;
+    line-height: 40px;
+}
 </style>
