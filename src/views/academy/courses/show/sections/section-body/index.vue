69e0645f6f3025a5958d6e85d06a7c4070155da4
drsoft28 <dif.rabie@gmail.com>
2023-02-08 10:24:58 +0100

    improve course page

--- src/views/academy/courses/show/sections/section-body/index.vue
+++ src/views/academy/courses/show/sections/section-body/index.vue
@@ -11,21 +11,60 @@
         <LecturesList />
        </div>
     </div>
+    <div class="row">
+      <div class="col-12 col-md-8">
+          <div class="course-show-page__details">
+          <h1 class="course-show-page__details-title">تفاصيل الدورة  :</h1>
+          <div class="course-show-page__details-content mt-2"> 
+            <p>أكثر من 140,000 طالب مسجلين حتى الآن في النسخة الإنجليزية من هذه الدورة في 196 دولة</p>
+<p>يرجى العلم بأن هذه الدورة تحتوي على ترجمة باللغة العربية</p>
+<p>هل أنت جاهز للارتقاء إلى درجة أعلى في حياتك المهنية؟ في هذه الدورة، ستتعلم كل ما تحتاج لمعرفته حول الأعمال والبيزنس ... بداية من إنشاء شركة وحتى طرحها للاكتتاب العام. وتغطي هذه الدورة كافة الموضوعات المهمة التي ستتعلمها عند الحصول على ماجستير إدارة الأعمال من أي كلية من الكليات المرموقة + مفاهيم الأعمال المستمدة من الخبرة العلمية والتي ستجعلك أكثر نجاحًا</p>
+</div>
+</div>
+          </div>
+          <div class="col-12 col-md-4"></div>
+    </div>
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <RateCourse :item="{}" />
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
 import LecturesList from './sidebar-lectures.vue'
 import previewLecture from './display-lecture.vue'
+import RateCourse from './rate-course/index'
 export default {
  name:'section-header',
  components:{
   LecturesList,
-  previewLecture
+  previewLecture,
+  RateCourse
  }
 }
 </script>
 
-<style>
+<style scoped>
+.course-show-page__details-title{
+  font-style: normal;
+font-weight: 400;
+font-size: 24px;
+line-height: 32px;
+/* identical to box height, or 133% */
+text-transform: capitalize;
+
+color: var(--m-color);
+}
+.course-show-page__details-text{
+  font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 17px;
+/* or 106% */
+
+color: #415C5E;
+}
 
 </style>
