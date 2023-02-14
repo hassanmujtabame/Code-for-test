2e01fd595510bd9c81f246eac357cd66ed1cacd0
drsoft28 <dif.rabie@gmail.com>
2023-02-14 19:20:06 +0100

    add tooltip

--- src/components/hover/index.vue
+++ src/components/hover/index.vue
@@ -1,16 +1,28 @@
 <template>
   <div @mouseover="hover = true"
+  
     @mouseleave="hover = false">
-    <slot :hover="hover"></slot>
+    <slot :hover="hover"  v-bind="$attrs"></slot>
     </div>
 </template>
 
 <script>
 export default {
  name:'d-hover',
+ props:{
+  tooltip:{
+    type:Boolean,
+    default:false,
+  }
+ },
  data:()=>({
     hover:false,
- })
+ }),
+ mounted(){
+  if(this.tooltip)
+  window.$('[data-toggle="tooltip"]').tooltip()
+ }
+
 }
 </script>
 
