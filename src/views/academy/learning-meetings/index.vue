d3836c75a36d3b4b1f59881a50c11991ffbae440
drsoft28 <dif.rabie@gmail.com>
2023-02-21 15:06:24 +0100

    add learning meeting btn

--- src/views/academy/learning-meetings/index.vue
+++ src/views/academy/learning-meetings/index.vue
@@ -1,21 +1,32 @@
 <template>
   <div style="margin-top: 85px " class="consult">
     <SectionHeader />
+  <div class="container mt-5 text-end">
+    <button @click="addItem" class="btn btn-custmer"><i class="fa fa-circle-plus"></i> إضافة لقاء تعليمي</button>
+    </div>
   <div class="container mt-5">
     <SectionFilterList />
   </div>
+  <AddMeetingDialog />
   </div>
 </template>
 
 <script>
 import SectionHeader from './parts/section-header/index';
 import SectionFilterList from './parts/section-filter-list/index';
+import AddMeetingDialog from './dialogs/add-meeting/index.vue'
 
 export default {
-  name: 'your-courses-page',
+  name: 'learing-meetings-list',
   components:{
     SectionHeader,
-    SectionFilterList
+    SectionFilterList,
+    AddMeetingDialog
+  },
+  methods:{
+    addItem(){
+            this.fireOpenDialog('add-meeting',{id:null,title:null,video:null})
+        },
   }
 }
 </script>
