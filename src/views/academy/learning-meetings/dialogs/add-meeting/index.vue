ebc1c92050a7c83af7d8fa7276331807fe5d5a34
drsoft28 <dif.rabie@gmail.com>
2023-02-22 09:29:45 +0100

    add title and sub title

--- src/views/academy/learning-meetings/dialogs/add-meeting/index.vue
+++ src/views/academy/learning-meetings/dialogs/add-meeting/index.vue
@@ -4,7 +4,11 @@
     :open-dialog="openDialog"
     :close-dialog="closeDialog"
     >
+    <template v-slot:header>
+        {{ itemDialog.id?$t('meeting-modification'):$t('add-meeting') }}
+        </template>
     <template v-slot:default>
+        <p class="t-c fs-r-16-24">يمكنك رفع فيديو تعليمي لمدة ساعة</p>
         <div v-if="showDialog" ref="form" tag="div">
         <ValidationObserver class="form-step" ref="form1" id="form-step-1" v-show="step==1">
                 <!--title-->
