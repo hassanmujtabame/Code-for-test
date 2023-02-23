9e1f8274f370161df75990a2ae9f7f76e6a923c6
drsoft28 <dif.rabie@gmail.com>
2023-02-23 09:39:30 +0100

    change font size search input

--- src/components/filter-list/index.vue
+++ src/components/filter-list/index.vue
@@ -21,7 +21,7 @@
             <div v-if="!hideSearch" :class="classColSearch">
             <slot name="search">
             <label for="" class="position-relative w-100">
-                <input class="form-control py-3 px-5" type="text" v-model="filter.search" @change="updateFilter" :placeholder="searchPlaceholder" />
+                <input class="form-control py-3 px-5 fs-r-12" type="text" v-model="filter.search" @change="updateFilter" :placeholder="searchPlaceholder" />
                 <p style="top: 25%; right: 7px" class="position-absolute">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
