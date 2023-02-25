ff0de5913037f0aa6efc3544946ab8d87bba2812
drsoft28 <dif.rabie@gmail.com>
2023-02-25 09:52:40 +0100

    update

--- src/views/academy/courses/show/recorded/sections/section-body/lecture-display/index.vue
+++ src/views/academy/courses/show/recorded/sections/section-body/lecture-display/index.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="academy-player-box">
+    <d-overlays-simple v-if="progressing" />
     <video ref="myVideoPreview" id="course-video-preview" class="rounded-3 w-100 h-100"
                     :src="lectureSelected.video"
                     controls 
@@ -15,6 +16,8 @@ export default {
     data:()=>{
         return {
             videoTag:null,
+            progressing:false,
+            loadedVideo:false,
             currentTime:'00:00',
             play:false,
             playing:false,
@@ -68,13 +71,17 @@ export default {
             let { h, m, s} = this.timeToParts(evt.target.currentTime)
             this.currentTime =  this.timeFormat(h,m,s);
         },
+        onWaiting(){
+            /*Fires when the video stops because it needs to buffer the next frame */
+        },
       async  onEnded(){
             this.playing = false;
             this.pause = false;
             this.play = false;
-            this.fireEvent('update-lectures',{item:{...this.lectureSelected}
-        ,type:'completed'
+            if(this.userAcademyRole !== 'student' || ! this.this.loadedVideo) return;
+            this.fireEvent('update-lectures',{item:{...this.lectureSelected},type:'completed'
         })
+
             try {
                 await academyAPI.lecturesAPI.postAsEnded(this.itemPage.id,this.lectureSelected.id)
 
@@ -82,6 +89,10 @@ export default {
                 window.DHelper.catchException.call(this,error)
             }
         },
+        onLoadstart(){
+            this.progressing = true;
+        },
+        onProgress(){},
         onPause(){
             this.playing = false;
             this.pause = true;
@@ -90,19 +101,34 @@ export default {
             this.playing = true;
             this.pause = false;
         },
+        onError(evt){
+            console.mylog('error video',evt)
+            this.progressing = false;
+        },
+        onLoadeddata(evt){
+            if(evt){
+                this.loadedVideo = true;
+            }
+        },
         onLoadedmetadata(evt){
+            
+            this.progressing = false;
+            if(!evt) return;
             this.videoTag = evt.target;
-        let { h, m, s} = this.timeToParts(evt.target.duration)
+            let { h, m, s} = this.timeToParts(evt.target.duration)
             var formattedTime =  this.timeFormat(h,m,s);
             this.time_duration_str = this.timeHuman(h,m,s)
             this.time_duration = formattedTime
+            
         },
         videoListener(evt){
             /** call function */
            let type = evt.type.charAt(0).toUpperCase() + evt.type.slice(1);
            let funcName = 'on'+type; 
            if(this[funcName] && typeof this[funcName] == "function"){
-            this[funcName]()
+            this[funcName](evt)
+           }else{
+            console.mylog('not found function',funcName)
            }
            console.mylog('video listener',type,evt,evt.type,this)
         }
@@ -139,3 +165,8 @@ export default {
  }
 }
 </script>
+<style scoped>
+.academy-player-box{
+    position: relative;
+}
+</style>
