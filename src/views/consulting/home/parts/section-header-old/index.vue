6e4122b7341153ba7ca4cd36b6cee9e4c4fe1f78
drsoft28 <dif.rabie@gmail.com>
2023-03-01 14:29:59 +0100

    add old header

---
+++ src/views/consulting/home/parts/section-header-old/index.vue
@@ -0,0 +1,43 @@
+<template>
+ <div class="consulting p-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1 class="fs-1 text-white">
+                            أستشارات رياديات
+                        </h1>
+                        <p class="text-white">
+                            تقدمي إلى الأمام وابدئي مشروعك باستشارة مهنية في أغلب المجالات 
+                        </p>
+                        <div class="btn-main-style">
+                            <a href="" class="btn-custmer shadow mt-2"> {{ $t('join-us') }} </a>
+                            <a class="btn-custmer-w bg-transparent text-white mt-2"> {{ $t('login-female') }} </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 m-auto">
+                        <div class="img-cons m-auto ">
+                            <img style="border-radius: 50%;position: absolute;width: 300px;height: 300px;" src="/assets/img/Group 289326.png" alt="" >
+                        </div>                        </div>
+
+                    </div>
+
+                </div>
+                <div class="star-cons">
+                    <img src="/assets/img/starrrr.png" alt="">
+                </div>
+                <div class="star-cons stars">
+                    <img src="/assets/img/starsss.png" alt="">
+                </div>
+
+            </div>
+</template>
+
+<script>
+export default {
+ name:'section-1'
+}
+</script>
+
+<style>
+
+</style>
