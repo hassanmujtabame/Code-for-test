e0389d6293d31cef912f09c3491a9f60f0446d1d
drsoft28 <dif.rabie@gmail.com>
2023-03-01 14:50:38 +0100

    update

---
+++ src/views/consulting/home/parts/section-goals/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+                <h2 class="text-center">
+                    أهداف الاستشارة من رياديات 
+                </h2>
+                <div class="row min-riadiat">
+                    <div class="col-md-4 ">
+                        <div class="box border text-center p-3 rounded-2">
+
+                            <div>
+                                <img src="/assets/img/es 1.png" alt="">
+                            </div>
+                            <p>
+                                إن كنت تنشئين مشروعـا أو تقومين بتأسيس أو تطويــر شركــــة ناشئة، فيمكننا أن نساعدك في كل مرحلة من مراحل العمل. 
+                            </p>
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-4 ">
+                        <div class="box border text-center p-3 rounded-2">
+
+                            <div>
+                                <img src="/assets/img/es 2.png" alt="">
+                            </div>
+                            <p>
+                                تقديم استشارات إلكترونية، وعقد جلسات استشاريـــــة متخصصة فـي مجال معين.
+                            </p>
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-4 ">
+                        <div class="box border text-center p-3 rounded-2">
+
+                            <div>
+                                <img src="/assets/img/es 3.png" alt="">
+                            </div>
+                            <p>
+                                تنفيذ الخدمات الاستشاريــة و تطويــــر الأعمـال  فــــي مـجــالات عـــمـــل رياديات.
+                            </p>
+                            
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+</template>
+
+<script>
+export default {
+  name:'section-goals'
+}
+</script>
+
+<style>
+
+</style>
