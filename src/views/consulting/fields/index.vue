87c96b5be101642e36a90988a8a1d81f4694a07f
drsoft28 <dif.rabie@gmail.com>
2023-03-01 15:45:42 +0100

    update

--- src/views/consulting/fields/index.vue
+++ src/views/consulting/fields/index.vue
@@ -1,15 +1,20 @@
 <template>
     <div style="margin-top: 85px " class="consult">
   <SectionHeader />
+  <div class="container mt-3">
+   <SectionFilterList />
+  </div>
 </div>
 </template>
 
 <script>
 import SectionHeader from './parts/section-top/index'
+import SectionFilterList from './parts/section-filter-list/index'
 export default {
  name:'page-consulting-categories',
  components:{
-    SectionHeader
+    SectionHeader,
+    SectionFilterList
  }
 }
 </script>
---
+++ src/views/consulting/fields/parts/section-filter-list/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <d-filter-list 
+  hideSide
+  :call-list="loadList"
+  :pluralName="$t('consulting-fields')"
+  :singleName="$t('field')"
+  searchPlaceholder="أبحث عن مجالات الاستشارة"
+  >
+  <template v-slot="{item}">
+    <consultingFieldCard :item="item" />
+    </template>
+  </d-filter-list>
+</template>
+
+<script>
+import consultingFieldCard from '@/components/cards/consulting-field.vue'
+import consultingAPI from '@/services/api/consulting'
+export default {
+    name:'section-filter-list',
+    components:{
+        consultingFieldCard
+    },
+ data:()=>({
+    items:[]
+ }),
+ methods:{
+    async loadList(){
+        try {
+          return await consultingAPI.fields.getAll()
+        } catch (error) {
+           // 
+        }
+    }
+ }
+
+}
+</script>
+
+<style>
+
+</style>
