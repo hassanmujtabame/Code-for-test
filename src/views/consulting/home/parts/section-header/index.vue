f4a9e149b270ea862c821215cf703860dfb3cf28
drsoft28 <dif.rabie@gmail.com>
2023-03-01 14:38:52 +0100

    updae header

--- src/views/consulting/home/parts/section-header/index.vue
+++ src/views/consulting/home/parts/section-header/index.vue
@@ -3,20 +3,17 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
-                        <h1 class="fs-1 text-white">
-                            أستشارات رياديات
+                        <h1 class="header-title-page">
+                            تحتاج الى المساعدة ؟
                         </h1>
-                        <p class="text-white">
-                            تقدمي إلى الأمام وابدئي مشروعك باستشارة مهنية في أغلب المجالات 
+                        <p class="header-desc-page">
+                            أحجز جلستك الان مع افضل المستشارين و ِأحجز جلستك الان مع افضل المستشارين و ِأحجز جلستك الان مع افضل المستشارين
                         </p>
-                        <div class="btn-main-style">
-                            <a href="" class="btn-custmer shadow mt-2"> {{ $t('join-us') }} </a>
-                            <a class="btn-custmer-w bg-transparent text-white mt-2"> {{ $t('login-female') }} </a>
-                        </div>
+                        
                     </div>
                     <div class="col-md-6 m-auto">
-                        <div class="img-cons m-auto ">
-                            <img style="border-radius: 50%;position: absolute;width: 300px;height: 300px;" src="/assets/img/Group 289326.png" alt="" >
+                        <div class="">
+                            <img  src="/assets/img/v781.png" alt="" >
                         </div>                        </div>
 
                     </div>
