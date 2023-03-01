b75958304e99ee12322d2a644e067b539c1e6cfe
drsoft28 <dif.rabie@gmail.com>
2023-03-01 14:42:29 +0100

    rename section 4 to consulting-categories

--- src/views/consulting/home/index.vue
+++ src/views/consulting/home/index.vue
@@ -8,7 +8,7 @@
           <!--#section 2-->
 
          <!-- section 4-->
-         <SectionFour />
+         <SectionConsultingCategories />
           <!--#section 4-->
           <!-- section 5-->
           <SectionFive/>
@@ -21,23 +21,17 @@
   <script>
   import SectionHeader from './parts/section-header/index.vue'
   import SectionHelpToStart from './parts/section-help-tostart/index.vue'
-  import SectionThree from './parts/section-3/index.vue'
-  import SectionFour from './parts/section-4/index.vue'
+  import SectionConsultingCategories from './parts/section-consulting-categories/index.vue'
   import SectionFive from './parts/section-5/index.vue'
-  import SectionSix from './parts/section-6/index.vue'
-  import SectionSeven from './parts/section-7/index.vue'
-  import SectionEight from './parts/section-8/index.vue'
+
   export default {
   name:"consulting-home",
   components:{
     SectionHeader,
     SectionHelpToStart,
-    SectionThree,
-    SectionFour,
+    SectionConsultingCategories,
     SectionFive,
-    SectionSix,
-    SectionSeven,
-    SectionEight
+
   }
   }
   </script>
