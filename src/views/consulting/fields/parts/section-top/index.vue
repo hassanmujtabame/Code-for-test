907057d0b0f9503c416ad315c9fd092776fd71d4
drsoft28 <dif.rabie@gmail.com>
2023-03-01 15:43:23 +0100

    update

--- src/views/consulting/fields/parts/section-top/index.vue
+++ src/views/consulting/fields/parts/section-top/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main-top py-5 position-relative">
-        <h1 class="text-white px-5 fs-1 position-relative z-index-1"> {{ $t('consulting-categories') }} </h1>
+        <h1 class="text-white px-5 fs-1 position-relative z-index-1"> {{ $t('consulting-fields') }} </h1>
        
         <div class="position-absolute top-0 h-100" :class="{'rotateY-180':$i18n.locale!='ar','right-0':$i18n.locale!='ar','left-0':$i18n.locale=='ar'}">
           <img src="/assets/img/anim-hwo-ar-you.png" class="h-100" alt="" />
