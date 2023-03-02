b1457e3d9117827a92803c0b19ed9281f5f2b594
drsoft28 <dif.rabie@gmail.com>
2023-03-02 19:15:53 +0100

    update

--- src/views/consulting/consultant/dialogs/add-booking/index.vue
+++ src/views/consulting/consultant/dialogs/add-booking/index.vue
@@ -23,6 +23,25 @@
         <d-error-input :errors="errors" v-if="errors.length>1"/>
     </ValidationProvider>
     </div>
+    <div class="mt-3">
+    <ValidationProvider
+    :name="$t('booking-time')"
+    tag="div"
+    vid="time"
+    v-slot="{errors}"
+    >
+        <label>الوقت المتاح</label>
+        <div class="d-flex">
+            <span class="time-card" 
+            :class="{'selected':itemForm.time == it}"
+                v-for="(it,i) in times" 
+                :key="i" 
+                @click="itemForm.time = it"
+            >{{ it }}</span>
+        </div>
+        <d-error-input :errors="errors" v-if="errors.length>1"/>
+    </ValidationProvider>
+    </div>
      <!-- desc -->
      <div class="form-group mt-3">
     <ValidationProvider
@@ -53,7 +72,8 @@ export default {
     data: () => ({
         showDialog: false,
         itemDailog: {},
-        itemForm: {date:null,desc:''}
+        itemForm: {date:null,desc:'',time:''},
+        times:['17:30','18:00','18:30','19:00']
     }),
     methods: {
         openDialog(dataEvt) {
@@ -72,6 +92,31 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.label-dialog{
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 17px;
+    /* identical to box height, or 106% */
+    color: #979797;
+}
+.time-card{
+    cursor: pointer;
+    border: 1px solid #c1c1c1;
+    padding: 5px;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 17px;
+    /* identical to box height, or 142% */
+    color: #737373;
+    border-radius: 4px;
+    min-width: 60px;
+    text-align: center;
+    margin: 0 5px;
+}
+.time-card.selected{
+    background: #1FB9B3;
+    color:white;
+}
 </style>
