b81afac4381111085d3aaf4a7c7f4bdbe7f558f1
drsoft28 <dif.rabie@gmail.com>
2023-03-04 14:10:19 +0100

    improve

--- src/views/network/home/guest/parts/section-network-goals/index.vue
+++ src/views/network/home/guest/parts/section-network-goals/index.vue
@@ -3,60 +3,37 @@
       <h2 class="home-section-title text-center"> أهداف شبكة رياديات</h2>
       <div class="row min-riadiat network">
           <div class="col-md-3 mt-2 ">
-              <div class="network-goal-item box border d-flex align-items-center rounded-2">
-                  <div>
-                      <img src="/assets/img/a1.svg" alt="">
-                  </div>
-                  <div>
-                      جمع والهــــــام وتمكين النســاء </div>
-              </div>
+            <CardGoal src="/assets/img/a1.svg" >
+                جمع والهــــــام وتمكين النســاء
+            </CardGoal>
+
           </div>
           <div class="col-md-3 mt-2 ">
-              <div class="network-goal-item box border d-flex align-items-center rounded-2">
-
-                  <div>
-                      <img src="/assets/img/a2.svg" alt="">
-                  </div>
-                  <div>
-                      بناء شبكــــــــــة علاقــــــــــــــات </div>
-              </div>
+            <CardGoal src="/assets/img/a2.svg" >
+                بناء شبكــــــــــة علاقــــــــــــــات
+            </CardGoal>
           </div>
           <div class="col-md-3 mt-2 ">
-              <div class="network-goal-item box border d-flex align-items-center rounded-2">
-
-                  <div>
-                      <img src="/assets/img/a3.png" alt="">
-                  </div>
-                  <div>
-                      شراكات مـــــــع
+            <CardGoal src="/assets/img/a3.png" >
+                شراكات مـــــــع
                       مختلف الجهـات
-                  </div>
-              </div>
+            </CardGoal>
           </div>
           <div class="col-md-3 mt-2 ">
-              <div class="network-goal-item box border d-flex align-items-center rounded-2">
-                  <div>
-                      <img src="/assets/img/a4.png" alt="">
-                  </div>
-                  <div>
-                      تطوير رائـــــدات الاعمــــــــــــــال
-                  </div>
-              </div>
+            <CardGoal src="/assets/img/a4.png" >
+                تطوير رائـــــدات الاعمــــــــــــــال
+            </CardGoal>
           </div>
       </div>
   </div>
 </template>
 <script>
+import CardGoal from './card-item.vue'
 export default {
-  name: 'network-goals'
+  name: 'network-goals',
+  components:{
+    CardGoal
+  }
 }
 </script>
 
-<style scoped>
-.network-goal-item[data-v-7e87801c] {
-  box-shadow: 0px 0px 6px 0.1px #1fb9b369;
-}
-.network-goal-item:hover{
-  box-shadow: 0px 0px 10px 2px var( --m-color);
-}
-</style>
