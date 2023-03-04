68b120aeb154b711da319ad0e62a5bfdce526c20
drsoft28 <dif.rabie@gmail.com>
2023-03-04 10:39:12 +0100

    update css for opionion card

--- src/components/cards/opinion-item.vue
+++ src/components/cards/opinion-item.vue
@@ -2,15 +2,15 @@
     <div class="item">
         <div class="row align-items-center p">
             <div class="col-md-8 osition-relative">
-                <p v-html="desc"></p>
-                <h6 style="color: #FFBC00;">{{ name }}
+                <p class="opinion-text" v-html="desc"></p>
+                <h6 class="opinion-name">{{ name }}
                 </h6>
                 <div class="position-absolute dote">
                     <img src="/assets/img/dot.png" alt="">
                 </div>
             </div>
             <div class="col-md-4">
-                <img class="rounded-3  " :src="image" :alt="name" width="368" height="368">
+                <img class="opinion-img rounded-3" :src="image" :alt="name" width="368" height="368">
             </div>
         </div>
     </div>
@@ -32,6 +32,27 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.opinion-text{
+    font-style: normal;
+font-weight: 400;
+font-size: 24px;
+line-height: 32px;
+/* or 133% */
+text-transform: capitalize;
+color: #737373;
+}
+.opinion-img {
+    width: 368px;
+    height:368px;
+}
+.opinion-name{
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 32px;
+    /* or 133% */
+    text-transform: capitalize;
+    color: #FFBC00;
+}
 </style>
