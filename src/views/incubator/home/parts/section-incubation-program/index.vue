3b05c6a85f3ca4e9f6f566478a35d7d3b20009ba
drsoft28 <dif.rabie@gmail.com>
2023-03-26 12:29:27 +0100

    update

--- src/views/incubator/home/parts/section-incubation-program/index.vue
+++ src/views/incubator/home/parts/section-incubation-program/index.vue
@@ -4,14 +4,14 @@
                     <img class="img-fluid rounded-3" src="/assets/img/Rectangle 1777az.png" alt="">
                 </div>
                 <div class="col-md-6">
-                    <h2>
+                    <h2 class="home-section-title">
                         برنامج الاحتضان
                     </h2>
-                    <p  style="line-height:40px ;" class="s-c">
+                    <p  style="line-height:40px ;" class="home-section-text">
                         برنامج لأصحاب الأفكار يحتوي على جلسات استشارية وإرشادية وبرامج تدريبية وتصميم الهوية التجارية ونموذج العمل دراسة الجدوى والتمويل الاستثمار
                     </p>
                     <div>
-                        <button class="btn-main">  سجلي الان   </button>
+                        <button class="btn btn-custmer">  سجلي الان   </button>
                     </div>
                 </div>
             </div>
