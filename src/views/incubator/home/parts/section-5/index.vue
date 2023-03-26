689e150759472849dbe75d5854a27e57c329f825
drsoft28 <dif.rabie@gmail.com>
2023-03-26 12:27:43 +0100

    update

--- src/views/incubator/home/parts/section-5/index.vue
+++ src/views/incubator/home/parts/section-5/index.vue
@@ -188,30 +188,6 @@
                   </div>
                 </div>
               </div>
-
-          
-              <div class="row mt-5  ">
-                <div class="col-md-6">
-                    <img class="img-fluid rounded-3" src="/assets/img/Rectangle 1777az.png" alt="">
-
-                </div>
-                <div class="col-md-6">
-                    <h2>
-                        برنامج الاحتضان
-                    </h2>
-                    <p  style="line-height:40px ;" class="s-c">
-                        برنامج لأصحاب الأفكار يحتوي على جلسات استشارية وإرشادية وبرامج تدريبية وتصميم الهوية التجارية ونموذج العمل دراسة الجدوى والتمويل الاستثمار
-                    </p>
-                    <div>
-                
-                     
-                        <button class="btn-main">  سجلي الان   </button>
-                    </div>
-
-                </div>
-
-
-            </div>
         </div>
 </template>
 
