4620cd03b7bb29d58cd0d04af92d884806985392
drsoft28 <dif.rabie@gmail.com>
2023-03-27 12:55:57 +0100

    update

--- src/components/expanded-panel/ExpandedPanelItem.vue
+++ src/components/expanded-panel/ExpandedPanelItem.vue
@@ -2,10 +2,10 @@
    <div class="accordion-item  m-2" :class="{'border-bottom':borderBottom}">
                           <h2 v-if="title" class="accordion-header" :id="idh">
                             <slot name="before-btn"></slot>
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="`#${id}`" aria-expanded="false" :aria-controls="id">{{title}}</button>
+                            <button class="accordion-button" :class="{collapsed:!opened_}"  type="button" data-bs-toggle="collapse" :data-bs-target="`#${id}`" :aria-expanded="opened_" :aria-controls="id">{{title}}</button>
                           </h2>
                           <slot  name="title" :id="id"></slot>
-                          <div :id="id" class="accordion-collapse collapse" :aria-labelledby="idh" data-bs-parent="#accordionFlushExample1">
+                          <div :id="id" class="accordion-collapse" :class="{'collapse show':opened_}"   :aria-labelledby="idh" data-bs-parent="#accordionFlushExample1">
                             <div class="accordion-body">
                                 <slot></slot>
                             </div>
@@ -21,15 +21,21 @@ props:{
         type:String,
         require:true
     },
+    opened:{
+      type:Boolean,
+        default:false
+    },
     borderBottom:{
         type:Boolean,
         require:true
     }
 },
-data:()=>({
+data:(vm)=>{
+  return{
   id:'',
-  idh:''
-}),
+  idh:'',
+  opened_:vm.opened
+}},
 mounted () {
   //console.log('this',this._uid,this.$parent.$children[0]._uid)
   let id=this.$parent.$attrs.id;
