fa00671dc66cbb809fcad8edede74700e8bb4a35
drsoft28 <dif.rabie@gmail.com>
2023-03-30 13:52:29 +0100

    add API list provider

---
+++ src/services/api/service-provider/provider/index.js
@@ -0,0 +1,15 @@
+import BaseApi from "../../base-service";
+import readyService from "./ready-service";
+import myRequests from "./my-requests";
+import requestPurchases from "./request-purchases";
+
+class servicesProviderApi extends BaseApi{
+    readyService = readyService;
+    myRequests = myRequests;
+    requestPurchases = requestPurchases;
+    getAll(params={}){
+        let s=this.generateQueryUrl(params)
+        return window.axios.get(`service-provider/user/services-providers?${s}`);
+    }
+}
+export default new servicesProviderApi();
