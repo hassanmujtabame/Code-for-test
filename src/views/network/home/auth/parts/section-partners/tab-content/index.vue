0a504b85a3e647d11a6d0912737f48ee31c0b0e9
drsoft28 <dif.rabie@gmail.com>
2023-04-05 23:52:01 +0100

    update

--- src/views/network/home/auth/parts/section-partners/tab-content/index.vue
+++ src/views/network/home/auth/parts/section-partners/tab-content/index.vue
@@ -3,6 +3,9 @@
     <div v-for="(im,i) in items" :key="i" class="col-6 col-md-3 text-center" data-aos="fade-up" data-aos-duration="1000">
                     <ImgAvatar :img="im.image_path" :alt="im.name??''"  size=180 />
                 </div>
+                <div v-if="items.length==0" style="min-height:180px" class="col-12  d-flex align-items-center">
+                  <h3 class="t-c">{{ $t('no-items') }}</h3>
+                </div>
             </div>
 </template>
 
