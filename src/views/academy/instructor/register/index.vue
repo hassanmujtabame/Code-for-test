36e2b2c2787bfca96955861333588b01db158812
drsoft28 <dif.rabie@gmail.com>
2023-04-09 22:16:21 +0100

    update header register instructor

--- src/views/academy/instructor/register/index.vue
+++ src/views/academy/instructor/register/index.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="mt-5">
+   <div style="margin-top: 86px;">
         <!-- section header-->
         <SectionHeader />
         <!--features-->
--- src/views/academy/instructor/register/parts/section-header/index.vue
+++ src/views/academy/instructor/register/parts/section-header/index.vue
@@ -1,21 +1,15 @@
 <template>
-  <div class="main-top p-5 position-relative">
-            <h1 class="text-white p-5 fs-1"> أنضم الى كوكبة مدربينا </h1>
-            <a href="#" @click="scollToElement('regestr',$event)" class="btn-go-register mx-5">
-                <svg width="24" height="23" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M12 21.5319C6.07 21.5319 1.25 17.0351 1.25 11.5028C1.25 5.97043 6.07 1.47363 12 1.47363C17.93 1.47363 22.75 5.97043 22.75 11.5028C22.75 17.0351 17.93 21.5319 12 21.5319ZM12 2.87305C6.9 2.87305 2.75 6.74477 2.75 11.5028C2.75 16.2608 6.9 20.1325 12 20.1325C17.1 20.1325 21.25 16.2608 21.25 11.5028C21.25 6.74477 17.1 2.87305 12 2.87305Z"
-                        fill="white" />
-                    <path
-                        d="M11.9999 17.3338C11.7899 17.3338 11.5899 17.3058 11.4199 17.2498C9.26986 16.5687 5.85986 14.1244 5.85986 10.4859C5.85986 8.54542 7.53986 6.96875 9.60986 6.96875C10.5099 6.96875 11.3399 7.25796 12.0099 7.78041C12.6799 7.25796 13.5199 6.96875 14.3999 6.96875C16.4699 6.96875 18.1499 8.54542 18.1499 10.4859C18.1499 14.1151 14.7399 16.5594 12.5799 17.2498C12.4099 17.3058 12.1999 17.3338 11.9999 17.3338ZM9.59987 8.37748C8.35987 8.37748 7.34987 9.3291 7.34987 10.4953C7.34987 13.7513 10.7199 15.5612 11.8999 15.9343C11.9399 15.9437 12.0399 15.9437 12.0799 15.9343C13.2599 15.5612 16.6299 13.7513 16.6299 10.4953C16.6299 9.3291 15.6199 8.37748 14.3799 8.37748C13.6799 8.37748 13.0099 8.69468 12.5899 9.22646C12.3099 9.58098 11.6699 9.58098 11.3899 9.22646C10.9699 8.68535 10.3199 8.37748 9.59987 8.37748Z"
-                        fill="white" />
-                </svg>
-
-                سجل الان
-            </a>
-            <div class="position-absolute anim-hwo-ar-you">
-                <img src="/assets/svg/wave-top.svg" alt="">
+  <div class="section-header-register-instructor position-relative">
+    <div class="position-absolute anim-hwo-ar-you">
+                <img src="/assets/svg/arabsstock_P34769_large1.svg"  height="612" alt="">
             </div>
+            <div class="section-header-register-instructor__wrapper px-5" >
+            <h1 class="section-header-register-instructor__title">شاركي خبراتك ومهاراتك</h1>
+            <p class="section-header-register-instructor__text"></p>
+            <a href="#" @click="scollToElement('regestr',$event)" class="btn-go-register m-5">
+                أنضمي كمدربة
+            </a>
+        </div>
             <div class="star position-absolute">
                 <img class="landing" src="/assets/svg/start.svg" alt="">
             </div>
@@ -46,4 +40,47 @@ export default {
     color: white;
 }
 
+.section-header-register-instructor{
+    height:612px;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+}
+.section-header-register-instructor__wrapper{
+    position: relative;
+    z-index: 2;
+   
+}
+.section-header-register-instructor__title{
+    font-style: normal;
+font-weight: 600;
+font-size: 56px;
+line-height: 72px;
+/* identical to box height, or 129% */
+
+text-align: center;
+text-transform: capitalize;
+
+color: #FFFFFF;
+
+/* بب */
+
+text-shadow: 0px 4px 16px rgba(0, 0, 0, 0.27);
+}
+.section-header-register-instructor__text{
+    font-style: normal;
+font-weight: 400;
+font-size: 24px;
+line-height: 32px;
+/* identical to box height, or 133% */
+
+text-align: right;
+text-transform: capitalize;
+
+color: #FFFFFF;
+
+/* ف */
+
+text-shadow: 0px 4px 16px rgba(0, 0, 0, 0.27);
+}
 </style>
