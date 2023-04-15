1d36b52c4a3922287df529b0e700f5ed1c5a9b43
drsoft28 <dif.rabie@gmail.com>
2023-04-15 12:45:08 +0100

    update

--- src/views/academy/learning-meetings/parts/section-filter-list/sidebar.vue
+++ src/views/academy/learning-meetings/parts/section-filter-list/sidebar.vue
@@ -3,55 +3,30 @@
       <h4>
           {{ $t('filter') }}
       </h4>
-      <div class="accordion" id="accordionPanelsStayOpenExample">
-          <div class="accordion-item">
-              <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                  <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                      data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
-                      aria-controls="panelsStayOpen-collapseOne">
-                      {{ $t('Category') }}
-                  </button>
-              </h2>
-              <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
-                  aria-labelledby="panelsStayOpen-headingOne">
-                  <div class="accordion-body">
-                      <div>
-                          <div v-for="(state,i) in states" :key="i" class="form-check">
-                              <input class="form-check-input" type="radio" :value="state.id" v-model="filter.type" 
-                              :selected="state.id===filter.type"
-                              name="stateRadioDefault"
-                                  id="flexRadioDefault1">
-                              <label class="form-check-label" for="flexRadioDefault1">
-                                 {{state.name}}
-                              </label>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="accordion-item  show">
-              <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                  <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                      data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="true"
-                      aria-controls="panelsStayOpen-collapseTwo">
-                      مجالات القاء
-                  </button>
-              </h2>
-              <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse show"
-                  aria-labelledby="panelsStayOpen-headingTwo">
-                  <div class="accordion-body show">
-                      <div v-for="(cat,i) in categories" :key="i" class="form-check">
-                          <input v-model="filter.category_id" :value="cat.id" class="form-check-input" type="checkbox">
-                          <label class="form-check-label" for="defaultCheck1">
-                              {{cat.name}}
-                          </label>
-                      </div>
-                
-                  </div>
-              </div>
-          </div>
-       
-      </div>
+      <d-expanded-panel id="accordionPanelsStayOpenExample">
+        <d-expanded-panel-item :title="$t('Category')">
+            <div>
+                <div v-for="(state,i) in states" :key="i" class="form-check">
+                    <input class="form-check-input" type="radio" :value="state.id" v-model="filter.type" 
+                    :selected="state.id===filter.type"
+                    name="stateRadioDefault"
+                        id="flexRadioDefault1">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        {{state.name}}
+                    </label>
+                </div>
+            </div>
+        </d-expanded-panel-item>
+        <d-expanded-panel-item :title="$t('meeting-categories')">
+            <div v-for="(cat,i) in categories" :key="i" class="form-check">
+                            <input v-model="filter.category_id" :value="cat.id" class="form-check-input" type="checkbox">
+                            <label class="form-check-label" for="defaultCheck1">
+                                {{cat.name}}
+                            </label>
+            </div>
+        </d-expanded-panel-item>
+      
+      </d-expanded-panel>
       <div class="mt-3 text-center">
           <button @click="updateFilter" class="btn-custmer">
               {{$t('save')}}
