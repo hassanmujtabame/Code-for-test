77cdab9707db9c77348cc5fa844e92be1326ccb9
drsoft28 <dif.rabie@gmail.com>
2023-04-16 17:20:25 +0100

    update

--- src/views/consulting/fields/consultants/parts/section-filter-list/index.vue
+++ src/views/consulting/fields/consultants/parts/section-filter-list/index.vue
@@ -31,6 +31,7 @@ export default {
     items:[],
     filterSide:{
       during:null,
+      category_id:[],
       min_price:0,
       max_price:1000
     },
@@ -38,6 +39,7 @@ export default {
             created_at:'asc',
             search:null,
             during:null,
+            category_id:[],
             min_price:0,
             max_price:1000
         }
