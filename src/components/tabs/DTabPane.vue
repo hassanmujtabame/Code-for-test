3332bc05630589606c8bcbe4acbf0c0671f98898
drsoft28 <dif.rabie@gmail.com>
2023-05-06 17:25:15 +0100

    add new component tab

---
+++ src/components/tabs/DTabPane.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="tab-pane fade" :class="{'show active':active}" :id="`${group}-${tag}`" role="tabpanel" :aria-labelledby="`${group}-${tag}-tab`" tabindex="0">
+            <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+   name:'d-tab-btn',
+   props:{
+      group:{
+          type:String,
+          default:'pills'
+      },
+      tag:{},
+      active:{
+          type:Boolean,
+          default:false,
+      }
+  
+   },
+   watch:{
+    active(){}
+   }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
