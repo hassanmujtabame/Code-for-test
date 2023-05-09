e6ab5a3a6b56fa773d48d941cf01afe1dbff8668
drsoft28 <dif.rabie@gmail.com>
2023-05-09 14:52:29 +0100

    update

--- src/common/dialogs/share-social-media/index.vue
+++ src/common/dialogs/share-social-media/index.vue
@@ -4,11 +4,14 @@
   :openDialog="openDialog"
   :closeDialog="closeDialog"
   >
+  <template  v-slot:header>
+    مشاركة الصفحة
+  </template>
     <template v-slot>
         <div class="text-center" v-if="showDialog">
           <div class="d-flex gap-4 justify-content-center p-4 icon-social-exibition">
                             <!--facebook-->
-                            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large"  data-show-count="false">Tweet</a>
+                            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large"  data-show-count="false"></a>
                            
                            <span v-if="false"> <i  class="fab fa-instagram" ></i></span>
 
