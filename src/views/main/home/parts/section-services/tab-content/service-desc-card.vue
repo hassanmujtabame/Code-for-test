2926c3ab8084ab606430f4417e2573019f220522
Mac <beshoy.moawad@revgate.net>
2023-05-21 11:35:12 +0300

    atts for aos

--- src/views/main/home/parts/section-services/tab-content/service-desc-card.vue
+++ src/views/main/home/parts/section-services/tab-content/service-desc-card.vue
@@ -1,62 +1,57 @@
 <template>
-    <div class="text-network p-4"
-    v-bind="atts">
-                    <h3>{{title}}</h3>
-                    
-                    <p class="">
-                        <slot></slot>
-                    </p>
-                    <button @click="router_push(routeName)" class="btn btn-custmer my-2">
-                  
-                      <i class="fa fa-arrow-up-right-from-square"></i>
-                     {{ $t('know-more') }}
-                  
-                  </button>
-                  </div>
+  <div class="text-network p-4" v-bind="atts">
+    <h3>{{title}}</h3>
+
+    <p class>
+      <slot></slot>
+    </p>
+    <button @click="router_push(routeName)" class="btn btn-custmer my-2">
+      <i class="fa fa-arrow-up-right-from-square"></i>
+
+      {{ $t('know-more') }}
+    </button>
+  </div>
 </template>
 
 <script>
 //import OpenLinkSVG from '@/components/icon-svg/open-link.vue'
 //import BtnSimple from '@/components/btns/BtnSimple.vue'
 export default {
-  props:{
-    title:{
-        type:String
+  props: {
+    title: {
+      type: String
     },
-    selected:{
-        type:Boolean,
-
+    selected: {
+      type: Boolean
     },
-    routeName:{
-      type:[String],
-      default:'index'
+    routeName: {
+      type: [String],
+      default: "index"
     }
   },
-  watch:{
-    selected:{
-        immediate:true,
-        handler(){
-          if(window.AOS)
-          window.AOS.init()
-        }
+  watch: {
+    selected: {
+      immediate: true,
+      handler() {
+        if (window.AOS) window.AOS.init();
+      }
     }
   },
-  computed:{
-    atts(){
-        return {
-          'data-aos-once':"false",
-            'data-aos-duration':2000,
-            'data-aos':"fade-right",
-        }
+  computed: {
+    atts() {
+      return {
+        "data-aos-once": "false",
+        "data-aos-duration": 2000,
+        "data-aos": "fade-right"
+      };
     }
   },
-  components:{
-   /* OpenLinkSVG,*/
-  //  BtnSimple
+  components: {
+    /* OpenLinkSVG,*/
+    //  BtnSimple
   }
-}
+};
 </script>
 
 <style>
-
 </style>
