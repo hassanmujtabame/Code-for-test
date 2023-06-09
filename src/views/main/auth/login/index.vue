ce8ac07ec99853a2d70af498d50913fa9d11815b
beshoy2549 <beshoymoawed@gmail.com>
2023-06-09 17:12:25 +0400

    fix structure ui of login page

--- src/views/main/auth/login/index.vue
+++ src/views/main/auth/login/index.vue
@@ -1,46 +1,56 @@
 <template>
- <div class="container">
- <LoginForm @change-form="afterSuccessForm" v-if="status=='form'" />
- <ResendCodeView @success="afterSuccessCheck" @cancel="status='form'" :dataInfo="dataCheck" v-if="status=='resend-code'" />
+  <div class="login-page">
+    <b-container>
+      <LoginForm @change-form="afterSuccessForm" v-if="status == 'form'" />
+      <ResendCodeView
+        @success="afterSuccessCheck"
+        @cancel="status = 'form'"
+        :dataInfo="dataCheck"
+        v-if="status == 'resend-code'"
+      />
+    </b-container>
   </div>
 </template>
 
 <script>
-import ResendCodeView from './ResendCode.vue'
-import LoginForm from './loginForm.vue'
+import ResendCodeView from "./ResendCode.vue";
+import LoginForm from "./loginForm.vue";
 
 export default {
- name:'login-page',
- metaInfo(){
-      return {title:this.$t('login')}
- },
- components:{
-   LoginForm,
-   ResendCodeView
- },
- data:()=>({
-    status:'form',
+  name: "login-page",
+  metaInfo() {
+    return { title: this.$t("login") };
+  },
+  components: {
+    LoginForm,
+    ResendCodeView
+  },
+  data: () => ({
+    status: "form",
 
-    dataCheck:{},
-    dataFinish:{}
- }),
- methods:{
-    afterSuccessForm(evt){
-        this.dataCheck=evt
-        this.status='resend-code';
+    dataCheck: {},
+    dataFinish: {}
+  }),
+  methods: {
+    afterSuccessForm(evt) {
+      this.dataCheck = evt;
+      this.status = "resend-code";
     },
-    afterSuccessCheck(/*evt*/){
+    afterSuccessCheck(/*evt*/) {
       /*this.dataFinish= evt*/
-      window.SwalSuccess('تم تفيعل حسابك بنجاح')
-        this.status='form';
-    },
- },
- mounted(){
-    this.status='form';
- }
-}
+      window.SwalSuccess("تم تفيعل حسابك بنجاح");
+      this.status = "form";
+    }
+  },
+  mounted() {
+    this.status = "form";
+  }
+};
 </script>
 
-<style>
-
+<style scoped> 
+.login-page {
+   background-color: #f6f8f9;
+   padding: 3em 0 6em;
+}
 </style>
