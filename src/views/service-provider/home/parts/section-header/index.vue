a8b411e26be241b3d9d8074f19f1a3f9cc76fe64
beshoy2549 <beshoymoawed@gmail.com>
2023-06-09 21:01:55 +0400

    fix header service provider

--- src/views/service-provider/home/parts/section-header/index.vue
+++ src/views/service-provider/home/parts/section-header/index.vue
@@ -1,51 +1,51 @@
 <template>
-  <div class="consulting" :class="[isMobile ? 'p-3' : 'p-5']">
-    <div class="" :class="{ container: !isMobile }">
-      <div class="row">
-        <div class="col-8 col-md-6">
-          <h1 class="header-title-page">
-            {{ $t("service-providers") }}
-          </h1>
+  <div class="consulting pt-3 mobile-center">
+    <b-container>
+      <b-row>
+        <b-col lg="6">
+          <h1 class="header-title-page">{{ $t("service-providers") }}</h1>
           <p class="header-desc-page text-white">
             منصة تجمع أهم وألمع رائدين و رائدات الأعمال في السعودية والخليج في
             مكان واحد
           </p>
-        </div>
-        <div class="col-3 col-md-6 m-auto">
-          <div style="overflow: inherit" class="img-cons m-auto">
-            <img
-              :src="`${publicPath}assets/img/1664280407882 1.png`"
-              alt="service-provider-home"
-            />
+
+          <div class="actions pt-2">
+            <a
+              @click="shouldLoginMsg"
+              class="btn-customer shadow d-inline-block mt-1"
+            >{{ $t("join-us") }}</a>
+            <router-link
+              :to="getRouteLocale('login')"
+              class="btn-custmer-w bg-transparent d-inline-block mx-4 mt-1"
+            >{{ $t("login-female") }}</router-link>
           </div>
-        </div>
-        <div class="col-12 mb-3">
-          <a @click="shouldLoginMsg" class="btn-custmer shadow">
-            {{ $t("join-us") }}
-          </a>
-          <router-link
-            :to="getRouteLocale('login')"
-            class="btn-custmer-w bg-transparent"
-          >
-            {{ $t("login-female") }}</router-link
-          >
-        </div>
-      </div>
+        </b-col>
+        <b-col lg="6" class="m-auto mobile-hide">
+          <div style="overflow: inherit" class="img-cons m-auto">
+            <img :src="`${publicPath}assets/img/1664280407882 1.png`" alt="service-provider-home" />
+          </div>
+        </b-col>
+      </b-row>
       <div class="star-cons">
-        <img :src="`${publicPath}assets/img/starrrr.png`" alt="" />
+        <img :src="`${publicPath}assets/img/starrrr.png`" alt />
       </div>
       <div class="star-cons stars">
-        <img :src="`${publicPath}assets/img/starsss.png`" alt="" />
+        <img :src="`${publicPath}assets/img/starsss.png`" alt />
       </div>
-    </div>
+    </b-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: "section-header",
+  name: "section-header"
 };
 </script>
 
-<style>
-</style>
+<style  scoped>
+@media (max-width:991px) {
+  .consulting {
+    padding: 30px 0
+  }
+ }
+</style>>
