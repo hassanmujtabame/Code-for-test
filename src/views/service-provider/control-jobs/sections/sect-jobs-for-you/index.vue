a7eb62311e541d92b1fb448dd16be1020c50b168
beshoy2549 <beshoymoawed@gmail.com>
2023-06-19 00:58:55 +0400

    remove jobs not used

--- src/views/service-provider/control-jobs/sections/sect-jobs-for-you/index.vue
+++ src/views/service-provider/control-jobs/sections/sect-jobs-for-you/index.vue
@@ -1,34 +1,27 @@
 <template>
   <div class="mt-5 shadow rounded-2">
-    <h2 class="p-3">
-      وظائف مقدمة لك
-    </h2>
+    <h2 class="p-3">وظائف مقدمة لك</h2>
     <div v-for="(item,i) in items" :key="i">
       <CardInfo :item-id="item.id" />
-      <hr/>
+      <hr />
     </div>
-   
   </div>
 </template>
 
 <script>
-import CardInfo from './card.vue'
+import CardInfo from "./card.vue";
 export default {
-  name: 'section-jobs-for-you',
-  components:{
+  name: "section-jobs-for-you",
+  components: {
     CardInfo
   },
-  data:()=>{
+  data: () => {
     return {
-      items:[
-        {},
-        {},
-      ]
-    }
+      items: []
+    };
   }
-}
+};
 </script>
 
 <style>
-
 </style>
--- src/views/service-provider/control-jobs/sections/sect-jobs-you-apply/index.vue
+++ src/views/service-provider/control-jobs/sections/sect-jobs-you-apply/index.vue
@@ -8,78 +8,73 @@
       searchPlaceholder=" أبحث  في قائمة الوظائف  "
       :orderOpts="statuses"
       orderName="status"
-      >
-        <template v-slot:total>
-            <h4 class="fw-bold">وظائف قدمت عليها</h4>
-        </template>
+    >
+      <template v-slot:total>
+        <h4 class="fw-bold">وظائف قدمت عليها</h4>
+      </template>
 
-        <template v-slot:default="{item}">
-            <CardInfo :item-id="item.id"
-            :dateRequest="item.created_at"
-            :name="item.user_name"
-                          :title="item.title"
-                          :status="item.status"
-                          :state="item.state"
-                          :price="item.expected_salary"
-                          :job="item.job_name"
-                          :experience="item.required_experience"
-                          :description="item.desc"
-                          :type-work="item.type_job" 
-                          
-                            >
-                        </CardInfo>
-        </template>
-      </d-filter-list>
+      <template v-slot:default="{item}">
+        <CardInfo
+          :item-id="item.id"
+          :dateRequest="item.created_at"
+          :name="item.user_name"
+          :title="item.title"
+          :status="item.status"
+          :state="item.state"
+          :price="item.expected_salary"
+          :job="item.job_name"
+          :experience="item.required_experience"
+          :description="item.desc"
+          :type-work="item.type_job"
+        ></CardInfo>
+      </template>
+    </d-filter-list>
   </div>
 </template>
 
 <script>
-import userAPI from '@/services/api/user.js'
-import CardInfo from './card.vue'
+import userAPI from "@/services/api/user.js";
+import CardInfo from "./card.vue";
 export default {
-  name: 'section-jobs-for-apply',
-  components:{
+  name: "section-jobs-for-apply",
+  components: {
     CardInfo
   },
-    data:()=>{
-        return {
-            status:null,
-            statuses:[
-                {name:'عرض الجميع',id:null},
-                {name:'لم تراجع بعد',id:"waiting"},
-                {name:'مناسب',id:"underway"},
-            ],
-            filterItem:{
-                search:null,
-                created_at:'asc',
-                status:null
-            }
-        }
-        
+  data: () => {
+    return {
+      status: null,
+      statuses: [
+        { name: "عرض الجميع", id: null },
+        { name: "لم تراجع بعد", id: "waiting" },
+        { name: "مناسب", id: "underway" }
+      ],
+      filterItem: {
+        search: null,
+        created_at: "asc",
+        status: null
+      }
+    };
+  },
+  methods: {
+    changeFilter(val) {
+      this.filterItem = { ...this.filterItem, ...val };
+      this.fireEvent("d-filter-list-refresh");
     },
-    methods:{
-        changeFilter(val){
-            this.filterItem = {...this.filterItem,...val}
-            this.fireEvent('d-filter-list-refresh')
-        },
-        async loadList(metaInfo) {
-            try {
-                let params = {
-                    page: metaInfo.current_page,
-                    ...this.filterItem
-                }
-                return await userAPI.getMyJobsApplied(params)
-
-            } catch (error) {
-                console.log('error', error)
-                console.log('response', error.response)
-            }
-        }
+    async loadList(metaInfo) {
+      try {
+        let params = {
+          page: metaInfo.current_page,
+          ...this.filterItem
+        };
+        return await userAPI.getMyJobsApplied(params);
+      } catch (error) {
+        console.log("error", error);
+        console.log("response", error.response);
+      }
     }
-}
-
+  }
+};
 </script>
 
 <style>
-
 </style>
