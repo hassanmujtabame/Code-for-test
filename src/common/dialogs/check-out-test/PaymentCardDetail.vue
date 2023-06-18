79251bcc77c3a622bc27a79d774af82364532e17
beshoy2549 <beshoymoawed@gmail.com>
2023-06-18 23:43:53 +0400

    remove payment methods form profile

---
+++ src/common/dialogs/check-out-test/PaymentCardDetail.vue
@@ -0,0 +1,70 @@
+<!-- PaymentCardDetail.vue -->
+<template>
+  <b-card class="mt-2 payment-card-detail" no-body>
+    <b-card-title class="checkout-title">{{ title }}</b-card-title>
+    <b-form @submit.prevent="payment">
+      <b-form-group class="mt-2">
+        <ValidationProvider rules="numeric" v-slot="{ errors }">
+          <b-form-input v-model="itemForm.coupon" class="form-control" placeholder="ادخل الكود"></b-form-input>
+          <span v-if="errors[0]" class="text-danger">{{ errors[0] }}</span>
+        </ValidationProvider>
+      </b-form-group>
+      <p class="text-center small">
+        بإتمامك لعملية الشراء أنت توافق على
+        <b-link href="#" class="m-c">شروط الاستخدام</b-link>
+      </p>
+      <div class="text-center">
+        <button class="p-2 text-white btn-main-v" :disabled="loading" type="submit">
+          <b-spinner small v-if="loading"></b-spinner>تاكيد الدفع
+        </button>
+      </div>
+    </b-form>
+  </b-card>
+</template>
+  
+  <script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "تفاصيل الدفع"
+    },
+    changeable: {
+      type: Boolean,
+      default: false
+    },
+    itemForm: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      loading: false
+    };
+  },
+  methods: {
+    async payment() {
+      this.loading = true;
+      try {
+        this.$emit("payment", this.itemForm);
+      } catch (error) {
+        console.error(error);
+        // Handle the error based on your application logic.
+      } finally {
+        this.loading = false;
+      }
+    }
+  }
+};
+</script>
+  
+  <style scoped>
+.checkout-title {
+  word-break: break-all;
+}
+.payment-card-detail {
+  padding: 55px 12px;
+}
+</style>
+  
--- src/views/common/my-profile/parts/sidebar/index.vue
+++ src/views/common/my-profile/parts/sidebar/index.vue
@@ -1,34 +1,32 @@
 <template>
-                <div id="list-example" class="list-group position-fixed ">
-                  <a class="list-group-item list-group-item-action" href="#list-item-1">معلومات العرض</a>
-                  <a class="list-group-item list-group-item-action" href="#list-item-2">معلومات شخصية</a>
-                  <a class="list-group-item list-group-item-action" href="#list-item-payment">بطاقات الدفع</a>
-                  <a class="list-group-item list-group-item-action" href="#list-item-3">معلومات الدراسة</a>
-                  <a class="list-group-item list-group-item-action" href="#list-item-4"> معلومات مهنية</a>
-                  <a class="list-group-item list-group-item-action" href="#list-item-5">معلومات الاقامة </a>
-                  <a class="list-group-item list-group-item-action" href="#list-item-6">معلومات التواضل </a>
-                  <a class="list-group-item list-group-item-action" href="#list-item-7"> اعدادات الاشعارت</a>
-                  <a class="list-group-item list-group-item-action" href="#list-item-8">  قائمة الحظر</a>
-                  <a class="list-group-item list-group-item-action" href="#list-item-9">  معرض الاعمال</a>
-
-                </div>
+  <div id="list-example" class="list-group position-fixed">
+    <a class="list-group-item list-group-item-action" href="#list-item-1">معلومات العرض</a>
+    <a class="list-group-item list-group-item-action" href="#list-item-2">معلومات شخصية</a>
+    <a class="list-group-item list-group-item-action" href="#list-item-3">معلومات الدراسة</a>
+    <a class="list-group-item list-group-item-action" href="#list-item-4">معلومات مهنية</a>
+    <a class="list-group-item list-group-item-action" href="#list-item-5">معلومات الاقامة</a>
+    <a class="list-group-item list-group-item-action" href="#list-item-6">معلومات التواضل</a>
+    <a class="list-group-item list-group-item-action" href="#list-item-7">اعدادات الاشعارت</a>
+    <a class="list-group-item list-group-item-action" href="#list-item-8">قائمة الحظر</a>
+    <a class="list-group-item list-group-item-action" href="#list-item-9">معرض الاعمال</a>
+  </div>
 </template>
 
 <script>
 export default {
- name:"side-bar"
-}
+  name: "side-bar"
+};
 </script>
 
 <style scoped>
 .list-group-item.active {
-    z-index: 2;
-    color: #1FB9B3;
-    background-color: transparent !important;
-    border: 1px solid #CDD7D8;
-    border-right: 2px solid #1FB9B3;
+  z-index: 2;
+  color: #1fb9b3;
+  background-color: transparent !important;
+  border: 1px solid #cdd7d8;
+  border-right: 2px solid #1fb9b3;
 }
- .list-group-item {
-    padding: 10px 10px 0 150px;
+.list-group-item {
+  padding:15px 10px 15px 150px
 }
 </style>
