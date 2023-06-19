cca22e21c40341550a8f46309912c7fc2811f80a
beshoy2549 <beshoymoawed@gmail.com>
2023-06-20 03:49:41 +0400

    add fav icon

--- src/views/main/home/parts/section-partners/index.vue
+++ src/views/main/home/parts/section-partners/index.vue
@@ -1,62 +1,58 @@
 <template>
-    <div class="sec-six">
-        <div class="container p-3">
-            <h2>{{ $t('our-partners-in-riadiat') }}</h2>
-            <div class="row justify-content-center  gap-2">
-                <div v-for="(partner,i) in items" :key="i" class=" col-12 col-md-3 text-center" data-aos="fade-up" data-aos-duration="2000">
-                  <div class="mx-auto" style="width:fit-content">
-                    <ImgAvatar class="filter-shadow-partner mx-auto" :img="partner.image_path"  size=180 />
-                  </div>
-                </div>
-            </div>
+  <div class="sec-six" v-if="items.length">
+    <div class="container p-3">
+      <h2>{{ $t('our-partners-in-riadiat') }}</h2>
+      <div class="row justify-content-center gap-2">
+        <div
+          v-for="(partner,i) in items"
+          :key="i"
+          class="col-12 col-md-3 text-center"
+          data-aos="fade-up"
+          data-aos-duration="2000"
+        >
+          <div class="mx-auto" style="width:fit-content">
+            <ImgAvatar class="filter-shadow-partner mx-auto" :img="partner.image_path" size="180" />
+          </div>
         </div>
       </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import partnersAPI from '@/services/api/partners.js'
-import ImgAvatar from '@/components/avatars/img-avatar.vue';
+import partnersAPI from "@/services/api/partners.js";
+import ImgAvatar from "@/components/avatars/img-avatar.vue";
 export default {
-  name:'partners-riadiat',
-    components:{
-        ImgAvatar
-    },
- data:(vm)=>{
-  return{
-    partners:[
-        {image_path:`${vm.publicPath}assets/img/Ellipse 24.png`,name:'partner 01'},
-        {image_path:`${vm.publicPath}assets/img/Ellipse 25.png`,name:'partner 02'},
-        {image_path:`${vm.publicPath}assets/img/Ellipse 24.png`,name:'partner 03'},
-        {image_path:`${vm.publicPath}assets/img/Ellipse 25.png`,name:'partner 04'},
-        {image_path:`${vm.publicPath}assets/img/Ellipse 24.png`,name:'partner 05'},
-        {image_path:`${vm.publicPath}assets/img/Ellipse 25.png`,name:'partner 06'},
-        {image_path:`${vm.publicPath}assets/img/Ellipse 24.png`,name:'partner 07'},
-        {image_path:`${vm.publicPath}assets/img/Ellipse 25.png`,name:'partner 08'},
-    ]
-    ,
-    loading:true,
-    items:[]
- }}
-  ,methods:{
-    async initlizing(){
+  name: "partners-riadiat",
+  components: {
+    ImgAvatar
+  },
+  data() {
+    return {
+      partners: [],
+      loading: true,
+      items: []
+    };
+  },
+  methods: {
+    async initlizing() {
       this.loading = true;
-        try {
-          let { data } =  await partnersAPI.getPartnersHome();
-          if(data.success){
-            this.items = data.data
-          }
-        } catch (error) {
-          console.log('error',error)
+      try {
+        let { data } = await partnersAPI.getPartnersHome();
+        if (data.success) {
+          this.items = data.data;
         }
+      } catch (error) {
+        console.log("error", error);
+      }
       this.loading = false;
     }
   },
-  mounted(){
-    this.initlizing()
- }
-}
+  mounted() {
+    this.initlizing();
+  }
+};
 </script>
 
 <style>
-
 </style>
