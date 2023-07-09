78a771c07f4d37962b73513a74e89d78bf685f72
Ahmed Mostafa <ahmed.mostafa.dev.eg@gmail.com>
2023-07-09 14:05:10 +0300

    fix  project ui

--- src/views/network/investment-project/parts/sect-invest-project-2/index.vue
+++ src/views/network/investment-project/parts/sect-invest-project-2/index.vue
@@ -1,84 +1,91 @@
 <template>
     <div class="row">
-          <div  class="d-flex justify-content-between align-items-center container">
-          <d-filter-list
-          group="d-filter-list-2"
-          classColCard="col-md-6 mt-3"
-          :call-list="initializing"
-          hideSide
-          hideTitle
-          :orderOpts="[
+        <div class="_d-flex justify-content-between align-items-center container">
+            <DMasterFilter
+                    group="d-filter-list-1"
+                    classColCard="col-md-6   mt-3"
+                    :call-list="initializing"
+                    classTotal="col-md-6"
+                    hideSide
+                    hideTitle
+                    HeadEndClasses="col-md-6"
+                    class-col-order="d-flex justify-content-end gap-3"
+                    :orderOpts="[
                 {id:'asc',name:'المضاف حديثا'},
                 {id:'desc',name:'الاقدم'}
-            ]"
-             @change="changeFilter"
-          >
-          <template v-slot:total>
-              <h1 class="text-bold">  {{ $t('Moral_investment_projects') }}</h1>
-          </template>
-          <template v-slot:search>
+                ]"
+                    @change="changeFilter"
+            >
+                <template v-slot:total>
+                    <h1 class="text-bold"> {{ $t('Moral_investment_projects') }}</h1>
+                </template>
+                <template v-slot:search>
                     <!-- for hide search and stay its place-->
                     <div></div>
-                    </template>
-              <template v-slot:head-end>
+                </template>
+                <template v-slot:append-order>
+                    <router-link :to="getRouteLocale('network-investment-project-request-add')"
+                                 class="btn bg-main text-white p-2">
+                        أعرض مشروعك لاستثمار
+                    </router-link>
+                </template>
+                <template v-slot:default="{item}">
 
-                  <router-link :to="getRouteLocale('network-investment-project-request-add')" class="btn bg-main text-white p-2">
-                      أعرض مشروعك لاستثمار
-                  </router-link>
-              </template>
-       <template v-slot:default="{item}">
-        
-              <router-link class="router-link h-100" :to="getRouteLocale('network-investment-project-show',{id:item.id})">   
-              <DMoralProjectLarge
-                 :title="item.title"
-                 :description="item.description"
-                 :publisher="item.user_info.name"
-                 :date-publish="item.created_at"
-                 :rest-day="item.rest_days"
-                 :offers="item.count_invest"
-                 :offered_property="item.offered_property"
-                 :minRent="item.amount_financing_required"
-                 :maxRent="item.minimum_investment"
-                 :place="item.place"
-                class="h-100"
-                 />
-              </router-link>
-          </template>
-          </d-filter-list>
-      </div>
-      </div>
+                    <router-link class="router-link h-100"
+                                 :to="getRouteLocale('network-investment-project-show',{id:item.id})">
+                        <DMoralProjectLarge
+                                :title="item.title"
+                                :description="item.description"
+                                :publisher="item.user_info.name"
+                                :date-publish="item.created_at"
+                                :rest-day="item.rest_days"
+                                :offers="item.count_invest"
+                                :offered_property="item.offered_property"
+                                :minRent="item.amount_financing_required"
+                                :maxRent="item.minimum_investment"
+                                :place="item.place"
+                                class="h-100"
+                        />
+                    </router-link>
+                </template>
+            </DMasterFilter>
+        </div>
+    </div>
 </template>
 
 <script>
 import projectsAPI from '@/services/api/projects.js'
 import DMoralProjectLarge from '@/components/cards/projects/moral/large.vue';
+import DMasterFilter from '@/components/filter-list/master.vue';
+
 //import investmentProject from '@/components/cards/offer-investment-project.vue';
 export default {
-name:'sect-invest-project-moral',
-components:{
-  DMoralProjectLarge
-},
-data:()=>({
-    filterItem:{
-        search:'',created_at:'asc'
-    }
-
- }),
- methods:{
-    changeFilter(val){
-            this.filterItem = {...this.filterItem,...val}
-            this.fireEvent('d-filter-list-2-refresh')
+    name: 'sect-invest-project-moral',
+    components: {
+        DMoralProjectLarge,
+        DMasterFilter
     },
-  async initializing(metaInfo){
-   
-              let params = {
-              page: metaInfo.current_page,
-              ...this.filterItem
-          }
-              return await projectsAPI.getAllPage('moral',4,params)
+    data: () => ({
+        filterItem: {
+            search: '', created_at: 'asc'
+        }
 
-  }
-}
+    }),
+    methods: {
+        changeFilter(val) {
+            this.filterItem = {...this.filterItem, ...val}
+            this.fireEvent('d-filter-list-2-refresh')
+        },
+        async initializing(metaInfo) {
+
+            let params = {
+                page: metaInfo.current_page,
+                ...this.filterItem
+            }
+            return await projectsAPI.getAllPage('moral', 4, params)
+
+        }
+    }
 }
 </script>
 
--- src/views/network/investment-project/parts/sect-project-near-expired/index.vue
+++ src/views/network/investment-project/parts/sect-project-near-expired/index.vue
@@ -1,66 +1,68 @@
 <template>
-  <div class=" mt-5 p-3">
+    <div class=" mt-5 p-3">
         <div class="">
-          <div
-            class="d-flex justify-content-between align-items-center container"
-          >
-            <h1 class="text-bold">جولات تنتهي هذا الاسبوع</h1>
-            <div>
-     
-                  <router-link :to="getRouteLocale('network-investment-project-request-add')" class="btn bg-main text-white p-2">
-                    أعرض مشروعك لاستثمار
-                  </router-link>
-              
-              </div>
-          </div>
-          <d-swiper
-          v-if="!loading"
-          :items="items"
-          is-auto
-          :slides-per-view="4"
-          :space-bitween="10"
-          >
-          <template v-slot="{item}">
-            <router-link class="router-link h-100" :to="getRouteLocale('network-investment-project-show',{id:item.id})"> 
-            <DPhysicalProjectSimple v-if="item.investment_type=='physical'" style="width:95%"
-            :image="item.image"
-                   :title="item.title"
-                   :description="item.description"
-                   :publisher="item.user_info.name"
-                   :date-publish="item.created_at"
-                   :rest-day="item.rest_days"
-                   :investor="item.count_invest"
-                   :minimum-goal="item.minimum_investment"
-                   :offered_property="item.offered_property"
-                   :amount="item.amount_financing_required"
-                   classRestDays="text-danger"
-                   class="h-100"
-                   />
-            <DMoralProjectSimple v-else style="width:95%"
-            :image="item.image"
-                   :title="item.title"
-                   :description="item.description"
-                   :publisher="item.user_info.name"
-                   :date-publish="item.created_at"
-                   :rest-day="item.rest_days"
-                   :offers="item.count_invest"
-                   :maxRent="item.minimum_investment"
-                   :offered_property="item.offered_property"
-                   :minRent="item.amount_financing_required"
-                   classRestDays="text-danger"
-                   class="h-100"
-                   />
-            </router-link>
-          </template>
-              
-    
-       
-          </d-swiper>
-          
+            <div
+                    class="d-flex justify-content-between align-items-center container"
+            >
+                <h1 class="text-bold">جولات تنتهي هذا الاسبوع</h1>
+                <div>
+
+                    <router-link :to="getRouteLocale('network-investment-project-request-add')"
+                                 class="btn bg-main text-white p-2">
+                        أعرض مشروعك لاستثمار
+                    </router-link>
+
+                </div>
+            </div>
+            <d-swiper
+                    v-if="!loading && items.length"
+                    :items="items"
+                    is-auto
+                    :slides-per-view="4"
+                    :space-bitween="10"
+            >
+                <template v-slot="{item}">
+                    <router-link class="router-link h-100"
+                                 :to="getRouteLocale('network-investment-project-show',{id:item.id})">
+                        <DPhysicalProjectSimple v-if="item.investment_type=='physical'" style="width:95%"
+                                                :image="item.image"
+                                                :title="item.title"
+                                                :description="item.description"
+                                                :publisher="item.user_info.name"
+                                                :date-publish="item.created_at"
+                                                :rest-day="item.rest_days"
+                                                :investor="item.count_invest"
+                                                :minimum-goal="item.minimum_investment"
+                                                :offered_property="item.offered_property"
+                                                :amount="item.amount_financing_required"
+                                                classRestDays="text-danger"
+                                                class="h-100"
+                        />
+                        <DMoralProjectSimple v-else style="width:95%"
+                                             :image="item.image"
+                                             :title="item.title"
+                                             :description="item.description"
+                                             :publisher="item.user_info.name"
+                                             :date-publish="item.created_at"
+                                             :rest-day="item.rest_days"
+                                             :offers="item.count_invest"
+                                             :maxRent="item.minimum_investment"
+                                             :offered_property="item.offered_property"
+                                             :minRent="item.amount_financing_required"
+                                             classRestDays="text-danger"
+                                             class="h-100"
+                        />
+                    </router-link>
+                </template>
+
+
+            </d-swiper>
+            <h1 v-else class="t-c text-center">لا يوجد عناصر</h1>
+
 
         </div>
 
-      </div>
+    </div>
 </template>
 
 <script>
@@ -69,39 +71,48 @@ import DMoralProjectSimple from '@/components/cards/projects/moral/simple.vue';
 import DPhysicalProjectSimple from '@/components/cards/projects/physical/simple.vue';
 
 export default {
- name:'sect-recent-courses',
- components:{
-  DMoralProjectSimple,
-  DPhysicalProjectSimple
- },
- data:()=>({
-  loading:true,
-  itemsTest:[
-        {id:1,title:'تكنولوجيا القلم الديجيتال',publisher:'خالد أسماعيل',datePublish:'22/12/2022',offered_property:30,amount:'500,000',restDay:40,investor:500,minimumGoal:50},
-        {id:2,title:'تكنولوجيا القلم الديجيتال',publisher:'خالد أسماعيل',datePublish:'22/12/2022',offered_property:30,amount:'500,000',restDay:40,investor:500,minimumGoal:50},
-        {id:3,title:'تكنولوجيا القلم الديجيتال',publisher:'خالد أسماعيل',datePublish:'22/12/2022',offered_property:30,amount:'500,000',restDay:40,investor:500,minimumGoal:50},
-        {id:4,title:'تكنولوجيا القلم الديجيتال',publisher:'خالد أسماعيل',datePublish:'22/12/2022',offered_property:30,amount:'500,000',restDay:40,investor:500,minimumGoal:20},
-    ],
-    items:[]
- }),
- methods:{
-  async loadList(){
-    this.loading = true;
-    try {
-      let {data} = await projectsAPI.getEndWeek()
-      if(data.success){
-        this.items = data.data.filter((x,i)=>i<5)
-      }
-    } catch (error) {
-      console.log('error',error)
-      console.log('error response',error.response)
+    name: 'sect-recent-courses',
+    components: {
+        DMoralProjectSimple,
+        DPhysicalProjectSimple
+    },
+    data: () => ({
+        loading: true,
+        itemsTest: [
+            {
+                id: 1,
+                name: 'تكنولوجيا القلم الديجيتال',
+                publisher: 'خالد أسماعيل',
+                datePublish: '22/12/2022',
+                offered_property: 30,
+                amount: '500,000',
+                restDay: 40,
+                investor: 500,
+                minimumGoal: 50
+            },
+
+        ],
+        items: []
+    }),
+    methods: {
+        async loadList() {
+            this.loading = true;
+            try {
+                let {data} = await projectsAPI.getEndWeek()
+                if (data.success) {
+                    this.items = data.data.filter((x, i) => i < 5)
+
+                }
+            } catch (error) {
+                console.log('error', error)
+                console.log('error response', error.response)
+            }
+            this.loading = false;
+        }
+    },
+    mounted() {
+        this.loadList()
     }
-    this.loading = false;
-  }
- },
- mounted(){
-  this.loadList()
- }
 }
 </script>
 
