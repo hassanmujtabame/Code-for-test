1e3281abe87bcb80b42e1a020409485425b157d1
Ëman El-Swerky <eman.swerky@varit.net>
2023-08-08 02:24:07 +0300

    edit

--- src/views/academy/common-components/section-hear/index.vue
+++ src/views/academy/common-components/section-hear/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="sex-eight mt-5  max-width-100-hidden">
+    <div class="sex-eight mt-5  max-width-100-hidden" v-if="items && items.length>0">
         <div class="container">
           <h2 class="text-center">ماذا يقولون المشتركات في اكاديمية رياديات</h2>
             <div :id="carouselId" class="hear-carousel owl-carousel owl-theme  mt-5">
