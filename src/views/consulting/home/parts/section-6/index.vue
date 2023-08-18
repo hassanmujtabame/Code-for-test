f147e1c3f172cf37d1d3917d3f3dfe38a08a8a88
Ëman El-Swerky <eman.swerky@varit.net>
2023-08-18 16:50:00 +0300

    edit

--- src/views/consulting/home/parts/section-6/index.vue
+++ src/views/consulting/home/parts/section-6/index.vue
@@ -1,69 +1,23 @@
 <template>
-<div class="container">
+  <div class="container" v-if="items && items.length>0">
 
 <div class="mt-5">
   <div class="riadiat-of-number">
-      <h2 class="text-center">مركز الاستشارات في ارقام  </h2>
-      <div class="row justify-content-center ">
-          <div class="col-6 col-md-auto mt-3">
+      <h2 class="text-center"> حاضنة رياديات في أرقام  </h2>
+      <div v-if="true" class="row justify-content-center ">
+          <div v-for="(item,i) in items" :key="i" class="col-6 col-sm-3 col-md-2 mt-3">
               <div class="box">
 
                   <h5 class="m-c fw-bolder">
-                      60000+
+                     {{item.value}}
                   </h5>
                   <p>
-                      اشتراك في برامجنا
-
+                    {{item.title}}
                   </p>
               </div>
           </div>
-          <div class="col-6 col-md-auto mt-3">
-              <div class="box">
-
-                  <h5 class="m-c fw-bolder">
-                      5222+
-                  </h5>
-                  <p>
-                        ساعة تدربية
-                  </p>
-              </div>
-          </div>
-          <div class="col-6 col-md-auto mt-3">
-              <div class="box">
-
-                  <h5 class="m-c fw-bolder">
-                      898+
-                  </h5>
-                  <p>
-                  اشتراك في الدورات
-
-                  </p>
-              </div>
-          </div>
-          <div class="col-6 col-md-auto mt-3">
-              <div class="box">
-
-                  <h5 class="m-c fw-bolder">
-                      9+
-                  </h5>
-                  <p>
-                       مجالات تدريبية 
-                  </p>
-              </div>
-          </div>
-          <div class="col-6 col-md-auto mt-3">
-              <div class="box">
-
-                  <h5 class="m-c fw-bolder">
-                      876+
-                  </h5>
-                  <p>
-                       محاضرة 
-                  </p>
-              </div>
           </div>
 
-      </div>
 
   </div>
 
@@ -72,8 +26,27 @@
 </template>
 
 <script>
+import incubatorAPI from '@/services/api/consulting/index.js'
 export default {
- name:'section-6'
+    name:'section-numbers',
+    data:()=>({
+        items:[]
+    }),
+    methods:{
+       async initializing(){
+            try{
+                let {data} = await incubatorAPI.getNumbers()
+                if(data.success){
+                    this.items = data.data
+                }
+            }catch(error){
+                //
+            }
+        }
+    },
+    mounted(){
+        this.initializing()
+    }
 }
 </script>
 
