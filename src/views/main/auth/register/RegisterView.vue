ea418b35bd9dc627ed6799fe23d28ddc9e36e616
Ahmed Mostafa <ahmed.mostafa.dev.eg@gmail.com>
2023-08-22 16:29:22 +0300

    Fix register password issue

--- src/views/main/auth/register/RegisterView.vue
+++ src/views/main/auth/register/RegisterView.vue
@@ -4,7 +4,7 @@
       <b-col xl="7">
         <div class="login-form">
           <div
-            :style="{ top: '17px', left: $i18n.locale == 'ar' ? 'auto' : '0', right: $i18n.locale !== 'ar' ? 'auto' : '0' }"
+              :style="{ top: '17px', left: $i18n.locale == 'ar' ? 'auto' : '0', right: $i18n.locale !== 'ar' ? 'auto' : '0' }"
           >
             <h6>
               لديك حساب بالفعل؟
@@ -15,94 +15,95 @@
           <p>انشئ حساب جديد وأنضمي الى عالم رياديات</p>
           <ValidationObserver ref="form">
             <ValidationProvider
-              :name="$t('Full-name')"
-              vid="name"
-              rules="required"
-              v-slot="{ errors }"
-              tag="div"
-              class="mb-3"
+                :name="$t('Full-name')"
+                vid="name"
+                rules="required"
+                v-slot="{ errors }"
+                tag="div"
+                class="mb-3"
             >
               <b-form-input
-                type="text"
-                :placeholder="$t('Full-name')"
-                v-model="form.name"
-                required
+                  type="text"
+                  :placeholder="$t('Full-name')"
+                  v-model="form.name"
+                  required
               />
               <div class="text-input-error">{{ errors[0] }}</div>
             </ValidationProvider>
             <ValidationProvider
-              :name="$t('Email')"
-              vid="email"
-              rules="required|email"
-              v-slot="{ errors }"
-              tag="div"
-              class="mb-3"
+                :name="$t('Email')"
+                vid="email"
+                rules="required|email"
+                v-slot="{ errors }"
+                tag="div"
+                class="mb-3"
             >
-              <b-form-input type="email" :placeholder="$t('Email')" v-model="form.email" required />
+              <b-form-input type="email" :placeholder="$t('Email')" v-model="form.email" required/>
               <div class="text-input-error">{{ errors[0] }}</div>
             </ValidationProvider>
 
             <div class="cols-12 d-flex gap-1 mb-3 align-items-start">
               <ValidationProvider
-                :name="$t('Phone')"
-                tag="div"
-                class="flex-grow-1"
-                vid="phone"
-                rules="required|numeric|min:10"
-                v-slot="{ errors }"
-                
+                  :name="$t('Phone')"
+                  tag="div"
+                  class="flex-grow-1"
+                  vid="phone"
+                  rules="required|numeric|min:10"
+                  v-slot="{ errors }"
+
               >
                 <b-form-input
-                  type="number"
-                  :placeholder="$t('Phone')"
-                  v-model="form.phone"
-                  required
+                    type="number"
+                    :placeholder="$t('Phone')"
+                    v-model="form.phone"
+                    required
                 />
                 <div class="text-input-error">{{ errors[0] }}</div>
               </ValidationProvider>
               <ValidationProvider
-                :name="$t('country-phone')"
-                tag="div"
-                class="flex-grow-1"
-                vid="phone_code"
-                rules="required"
-                v-slot="{ errors }"
+                  :name="$t('country-phone')"
+                  tag="div"
+                  class="flex-grow-1"
+                  vid="phone_code"
+                  rules="required"
+                  v-slot="{ errors }"
               >
                 <d-select-input
-                  :errors="errors"
-                  type="text"
-                  class="text-flag  rounded-3 align-items-center"
-                  v-model="form.phone_code"
+                    :errors="errors"
+                    type="text"
+                    class="text-flag  rounded-3 align-items-center"
+                    v-model="form.phone_code"
                 >
                   <option
-                    v-for="(it,i) in phone_codes"
-                    :key="i"
-                    :value="it.phone_code"
-                  >{{it.flag}}({{ it.phone_code }})</option>
+                      v-for="(it,i) in phone_codes"
+                      :key="i"
+                      :value="it.phone_code"
+                  >{{ it.flag }}({{ it.phone_code }})
+                  </option>
                 </d-select-input>
               </ValidationProvider>
             </div>
 
             <ValidationProvider
-              vid="password"
-              rules="required"
-              :name="$t('Password')"
-              v-slot="{ errors }"
-              tag="div"
-              class="mb-3"
+                vid="password"
+                rules="required"
+                :name="$t('Password')"
+                v-slot="{ errors }"
+                tag="div"
+                class="mb-3"
             >
               <div class="position-relative">
                 <b-form-input
-                  id="password-field"
-                  :type="show ? 'text' : 'password'"
-                  v-model="form.password"
-                  class="form-control"
-                  :placeholder="$t('Password')"
+                    id="password-field"
+                    :type="show ? 'text' : 'password'"
+                    v-model="form.password"
+                    class="form-control"
+                    :placeholder="$t('Password')"
                 />
                 <span
-                  @click="show = !show"
-                  class="icon-input-end fa-regular position-absolute"
-                  :class="{ 'fa-eye': !show, 'fa-eye-slash': show }"
+                    @click="show = !show"
+                    class="icon-input-end fa-regular position-absolute"
+                    :class="{ 'fa-eye': !show, 'fa-eye-slash': show }"
                 ></span>
               </div>
               <div class="text-input-error">{{ errors[0] }}</div>
@@ -110,25 +111,25 @@
             </ValidationProvider>
 
             <ValidationProvider
-              vid="passwordConfirm"
-              rules="required"
-              :name="$t('Password-confirm')"
-              v-slot="{ errors }"
-              tag="div"
-              class="mb-3"
+                vid="passwordConfirm"
+                rules="required"
+                :name="$t('Password-confirm')"
+                v-slot="{ errors }"
+                tag="div"
+                class="mb-3"
             >
               <div class="position-relative">
                 <b-form-input
-                  id="password-register-confirm"
-                  :type="showC ? 'text' : 'password'"
-                  v-model="form.passwordConfirm"
-                  class="form-control"
-                  :placeholder="$t('Password-confirm')"
+                    id="password-register-confirm"
+                    :type="showC ? 'text' : 'password'"
+                    v-model="form.passwordConfirm"
+                    class="form-control"
+                    :placeholder="$t('Password-confirm')"
                 />
                 <span
-                  @click="showC = !showC"
-                  class="icon-input-end fa-regular position-absolute"
-                  :class="{ 'fa-eye': !showC, 'fa-eye-slash': showC }"
+                    @click="showC = !showC"
+                    class="icon-input-end fa-regular position-absolute"
+                    :class="{ 'fa-eye': !showC, 'fa-eye-slash': showC }"
                 ></span>
               </div>
               <div class="text-input-error">{{ errors[0] }}</div>
@@ -142,17 +143,18 @@
               <p class="py-4">
                 بانشاءك حساب جديد، انت توافق على
                 <router-link
-                  :to="getRouteLocale('terms-and-conditions')"
-                  class="m-c"
-                  target="_blank"
-                >شروط الاستخدام وسياسة الخصوصيه</router-link>
+                    :to="getRouteLocale('terms-and-conditions')"
+                    class="m-c"
+                    target="_blank"
+                >شروط الاستخدام وسياسة الخصوصيه
+                </router-link>
               </p>
             </div>
           </ValidationObserver>
         </div>
       </b-col>
       <b-col xl="5" class="tablet-hide">
-        <b-img class="main-img" :src="`${publicPath}assets/svg/riadiat-green-card.svg`" />
+        <b-img class="main-img" :src="`${publicPath}assets/svg/riadiat-green-card.svg`"/>
       </b-col>
     </b-row>
   </div>
@@ -165,7 +167,7 @@ export default {
     show: false,
     showC: false,
     phone_codes: [],
-    errorPasswordConfirm:null,
+    errorPasswordConfirm: null,
     form: {
       email: process.env.EMAIL || "",
       password: process.env.PASSWORD || "",
@@ -180,11 +182,11 @@ export default {
   methods: {
     async loadCountries() {
       try {
-        let { data } = await commonAPI.getListCountries();
+        let {data} = await commonAPI.getListCountries();
         if (!data.message) {
           let countries = data;
           this.phone_codes = window.DHelper.convertCountriesToPhoneCodes(
-            countries
+              countries
           );
         }
       } catch (error) {
@@ -201,7 +203,7 @@ export default {
         return;
       }
       try {
-        let { data } = await this.$axios.post("user/auth/register", this.form);
+        let {data} = await this.$axios.post("user/auth/register", this.form);
         if (data.success) {
           window.successMsg(data.message);
           let info = {
@@ -229,15 +231,21 @@ export default {
       }
     }
   },
-    watch: {
-         'form.passwordConfirm': function (val) {
-           console.log(val);
-           if (val != this.form.password) {
-                return this.errorPasswordConfirm = "كلمة السر غير مطابقة"  
-           }
-            return  this.errorPasswordConfirm=null
-         }
-        },
+  watch: {
+    'form.passwordConfirm': function (val) {
+      console.log(val);
+      if (val != this.form.password) {
+        return this.errorPasswordConfirm = "كلمة السر غير مطابقة"
+      }
+      return this.errorPasswordConfirm = null
+    },
+    'form.password': function (val) {
+      if (val != this.form.passwordConfirm) {
+        return this.errorPasswordConfirm = "كلمة السر غير مطابقة"
+      }
+      return this.errorPasswordConfirm = null
+    }
+  },
   mounted() {
     this.loadCountries();
   }
@@ -247,9 +255,11 @@ export default {
 .login-form {
   padding: 2em;
 }
+
 .no-have-account {
   text-align: end;
 }
+
 .form {
   padding-top: 2.5rem;
 }
@@ -263,9 +273,11 @@ html[lang="en"] .icon-input-end {
   left: auto;
   right: 15px;
 }
+
 .form-control {
   padding: 0.5rem 0.75rem;
 }
+
 .icon-input-end {
   color: #cdd7d8;
   font-size: 23px;
@@ -276,6 +288,7 @@ html[lang="en"] .icon-input-end {
   left: auto;
   right: 15px;
 }
+
 .flag-icon {
   height: 1.5em;
   width: 1.5em;
@@ -285,6 +298,7 @@ html[lang="en"] .icon-input-end {
   display: inline-block;
   vertical-align: middle;
 }
+
 .main-img {
   display: block;
   width: 100%;
