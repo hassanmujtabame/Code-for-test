fdbc04fb39799f6edf3eb71eafd655d7e31b5623
Ëman El-Swerky <eman.swerky@varit.net>
2023-09-04 18:09:54 +0300

    edit

--- src/views/academy/courses/show/guest/sections/section-desc/index.vue
+++ src/views/academy/courses/show/guest/sections/section-desc/index.vue
@@ -17,10 +17,16 @@
     <div v-if="itemPage.type=='on-site'" class="course-desc_info mt-3">
       <div><i class="far fa-clock"></i> {{ itemPage.number_day??'N/A' }}</div>
       <div><i class="far fa-calendar-days"></i> {{ itemPage.start_date }}</div>
-      <div><i class="fa fa-location-dot"></i> <a class="text-underline" target="_blank" :href="itemPage.place_map??'!#'" @click="(evt)=>{
-        
+      <div><i class="fa fa-location-dot"></i> 
+      <!-- <a class="text-underline" target="_blank" :href="itemPage.place_map??'!#'" @click="(evt)=>{
+          
         if(!this.itemPage.place_map) evt.preventDefault();
-      }">{{ itemPage.place_name??$t('Course-headquarters') }}</a></div>
+      }">{{ itemPage.place_name??$t('Course-headquarters') }}</a> -->
+          <a v-if="itemPage && itemPage.map_address !=null"  :href="`https://www.google.com/maps/search/${itemPage.map_address.lat},${itemPage.map_address.lng}`" target="_blank">
+          {{ itemPage.place_name??$t('Course-headquarters') }}
+          </a>
+      
+      </div>
     </div>
     </div>
   </div>
