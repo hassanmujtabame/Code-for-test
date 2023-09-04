4d1bc4e7b6499e82fb4b817093ddc3596a01cc81
Ëman El-Swerky <eman.swerky@varit.net>
2023-09-04 14:57:49 +0300

    add sharing modal to course

--- src/views/academy/courses/show/live/sections/section-header-owner/actions-page.vue
+++ src/views/academy/courses/show/live/sections/section-header-owner/actions-page.vue
@@ -20,7 +20,7 @@
                         </button>
                     </div>
                     <div>
-                        <button  class="btn-main btn-action-page btn-warning  px-3 w-100 border-0 rounded-2" role="button">
+                        <button v-b-modal="'my-modal'" class="btn-main btn-action-page btn-warning  px-3 w-100 border-0 rounded-2" role="button">
                             <i class="fa-solid fa-share-nodes"></i>
                                 شارك
                         </button>
@@ -31,6 +31,56 @@
                          حذف
                         </button>
                     </div>
+                      <!-- The modal -->
+  <b-modal id="my-modal" :hide-header='true' :hide-footer='true'>
+      <h5 style="color:#ebae05;" class="py-3"> شارك الدورة على مواقع التواصل الاجتماعي</h5>
+            <img style="    display: flex; margin: auto;" :src="`${publicPath}assets/img/Group 1171276011.png`" >
+         <div class="d-flex justify-content-center  gap-4  p-4  p-4 icon-social-exibition mt-4 ">
+                   <button style="background: transparent; border: 0;">
+                      <ShareNetwork
+                          network="twitter"
+                          :url="shareLink"
+                          title="Share in twitter"
+                          description="This is another awesome article for awesome readers"
+                          >
+                          <img class="h-100" :src="`${publicPath}assets/img/Twitter.png`" alt="" />
+                          <!-- twitter-user="LindaOjo_" -->
+                      </ShareNetwork>
+                  </button>
+                    <!-- <button>
+                      <ShareNetwork
+                          network="Instagram"
+                          :url="shareLink"
+                          title="Share in instagram"
+                          description="This is another awesome article for awesome readers"
+                          >
+                          <img class="h-100" :src="`${publicPath}assets/img/Instagram.png`" alt="" />
+                      </ShareNetwork>
+                  </button> -->
+                    <button style="background: transparent; border: 0;">
+                      <ShareNetwork
+                          network="Linkedin"
+                          :url="shareLink"
+                          title="Share in Linkedin"
+                          description="This is another awesome article for awesome readers"
+                          >
+                        <img class="h-100" :src="`${publicPath}assets/img/Linkedin.png`" alt="" />
+                      </ShareNetwork>
+                  </button>
+                  <button style="background: transparent; border: 0;">
+                    <ShareNetwork
+                        network="Facebook"
+                        :url="shareLink"
+                        title="Share in facebook"
+                        description="This is another awesome article for awesome readers"
+                        >
+                        <img class="h-100" :src="`${publicPath}assets/img/Facebook.png`" alt="" />
+                    </ShareNetwork>
+                </button>
+            <div class="fb-share-button" :data-href="currentUrl"  data-layout="button_count">
+              </div>
+            </div>
+  </b-modal>
                 </div>
 </template>
 
@@ -41,6 +91,11 @@ export default {
  props:{
     itemPage:{}
  },
+    data() {
+      return {
+      shareLink:''
+      }
+    },
 watch:{
     itemPage:{
         deep:true,
@@ -123,7 +178,10 @@ methods:{
                 
       
     },
-}
+},
+  mounted() {
+    this.shareLink = window.location.href;
+  },
 }
 </script>
 
