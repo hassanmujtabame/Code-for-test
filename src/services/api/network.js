34346b55c231919a156e9e8e9121cae85a828421
Ëman El-Swerky <eman.swerky@varit.net>
2023-09-09 07:29:51 +0300

    edit

--- src/services/api/network.js
+++ src/services/api/network.js
@@ -66,6 +66,7 @@ class NetworkApi extends BaseApi {
 		return window.axios.post(`checkout`, data);
 	}
 	PayPackageSelect(data) {
+	window.axios.defaults.baseURL = "https://cp.riadiat.sa/";
 		// return window.axios.post(`pay`, data);
 		return window.axios.post(`payments/hyperbill/callback`, data);
 	}
