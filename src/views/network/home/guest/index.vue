ea28752856e207ac715426c0b08e2e750e946033
mohamedkhaled2000 <mohhaaaak4@gmail.com>
2023-09-17 00:26:12 +0300

    update styles

--- src/views/network/home/guest/index.vue
+++ src/views/network/home/guest/index.vue
@@ -1,51 +1,51 @@
 <template>
-   <div style="" class="consult">
-        <!-- section header -->
-        <SectionHeader />
-        <!-- #section Header -->
-        <!-- section 2-->
-        <div class="container mt-5">
-    <!-- شبكة رياديات: وسع دائرة معارفك-->
-            <SectionGrowYourNetwork />
-            <!--section network's goals-->
-            <SectionNetworkGoals />
-            <!--معسكر ريادة الاعمال-->
-            <SectionCampLeadership v-if="false"/>
-            </div>
-            <div class=" mt-5">
-        <SectionAds department="network" url="network/home-banners" />
-        </div>
-        
-        <!-- #section 2-->
-         <!-- section 3-->
-         <SectionThree/>
-        <!-- #section 3-->
-        <!-- section 4-->
-        <SectionMembership />
-        <SectionSchedules />
-        <!-- #section 4-->
-        <!-- section 5-->
-        <SectionTopMember />
-        <!-- #section 5-->
-        <!-- section subscribe-->
-        <SectionPartners />
-        <!-- #section subscribe-->
-          <!-- section join us-->
-          <SectionJoin />
-        <!-- #section join us-->
-       <!-- section numbers in network-->
-       <SectionNumbers />
-        <!-- #section numbers in network-->
-       <!-- section influenceurs -->
-       <SectioninFluenceurs />
-       <!-- #section influenceurs -->
-     <!-- section continue learning -->
-     <SectionContinueLearning />
-       <!-- #section continue learning -->
-      <!-- section hear about us -->
-      <SectionHear />
-       <!-- #section hear about us -->
-</div>
+  <div style="" class="consult">
+    <!-- section header -->
+    <SectionHeader />
+    <!-- #section Header -->
+    <!-- section 2-->
+    <div class="container mt-5">
+      <!-- شبكة رياديات: وسع دائرة معارفك-->
+      <SectionGrowYourNetwork />
+      <!--section network's goals-->
+      <SectionNetworkGoals />
+      <!--معسكر ريادة الاعمال-->
+      <SectionCampLeadership v-if="false" />
+    </div>
+    <div class=" mt-5">
+      <SectionAds department="network" url="network/home-banners" />
+    </div>
+
+    <!-- #section 2-->
+    <!-- section 3-->
+    <SectionThree />
+    <!-- #section 3-->
+    <!-- section 4-->
+    <SectionMembership />
+    <SectionSchedules />
+    <!-- #section 4-->
+    <!-- section 5-->
+    <SectionTopMember />
+    <!-- #section 5-->
+    <!-- section subscribe-->
+    <SectionPartners />
+    <!-- #section subscribe-->
+    <!-- section join us-->
+    <SectionJoin />
+    <!-- #section join us-->
+    <!-- section numbers in network-->
+    <SectionNumbers />
+    <!-- #section numbers in network-->
+    <!-- section influenceurs -->
+    <SectioninFluenceurs />
+    <!-- #section influenceurs -->
+    <!-- section continue learning -->
+    <SectionContinueLearning />
+    <!-- #section continue learning -->
+    <!-- section hear about us -->
+    <SectionHear />
+    <!-- #section hear about us -->
+  </div>
 </template>
 <script>
 import SectionHeader from './parts/section-header/index.vue';
@@ -64,8 +64,8 @@ import SectionNetworkGoals from './parts/section-network-goals/index.vue'
 import SectionCampLeadership from './parts/cam-leadership/index.vue'
 import SectionGrowYourNetwork from './parts/grow-your-network/index.vue'
 export default {
-name:'IndexPageGuest',
-components:{
+  name: 'IndexPageGuest',
+  components: {
     SectionHeader,
 
     SectionThree,
@@ -80,25 +80,25 @@ components:{
     SectionHear,
     SectionAds,
     SectionGrowYourNetwork,
-  SectionNetworkGoals,
-  SectionCampLeadership
-},
-methods:{
-        loadJS(src,async=true,defer=false){
-          const plugin = document.createElement("script");
-          plugin.setAttribute(
-          "src",
-          src
-        );
-        if(defer)
-        plugin.setAttribute("defer",true)
-        plugin.async = async;
-        document.body.appendChild(plugin);
-        }
-      },
-created(){
-    
-}
+    SectionNetworkGoals,
+    SectionCampLeadership
+  },
+  methods: {
+    loadJS(src, async = true, defer = false) {
+      const plugin = document.createElement("script");
+      plugin.setAttribute(
+        "src",
+        src
+      );
+      if (defer)
+        plugin.setAttribute("defer", true)
+      plugin.async = async;
+      document.body.appendChild(plugin);
+    }
+  },
+  created() {
+
+  }
 }
 </script>
 
