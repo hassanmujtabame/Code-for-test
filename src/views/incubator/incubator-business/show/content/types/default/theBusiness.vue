2fd7c302133178c87ac221ce4ddc93b9d047119f
ahmed-tahoon <77838959+ahmed-tahoon@users.noreply.github.com>
2023-10-23 16:49:59 +0300

    incupart2

---
+++ src/views/incubator/incubator-business/show/content/types/default/theBusiness.vue
@@ -0,0 +1,144 @@
+<template>
+  <div id="business-main-content" class="container">
+    <h1 class="mt-3">
+      <span class="" style="color: #1fb9b3">نموذج : </span> العمل التجاري
+    </h1>
+    <div class="col-12 mt-5">
+      <table>
+        <thead>
+          <tr>
+            <th>شرائح العملاء</th>
+            <th>العلاقات مع العملاء</th>
+            <th>القيم المقترحة </th>
+            <th>الانشطة المقترحة</th>
+            <th>الشراكات الرئيسية</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr style="height: 150px !important;">
+            <td rowspan="3"><input /></td>
+            <td><input /></td>
+            <td rowspan="3"><input /></td>
+            <td><input /></td>
+            <td rowspan="3"><input /></td>
+          </tr>
+          <tr style="background: rgba(255, 242, 205, 1);"  >
+            <!-- <td><input /></td> -->
+            <td>القنوات</td>
+            <!-- <td><input /></td> -->
+            <td>الموارد الرئيسية</td>
+            <!-- <td><input /></td> -->
+          </tr>
+          <tr style="height: 150px !important;">
+            <!-- <td><input /></td> -->
+            <!-- <td><input /></td> -->
+            <!-- <td><input /></td> -->
+            <td><input /></td>
+            <td><input /></td>
+          </tr>
+
+          <tr style="height: 100px !important;">
+            <td class="text-end p-3 special_td" colspan="2"><small> مصادر الايرادات :</small> <input /></td>
+            <td class="text-end p-3 special_td" colspan="3"><small> هيكل التكاليف :</small> <input /></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <button @click="createPDF" class="btn-custmer mt-5">{{ $t("save") }}</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    createPDF() {
+      var sTable = document.getElementById('business-main-content').innerHTML;
+      // add all the css and styling for the table 
+      var style = "<style>";
+      style = style + "table {width: 100%;font: 17px Calibri;}";
+      style = style + "table, th, td {border: solid 1px #000; border-collapse: collapse;";
+      style = style + "padding: 2px 3px;text-align: center;}";
+      style = style + "</style>";
+
+      // here we creat our window object
+      var win = window.open('', '', 'height=700,width=700');
+
+      win.document.write('<html><head>');
+      win.document.write('<title>my model</title>');   // title or name for pdf
+      win.document.write(style);          // add our css.
+      win.document.write('</head>');
+      win.document.write('<body>');
+      win.document.write(sTable);         // table content. 
+      win.document.write('</body></html>');
+
+      win.document.close();   // close the window.
+
+      win.print();    // print the table.
+    }
+  },
+};
+</script>
+
+<style scoped>
+.d-flex .col-3 img {
+  width: 200px;
+}
+
+label {
+  width: 100%;
+  text-align: start;
+}
+
+.img-zone {
+  max-width: 430px;
+}
+
+p {
+  font-size: 20px;
+}
+
+th,
+td {
+  height: 50px;
+}
+
+th,
+td,
+table {
+  border: 1px solid rgba(200, 204, 204, 1);
+  text-align: center;
+}
+
+th, .special_td {
+  font-weight: 200;
+  padding: 10px 15px;
+  background: #E3F1F1;
+  color: #0F7774;
+  border-top: 0px !important;
+  border-right: 1px solid rgba(31, 185, 179, 0.5) !important;
+  border-left: 1px solid rgba(31, 185, 179, 0.5) !important;
+}
+
+table {
+  border-right: 1px solid rgba(31, 185, 179, 0.5) !important;
+  border-left: 1px solid rgba(31, 185, 179, 0.5) !important;
+  border-top: 0px !important;
+  border-radius: 25px;
+
+}
+.special_td input {
+  background: transparent;
+}
+input {
+  outline: none;
+}
+input:focus {
+  outline: none;
+}
+td input {
+  width: 100%;
+  height: 100%;
+  border: 0;
+}
+</style>
---
+++ src/views/incubator/incubator-business/show/content/types/default/theNotifications.vue
@@ -0,0 +1,218 @@
+<template>
+  <div id="notifications-main-content" class="container">
+    <h1>
+      <span class="" style="color: #1fb9b3">نموذج : </span> العلامة التجارية (الشعار)
+    </h1>
+    <div class="mt-3">
+      <p> ما هو اسم مشروعك أو شركتك ؟</p>
+      <input class="form-control py-3 px-5 fs-r-12" type="text" />
+    </div>
+
+    <div class="mt-3">
+      <p>ما هي القصة وراء اختيار هذا الاسم؟ </p>
+      <input class="form-control py-3 px-5 fs-r-12" type="text" />
+    </div>
+    <div class="mt-3 col-12">
+      <p>أوصف لنا فكرة شركتك أو مشروعك وصفًا تفصيليًا قدر الإمكان حتى يساعدنا على الوصول إلى الشعار المناسب؟</p>
+      <input class="form-control py-3 px-5 fs-r-12" type="text" />
+    </div>
+    <div class="mt-3">
+      <p> من هي الفئة المستهدفة؟</p>
+      <input class="form-control py-3 px-5 fs-r-12" type="text" />
+    </div>
+
+    <div class="mt-3">
+      <p>ما هو شكل ونوع الشعار الذي تفضله؟</p>
+      <input class="form-control py-3 px-5 fs-r-12" type="text" />
+    </div>
+    <div class="mt-3">
+      <p class="t-c">
+        (ملحوظة : لا يجب أن يكون الشعار يرمز للمنتج أو المشروع فأمثلة على ذلك شعار apple فهو لا يرمز على انه يقدم المنتجات
+        الإلكترونية , الأهم من أن الشعار يعني أو يقصد به منتج معين , أن يكون بسيط و سهل التذكر و يخلق دائمة الجدل حوله)
+      </p>
+    </div>
+
+    <div class="mt-4 col-12">
+      <div class="d-flex justify-content-center flex-wrap">
+        <div class="col-3 text-center mt-3">
+          <p>الشعارات الحرفية</p>
+          <img src="@/assets/img/logos_1.png">
+        </div>
+
+        <div class="col-3 text-center mt-3">
+          <p>الشعارات النصية</p>
+          <img src="@/assets/img/logos_2.png">
+        </div>
+
+        <div class="col-3 text-center mt-3">
+          <p>الشعارات الايقونية</p>
+          <img src="@/assets/img/logos_3.png">
+        </div>
+
+        <div class="col-3 text-center mt-3">
+          <p> الشعارات التجريدية</p>
+          <img src="@/assets/img/logos_4.png">
+        </div>
+
+        <div class="col-3 text-center mt-3">
+          <p> الشعارات المزيج </p>
+          <img src="@/assets/img/logos_5.png">
+        </div>
+
+        <div class="col-3 text-center mt-3">
+          <p> شعار ايقونة نصي</p>
+          <img src="@/assets/img/logos_6.png">
+        </div>
+      </div>
+
+      <div class="mt-3">
+        <p>هل لديك وصفًا مختصرًا لشركتك تودين وضعه أسفل الشعار؟</p>
+        <input class="form-control py-3 px-5 fs-r-12" type="text" />
+      </div>
+      <div class="mt-3 col-12">
+        <p>من هي الشركات المنافسة أو المشابهة بالعمل سواء كانت أجنبية أم عربية؟</p>
+        <input class="form-control py-3 px-5 fs-r-12" type="text" />
+      </div>
+      <div class="mt-3">
+        <p> ما هي رسالتك الرئيسية التي تريد إيصالها لعملائك من خلال شعارك؟</p>
+        <input class="form-control py-3 px-5 fs-r-12" type="text" />
+      </div>
+
+      <div class="mt-3">
+        <p>هل تفضل ألوان معينة أم تترك اختيار الألوان للمصمم؟</p>
+        <input class="form-control py-3 px-5 fs-r-12" type="text" />
+      </div>
+      <div class="mt-3">
+        <p class="t-c">
+          (ملحوظة : هذه بعض الألوان التي تساعدك على اختيار ألوان شعار , أشعر باللون قبل اختياره )
+        </p>
+      </div>
+      <div class="mt-5 mb-5">
+        <div class="colors_img">
+          <img src="@/assets/img/colors_image.png" alt="">
+        </div>
+      </div>
+
+      <div class="mt-3">
+        <p>
+          هل لديك فكرة معينة تود تنفيذها في الشعار/اصنع شعارك ؟
+        </p>
+        <input class="form-control py-3 px-5 fs-r-12" type="text" />
+      </div>
+      <div class="mt-3">
+        <p>
+          يرجى إرسال صور لشعارات تعجبك من نفس مجالك (لا تقل عن 4 صور)  وتريد أن نستلهم منها فكرة الشعار. وضع ملحوظات ع
+          الشعارات اذا امكن .
+        </p>
+        <input class="form-control py-3 px-5 fs-r-12" type="text" />
+      </div>
+      <div class="mt-3">
+        <p>
+          يرجى إرسال صور لشعارات تعجبك من مجالات مختلفة (لا يقل عن 4 صور) وتريد أن يكون شعارك بنفس الإستايل الخاص بها.
+        </p>
+        <input class="form-control py-3 px-5 fs-r-12" type="text" />
+      </div>
+      <div class="mt-3">
+        <p>
+          ماهي الصفات الرئيسية التي تريد جمهورك أن يفكر فيها عندما يرى علامتك التجارية؟
+        </p>
+        <input class="form-control py-3 px-5 fs-r-12" type="text" />
+      </div>
+      <div class="mt-3">
+        <p>
+          ما هي القيم الأساسية الثلاث لعلامتك التجارية؟
+        </p>
+        <input class="form-control py-3 px-5 fs-r-12" type="text" />
+      </div>
+      <button @click="createPDF" class="btn-custmer mt-5">{{ $t("save") }}</button>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    createPDF() {
+      var sTable = document.getElementById('notifications-main-content').innerHTML;
+      // add all the css and styling for the table 
+      var style = "<style>";
+      style = style + "table {width: 100%;font: 17px Calibri;}";
+      style = style + "table, th, td {border: solid 1px #000; border-collapse: collapse;";
+      style = style + "padding: 2px 3px;text-align: center;}";
+      style = style + "</style>";
+
+      // here we creat our window object
+      var win = window.open('', '', 'height=700,width=700');
+
+      win.document.write('<html><head>');
+      win.document.write('<title>my model</title>');   // title or name for pdf
+      win.document.write(style);          // add our css.
+      win.document.write('</head>');
+      win.document.write('<body>');
+      win.document.write(sTable);         // table content. 
+      win.document.write('</body></html>');
+
+      win.document.close();   // close the window.
+
+      win.print();    // print the table.
+    }
+  },
+};
+</script>
+
+<style scoped>
+.d-flex .col-3 img {
+  width: 200px;
+}
+
+label {
+  width: 100%;
+  text-align: start;
+}
+
+.img-zone {
+  max-width: 430px;
+}
+
+p {
+  font-size: 20px;
+}
+
+th,
+td {
+  height: 50px;
+}
+
+th,
+td,
+table {
+  border: 1px solid rgba(200, 204, 204, 1);
+  text-align: center;
+}
+
+th {
+  font-weight: 200;
+  padding: 10px 15px;
+  background: #E3F1F1;
+  color: #0F7774;
+  border-top: 0px !important;
+  border-right: 1px solid rgba(31, 185, 179, 0.5) !important;
+  border-left: 1px solid rgba(31, 185, 179, 0.5) !important;
+}
+
+table {
+  border-right: 1px solid rgba(31, 185, 179, 0.5) !important;
+  border-left: 1px solid rgba(31, 185, 179, 0.5) !important;
+  border-top: 0px !important;
+  border-radius: 25px;
+
+}
+
+td input {
+  width: 100%;
+  height: 100%;
+  border: 0;
+}
+</style>
