17d7934ebf46b2fc2efb94bf3d1a937035eec883
hamdy elgazar <aljzarh46@gmail.com>
2023-11-16 16:55:15 +0200

    affiliate-dashboard-summary

--- src/views/affiliate-marketing/main/summary/index.vue
+++ src/views/affiliate-marketing/main/summary/index.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1 style="margin: 200px;">الملخص</h1>
+    <div style="margin-top:200px">
+        <div class="container dashbord">
+            <div class="mt-5">
+                <div class="row">
+                    <h1>
+                        أخر الانجازات
+                    </h1>
+
+                </div>
+                <SectionBalance />
+            </div>
+            <div class="mt-5">
+                <line-chart></line-chart>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+import SectionBalance from './parts/section-balance/index.vue'
+import LineChart from './parts/section-balance/BarChart.vue'
+export default {
+    name: 'affiliate-marketing-dashboard',
+    components: {
+        SectionBalance,
+        LineChart,
+    }
+}
+</script>
+
+<style></style>
