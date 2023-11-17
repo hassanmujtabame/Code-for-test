2f340b1fc8e23f7e8ef5703ed32ff7093edf2bea
hamdy elgazar <aljzarh46@gmail.com>
2023-11-17 20:14:58 +0200

    affiliate-summary

--- src/views/affiliate-marketing/main/summary/index.vue
+++ src/views/affiliate-marketing/main/summary/index.vue
@@ -3,9 +3,19 @@
         <div class="dashbord">
             <div>
                 <div class="row">
-                    <h1>
-                        أخر الانجازات
+                    <h1 class="col-2">
+                        الملخص
                     </h1>
+                    <div class="col-10 d-flex" style="height: 35px; gap: 4%">
+                        <!-- <input type="text" style="width: 32%">
+                        <input type="text" style="width: 32%"> -->
+                        <date-pick :months="months" nextMonthCaption="" prevMonthCaption="" v-model="date"></date-pick>
+
+                        <button class="btn btn-primary" style="width: 32%">
+                            فلتره
+                        </button>
+
+                    </div>
 
                 </div>
                 <SectionBalance />
@@ -18,6 +28,11 @@
 </template>
 
 <script>
+
+import DatePick from 'vue-date-pick';
+import 'vue-date-pick/dist/vueDatePick.css';
+
+
 import SectionBalance from './parts/section-balance/index.vue'
 import LineChart from './parts/section-balance/BarChart.vue'
 export default {
@@ -25,8 +40,57 @@ export default {
     components: {
         SectionBalance,
         LineChart,
-    }
+        DatePick
+    },
+    data: () => ({
+        date: '2019-01-01',
+        months: [
+            "يناير",
+            "فبراير",
+            "مارس",
+            "أبريل",
+            "مايو",
+            "يونيو",
+            "يوليو",
+            "أغسطس",
+            "سبتمبر",
+            "أكتوبر",
+            "نوفمبر",
+            "ديسمبر"
+        ],
+        // weekdays: [
+        //     "الأحد",
+        //     "الإثنين",
+        //     "الثلاثاء",
+        //     "الأربعاء",
+        //     "الخميس",
+        //     "الجمعة",
+        //     "السبت"
+        // ]
+    })
 }
 </script>
 
-<style></style>
+<style>
+.vdpArrowPrev:after {
+    border-right-color: #2db7b3;
+}
+
+.vdpArrowNext:after {
+    border-left-color: #2db7b3;
+}
+
+.vdpCell.selectable:hover .vdpCellContent,
+.vdpCell.selected .vdpCellContent {
+    background: #2db7b3;
+}
+
+.vdpCell.today {
+    color: #2db7b3;
+}
+
+.vdpTimeUnit>input:hover,
+.vdpTimeUnit>input:focus {
+    border-bottom-color: #2db7b3;
+}
+</style>
