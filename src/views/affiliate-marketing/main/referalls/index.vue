1a694a0e2a9bb5733c275875fa1cb0fdbcaf979e
hamdy elgazar <aljzarh46@gmail.com>
2023-11-26 08:42:53 +0200

    referalls && visits

--- src/views/affiliate-marketing/main/referalls/index.vue
+++ src/views/affiliate-marketing/main/referalls/index.vue
@@ -1,5 +1,145 @@
 <template>
-    <h1 style="margin: 200px;">
-        الاحالات
-    </h1>
+    <div>
+        <div class="dashbord">
+            <div>
+                <div class="row justify-content-between">
+                    <h1 class="col-2">
+                        الاحالات
+                    </h1>
+                    <div class="col-10 d-flex justify-content-end" style="height: 35px; gap: 4%">
+                        <!-- <input type="text" style="width: 32%">
+                        <input type="text" style="width: 32%"> -->
+                        <date-pick :months="months" nextMonthCaption="" prevMonthCaption=""
+                            v-model="start_date"></date-pick>
+                        <date-pick :months="months" nextMonthCaption="" prevMonthCaption="" v-model="end_date"></date-pick>
+
+                        <button class="text-white border-0"
+                            style="border-radius : 5px; box-shadow: 4px 4px 7px 1px rgba(0, 0, 0, 0.125); padding:10px; height:auto; margin-right: 5px; display: flex; justify-content: center; align-items: center; width: 25%; background-color: #2DB7B3">
+                            فلتره
+                        </button>
+
+                    </div>
+
+                </div>
+                <div style="padding: 20px; border: 1px solid #2DB7B3">
+
+                    <SectionTable />
+                </div>
+                
+            </div>
+
+        </div>
+    </div>
 </template>
+
+<script>
+
+import DatePick from 'vue-date-pick';
+import 'vue-date-pick/dist/vueDatePick.css';
+
+
+import SectionTable from './parts/section-table/index.vue'
+
+export default {
+    name: 'affiliate-marketing-referalls',
+    components: {
+        SectionTable,
+        DatePick
+    },
+    data: () => ({
+        chartDataVisits: {
+            labels: ['1 jun', '2 jun', '3 jun', '4 jun', '5 jun', '6 jun', '7 jun', '8 jun', '9 jun', '10 jun',],
+            datasets:
+                [
+                    {
+                        label: 'الزيارات',
+                        backgroundColor: '#2DB7B3',
+                        // borderColor: 'rgba(75, 192, 192, 1)',
+                        // borderWidth: 1, 
+                        data: [40, 20, 12, 40, 20, 12, 30, 40, 20, 12]
+                    }]
+        },
+        chartOptionsVisits: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'bottom',
+                    labels: {
+                        font: {
+                            size: 32
+                        }
+                    }
+                },
+                tooltip: {
+                    enabled: true
+                }
+            },
+        },
+        chartDataRefers: {
+            labels: ['1 jun', '2 jun', '3 jun', '4 jun', '5 jun', '6 jun', '7 jun', '8 jun', '9 jun', '10 jun',],
+            datasets:
+                [
+                    {
+                        label: 'الاحالات',
+                        backgroundColor: '#2DB7B3',
+                        // borderColor: 'rgba(75, 192, 192, 1)',
+                        // borderWidth: 1, 
+                        data: [20, 30, 12, 10, 5, 30, 32, 28, 18, 17]
+                    }]
+        },
+        chartOptionsRefers: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'bottom',
+                    labels: {
+                        font: {
+                            size: 32
+                        }
+                    }
+                },
+                tooltip: {
+                    enabled: true
+                }
+            },
+        },
+        start_date: '2023-01-01',
+        end_date: '2024-01-01',
+        months: [
+            "يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"
+        ],
+    })
+}
+</script>
+
+<style>
+.vdpArrowPrev:after {
+    border-right-color: #2db7b3;
+}
+
+.vdpArrowNext:after {
+    border-left-color: #2db7b3;
+}
+
+.vdpCell.selectable:hover .vdpCellContent,
+.vdpCell.selected .vdpCellContent {
+    background: #2db7b3;
+}
+
+.vdpCell.today {
+    color: #2db7b3;
+}
+
+.vdpTimeUnit>input:hover,
+.vdpTimeUnit>input:focus {
+    border-bottom-color: #2db7b3;
+}
+
+.vdpComponent.vdpWithInput>input {
+    height: 100%;
+    background: #eee;
+    border-radius: 5px;
+    border-color: #979797;
+    box-shadow: 4px 4px 7px 1px rgba(0, 0, 0, 0.125);
+}
+</style>
---
+++ src/views/affiliate-marketing/main/visits/parts/section-table/index.vue
@@ -0,0 +1,101 @@
+<template>
+    <table class="styled-table">
+        <thead>
+          <tr>
+            <th>التاريخ و الوقت</th>
+            <th>كود الزائر</th>
+            <th>رابط الزياره</th>
+            <th>عدد الزيارات</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jun 21,2023 10:30 am</td>
+            <td>127.0.0.1</td>
+            <td>riadiat.sa/ar/network/4325</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>Jun 21,2023 10:30 am</td>
+            <td>127.0.0.1</td>
+            <td>riadiat.sa/ar/network/4325</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>Jun 21,2023 10:30 am</td>
+            <td>127.0.0.1</td>
+            <td>riadiat.sa/ar/network/4325</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>Jun 21,2023 10:30 am</td>
+            <td>127.0.0.1</td>
+            <td>riadiat.sa/ar/network/4325</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>Jun 21,2023 10:30 am</td>
+            <td>127.0.0.1</td>
+            <td>riadiat.sa/ar/network/4325</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>Jun 21,2023 10:30 am</td>
+            <td>127.0.0.1</td>
+            <td>riadiat.sa/ar/network/4325</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>Jun 21,2023 10:30 am</td>
+            <td>127.0.0.1</td>
+            <td>riadiat.sa/ar/network/4325</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>Jun 21,2023 10:30 am</td>
+            <td>127.0.0.1</td>
+            <td>riadiat.sa/ar/network/4325</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>Jun 21,2023 10:30 am</td>
+            <td>127.0.0.1</td>
+            <td>riadiat.sa/ar/network/4325</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>Jun 21,2023 10:30 am</td>
+            <td>127.0.0.1</td>
+            <td>riadiat.sa/ar/network/4325</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+</template>
+
+<style scoped>
+.styled-table {
+    width: 100%;
+    font-family: Arial, sans-serif;
+  }
+  
+  .styled-table thead th {
+    color: #2DB7B3;
+    font-weight: bold;
+    text-align: center;
+    padding: 30px 12px;
+  }
+  
+  .styled-table tbody td {
+    text-align: center;
+    padding: 16px 8px;
+  }
+
+  thead, tbody, tfoot, tr, td, th {
+    border-color: #777;
+    border-style: solid;
+    border-width: 1px;
+    border-right-color: transparent;
+    border-left-color: transparent;
+}
+</style>
