8e2636e3ddda57389901b973e664913aa80942c7
hamdy elgazar <aljzarh46@gmail.com>
2023-11-26 09:43:40 +0200

    deals-page

---
+++ src/views/affiliate-marketing/main/deals/parts/section-table/index.vue
@@ -0,0 +1,149 @@
+<template>
+  <table class="styled-table">
+    <thead>
+      <tr>
+        <th>التاريخ و الوقت</th>
+        <th>حاله الطلب</th>
+        <th>بيانات الحساب </th>
+        <th>نوع الحساب</th>
+        <th>قيمه الطلب</th>
+        <th>تاريخ الطلب</th>
+        <th>رقم الطلب</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Jun 21,2023 10:30 am</td>
+        <td>تم الدفع</td>
+        <td>782343899</td>
+        <td>حساب بنكى</td>
+        <td>40 $</td>
+        <td>Jun 23, 2023 12:19 pm</td>
+        <td>4</td>
+      </tr>
+      <tr>
+        <td>Jun 21,2023 10:30 am</td>
+        <td>تم الدفع</td>
+        <td>782343899</td>
+        <td>حساب بنكى</td>
+        <td>40 $</td>
+        <td>Jun 23, 2023 12:19 pm</td>
+        <td>4</td>
+      </tr>
+      <tr>
+        <td>Jun 21,2023 10:30 am</td>
+        <td>تم الدفع</td>
+        <td>782343899</td>
+        <td>حساب بنكى</td>
+        <td>40 $</td>
+        <td>Jun 23, 2023 12:19 pm</td>
+        <td>4</td>
+      </tr>
+      <tr>
+        <td>Jun 21,2023 10:30 am</td>
+        <td>تم الدفع</td>
+        <td>782343899</td>
+        <td>حساب بنكى</td>
+        <td>40 $</td>
+        <td>Jun 23, 2023 12:19 pm</td>
+        <td>4</td>
+      </tr>
+      <tr>
+        <td>Jun 21,2023 10:30 am</td>
+        <td>تم الدفع</td>
+        <td>782343899</td>
+        <td>حساب بنكى</td>
+        <td>40 $</td>
+        <td>Jun 23, 2023 12:19 pm</td>
+        <td>4</td>
+      </tr>
+      <tr>
+        <td>Jun 21,2023 10:30 am</td>
+        <td>تم الدفع</td>
+        <td>782343899</td>
+        <td>حساب بنكى</td>
+        <td>40 $</td>
+        <td>Jun 23, 2023 12:19 pm</td>
+        <td>4</td>
+      </tr>
+      <tr>
+        <td>Jun 21,2023 10:30 am</td>
+        <td>تم الدفع</td>
+        <td>782343899</td>
+        <td>حساب بنكى</td>
+        <td>40 $</td>
+        <td>Jun 23, 2023 12:19 pm</td>
+        <td>4</td>
+      </tr>
+      <tr>
+        <td>Jun 21,2023 10:30 am</td>
+        <td>تم الدفع</td>
+        <td>782343899</td>
+        <td>حساب بنكى</td>
+        <td>40 $</td>
+        <td>Jun 23, 2023 12:19 pm</td>
+        <td>4</td>
+      </tr>
+      <tr>
+        <td>Jun 21,2023 10:30 am</td>
+        <td>تم الدفع</td>
+        <td>782343899</td>
+        <td>حساب بنكى</td>
+        <td>40 $</td>
+        <td>Jun 23, 2023 12:19 pm</td>
+        <td>4</td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <button class="text-white border-0"
+            style="border-radius : 5px; box-shadow: 4px 4px 7px 1px rgba(0, 0, 0, 0.125); padding:10px; height:auto; margin-right: 5px; display: flex; justify-content: center; align-items: center; width: 200px; background-color: #2DB7B3">
+          الاجمالى
+          </button>
+        </td>
+        <td style="color: red">
+          
+        </td>
+        <td style="color: red">
+          
+        </td>
+        <td style="color: red">$ 785</td>
+        <td style="color: red">
+          
+        </td>
+
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+.styled-table {
+  width: 100%;
+  font-family: Arial, sans-serif;
+}
+
+.styled-table thead th {
+  color: #2DB7B3;
+  font-weight: bold;
+  text-align: center;
+  padding: 30px 12px;
+}
+
+.styled-table tbody td {
+  text-align: center;
+  padding: 16px 8px;
+}
+
+thead,
+tbody,
+tfoot,
+tr,
+td,
+th {
+  border-color: #777;
+  border-style: solid;
+  border-width: 1px;
+  border-right-color: transparent;
+  border-left-color: transparent;
+}
+</style>
