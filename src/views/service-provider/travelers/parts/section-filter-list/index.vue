1c579194646959111bd4fd60ea2535afc858b70f
hamdy elgazar <aljzarh46@gmail.com>
2023-11-28 17:13:20 +0200

    service-provider-tr

---
+++ src/views/service-provider/travelers/parts/section-filter-list/index.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="mt-5 blog">
+        <d-filter-list :call-list="loadList" 
+        @change="changeFilter"
+        singleName=""
+        classColCard="col-12 col-md-6 mt-3"
+        pluralName="مقدمو الخدمة"
+        :searchPlaceholder="$t('search_by_service_provider')"
+        orderName="rate"
+        
+        :orderOpts="
+             [
+                {id:'asc',name:'الأقل تقييما'},
+                {id:'desc',name:'الأعلى تقييما',}
+            ]">
+            <template v-slot:side>
+                <sidebarFilter   @change="changeFilter" :filter-item="fitlterSide" />
+                </template>
+            
+
+            <template v-slot="{ item }">
+                <router-link  class="router-link" :to="getRouteLocale('service-provider-show-profile', { id: item.id })">
+                    <cardInfoVue 
+                    :name="item.name"
+                    :bio="item.bio"
+                    :job="item.job"
+                    :image="item.image"
+                    :rate="item.rate"
+                    />
+                </router-link>
+            </template>
+
+
+        </d-filter-list>
+    </div>
+</template>
+
+<script>
+import serviceProviderAPIs from '@/services/api/service-provider/provider/index'
+//import readyServiceCard from '@/components/cards/ready-service.vue';
+import sidebarFilter from './sidebar-filter.vue'
+import cardInfoVue from './card-item'
+export default {
+    name: 'section-filter-list',
+    components:{
+        cardInfoVue,
+        sidebarFilter
+    },
+    data: () => ({
+        fitlterSide:{
+            state:null,
+            category_id:[],
+            prd:null,
+            rate_value:null,
+        },
+        filterItem:{
+            search:null,
+            rate:'asc',
+            state:null,
+            category_id:[],
+            prd:null,
+           rate_value:null,
+        }
+    }),
+    methods: {
+        changeFilter(val){
+            this.filterItem = {...this.filterItem,...val}
+            this.fireEvent('d-filter-list-refresh')
+        },
+        openAddService(evt){
+        evt.preventDefault();
+        this.fireOpenDialog('add-ready-service-dialog')
+      },
+      closeAddService(evt){
+        evt.preventDefault();
+        this.fireCloseDialog('add-ready-service-dialog')
+      },
+        async loadList(metaInfo) {
+            let params = {
+                page: metaInfo.current_page,
+        
+                ...this.filterItem
+            }
+            return await serviceProviderAPIs.getAll(params)
+        }
+    },
+    mounted() {
+    }
+}
+</script>
+
+<style>
+
+</style>
