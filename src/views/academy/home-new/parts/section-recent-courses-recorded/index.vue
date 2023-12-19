08f9345ba0197caba9a273f7ee74ae01dc33b982
hamdy elgazar <aljzarh46@gmail.com>
2023-12-19 02:25:19 +0200

    academy

---
+++ src/views/academy/home-new/parts/section-recent-courses-recorded/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class=" mt-5 p-3" v-if="items.length">
+            <div class="container">
+              <div
+                class="d-flex justify-content-between align-items-center container"
+              >
+              <h1 class="home-section-title"> أحدث دورتنا المسجلة  </h1>
+                <div>
+                    <button class="more">
+                      <router-link :to="getRouteLocale('academy-courses',{},{type:'recorded'})" class="text-dark">
+                          {{ $t('more') }}
+                      </router-link>
+                    </button>
+                  </div>
+              </div>
+              <d-swiper
+              class="custom-width"
+              v-if="!loading"
+              :slides-per-view="slidesperview"
+              :space-between="10"
+              is-auto
+                :items="items"
+            >
+       
+            <template  v-slot:default="{item}" >
+              
+                <CardVue  
+                :item="item"
+                :url="getRouteLocale('academy-course-show',{id:item.id})"
+                />
+              
+                </template>
+              </d-swiper>
+            </div>
+          </div>
+</template>
+
+<script>
+import CardVue from '@/components/cards/academy-course.vue'
+import coursesApI from '@/services/api/academy/courses.js'
+export default {
+    name:'section-recent-courses-recorded',
+    components:{
+        CardVue
+    },
+    data:()=>({
+        items:[],
+        loading:true,
+        slidesperview:4
+    }),
+    methods:{
+     async loadList(){
+      this.loading =  true;
+        try {
+          let {data} = await coursesApI.getRecentCourses({paginate:6,type:'recorded'})
+          if(data.success){
+            this.items = data.data
+            if(this.items.length<=4) this.slidesperview=3
+          }
+        } catch (error) {
+          console.log('error',error)
+        }
+        this.loading =  false;
+      }
+    },
+    mounted(){
+      this.loadList()
+    }
+}
+</script>
+
+<style>
+.custom-width .swiper-slide{
+  /* width:257.5px !important */
+  width:280px !important
+
+}
+</style>
---
+++ src/views/academy/home-new/parts/section-subscribe-now/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container mt-5">
+            <div class="row align-items-center justify-content-between m-auto">
+                <div class="col-12 col-md-5">
+                    <div>
+                        <div class="bg-back position-relative">
+                            <img  class="img-fluid" :src="`${publicPath}assets/img/Rectangle 1777_2.png`" alt="">
+                       
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-12 col-md-6 text">
+                    <h2 class="home-section-title">
+                        اشتركى فى الاكاديميه الان
+                    </h2>
+                    <p class="home-section-text">
+                        باشتراكك في اكاديمية ريايات ستتمكنين من الحصول على  الدورات التدريبة  باسعار مخفضة بدلا من شراء كل دورة على حدى كما انك ستحصلين على العديد من المميزات و من أهم هذه المميزات  :
+                    </p>
+                    <p class="text-list">
+                        <d-flower-icon /> الحصول على عضوية في شبكة رياديات
+                    </p>
+                    <p class="text-list">
+                        <d-flower-icon /> توفير فرص وظيفية في نفس المجال
+                    </p>
+                    <p class="text-list">
+                        <d-flower-icon /> فرصة تقديم الخدمات في منصة مقدمي خدمة رياديات
+                    </p>
+                </div>
+
+            </div>
+           
+    
+        </div>
+</template>
+
+<script>
+export default {
+name:'section-2'
+}
+</script>
+
+<style scoped>
+.home-section-text{
+   margin: 0;
+}
+.text-list{
+    font-size: 20px;
+line-height: 24px;
+/* or 120% */
+
+display: flex;
+align-items: center;
+text-align: start;
+text-transform: capitalize;
+margin: 0;
+color: #979797;
+}
+</style>
