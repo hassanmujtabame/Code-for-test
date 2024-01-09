29e4ccdd948f79282e1f43f84dbac0fdcdaef797
hamdy elgazar <aljzarh46@gmail.com>
2024-01-10 00:51:00 +0200

    new-main-page-header

--- src/components/pages/home/header/main-slider.vue
+++ src/components/pages/home/header/main-slider.vue
@@ -1,22 +1,36 @@
 <template>
-  <div class="p-0 m-0 sec-one" style="background: var(--color-primary)">
-    <div class="img-sec-one" :class="{ 'rotateY-180': $i18n.locale !== 'ar' }">
-      <img class="img-fluid" src="@/assets/img/Group.png" alt="home-section-background" />
-    </div>
-    <div class="container" style="position:relative" :class="[isMobile ? 'p-3' : 'p-5 pb-0']">
-      <div class="row align-items-center pt-5 pb-5">
-        <div class="col-xl-7 text">
-          <h1 class="header-title-page text-white">{{ $t('page-home-title') }}</h1>
-          <p class="header-desc-page text-white">{{ $t('page-home-desc') }}</p>
+  <div class="p-0 m-0 sec-one" style="background: #fff">
+    <div class="container" style="position:relative" :class="[isMobile ? 'p-3' : '']">
+      <div class="row pb-5">
+        <div class="col-12 col-md-4">
+          <h1 class="header-title-page ">
+            <!-- {{ $t('page-home-title') }} -->
+<h1 class="header-title-page select-title" style="display: inline;">
+  رياديات
+  <img src="@/assets/img/new/main-page/header/Vector 14.png" alt="">
+</h1>
+            
+            شبكة نسائية في جميع أنحاء العالم
+            <br>
+
+          </h1>
+          <p class="header-desc-page ">
+            طموحة و تسعين لتحقيق حلمك و شغفك ؟
+            <br>
+            شبكة رياديات بتساعدك في مشروعك من الالف الى الياء
+            <!-- {{ $t('page-home-desc') }} -->
+          </p>
           <div class v-if="!isAuthenticated">
-            <button class="btn-custmer shadow mt-2 " @click="router_push('register')">انضمي الان</button>
-            <router-link custom :to="getRouteLocale('login')" v-slot="{ navigate }">
-              <button class="btn-custmer-w bg-transparent text-white mt-2 mx-3" @click="navigate">سجلي دخولك</button>
-            </router-link>
+            <button class="px-3 btn-custmer shadow" style="background-color: #ff5722 !important; border: none !important; " @click="router_push('register')">انضمي الان</button>
+            <button class="px-3 btn-custmer-w shadow mx-3" @click="router_push('login')">سجلي دخولك</button>
+
+            <!-- <router-link :to="getRouteLocale('login')">
+              <span class="px-3 btn-custmer shadow mx-3" @click="navigate">سجلي دخولك</span>
+            </router-link> -->
           </div>
         </div>
-        <div v-if="!isMobile"  style="overflow: hidden; background:rgba(255, 188, 0, 1);" class="img-cons m-auto ">
-          <img src="@/assets/img/main-page.png" alt="" style="height: 300px !important; object-fit: contain !important;">
+        <div v-if="!isMobile" class="col-8 d-flex justify-content-end">
+          <img  style="width: 75%;" class="img-fluid" src="@/assets/img/new/main-page/header/Group 50.png" alt="home-section-background" />
         </div>
       </div>
     </div>
@@ -39,5 +53,15 @@ export default {
 .header-title-page,
 .header-desc-page {
   color: black;
+}
+.select-title{
+  position: relative;
+}
+.select-title img {
+  content: '';
+  position: absolute;
+  right: 0;
+  bottom: 0px;
+  width: 100%;
 }
 </style>
