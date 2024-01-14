d12bb0f39b4dad76482f2f4f4a167761d55b14c7
hamdy elgazar <aljzarh46@gmail.com>
2024-01-14 18:59:33 +0200

    text animation

--- src/components/pages/home/header/main-slider.vue
+++ src/components/pages/home/header/main-slider.vue
@@ -5,11 +5,11 @@
         <div class="col-12 col-md-4">
           <h1 class="header-title-page text-black">
             <!-- {{ $t('page-home-title') }} -->
-<h1 class="header-title-page select-title-main text-black" style="display: inline;">
-  رياديات
-  <img src="@/assets/img/new/main-page/header/Vector 14.png" alt="">
-</h1>
-            
+            <h1 class="header-title-page select-title-main text-black" style="display: inline;">
+              رياديات
+              <img src="@/assets/img/new/main-page/header/Vector 14.png" alt="">
+            </h1>
+
             شبكة نسائية في جميع أنحاء العالم
             <br>
 
@@ -21,7 +21,9 @@
             <!-- {{ $t('page-home-desc') }} -->
           </p>
           <div class v-if="!isAuthenticated">
-            <button class="px-3 btn-custmer shadow" style="background-color: #ff5722 !important; border: none !important; " @click="router_push('register')">انضمي الان</button>
+            <button class="px-3 btn-custmer shadow"
+              style="background-color: #ff5722 !important; border: none !important; "
+              @click="router_push('register')">انضمي الان</button>
             <button class="px-3 btn-custmer-w shadow mx-3" @click="router_push('login')">سجلي دخولك</button>
 
             <!-- <router-link :to="getRouteLocale('login')">
@@ -29,12 +31,64 @@
             </router-link> -->
           </div>
         </div>
-        <div v-if="!isMobile" class="col-8 d-flex justify-content-end">
-          <img  style="  height: auto;
-          object-fit: contain;width: 75%;" class="img-fluid" src="@/assets/img/new/main-page/header/Group 50.png" alt="home-section-background" />
+        <div v-if="!isMobile" style="position: relative " class="col-8 d-flex justify-content-end">
+          <img style="  height: auto;
+            object-fit: contain;width: 250px;" class="img-fluid" src="@/assets/img/new/main-page/header/Student.png"
+            alt="home-section-background" />
+
+          <span class="p-2" style="background: #607D8B;
+              height: fit-content;
+              color: #fff;
+              border-radius: 10px 10px 0px 10px;
+              position: absolute;
+              top: 100px;right: 600px;
+              width:max-content;
+              ">
+            {{ animatedText }}
+          </span>
+          <span v-if="showSecondText" class="p-2" style="background: #607D8B;
+            height: fit-content;
+            color: #fff;
+            border-radius: 10px 10px 0px 10px;
+            position: absolute;
+            top: 150px;right: 600px;
+            width:max-content;
+            ">
+            {{ animatedSecondText }}
+          </span>
+          <span v-if="showThirdText" class="p-2" style="background: #2196F3;
+            height: fit-content;
+            color: #fff;
+            border-radius: 10px 10px 10px 0px;
+            position: absolute;
+            top: 220px;right: 600px;
+            width:max-content;
+            ">
+            {{ animatedThirdText }}
+          </span>
+          <span v-if="showFourthText" class="p-2" style="background: #2196F3;
+          height: fit-content;
+          color: #fff;
+          border-radius: 10px 10px 10px 0px;
+          position: absolute;
+          top: 270px;right: 540px;
+          width:max-content;
+          ">
+            {{ animatedFourthText }}
+          </span>
+
+
+          <img style="  height: auto;
+          object-fit: contain;width: 250px; transform: translate(-150px, 200px);" class="img-fluid"
+            src="@/assets/img/new/main-page/header/Teacher.png" alt="home-section-background" />
+          <!-- <img style="  height: auto;
+          object-fit: contain;width: 75%;" class="img-fluid" src="@/assets/img/new/main-page/header/Icons.png"
+            alt="home-section-background" /> -->
         </div>
       </div>
     </div>
+    <div id="text-container">{{ animatedText }}</div>
+
   </div>
 </template>
 
@@ -45,15 +99,89 @@ export default {
   computed: {
     ...mapGetters({
       isAuthenticated: 'auth/authenticated',
-    })
+    }),
+    textRight() {
+      return `${this.currentIndex}em`;
+    },
+    secondTextLeft() {
+      return `${this.secondTextIndex}em`;
+    },
+
+  },
+  data() {
+    return {
+      originalText: "  مرحبا",
+      secondText: "أنا ابحث عن بعض المساعدة فى بدء عملى الخاص",
+      animatedText: "",
+      animatedSecondText: "",
+      currentIndex: 0,
+      secondTextIndex: 0,
+      animationSpeed: 70, // Adjust the animation speed here
+      showSecondText: false,
+      thirdText: "انه لمن دواعى سرورنا مساعدتك.",
+      fourthText: "ما هي نوع الاعمال التى تفكرين فى بدءها؟",
+      animatedThirdText: "",
+      animatedFourthText: "",
+      thirdTextIndex: 0,
+      fourthTextIndex: 0,
+      showThirdText: false,
+      showFourthText: false,
+    };
+  },
+  methods: {
+    animateText() {
+      if (this.currentIndex <= this.originalText.length) {
+        this.animatedText = this.originalText.substr(0, this.currentIndex);
+        this.currentIndex++;
+        setTimeout(this.animateText, this.animationSpeed);
+      } else {
+        // Animation of the first text is complete
+        this.showSecondText = true;
+        this.animateSecondText();
+      }
+    },
+    animateSecondText() {
+      if (this.secondTextIndex <= this.secondText.length) {
+        this.animatedSecondText = this.secondText.substr(0, this.secondTextIndex);
+        this.secondTextIndex++;
+        setTimeout(this.animateSecondText, this.animationSpeed);
+      } else {
+        setTimeout(() => {
+          this.showThirdText = true;
+          this.animateThirdText();
+        }, 1000);
+      }
+    },
+    animateThirdText() {
+      if (this.thirdTextIndex <= this.thirdText.length) {
+        this.animatedThirdText = this.thirdText.substr(0, this.thirdTextIndex);
+        this.thirdTextIndex++;
+        setTimeout(this.animateThirdText, this.animationSpeed);
+      } else {
+        this.showFourthText = true;
+        this.animateFourthText();
+      }
+    },
+
+    animateFourthText() {
+      if (this.fourthTextIndex <= this.fourthText.length) {
+        this.animatedFourthText = this.fourthText.substr(0, this.fourthTextIndex);
+        this.fourthTextIndex++;
+        setTimeout(this.animateFourthText, this.animationSpeed);
+      }
+    },
+  },
+  mounted() {
+    this.animateText();
   }
-};
+}
 </script>
 
 <style>
-.select-title-main{
+.select-title-main {
   position: relative;
 }
+
 .select-title-main img {
   content: '';
   position: absolute;
