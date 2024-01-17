330520dde1cadcd4632948b63b057b35a5560dc1
hamdy elgazar <aljzarh46@gmail.com>
2024-01-17 08:15:39 +0200

    fix-writing

--- src/views/main/home/parts/section-header/index.vue
+++ src/views/main/home/parts/section-header/index.vue
@@ -48,7 +48,6 @@
                 height: fit-content;
                 color: #fff;
                 border-radius: 10px 10px 0px 10px;
-                width:max-content;
                 ">
                   {{ animatedSecondText }}
                 </p>
