a1b1f03a35c354dc0194ab1a511e55359d4adea9
hamdy elgazar <aljzarh46@gmail.com>
2024-01-17 08:41:45 +0200

    growing-network

--- src/views/network/home/auth/parts/grow-your-network/index.vue
+++ src/views/network/home/auth/parts/grow-your-network/index.vue
@@ -1,22 +1,27 @@
 <template>
-    <div class="row align-items-center m-auto">
-        <div class="col-md-5">
+    <div class="d-flex">
+        <div>
             <div>
                 <div class="bg-back position-relative">
-                    <img class="img-fluid" :src="`${publicPath}assets/img/pana.png`" alt="">
+                    <img  src="@/assets/img/new/network/Group 138.png" alt="">
                 </div>
             </div>
         </div>
-        <div class="col-md-7 text">
+        <div class="text">
             <h2 class="home-section-title">
                 شبكة رياديات: وسع دائرة معارفك
             </h2>
-            <p class="home-section-text">
+            <p class="home-section-text mb-3">
                 رياديات شبكة تضم نخبة من رائدات الأعمال والمستثمرات والشركاء، وتهدف إلى إلهام وتمكين وجمع رائدات الأعمال
                 معًا على منصة واحدة ليساعد بعضهن البعض الآخر على النجاح في أعمالهن. ويمكن من خلالها الحصول على المساعدة
                 والتشجيع للدخول في مجال ريادة الأعمال من خلال مجموعة من الخدمات والمزايا، وقد صممت لمساعدة رائدات
                 الأعمال على التعلم وزيادة نمو أعمالهن والتواصل مع الرياديات
             </p>
+            <router-link v-if="!user" :to="getRouteLocale('register')" 
+            class="btn-custmer shadow my-3 ">
+            {{ $t('join-us')
+            }}
+        </router-link>
         </div>
     </div>
 </template>
