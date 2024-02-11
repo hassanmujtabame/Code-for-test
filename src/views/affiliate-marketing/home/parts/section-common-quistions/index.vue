e938022d8c1785e51de2018b7e9bb9fa1d9c3db6
omarsabri666 <101377155+omarsabri666@users.noreply.github.com>
2024-02-11 17:01:50 +0200

    updated the commission page

--- src/views/affiliate-marketing/home/parts/section-common-quistions/index.vue
+++ src/views/affiliate-marketing/home/parts/section-common-quistions/index.vue
@@ -1,82 +1,108 @@
 <template>
-    <div class="container mt-5">
-        <h1>الأسئله الشائعه </h1>
-        <div class="row">
-            <div class="col-md-5 image-side" style="min-height:400px">
+  <div class="container mt-5">
+    <h1>الأسئله الشائعه</h1>
+    <div class="row">
+      <!-- <div class="col-md-5 image-side" style="min-height:400px">
                 <img src="@/assets/img/reset-password.png" style="width: 100%">
+            </div> -->
+      <div class="quistions-side">
+        <div class="accordion accordion-flush" id="accordionFlushExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseOne"
+                aria-expanded="false"
+                aria-controls="flush-collapseOne"
+              >
+                هل يوجد مده انتهاء للبرنامج؟
+              </button>
+            </h2>
+            <div
+              id="flush-collapseOne"
+              class="accordion-collapse collapse"
+              data-bs-parent="#accordionFlushExample"
+            >
+              <div class="accordion-body">
+                هل يوجد مده انتهاء للبرنامج؟ هل يوجد مده انتهاء للبرنامج؟ هل
+                يوجد مده انتهاء للبرنامج؟ هل يوجد مده انتهاء للبرنامج؟
+              </div>
             </div>
-            <div class=" quistions-side">
-                <div  class="accordion accordion-flush" id="accordionFlushExample">
-                    <div class="accordion-item">
-                      <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                          هل يوجد مده انتهاء للبرنامج؟
-                        </button>
-                      </h2>
-                      <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">
-                            هل يوجد مده انتهاء للبرنامج؟
-                            هل يوجد مده انتهاء للبرنامج؟
-                            هل يوجد مده انتهاء للبرنامج؟
-                            هل يوجد مده انتهاء للبرنامج؟
-                        </div>
-                      </div>
-                    </div>
-                    <div class="accordion-item">
-                      <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                          هل هناك مكافأت و مميزات أخرى؟
-                        </button>
-                      </h2>
-                      <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">
-                            هل يوجد مده انتهاء للبرنامج؟
-                            هل يوجد مده انتهاء للبرنامج؟
-                            هل يوجد مده انتهاء للبرنامج؟
-                            هل يوجد مده انتهاء للبرنامج؟
-                            هل يوجد مده انتهاء للبرنامج؟
-
-                            </div>
-                      </div>
-                    </div>
-                    <div class="accordion-item">
-                      <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                            هل يوجد مده انتهاء للبرنامج؟
-
-                        </button>
-                      </h2>
-                      <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">
-                            هل يوجد مده انتهاء للبرنامج؟
-                            هل يوجد مده انتهاء للبرنامج؟
-                            هل يوجد مده انتهاء للبرنامج؟
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseTwo"
+                aria-expanded="false"
+                aria-controls="flush-collapseTwo"
+              >
+                هل هناك مكافأت و مميزات أخرى؟
+              </button>
+            </h2>
+            <div
+              id="flush-collapseTwo"
+              class="accordion-collapse collapse"
+              data-bs-parent="#accordionFlushExample"
+            >
+              <div class="accordion-body">
+                هل يوجد مده انتهاء للبرنامج؟ هل يوجد مده انتهاء للبرنامج؟ هل
+                يوجد مده انتهاء للبرنامج؟ هل يوجد مده انتهاء للبرنامج؟ هل يوجد
+                مده انتهاء للبرنامج؟
+              </div>
             </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseThree"
+                aria-expanded="false"
+                aria-controls="flush-collapseThree"
+              >
+                هل يوجد مده انتهاء للبرنامج؟
+              </button>
+            </h2>
+            <div
+              id="flush-collapseThree"
+              class="accordion-collapse collapse"
+              data-bs-parent="#accordionFlushExample"
+            >
+              <div class="accordion-body">
+                هل يوجد مده انتهاء للبرنامج؟ هل يوجد مده انتهاء للبرنامج؟ هل
+                يوجد مده انتهاء للبرنامج؟
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'section-common-quistions'
-}
+  name: "section-common-quistions",
+};
 </script>
 
 <style scoped>
-.accordion-button{
-    font-size: large;
-    position: relative;
+.accordion-button {
+  font-size: large;
+  position: relative;
 }
 .accordion-button::before {
-    content: '';
-    width: 20px;
-    height: 20px;
-    background-color: #2eb9b3;
-    border-radius: 50%;
-    margin-left: 10px;
+  content: "";
+  width: 20px;
+  height: 20px;
+  background-color: #2eb9b3;
+  border-radius: 50%;
+  margin-left: 10px;
 }
 </style>
