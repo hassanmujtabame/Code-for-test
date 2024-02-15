36c9e027d572b3abea0ecdcbfb210ad6cf6df1f6
omarsabri666 <101377155+omarsabri666@users.noreply.github.com>
2024-02-15 19:57:06 +0200

    marketing added a new section

---
+++ src/components/Double-your-winnings.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-12 d-flex flex-column">
+        <!-- <div style="background-color: #1fb9b3" class="rounded-3"></div> -->
+        <h1 style="font-size: 48px" class="text-dark font-weight-bold">
+          <span style="color: #1fb9b3">ضاعف</span> ارباحك معنا
+        </h1>
+        <p class="text-dark">
+          أبداى معانا رحلة التسويق بالعمولة وروجى لمنتجاتك واحصلى على أعلى نسبة
+          مقابل كل عملية تسويقية فقط سجلى معانا الان
+        </p>
+        <router-link
+          class="text-light px-5 py-2"
+          style="background-color: #1fb9b3; width: fit-content"
+          :to="getRouteLocale('affiliate-marketing-home')"
+          >تعرف على المزيد
+        </router-link>
+      </div>
+      <div class="col-md-6 col-12">
+        <img
+          class="img-fluid rounded-3"
+          src="../../public/assets/img/laptop-com.png"
+          alt="laptop image"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dobule-winning",
+};
+</script>
+
+<style></style>
--- src/views/affiliate-marketing/home/index.vue
+++ src/views/affiliate-marketing/home/index.vue
@@ -1,87 +1,94 @@
 <template>
-    <div style="margin-top: 85px; overflow-x: hidden; overflow-y: hidden" class="consult">
-        <!--section-header-->
-        <SectionHeader />
-        <SectionIntro />
-        <SectionFeatures />
-        <SectionChoseUs />
-        <SectionHowWorkProgram />
-        <SectionJoin v-if="token && !user.affiliate"/>
-        <SectionJoin v-if="!token"/>
-        <SectionWhatWeProvide />
-        <SectionGraduated title="ابرز سفيرات التسويق بالعموله" />
-        <!-- <SectionDoubleWinings /> -->
-        <SectionAreYouMember />
-        <SectionRecognize />
-        <SectionCommonQuistions />
-        <!-- <SectionRecentCourses /> -->
-        <SectionGraduated title=" المسوقين الاكثر ربحا " />
-        <SectionContinueLearning />
-        <!-- <SectionHear :itemPage="itemPage" /> -->
+  <div
+    style="margin-top: 85px; overflow-x: hidden; overflow-y: hidden"
+    class="consult"
+  >
+    <!--section-header-->
+    <SectionHeader />
+    <SectionIntro />
+    <SectionFeatures />
+    <SectionChoseUs />
+    <SectionHowWorkProgram />
+    <SectionJoin v-if="token && !user.affiliate" />
+    <SectionJoin v-if="!token" />
+    <SectionWhatWeProvide />
+    <SectionGraduated title="ابرز سفيرات التسويق بالعموله" />
+    <!-- <SectionDoubleWinings /> -->
+    <SectionAreYouMember />
+    <SectionRecognize />
+    <div class="my-5">
+      <SectionWinning />
     </div>
+    <SectionCommonQuistions />
+    <!-- <SectionRecentCourses /> -->
+    <SectionGraduated title=" المسوقين الاكثر ربحا " />
+    <SectionContinueLearning />
+    <!-- <SectionHear :itemPage="itemPage" /> -->
+  </div>
 </template>
 
 <script>
-import SectionHeader from './parts/section-header/index.vue'
-import SectionWhatWeProvide from './parts/section-what-we-provide/index.vue'
-import SectionChoseUs from './parts/section-chose-us/index.vue'
-import SectionJoin from './parts/section-join/index.vue'
-import SectionIntro from './parts/section-intro/index.vue'
-import SectionHowWorkProgram from './parts/section-how-work-program/index.vue'
-import SectionFeatures from './parts/section-features/index.vue'
-import SectionAreYouMember from './parts/section-are-you-member/index.vue'
-import SectionDoubleWinings from './parts/section-double-winings/index.vue'
-import SectionRecognize from './parts/section-recognize-our-marketing/index.vue'
-import SectionGraduated from './parts/section-graduated/index.vue'
-import SectionRecentCourses from './parts/section-recent-courses-online/index.vue'
-import SectionCommonQuistions from './parts/section-common-quistions/index.vue'
-import SectionContinueLearning from '@/views/incubator/home/parts/section-continue-learning/index.vue'
-import SectionHear from './parts/section-hear/index.vue'
-import incubatorAPI from '@/services/api/incubator';
-
+import SectionHeader from "./parts/section-header/index.vue";
+import SectionWinning from "@/components/Double-your-winnings.vue";
+import SectionWhatWeProvide from "./parts/section-what-we-provide/index.vue";
+import SectionChoseUs from "./parts/section-chose-us/index.vue";
+import SectionJoin from "./parts/section-join/index.vue";
+import SectionIntro from "./parts/section-intro/index.vue";
+import SectionHowWorkProgram from "./parts/section-how-work-program/index.vue";
+import SectionFeatures from "./parts/section-features/index.vue";
+import SectionAreYouMember from "./parts/section-are-you-member/index.vue";
+import SectionDoubleWinings from "./parts/section-double-winings/index.vue";
+import SectionRecognize from "./parts/section-recognize-our-marketing/index.vue";
+import SectionGraduated from "./parts/section-graduated/index.vue";
+import SectionRecentCourses from "./parts/section-recent-courses-online/index.vue";
+import SectionCommonQuistions from "./parts/section-common-quistions/index.vue";
+import SectionContinueLearning from "@/views/incubator/home/parts/section-continue-learning/index.vue";
+import SectionHear from "./parts/section-hear/index.vue";
+import incubatorAPI from "@/services/api/incubator";
 
 export default {
-    name: 'affiliate-marketing-home',
-    data: () => ({
-        loading: true,
-        hasError: false,
-        itemPage: {}
-    }),
-    components: {
-        SectionWhatWeProvide,
-        SectionChoseUs,
-        SectionJoin,
-        SectionHeader,
-        SectionIntro,
-        SectionHowWorkProgram,
-        SectionFeatures,
-        SectionAreYouMember,
-        SectionRecognize,
-        SectionDoubleWinings,
-        SectionGraduated,
-        SectionRecentCourses,
-        SectionContinueLearning,
-        SectionHear,
-        SectionCommonQuistions
-    },
-    methods: {
-        async initializing() {
-            this.loading = true;
-            try {
-                let { data } = await incubatorAPI.getDepartment(this.$route.params.id)
-                if (data.success) {
-                    this.itemPage = data.data
-                }
-            } catch (error) {
-                console.log('error', error)
-            }
-            this.loading = false;
+  name: "affiliate-marketing-home",
+  data: () => ({
+    loading: true,
+    hasError: false,
+    itemPage: {},
+  }),
+  components: {
+    SectionWhatWeProvide,
+    SectionChoseUs,
+    SectionJoin,
+    SectionHeader,
+    SectionIntro,
+    SectionHowWorkProgram,
+    SectionFeatures,
+    SectionAreYouMember,
+    SectionRecognize,
+    SectionDoubleWinings,
+    SectionGraduated,
+    SectionRecentCourses,
+    SectionContinueLearning,
+    SectionHear,
+    SectionCommonQuistions,
+    SectionWinning,
+  },
+  methods: {
+    async initializing() {
+      this.loading = true;
+      try {
+        let { data } = await incubatorAPI.getDepartment(this.$route.params.id);
+        if (data.success) {
+          this.itemPage = data.data;
         }
+      } catch (error) {
+        console.log("error", error);
+      }
+      this.loading = false;
     },
-    mounted() {
-        this.initializing()
-    }
-}
+  },
+  mounted() {
+    this.initializing();
+  },
+};
 </script>
 
 <style></style>
