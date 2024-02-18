edac78b8d94af30df0e73c290c4e163e17a3a3b4
omarsabri666 <101377155+omarsabri666@users.noreply.github.com>
2024-02-18 19:19:27 +0200

    consulting

--- src/components/cards/consulting-field.vue
+++ src/components/cards/consulting-field.vue
@@ -1,68 +1,68 @@
 <template>
   <div class="consulting-category-card box">
-        <div class="consulting-category-card__img">
-            <img  :src="item.image_path" :alt="item.name"  class="w-100 h-100" >
-        </div>
-        <div class="consulting-category-card__content">
-           <h6 class="consulting-category-card__title">{{ item.name }}</h6>
-        </div>
+    <div class="consulting-category-card__img">
+      <img :src="item.image_path" :alt="item.name" class="w-100 h-100" />
     </div>
+    <div class="consulting-category-card__content">
+      <h6 class="consulting-category-card__title">{{ item.name }}</h6>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
- name:'consulting-category-card',
- props:{
-    item:{
-        default:()=>{
-            return {
-                image:'/assets/img/cuate.png',
-                title:' دراسة جدوى'
-            }
-        }
-    }
- }
-}
+  name: "consulting-category-card",
+  props: {
+    item: {
+      default: () => {
+        return {
+          image: "/assets/img/cuate.png",
+          title: " دراسة جدوى",
+        };
+      },
+    },
+  },
+};
 </script>
 
 <style scoped>
-.consulting-category-card{
-    border-radius: 8px 8px 8px 8px;
+.consulting-category-card {
+  border-radius: 8px 8px 8px 8px;
 }
-.consulting-category-card__img{
-    height:192px
+.consulting-category-card__img {
+  height: 192px;
 }
-.consulting-category-card__img,.consulting-category-card__img>img{
-    border-radius: 8px 8px 0px 0px;
+.consulting-category-card__img,
+.consulting-category-card__img > img {
+  border-radius: 8px 8px 0px 0px;
 }
-.consulting-category-card__title{
-    font-style: normal;
-font-weight: 400;
-font-size: 24px;
-line-height: 40px;
-/* identical to box height, or 167% */
-display: flex;
-align-items: center;
-text-align: center;
-text-transform: capitalize;
-color: #737373;
-
+.consulting-category-card__title {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 40px;
+  /* identical to box height, or 167% */
+  display: flex;
+  align-items: center;
+  text-align: center;
+  text-transform: capitalize;
+  color: #737373;
 }
-.consulting-category-card__content{
+.consulting-category-card__content {
   /* Auto layout */
 
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-padding: 24px 16px;
-gap: 8px;
-width: 100%;
-height: 88px;
-text-align: center;
-background: #FFFFFF;
-border: 1px solid #CDD7D8;
-/*box-shadow: 0px 0px 78.2699px rgba(187, 188, 189, 0.3);*/
-border-radius: 0px 0px 8px 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 24px 16px;
+  gap: 8px;
+  width: 100%;
+  height: 88px;
+  text-align: center;
+  background: #ffffff;
+  border: 1px solid #cdd7d8;
+  /*box-shadow: 0px 0px 78.2699px rgba(187, 188, 189, 0.3);*/
+  border-radius: 0px 0px 8px 8px;
 }
 </style>
--- src/views/consulting/fields/parts/section-filter-list/index.vue
+++ src/views/consulting/fields/parts/section-filter-list/index.vue
@@ -1,62 +1,64 @@
 <template>
-  <d-filter-list 
-  hideSide
-  :call-list="loadList"
-  :pluralName="$t('consulting-fields')"
-  :singleName="$t('field')"
-  searchPlaceholder="أبحث عن مجالات الاستشارة"
-  @change="changeFilter"
-  classColCard="col-12 col-sm-6 col-md-3 mt-2"
-  hideTotal
-  hideTitle
-  classSearchOrder="col-12 justify-content-between"
-  classColOrder="col-12 col-md-2"
-  >
-  <template v-slot="{item}">
-    <router-link class="router-link" :to="getRouteLocale('consulting-field-consultants',{id:item.id})">
-    <consultingFieldCard :item="item" />
-  </router-link>
-    </template>
-  </d-filter-list>
+  <div>
+    <d-filter-list
+      hideSide
+      :call-list="loadList"
+      :pluralName="$t('consulting-fields')"
+      :singleName="$t('field')"
+      searchPlaceholder="أبحث عن مجالات الاستشارة"
+      @change="changeFilter"
+      classColCard="col-12 col-sm-6 col-md-3 mt-2"
+      hideTotal
+      hideTitle
+      classSearchOrder="col-12 justify-content-between"
+      classColOrder="col-12 col-md-2"
+    >
+      <template v-slot="{ item }">
+        <router-link
+          class="router-link"
+          :to="getRouteLocale('consulting-field-consultants', { id: item.id })"
+        >
+          <consultingFieldCard :item="item" />
+        </router-link>
+      </template>
+    </d-filter-list>
+  </div>
 </template>
 
 <script>
-import consultingFieldCard from '@/components/cards/consulting-field.vue'
-import consultingAPI from '@/services/api/consulting'
+import consultingFieldCard from "@/components/cards/consulting-field.vue";
+import consultingAPI from "@/services/api/consulting";
 export default {
-    name:'section-filter-list',
-    components:{
-        consultingFieldCard
+  name: "section-filter-list",
+  components: {
+    consultingFieldCard,
+  },
+  data: () => ({
+    items: [],
+    filterItem: {
+      created_at: "asc",
+      search: null,
     },
- data:()=>({
-    items:[],
-    filterItem:{
-            created_at:'asc',
-            search:null
-        }
- }),
- methods:{
-   changeFilter(val){
-            this.filterItem = {...this.filterItem,...val}
-            this.fireEvent('d-filter-list-refresh')
-        },
-    async loadList(metaInfo){
+  }),
+  methods: {
+    changeFilter(val) {
+      this.filterItem = { ...this.filterItem, ...val };
+      this.fireEvent("d-filter-list-refresh");
+    },
+    async loadList(metaInfo) {
       let params = {
-                page: metaInfo.current_page,
-                field_id: this.$route.params.id,
-                ...this.filterItem
-            }
-        try {
-          return await consultingAPI.fields.getAll(params)
-        } catch (error) {
-           // 
-        }
-    }
- }
-
-}
+        page: metaInfo.current_page,
+        field_id: this.$route.params.id,
+        ...this.filterItem,
+      };
+      try {
+        return await consultingAPI.fields.getAll(params);
+      } catch (error) {
+        //
+      }
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
