7f43513668cd21c42e28dd9b246c0aae9e513202
omarsabri666 <101377155+omarsabri666@users.noreply.github.com>
2024-02-22 19:02:08 +0200

    edited the exhibitions

--- src/components/cards/exhibition.vue
+++ src/components/cards/exhibition.vue
@@ -1,161 +1,162 @@
 <template>
-   <div class="box border rounded-top" v-bind="$attrs">
-                    <div class="position-relative">
-                        <div v-if="userInfo && false" class="exibition-item__avatar">
-                            <img :src="userInfo.image" width="48" height="48" />
-                            <h6>{{ userInfo.name }}</h6>
-                        </div>
-                        <img class="w-100 rounded-top" :src="img" :alt="title"
-                            height="167">
-                            <button v-if="isShared" class="btn-shared-exibition"><tickCircleIcon />{{ $t('shared') }}</button>
-                    </div>
-                    <div class="p-2 text-start">
-                        <h6  style="" class="exhibition-card-title mt-2">
-                        {{title}}
-                        </h6>
-                        <p class="t-c text-two-lines exhibition-card-desc exhibition-card-text" v-html="description">
-                        </p>
-                        <div class="d-flex justify-content-between flex-wrap">
-                            <p class="exhibition-card-text">
-                                <d-empty-wallet-icon :size="24" color="currentColor" />
-                                <template v-if="price=='0'">
-                                   {{ $t('free-enter') }}
-                                </template>
-                                <template v-else>
-                            {{ $t('ticket') }}   {{ price??'N/A'}} {{ $t('curreny-rs') }}
-                            </template>
-                            </p>
-                            <p class="exhibition-card-text">
-                              <d-localisation-icon :size="24" color="currentColor" />
-                               {{place}}
-                            </p>
-                            <p class="exhibition-card-text">
-                                <d-school-wallet-icon :size="24" color="currentColor" />
+  <div class="box border rounded-top" v-bind="$attrs">
+    <div class="position-relative">
+      <div v-if="userInfo && false" class="exibition-item__avatar">
+        <img :src="userInfo.image" width="48" height="48" />
+        <h6>{{ userInfo.name }}</h6>
+      </div>
+      <img class="w-100 rounded-top" :src="img" :alt="title" height="167" />
+      <button v-if="isShared" class="btn-shared-exibition">
+        <tickCircleIcon />{{ $t("shared") }}
+      </button>
+    </div>
+    <div class="p-2 text-start">
+      <h6 style="color: black" class="exhibition-card-title mt-2">
+        {{ title }}
+      </h6>
+      <p
+        style="font-size: 14px"
+        class="t-c text-two-lines exhibition-card-desc exhibition-card-text"
+        v-html="description"
+      ></p>
+      <div class="d-flex justify-content-between flex-wrap">
+        <p style="font-size: 14px" class="exhibition-card-text">
+          <d-empty-wallet-icon :size="18" color="currentColor" />
+          <template v-if="price == '0'">
+            {{ $t("free-enter") }}
+          </template>
+          <template v-else>
+            {{ $t("ticket") }} {{ price ?? "N/A" }} {{ $t("curreny-rs") }}
+          </template>
+        </p>
+        <p style="font-size: 14px" class="exhibition-card-text">
+          <d-localisation-icon :size="18" color="currentColor" />
+          {{ place }}
+        </p>
+        <p style="font-size: 14px" class="exhibition-card-text">
+          <d-school-wallet-icon :size="18" color="currentColor" />
 
-                                {{userName}}
-
-                            </p>
-                            
-                        </div>
-                    </div>
-                </div>
+          {{ userName }}
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import tickCircleIcon from '../icon-svg/tick-circle.vue';
+import tickCircleIcon from "../icon-svg/tick-circle.vue";
 export default {
- name:'exhibition-card',
- components:{
-    tickCircleIcon
- },
- props:{
-    img:{
-        type:String,
-        require:true
+  name: "exhibition-card",
+  components: {
+    tickCircleIcon,
+  },
+  props: {
+    img: {
+      type: String,
+      require: true,
     },
-    isShared:{
-        type:[Boolean,Number],
-        require:false
+    isShared: {
+      type: [Boolean, Number],
+      require: false,
     },
-    title:{
-        type:String,
-        require:true
+    title: {
+      type: String,
+      require: true,
     },
-    description:{
-        type:String,
+    description: {
+      type: String,
     },
-    userName:{
-        type:String,
-        default:'N/A'
+    userName: {
+      type: String,
+      default: "N/A",
     },
-    place:{
-        type:String,
-        default:'N/A'
+    place: {
+      type: String,
+      default: "N/A",
     },
-    price:{
-        type:[String,Number],
-        default:'N/A'
+    price: {
+      type: [String, Number],
+      default: "N/A",
     },
-    userInfo:{
-        type:[Object,Array]
-    }
-}
-}
+    userInfo: {
+      type: [Object, Array],
+    },
+  },
+};
 </script>
 
 <style scoped>
-.exhibition-card-title{
-    font-style: normal;
-font-weight: 400;
-font-size: 24px;
-line-height: 32px;
-height: 35px;
-overflow: hidden;
-text-overflow: ellipsis;
-/* identical to box height, or 133% */
+.exhibition-card-title {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 32px;
+  height: 35px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  /* identical to box height, or 133% */
 
-text-align: start;
-text-transform: capitalize;
+  text-align: start;
+  text-transform: capitalize;
 
-color: #1FB9B3;
+  color: #1fb9b3;
 }
-.exhibition-card-desc{
-    height:54px;
+.exhibition-card-desc {
+  height: 54px;
 }
-.exhibition-card-text{
-    font-style: normal;
-font-weight: 400;
-font-size: 20px;
-line-height: 24px;
-/* or 120% */
-text-align: start;
-color: #A6A6A6;
+.exhibition-card-text {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+  /* or 120% */
+  text-align: start;
+  color: #a6a6a6;
 }
-.exhibition-card-text>p{
-    font-size: 20px;
-line-height: 24px;
+.exhibition-card-text > p {
+  font-size: 20px;
+  line-height: 24px;
 }
-.btn-shared-exibition{
-    font-size: 12px;
-    border-radius: 4px;
-    border:none;
-    height: 24px;
-    background:var(--m-color);
-    position:absolute;
-    top:5px;
-    color:#FFF;
-    left: 5px;
+.btn-shared-exibition {
+  font-size: 12px;
+  border-radius: 4px;
+  border: none;
+  height: 24px;
+  background: var(--m-color);
+  position: absolute;
+  top: 5px;
+  color: #fff;
+  left: 5px;
 }
-html[dir=ltr] .btn-shared-exibition{
-    right: 5px; 
-    left:auto
+html[dir="ltr"] .btn-shared-exibition {
+  right: 5px;
+  left: auto;
 }
-.btn-shared-exibition svg{
-    margin-left: 5px;
+.btn-shared-exibition svg {
+  margin-left: 5px;
 }
-html[dir=ltr] .btn-shared-exibition svg{
-    margin-left: 0;
-    margin-right: 5px;
+html[dir="ltr"] .btn-shared-exibition svg {
+  margin-left: 0;
+  margin-right: 5px;
 }
-.exibition-item__avatar{
-    position: absolute;
-    bottom: -10px;
-    right: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.exibition-item__avatar {
+  position: absolute;
+  bottom: -10px;
+  right: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-.exibition-item__avatar>img{
-
-    border-radius: 50%;
-    min-height: 48px;
-    min-width: 48px;
-    background-color: #fdf9f9a8;
+.exibition-item__avatar > img {
+  border-radius: 50%;
+  min-height: 48px;
+  min-width: 48px;
+  background-color: #fdf9f9a8;
 }
-.exibition-item__avatar>h6{
-    background: orange;
-    padding: 5px 10px;
-    border-radius: 3px;
-    margin: 0;
+.exibition-item__avatar > h6 {
+  background: orange;
+  padding: 5px 10px;
+  border-radius: 3px;
+  margin: 0;
 }
 </style>
--- src/views/network/exhibition-page/index.vue
+++ src/views/network/exhibition-page/index.vue
@@ -1,223 +1,282 @@
 <template>
-    <div style="margin-top: 96px;">
-        <d-overlays-simple v-if="loading" />
-        <div v-else-if="hasError">
-            {{ $t('has-error') }}
-        </div>
-        <div v-else class="container exhibition-page">
-            <div class=" position-relative">
-                <img class="w-100" :src="itemPage.image" :alt="itemPage.title" height="432">
-                <div class="d-flex gap-2 position-absolute bottom-0 data">
-                    <template v-if="itemPage.categories && itemPage.categories.length">
-                        <p v-for="(cat, c) in itemPage.categories" :key="c" :style="{ 'background-color': colors[c % 3] }"
-                            class="p-1 px-2 rounded-2 text-white m-0">
-                            {{ cat.name }}
-                        </p>
-                    </template>
-                    <p v-else :style="{ 'background-color': colors[0] }" class="p-1 px-2 rounded-2 text-white m-0">
-                        {{ itemPage.categoryName }}
-                    </p>
-                </div>
-            </div>
-            <div class="row mt-5">
-                <div class="row mb-3">
-                    <div class="col-md-6">
-
-                        <h3 class="m-c">
-                            {{ itemPage.title ?? 'معرض' }}
-                        </h3>
-                        <p v-if="!isOwner" class="t-c p-0 m-0">
-                            {{ $t('publish-date') }}: {{ itemPage.date_publish }}
-                        </p>
-                    </div>
-                    <div class="col-md-6  text-start ">
-                        <template v-if="isOwner">
-                            <button @click="openEditDialog" class="btn bg-main border-0 text-white p-2 mx-1">
-                                <img :src="`${publicPath}assets/svg/update.svg`" />
-                                {{ $t('modification') }}
-                            </button>
-                            <button @click="openDeleteDialog" class="btn bg-danger border-0 text-white p-2">
-                                <img :src="`${publicPath}assets/svg/trash-outline.svg`" />
-                                {{ $t('delete') }}
-                            </button>
-
-
-                        </template>
-                        <button @click="openDemandParticipateDialog" v-else class="btn-main">
-                            شارك في المعرض
-                        </button>
-                    </div>
-                </div>
-                <div class=" col-12 col-md-8">
-
-
-                    <p class="pargrapg content-word-break  mt-3" v-html="itemPage.content"></p>
-                    <div>
-                        <button v-if="isOwner" @click="router_push('network-exhibition-requests', { id: itemPage.id })"
-                            class="btn btn-show-exhibition-req">قائمة طلبات المشاركة في المعرض</button>
-                    </div>
-                    <div class="rounded-3 border mt-4 p-4">
-                        <h4 class="border-bottom">
-                            شارك المعرض
-                        </h4>
-                        <div class="d-flex gap-4 justify-content-center p-4 icon-social-exibition">
-                            <!--facebook-->
-                            <!-- <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large"  data-show-count="false">Tweet</a> -->
-
-                            <img class="h-100" :src="`${publicPath}assets/img/Twitter.png`" alt="" />
-                            <img class="h-100" :src="`${publicPath}assets/img/Instagram.png`" alt="" />
-                            <img class="h-100" :src="`${publicPath}assets/img/Linkedin.png`" alt="" />
-                            <img class="h-100" :src="`${publicPath}assets/img/Facebook.png`" alt="" />
-
-                            <!-- <span v-if="false"> <i  class="fab fa-instagram" ></i></span> -->
-
-                            <!-- <span v-if="false"> <i  class="fab fa-linkedin-in" ></i></span> -->
-                            <div class="fb-share-button" :data-href="currentUrl" data-layout="button_count">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-md-4">
-                    <d-user-info-li v-if="!isOwner" sizeImage="100" :member="itemPage.user_info" />
-                    <!--details-exhibition-->
-                    <detailsExhibitionSection :itemPage="itemPage" />
-                </div>
-            </div>
-        </div>
-        <!--dialogs-->
-        <ShareDialog />
-        <UpdateDialog @success="refreshPage()" />
-        <deleteDialog />
-
+  <div style="margin-top: 96px">
+    <d-overlays-simple v-if="loading" />
+    <div v-else-if="hasError">
+      {{ $t("has-error") }}
     </div>
-</template>
- 
-<script>
-import exhibitionsAPI from '@/services/api/exhibitions';
-import ShareDialog from '../exhibitions/parts/dialogs/share-exhibition.vue'
-import UpdateDialog from '../exhibitions/parts/dialogs/update-exhibition.vue'
-import deleteDialog from '../exhibitions/parts/dialogs/del-exhibition.vue'
-import detailsExhibitionSection from './details-exhibition.vue';
-export default {
-    name: 'exhibition-page',
-    components: {
-        detailsExhibitionSection,
-        UpdateDialog,
-        ShareDialog,
-        deleteDialog
-    },
-    data: () => {
-        return {
-            isOwner: false,
-            loading: true,
-            hasError: false,
-            itemPage: {},
-            colors: ['#F2631C', '#FFBC00', '#2C98B3']
-        }
-    },
-    methods: {
-        convertTime(time) {
-            if (!time) return time;
-            return time.substring(0, 5)
-        },
-        openDemandParticipatePage() {
-            this.$router.push(this.getRouteLocale('network-exhibition-demand-participate', { id: this.itemPage.id }))
-        },
-        openDemandParticipateDialog() {
-            let dataEvt = {
-                title: '',
-                description: `هل لديك منتجات تخص هذا المعرض وتود حجز بوث لك يمكنك من خلاله عرض منتجاتك ؟ فقط كل ما عليك فعله هو ملئ استمارة المشاركة وسنقوم بأرسالها الى صاحب المعرض ليختار المشاركين المناسبين له , نأمل انت تكون منهم بكل تأكيد !`,
-                image: `${this.publicPath}assets/img/Group 1171275567.png`,
-                btns: [
-                    { title: this.$t('fill-form'), action: this.openDemandParticipatePage }
-                ]
-            }
-            this.showConfirmMsg(dataEvt)
-            //this.fireOpenDialog('share-exhibition',this.itemPage)
-        },
-        openEditDialog() {
-            this.fireOpenDialog('update-dialog', this.itemPage)
-        },
-        openDeleteDialog() {
-            this.fireOpenDialog('delete-dialog', this.itemPage)
-        },
-        async initializing() {
-            this.loading = true;
-            this.hasError = false;
-            try {
-                let { data } = await exhibitionsAPI.getItem(this.$route.params.id)
-                if (data.success) {
-                    this.itemPage = data.data;
-                    this.itemPage.start_time = this.convertTime(this.itemPage.start_time)
-                    this.itemPage.end_time = this.convertTime(this.itemPage.end_time)
-                    this.isOwner = this.itemPage.user_info && this.itemPage.user_info.id == this.user.id
-                } else {
-                    this.hasError = true;
-                }
-            } catch (error) {
-                console.mylog('error', error)
-                console.mylog('error response', error.response)
-                this.hasError = true;
-            }
+    <div v-else class="container exhibition-page">
+      <div class="position-relative">
+        <img
+          class="w-100"
+          :src="itemPage.image"
+          :alt="itemPage.title"
+          height="432"
+        />
+        <div class="d-flex gap-2 position-absolute bottom-0 data">
+          <template v-if="itemPage.categories && itemPage.categories.length">
+            <p
+              v-for="(cat, c) in itemPage.categories"
+              :key="c"
+              :style="{ 'background-color': colors[c % 3] }"
+              class="p-1 px-2 rounded-2 text-white m-0"
+            >
+              {{ cat.name }}
+            </p>
+          </template>
+          <p
+            v-else
+            :style="{ 'background-color': colors[0] }"
+            class="p-1 px-2 rounded-2 text-white m-0"
+          >
+            {{ itemPage.categoryName }}
+          </p>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="row mb-3">
+          <div class="col-12">
+            <template v-if="isOwner">
+              <button
+                @click="openEditDialog"
+                class="btn bg-main border-0 text-white p-2 mx-1"
+              >
+                <img :src="`${publicPath}assets/svg/update.svg`" />
+                {{ $t("modification") }}
+              </button>
+              <button
+                @click="openDeleteDialog"
+                class="btn bg-danger border-0 text-white p-2"
+              >
+                <img :src="`${publicPath}assets/svg/trash-outline.svg`" />
+                {{ $t("delete") }}
+              </button>
+            </template>
+            <button
+              @click="openDemandParticipateDialog"
+              v-else
+              class="btn-main"
+            >
+              شارك في المعرض
+            </button>
+          </div>
+        </div>
+        <div class="col-12">
+          <h3 class="m-c">
+            {{ itemPage.title ?? "معرض" }}
+          </h3>
+          <p v-if="!isOwner" class="t-c p-0 m-0">
+            {{ $t("publish-date") }}: {{ itemPage.date_publish }}
+          </p>
+        </div>
+        <div class="col-12 col-md-8">
+          <p
+            class="pargrapg content-word-break mt-3"
+            v-html="itemPage.content"
+          ></p>
+          <div>
+            <button
+              v-if="isOwner"
+              @click="
+                router_push('network-exhibition-requests', { id: itemPage.id })
+              "
+              class="btn btn-show-exhibition-req"
+            >
+              قائمة طلبات المشاركة في المعرض
+            </button>
+          </div>
+          <div class="rounded-3 border mt-4 p-4">
+            <h4 class="border-bottom">شارك المعرض</h4>
+            <div
+              class="d-flex gap-4 justify-content-center p-4 icon-social-exibition"
+            >
+              <!--facebook-->
+              <!-- <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large"  data-show-count="false">Tweet</a> -->
 
-            this.loading = false;
-        }
+              <img
+                class="h-100"
+                :src="`${publicPath}assets/img/Twitter.png`"
+                alt=""
+              />
+              <img
+                class="h-100"
+                :src="`${publicPath}assets/img/Instagram.png`"
+                alt=""
+              />
+              <img
+                class="h-100"
+                :src="`${publicPath}assets/img/Linkedin.png`"
+                alt=""
+              />
+              <img
+                class="h-100"
+                :src="`${publicPath}assets/img/Facebook.png`"
+                alt=""
+              />
+
+              <!-- <span v-if="false"> <i  class="fab fa-instagram" ></i></span> -->
+
+              <!-- <span v-if="false"> <i  class="fab fa-linkedin-in" ></i></span> -->
+              <div
+                class="fb-share-button"
+                :data-href="currentUrl"
+                data-layout="button_count"
+              ></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4">
+          <d-user-info-li
+            v-if="!isOwner"
+            sizeImage="100"
+            :member="itemPage.user_info"
+          />
+          <!--details-exhibition-->
+          <detailsExhibitionSection :itemPage="itemPage" />
+        </div>
+      </div>
+    </div>
+    <!--dialogs-->
+    <ShareDialog />
+    <UpdateDialog @success="refreshPage()" />
+    <deleteDialog />
+  </div>
+</template>
+
+<script>
+import exhibitionsAPI from "@/services/api/exhibitions";
+import ShareDialog from "../exhibitions/parts/dialogs/share-exhibition.vue";
+import UpdateDialog from "../exhibitions/parts/dialogs/update-exhibition.vue";
+import deleteDialog from "../exhibitions/parts/dialogs/del-exhibition.vue";
+import detailsExhibitionSection from "./details-exhibition.vue";
+export default {
+  name: "exhibition-page",
+  components: {
+    detailsExhibitionSection,
+    UpdateDialog,
+    ShareDialog,
+    deleteDialog,
+  },
+  data: () => {
+    return {
+      isOwner: false,
+      loading: true,
+      hasError: false,
+      itemPage: {},
+      colors: ["#F2631C", "#FFBC00", "#2C98B3"],
+    };
+  },
+  methods: {
+    convertTime(time) {
+      if (!time) return time;
+      return time.substring(0, 5);
     },
-    mounted() {
-        console.mylog('route', this.$route, this.$router)
-        this.initializing()
-        this.loadJS('https://platform.twitter.com/widgets.js');
-        this.addScriptJs(`(function(d, s, id) {
+    openDemandParticipatePage() {
+      this.$router.push(
+        this.getRouteLocale("network-exhibition-demand-participate", {
+          id: this.itemPage.id,
+        })
+      );
+    },
+    openDemandParticipateDialog() {
+      let dataEvt = {
+        title: "",
+        description: `هل لديك منتجات تخص هذا المعرض وتود حجز بوث لك يمكنك من خلاله عرض منتجاتك ؟ فقط كل ما عليك فعله هو ملئ استمارة المشاركة وسنقوم بأرسالها الى صاحب المعرض ليختار المشاركين المناسبين له , نأمل انت تكون منهم بكل تأكيد !`,
+        image: `${this.publicPath}assets/img/Group 1171275567.png`,
+        btns: [
+          {
+            title: this.$t("fill-form"),
+            action: this.openDemandParticipatePage,
+          },
+        ],
+      };
+      this.showConfirmMsg(dataEvt);
+      //this.fireOpenDialog('share-exhibition',this.itemPage)
+    },
+    openEditDialog() {
+      this.fireOpenDialog("update-dialog", this.itemPage);
+    },
+    openDeleteDialog() {
+      this.fireOpenDialog("delete-dialog", this.itemPage);
+    },
+    async initializing() {
+      this.loading = true;
+      this.hasError = false;
+      try {
+        let { data } = await exhibitionsAPI.getItem(this.$route.params.id);
+        if (data.success) {
+          this.itemPage = data.data;
+          this.itemPage.start_time = this.convertTime(this.itemPage.start_time);
+          this.itemPage.end_time = this.convertTime(this.itemPage.end_time);
+          this.isOwner =
+            this.itemPage.user_info &&
+            this.itemPage.user_info.id == this.user.id;
+        } else {
+          this.hasError = true;
+        }
+      } catch (error) {
+        console.mylog("error", error);
+        console.mylog("error response", error.response);
+        this.hasError = true;
+      }
+
+      this.loading = false;
+    },
+  },
+  mounted() {
+    console.mylog("route", this.$route, this.$router);
+    this.initializing();
+    this.loadJS("https://platform.twitter.com/widgets.js");
+    this.addScriptJs(`(function(d, s, id) {
 var js, fjs = d.getElementsByTagName(s)[0];
 if (d.getElementById(id)) return;
 js = d.createElement(s); js.id = id;
 js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
 fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));`)
-    }
-}
+}(document, 'script', 'facebook-jssdk'));`);
+  },
+};
 </script>
 
 <style>
 .btn-show-exhibition-req {
-    width: 617px;
-    height: 108px;
-    background: rgba(31, 185, 179, 0.1);
-    /* CDD7D8 */
+  width: 617px;
+  height: 108px;
+  background: rgba(31, 185, 179, 0.1);
+  /* CDD7D8 */
 
-    border: 0.5px solid #CDD7D8;
-    /* box */
+  border: 0.5px solid #cdd7d8;
+  /* box */
 
-    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.12);
-    border-radius: 8px;
-    /** text */
-    font-weight: 400;
-    font-size: 24px;
-    line-height: 40px;
-    /* identical to box height, or 167% */
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.12);
+  border-radius: 8px;
+  /** text */
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 40px;
+  /* identical to box height, or 167% */
 
-    text-align: center;
-    text-transform: capitalize;
+  text-align: center;
+  text-transform: capitalize;
 
-    /* Dark gray */
+  /* Dark gray */
 
-    color: #414042;
+  color: #414042;
 }
 
-.icon-social-exibition>span,
+.icon-social-exibition > span,
 .icon-social-exibition span {
-    background: #979797;
-    text-align: center;
-    display: inline-block;
-    height: 56px;
-    width: 56px;
-    font-size: 40px;
-    color: white;
-    -webkit-border-radius: 50%;
-    -moz-border-radius: 50%;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  background: #979797;
+  text-align: center;
+  display: inline-block;
+  height: 56px;
+  width: 56px;
+  font-size: 40px;
+  color: white;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
