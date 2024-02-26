00afca84a168c22af8e6399899dd8f86d8edd091
omarsabri666 <101377155+omarsabri666@users.noreply.github.com>
2024-02-26 15:12:51 +0200

    network responsive/guest

--- src/views/network/home/guest/parts/section-category/index.vue
+++ src/views/network/home/guest/parts/section-category/index.vue
@@ -1,5 +1,66 @@
 <template>
-    <section class="container d-flex flex-row flex-nowrap justify-content-between">
+  <section class="container">
+    <div class="row">
+      <div
+        class="box col-md-4 col-12 rounded-2 p-3 d-flex gap-1"
+        style="background-color: #eee"
+      >
+        <img
+          style="object-fit: contain; width: 146px"
+          src="@/assets/img/new/network/Group 58.png"
+          alt=""
+        />
+        <div>
+          <h2>عروض و خصومات</h2>
+          <button
+            class="upgrade-button"
+            @click="$router.push({ name: 'network' })"
+          >
+            عرض التفاصيل
+          </button>
+        </div>
+      </div>
+      <div
+        class="box col-md-4 col-12 rounded-2 p-3 d-flex gap-1"
+        style="background-color: #eee"
+      >
+        <img
+          style="object-fit: contain; width: 146px"
+          src="@/assets/img/new/network/Frame.png"
+          alt=""
+        />
+        <div>
+          <h2>اماكن عمل</h2>
+          <button
+            class="upgrade-button"
+            @click="$router.push({ name: 'workspaces-home' })"
+          >
+            عرض التفاصيل
+          </button>
+        </div>
+      </div>
+      <div
+        class="box col-md-4 col-12 rounded-2 p-3 d-flex gap-1"
+        style="background-color: #eee"
+      >
+        <img
+          style="object-fit: contain; width: 146px"
+          src="@/assets/img/new/network/Group 5.png"
+          alt=""
+        />
+        <div>
+          <h2>مشاريع استثماريه</h2>
+          <button
+            class="upgrade-button"
+            @click="$router.push({ name: 'network-investment-project' })"
+          >
+            عرض التفاصيل
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- <section class="container d-flex flex-row flex-nowrap justify-content-between">
         <div class="box rounded-2 p-3 d-flex gap-1" style="background-color: #eee;">
             <img style="object-fit: contain; width: 146px" src="@/assets/img/new/network/Group 58.png" alt="">
             <div>
@@ -33,33 +94,31 @@
                 </button>
             </div>
         </div>
-    </section>
+    </section> -->
 </template>
 <script>
-
 export default {
-    name: 'section-category'
-}
+  name: "section-category",
+};
 </script>
 
 <style scoped>
 .upgrade-button {
-    padding: 12px 30px;
-    font-size: 20px;
-    font-weight: 600;
-    border: none;
-    border-radius: 50px;
-    cursor: pointer;
-    outline: none;
-    width: 180px;
-    transition: all 0.3s;
-    background: var(--pc);
-    color: #fff;
-    margin-bottom: 20px;
-
+  padding: 12px 30px;
+  font-size: 20px;
+  font-weight: 600;
+  border: none;
+  border-radius: 50px;
+  cursor: pointer;
+  outline: none;
+  width: 180px;
+  transition: all 0.3s;
+  background: var(--pc);
+  color: #fff;
+  margin-bottom: 20px;
 }
 
 .upgrade-button:hover {
-    opacity: 0.8;
+  opacity: 0.8;
 }
 </style>
--- src/views/network/home/guest/parts/section-subscribe/index.vue
+++ src/views/network/home/guest/parts/section-subscribe/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <!-- <div class="row mt-4">
+  <div>
+    <!-- <div class="row mt-4">
             <ul class="nav nav-pills mb-3 justify-content-center text-center">
                 <li class="nav-item" style="width: 150px">
                     <router-link :to="{ name: 'network-subscribe' }" class="nav-link active border t-c ">
@@ -21,6 +21,80 @@
             </ul>
         </div> -->
 
+    <div class="container p-5 mt-3">
+      <div
+        class="mb-3 mt-2 flex-column justify-content-center align-items-center d-flex"
+      >
+        <h1>اختر الخطه المناسبه لك</h1>
+        <br />
+        <span> اختر الخطه التى تناسبك, لا تتردد فى الاتصال بنا </span>
+      </div>
+    </div>
+
+    <div class="container py-5">
+      <div class="row position-relative">
+        <div
+          v-for="(pack, p) in packages"
+          :key="p"
+          class="mt-2 col-md-4 col-12"
+        >
+          <!-- <SectionCompany
+            class="mb-4"
+            v-if="pack && pack.name.includes('شرك')"
+          /> -->
+          <div class="">
+            <div class="mb-4">
+              <NetworkSubCard
+                :itemId="pack.name"
+                :pack="pack"
+                :title="pack.name"
+                :price="pack.price"
+                :features="pack.options.map((c) => c.name_ar)"
+                :type-subscribe="pack.name"
+                @chosed="choose"
+                :subscribed="subscribedType"
+                :typeSectionSub="'network'"
+              >
+              </NetworkSubCard>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="row">
+        <div class="col-md-4 col-12">
+          <SubscribeCard1 />
+        </div>
+        <div class="col-md-4 col-12">
+          <SubscribeCard2 />
+        </div>
+        <div class="col-md-4 col-12">
+          <SubscribeCard3 />
+        </div>
+      </div> -->
+    </div>
+  </div>
+  <!-- <div>
+         <div class="row mt-4">
+            <ul class="nav nav-pills mb-3 justify-content-center text-center">
+                <li class="nav-item" style="width: 150px">
+                    <router-link :to="{ name: 'network-subscribe' }" class="nav-link active border t-c ">
+                        الشبكه
+                    </router-link>
+                </li>
+                <li class="nav-item" style="width: 150px">
+                    <router-link :to="{ name: 'academy-subscribe' }" class="nav-link  border t-c ">
+                        الاكاديميه
+                    </router-link>
+                </li>
+
+                <li class="nav-item" style="width: 150px">
+                    <router-link :to="{ name: 'incubator-subscribe' }" class="nav-link  border t-c ">
+                        الحاضنه
+                    </router-link>
+                </li>
+            </ul>
+        </div> 
+
 
         <div class="container p-5 mt-3">
             <div class="mb-3 mt-2 flex-column justify-content-center align-items-center d-flex">
@@ -38,121 +112,133 @@
             <SubscribeCard2 />
             <SubscribeCard3 />
         </div>
-    </div>
+    </div> -->
 </template>
 <style scoped>
 ul li {
-    background-color: #eee;
+  background-color: #eee;
 }
 
 ul li.active {
-    background: var(--pc);
-    color: white;
+  background: var(--pc);
+  color: white;
 }
 </style>
 <script>
-import SubscribeCard1 from './subscribe-card1.vue';
-import SubscribeCard2 from './subscribe-card2.vue';
-import SubscribeCard3 from './subscribe-card3.vue';
-import CardVue from '@/components/cards/incubator-dept-circle.vue'
-import networkAPI from '@/services/api/network.js';
-import SectionHeader from "./parts/section-header/index.vue"
-import SectionCompany from "./parts/section-company/index.vue"
-
-
+import SubscribeCard1 from "./subscribe-card1.vue";
+import SubscribeCard2 from "./subscribe-card2.vue";
+import SubscribeCard3 from "./subscribe-card3.vue";
+import CardVue from "@/components/cards/incubator-dept-circle.vue";
+import networkAPI from "@/services/api/network.js";
+import SectionHeader from "./parts/section-header/index.vue";
+import SectionCompany from "./parts/section-company/index.vue";
+import NetworkSubCard from "@/components/network-sub-card.vue";
 
 export default {
-    name: 'network-subscribe',
-    components: {
-        SubscribeCard1,
-        SubscribeCard2,
-        SubscribeCard3,
-        CardVue,
-        SectionHeader,
-        SectionCompany,
-
+  name: "network-subscribe",
+  components: {
+    NetworkSubCard,
+    SubscribeCard1,
+    SubscribeCard2,
+    SubscribeCard3,
+    CardVue,
+    SectionHeader,
+    SectionCompany,
+  },
+  data: () => ({
+    fieldIds: [],
+    packages: [],
+    subscribedType: "",
+  }),
+  methods: {
+    getTypePackage(data) {
+      switch (data.type) {
+        case "free":
+          return this.$t("free");
+        case "month":
+          return this.$t("monthly");
+        case "year":
+          return this.$t("annually");
+        default:
+          return "N/A";
+      }
     },
-    data: () => ({
-        fieldIds: [],
-        packages: [],
-        subscribedType: ''
-    }),
-    methods: {
-        getTypePackage(data) {
-            switch (data.type) {
-                case 'free': return this.$t('free');
-                case 'month': return this.$t('monthly');
-                case 'year': return this.$t('annually');
-                default:
-                    return 'N/A';
-            }
-        },
-        async choose(selectedPackage) {
-            if (selectedPackage.type == 'free') {
-                try {
-                    let { data } = await networkAPI.checkoutPackageFree({ package_id: selectedPackage.id });
-                    if (data.success) {
-                        console.log('itsfree', data.data)
-                    } else {
-                        window.SwalError(data.message)
-                    }
-                } catch (error) {
-                    console.log('error', error)
-                }
-
-            } else {
-                try {
-                    let { data } = await networkAPI.PayPackageSelect({
-                        package_id: selectedPackage.id,
-                        user_id: this.user.id
-                    });
-                    if (data.success) {
-                        window.location.href = data.data.payment_url;
-                    } else {
-                        window.SwalError(data.message)
-                    }
-                } catch (error) {
-                    console.log('error', error)
-                }
-            }
-        },
-
-        async loadPackages() {
-            try {
-                let { data } = await networkAPI.getPackages();
-                if (data.success) {
-                    this.packages = data.data
-                }
-            } catch (error) {
-                console.log('error', error)
-            }
-        },
-        checkSubscriptionOptions() {
-            for (let index = 0; index < this.user.subscription_options.length; index++) {
-                const element = this.user.subscription_options[index];
-                if (element.key == 'show_exhibitions') {
-                    console.log('444', element);
-
-                }
-            }
-        },
-
-        checkTypePackage() {
-            let date = this.dateToString(new Date());
-            if (this.user.system_subscriptions) {
-                for (let index = 0; index < this.user.system_subscriptions.length; index++) {
-                    const element = this.user.system_subscriptions[index];
-                    if (element.system_package.related_to.key == 'network' && element.end_at > date) {
-                        this.subscribedType = element.system_package.id
-                    }
-                }
-            }
-        },
+    async choose(selectedPackage) {
+      if (selectedPackage.type == "free") {
+        try {
+          let { data } = await networkAPI.checkoutPackageFree({
+            package_id: selectedPackage.id,
+          });
+          if (data.success) {
+            console.log("itsfree", data.data);
+          } else {
+            window.SwalError(data.message);
+          }
+        } catch (error) {
+          console.log("error", error);
+        }
+      } else {
+        try {
+          let { data } = await networkAPI.PayPackageSelect({
+            package_id: selectedPackage.id,
+            user_id: this.user.id,
+          });
+          if (data.success) {
+            window.location.href = data.data.payment_url;
+          } else {
+            window.SwalError(data.message);
+          }
+        } catch (error) {
+          console.log("error", error);
+        }
+      }
     },
-    mounted() {
-        this.loadPackages()
-        this.checkTypePackage()
-    }
-}
+
+    async loadPackages() {
+      try {
+        let { data } = await networkAPI.getPackages();
+        if (data.success) {
+          this.packages = data.data;
+        }
+      } catch (error) {
+        console.log("error", error);
+      }
+    },
+    checkSubscriptionOptions() {
+      for (
+        let index = 0;
+        index < this.user.subscription_options.length;
+        index++
+      ) {
+        const element = this.user.subscription_options[index];
+        if (element.key == "show_exhibitions") {
+          console.log("444", element);
+        }
+      }
+    },
+
+    checkTypePackage() {
+      let date = this.dateToString(new Date());
+      if (this.user.system_subscriptions) {
+        for (
+          let index = 0;
+          index < this.user.system_subscriptions.length;
+          index++
+        ) {
+          const element = this.user.system_subscriptions[index];
+          if (
+            element.system_package.related_to.key == "network" &&
+            element.end_at > date
+          ) {
+            this.subscribedType = element.system_package.id;
+          }
+        }
+      }
+    },
+  },
+  mounted() {
+    this.loadPackages();
+    this.checkTypePackage();
+  },
+};
 </script>
-
