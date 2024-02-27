597bab2d847a638a1f9cbdf85d940444668442c0
omarsabri666 <101377155+omarsabri666@users.noreply.github.com>
2024-02-27 14:38:09 +0200

    slider fixed

--- src/views/incubator/home/parts/section-departments/index.vue
+++ src/views/incubator/home/parts/section-departments/index.vue
@@ -1,55 +1,49 @@
 <template>
-   <div class="sec-five2 mt-5 p-5 bg-transparent">
-            <div class="">
-              <div
-                class="d-flex justify-content-center align-items-center"
-              >
-                <h1 class="home-section-title">
-                    تعرفي على مجالات الحاضنة           
-                </h1>
-        
-              </div>
-              <DSwiper
-              v-if="!loading"
-            :slides-per-view="6"
-            :space-between="5"
-              
-              :navigation="true"
-              :items="items"
-            >
-       
-            <template  v-slot:default="{item}" >
-                <CardVue :title="item.name" :img="item.image_path"
-                :url="getRouteLocale('incubator-program-incubator',{id:item.id})"
-
-                />
-                </template>
-                </DSwiper>
-                
-            </div>
-          </div>
+  <div class="sec-five2 mt-5 p-5 bg-transparent">
+    <div class="">
+      <div class="d-flex justify-content-center align-items-center">
+        <h1 class="home-section-title">تعرفي على مجالات الحاضنة</h1>
+      </div>
+      <DSwiper
+        v-if="!loading"
+        :slides-per-view="calculateSlidesPerView()"
+        :space-between="5"
+        :navigation="true"
+        :items="items"
+      >
+        <template v-slot:default="{ item }">
+          <CardVue
+            :title="item.name"
+            :img="item.image_path"
+            :url="
+              getRouteLocale('incubator-program-incubator', { id: item.id })
+            "
+          />
+        </template>
+      </DSwiper>
+    </div>
+  </div>
 </template>
 <style>
-
-.sec-five2{
-  background-color: #E9F8F7 !important;
+.sec-five2 {
+  background-color: #e9f8f7 !important;
 }
 </style>
 <script>
-import DSwiper from '@/components/swiper/index.vue'
-import CardVue from '@/components/cards/incubator-dept-circle.vue'
-import incubatorAPI from '@/services/api/incubator';
+import DSwiper from "@/components/swiper/index.vue";
+import CardVue from "@/components/cards/incubator-dept-circle.vue";
+import incubatorAPI from "@/services/api/incubator";
 export default {
-    name:'section-incubation-departments',
-    components:{
-        DSwiper,
-        CardVue
-    },
-    data:(/*vm*/)=>{
-      return{
+  name: "section-incubation-departments",
+  components: {
+    DSwiper,
+    CardVue,
+  },
+  data: (/*vm*/) => {
+    return {
       loading: false,
-        items:[
-          /*
+      items: [
+        /*
         {name:'المجوهرات',image_path:`${vm.publicPath}assets/svg/jewelry.svg`},
             {name:'الازيــــــاء',image_path:`${vm.publicPath}assets/svg/fashion.svg`},
             {name:'التقنية',image_path:`${vm.publicPath}assets/svg/techinic.svg`},
@@ -65,30 +59,42 @@ export default {
             {name:'الاستراتيجية والقيادة',image_path:`${vm.publicPath}assets/svg/stratigy-leadership.svg`},
             {name:'الاستراتيجية والقيادة',image_path:`${vm.publicPath}assets/svg/stratigy-leadership.svg`},
 */
-    ]
-  }
+      ],
+    };
   },
-    methods:{
-    async initializing(){
-      this.loading  = true;
+  methods: {
+    async initializing() {
+      this.loading = true;
       try {
-        let { data } = await incubatorAPI.getDepartments({paginate:14})
-        if(data.success){
-          this.items = data.data
+        let { data } = await incubatorAPI.getDepartments({ paginate: 14 });
+        if (data.success) {
+          this.items = data.data;
         }
       } catch (error) {
-          console.mylog('error',error)
+        console.mylog("error", error);
         //
       }
-      this.loading  = false;
-    }
+      this.loading = false;
     },
-    mounted(){
-      this.initializing()
-    }
-}
+    calculateSlidesPerView() {
+      const screenWidth = window.innerWidth;
+      const breakpoints = [576, 768, 992, 1200];
+      const slidesPerViewValues = [1, 2, 3, 4, 5];
+
+      for (let i = breakpoints.length - 1; i >= 0; i--) {
+        if (screenWidth >= breakpoints[i]) {
+          return slidesPerViewValues[i];
+        }
+      }
+
+      return slidesPerViewValues[0];
+    },
+  },
+
+  mounted() {
+    this.initializing();
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
