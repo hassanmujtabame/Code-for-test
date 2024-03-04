683d222885bfaab986d1f50e1a5f6fe276a6485e
omarsabri666 <101377155+omarsabri666@users.noreply.github.com>
2024-03-04 17:48:49 +0200

    dashboard/academy

--- src/views/academy/dashboard/index.vue
+++ src/views/academy/dashboard/index.vue
@@ -1,29 +1,36 @@
 <template>
-  <div style="margin-top:85px">
+  <div style="margin-top: 85px">
     <div class="container dashbord">
-      <SectionHeader />
       <SectionBalance />
+      <!-- <div>
+
+      <SectionHeader />
+      </div> -->
       <SectionWidgets v-if="userAcademyRole == 'student'" />
       <SectionInstructorWidgets v-if="userAcademyRole == 'instructor'" />
+      <div>
+        <SectionAsk />
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import SectionHeader from './parts/section-header/index.vue'
-import SectionBalance from './parts/section-balance/index.vue'
-import SectionWidgets from './parts/section-widgets/index.vue'
-import SectionInstructorWidgets from './parts/section-instructor-widgets/index.vue'
+import SectionHeader from "./parts/section-header/index.vue";
+import SectionBalance from "./parts/section-balance/index.vue";
+import SectionWidgets from "./parts/section-widgets/index.vue";
+import SectionAsk from "@/components/ask-us.vue";
+import SectionInstructorWidgets from "./parts/section-instructor-widgets/index.vue";
 export default {
-  name: 'your-courses-page',
+  name: "your-courses-page",
   components: {
     SectionHeader,
     SectionBalance,
+    SectionAsk,
     SectionWidgets,
-    SectionInstructorWidgets
-  }
-}
+    SectionInstructorWidgets,
+  },
+};
 </script>
 
 <style></style>
