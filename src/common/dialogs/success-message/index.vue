82600c0745dc74d45bf166f0d6a42afaad8b3a3f
omarsabri666 <101377155+omarsabri666@users.noreply.github.com>
2024-03-20 19:28:02 +0200

    academy

--- src/common/dialogs/success-message/index.vue
+++ src/common/dialogs/success-message/index.vue
@@ -1,7 +1,51 @@
 <template>
-  <d-dialog-large :group="group" :xl="false" :openDialog="openDialog" :closeDialog="closeDialog">
+  <d-dialog-large
+    :group="group"
+    :xl="false"
+    :openDialog="openDialog"
+    :closeDialog="closeDialog"
+  >
     <template v-slot>
       <div class="text-center" v-if="showed">
+        <div v-if="!itemDialog.icon">
+          <img :src="itemDialog.image" alt="" />
+        </div>
+        <div>
+          <h4 :class="[itemDialog.titleClass ?? '']">
+            {{ itemDialog.title }}
+          </h4>
+          <p
+            class="t-c fs-r-16-24"
+            v-html="itemDialog.description"
+            :class="[itemDialog.descriptionClass ?? '']"
+          ></p>
+        </div>
+        <div v-if="itemDialog.icon">
+          <img :src="itemDialog.iconUrl" alt="" />
+        </div>
+        <div v-if="itemDialog.btns" :class="[itemDialog.groupBtns ?? '']">
+          <div v-for="(btn, i) in itemDialog.btns" :key="i" class="mt-3">
+            <button
+              @click="callAction(btn)"
+              style="height: 40px"
+              :class="[btn.class ?? 'btn btn-custmer']"
+            >
+              {{ btn.title }}
+            </button>
+          </div>
+        </div>
+        <div v-else-if="itemDialog.hideBtn" class="mt-3"></div>
+        <div v-else class="mt-3">
+          <button
+            @click="closeEvent"
+            style="height: 40px"
+            class="btn btn-custmer"
+          >
+            {{ $t("Home") }}
+          </button>
+        </div>
+      </div>
+      <!-- <div class="text-center" v-if="showed">
         <div v-if="!itemDialog.icon">
           <img :src="itemDialog.image" alt="">
         </div>
@@ -28,64 +72,68 @@
             {{ $t('Home') }}</button>
         </div>
 
-      </div>
+      </div> -->
     </template>
-
   </d-dialog-large>
 </template>
 
 <script>
 export default {
-  name: 'standard-success-message',
+  name: "standard-success-message",
   props: {
     group: {
       type: String,
-      default: 'standard-success-message'
-    }
+      default: "standard-success-message",
+    },
   },
   data: (vm) => {
     return {
-      itemDialog: { title: null, description: null, btns: null, image: `${vm.publicPath}assets/img/cuate-2.png` },
+      itemDialog: {
+        title: null,
+        description: null,
+        btns: null,
+        image: `${vm.publicPath}assets/img/cuate-2.png`,
+      },
       showed: false,
-    }
+    };
   },
   methods: {
     async callAction(btn) {
       if (btn.action) {
         btn.loading = true;
-        await btn.action()
+        await btn.action();
         btn.loading = false;
       }
-      this.closeEvent()
+      this.closeEvent();
     },
     openDialog(data) {
       this.itemDialog.onClose = null;
       if (data.btns) {
-        data.btns.forEach(btn => btn.loading = false)
+        data.btns.forEach((btn) => (btn.loading = false));
       }
       this.itemDialog = Object.assign({}, data);
       if (!data.btns) this.itemDialog.btns = null;
 
-
-      if (!data.image) this.itemDialog.image = `${this.publicPath}assets/img/cuate-2.png`
-      if (!data.iconUrl) this.itemDialog.iconUrl = `${this.publicPath}assets/svg/tick-square.svg`
-      this.showed = true
+      if (!data.image)
+        this.itemDialog.image = `${this.publicPath}assets/img/cuate-2.png`;
+      if (!data.iconUrl)
+        this.itemDialog.iconUrl = `${this.publicPath}assets/svg/tick-square.svg`;
+      this.showed = true;
       return true;
     },
     closeDialog(evt, btn) {
-      console.mylog(evt, btn, this.itemDialog)
+      console.mylog(evt, btn, this.itemDialog);
       this.showed = false;
-      if (this.itemDialog.onClose && btn)
-        this.itemDialog.onClose()
+      if (this.itemDialog.onClose && btn) this.itemDialog.onClose();
       this.itemDialog.onClose = null;
       return true;
     },
     closeEvent() {
-      this.fireEvent(this.group + '-close-dialog')
+      this.fireEvent(this.group + "-close-dialog");
       // this.$router.push('/')
-    }
-  }
-}
+    },
+  },
+};
 </script>
-  
+
 <style></style>
--- src/views/academy/common-components/section-continue-learning/index.vue
+++ src/views/academy/common-components/section-continue-learning/index.vue
@@ -1,5 +1,111 @@
 <template>
-    <div class="main-top py-2 px-5 position-relative mt-5">
+  <div
+    style="background-color: #1fb9b3"
+    class="py-2 px-5 position-relative mt-5"
+  >
+    <div class="row align-items-center text-center m-auto">
+      <div class="col-md-6 mt-4">
+        <h2 class="text-white fs-1">
+          أكملي دوراتك، مراسلاتك وشاركي فـــــــي الشبكة والحاضنة من خلال تطبيق
+          رياديــــــات
+        </h2>
+        <div class="mt-4">
+          <a href="">
+            <img
+              :src="`${publicPath}assets/img/34-349265_app-store-google-play-svg 3.png`"
+              alt=""
+            />
+          </a>
+          <a href="">
+            <img
+              :src="`${publicPath}assets/img/34-349265_app-store-google-play-svg 1.png`"
+              alt=""
+            />
+          </a>
+        </div>
+      </div>
+      <div class="col-md-6 mt-2">
+        <div class="">
+          <img
+            class="img-fluid"
+            :src="`${publicPath}assets/img/mobile-screen 1.png`"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="star position-absolute">
+      <img class="landing" :src="`${publicPath}assets/svg/start.svg`" alt="" />
+    </div>
+    <div class="square position-absolute">
+      <img class="landing" :src="`${publicPath}assets/img/square.png`" alt="" />
+    </div>
+    <div class="star-two position-absolute">
+      <img class="landing" :src="`${publicPath}assets/svg/start.svg`" alt="" />
+    </div>
+    <div class="star-big position-absolute">
+      <img
+        class="landing"
+        :src="`${publicPath}assets/svg/star-big.svg`"
+        alt=""
+      />
+    </div>
+  </div>
+  <!-- <div
+    style="background-color: #1fb9b3"
+    class="py-2 px-5 position-relative mt-5"
+  >
+    <div class="row align-items-center text-center m-auto">
+      <div class="col-md-6 mt-4">
+        <h2 class="text-white fs-1">
+          أكملي دوراتك، مراسلاتك وشاركي فـــــــي الشبكة والحاضنة من خلال تطبيق
+          رياديــــــات
+        </h2>
+        <div class="mt-4">
+          <a href="">
+            <img
+              :src="`${publicPath}assets/img/34-349265_app-store-google-play-svg 3.png`"
+              alt=""
+            />
+          </a>
+          <a href="">
+            <img
+              :src="`${publicPath}assets/img/34-349265_app-store-google-play-svg 1.png`"
+              alt=""
+            />
+          </a>
+        </div>
+      </div>
+      <div class="col-md-6 mt-2">
+        <div class="">
+          <img
+            class="img-fluid"
+            :src="`${publicPath}assets/img/mobile-screen 1.png`"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="star position-absolute">
+      <img class="landing" :src="`${publicPath}assets/svg/start.svg`" alt="" />
+    </div>
+    <div class="square position-absolute">
+      <img class="landing" :src="`${publicPath}assets/img/square.png`" alt="" />
+    </div>
+    <div class="star-two position-absolute">
+      <img class="landing" :src="`${publicPath}assets/svg/start.svg`" alt="" />
+    </div>
+    <div class="star-big position-absolute">
+      <img
+        class="landing"
+        :src="`${publicPath}assets/svg/star-big.svg`"
+        alt=""
+      />
+    </div>
+  </div> -->
+  <!-- <div class="main-top py-2 px-5 position-relative mt-5">
         <div class="row align-items-center text-center m-auto">
             <div class="col-md-6 mt-2">
                 <div class="img-screen">
@@ -39,13 +145,13 @@
         <div class="star-big position-absolute">
             <img class="landing" :src="`${publicPath}assets/svg/star-big.svg`" alt="">
         </div>
-    </div>
+    </div> -->
 </template>
 
 <script>
 export default {
-    name: 'section-continue-learning'
-}
+  name: "section-continue-learning",
+};
 </script>
 
 <style></style>
