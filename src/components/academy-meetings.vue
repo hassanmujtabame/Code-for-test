51d4a0d64d778f6ee9098cfd725ca4c5a44bdc62
omarsabri666 <101377155+omarsabri666@users.noreply.github.com>
2024-03-20 15:22:20 +0200

    academy/meet

---
+++ src/components/academy-meetings.vue
@@ -0,0 +1,549 @@
+<template>
+  <div></div>
+  <!-- <div
+    style="
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      min-height: 250px;
+    "
+    class="d-flex gap-4 flex-column align-items-center shadow justify-content-center"
+  >
+    <div style="height: 164px; width: 100%" class="">
+      <router-link class="" v-if="to" :to="to">
+        <img
+          style="
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            height: 100%;
+            width: 100%;
+          "
+          class="img-fluid"
+          :src="img"
+          alt
+        />
+      </router-link>
+      <img
+        style="
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+          height: 100%;
+          width: 100%;
+        "
+        class="img-fluid"
+        v-else
+        @click="$emit('click-image')"
+        :src="img"
+        alt
+      />
+    </div>
+    <div style="min-height: 86px" class="text-center">
+      <h3>
+        {{ name }}
+      </h3>
+      <p style="color: #1fb9b3" class="my-2">
+        {{ description }}
+      </p>
+    </div>
+  </div> -->
+  <!-- <div
+    style="
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      min-height: 250px;
+    "
+    class="d-flex gap-4 flex-column align-items-center shadow justify-content-center"
+  >
+    <div style="height: 164px; width: 100%" class="">
+      <router-link class="" v-if="to" :to="to">
+        <img
+          style="
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            height: 100%;
+            width: 100%;
+          "
+          class="img-fluid"
+          :src="img"
+          alt
+        />
+      </router-link>
+      <img
+        style="
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+          height: 100%;
+          width: 100%;
+        "
+        class="img-fluid"
+        v-else
+        @click="$emit('click-image')"
+        :src="img"
+        alt
+      />
+    </div>
+    <div style="min-height: 86px" class="text-center">
+      <h3>
+        {{ name }}
+      </h3>
+      <p style="color: #1fb9b3" class="my-2">
+        {{ description }}
+      </p>
+    </div>
+  </div> -->
+  <!-- <div
+    style="
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      min-height: 250px;
+    "
+    class="d-flex gap-4 flex-column align-items-center shadow justify-content-center"
+  >
+    <div style="height: 164px; width: 100%" class="">
+      <router-link class="" v-if="to" :to="to">
+        <img
+          style="
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            height: 100%;
+            width: 100%;
+          "
+          class="img-fluid"
+          :src="img"
+          alt
+        />
+      </router-link>
+      <img
+        style="
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+          height: 100%;
+          width: 100%;
+        "
+        class="img-fluid"
+        v-else
+        @click="$emit('click-image')"
+        :src="img"
+        alt
+      />
+    </div>
+    <div style="min-height: 86px" class="text-center">
+      <h3>
+        {{ name }}
+      </h3>
+      <p style="color: #1fb9b3" class="my-2">
+        {{ description }}
+      </p>
+    </div>
+  </div> -->
+  <!-- <div
+    style="
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      min-height: 250px;
+    "
+    class="d-flex gap-4 flex-column align-items-center shadow justify-content-center"
+  >
+    <div style="height: 164px; width: 100%" class="">
+      <router-link class="" v-if="to" :to="to">
+        <img
+          style="
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            height: 100%;
+            width: 100%;
+          "
+          class="img-fluid"
+          :src="img"
+          alt
+        />
+      </router-link>
+      <img
+        style="
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+          height: 100%;
+          width: 100%;
+        "
+        class="img-fluid"
+        v-else
+        @click="$emit('click-image')"
+        :src="img"
+        alt
+      />
+    </div>
+    <div style="min-height: 86px" class="text-center">
+      <h3>
+        {{ name }}
+      </h3>
+      <p style="color: #1fb9b3" class="my-2">
+        {{ description }}
+      </p>
+    </div>
+  </div> -->
+  <!-- <div
+    style="
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      min-height: 250px;
+    "
+    class="d-flex gap-4 flex-column align-items-center shadow justify-content-center"
+  >
+    <div style="height: 164px; width: 100%" class="">
+      <router-link class="" v-if="to" :to="to">
+        <img
+          style="
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            height: 100%;
+            width: 100%;
+          "
+          class="img-fluid"
+          :src="img"
+          alt
+        />
+      </router-link>
+      <img
+        style="
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+          height: 100%;
+          width: 100%;
+        "
+        class="img-fluid"
+        v-else
+        @click="$emit('click-image')"
+        :src="img"
+        alt
+      />
+    </div>
+    <div style="min-height: 86px" class="text-center">
+      <h3>
+        {{ name }}
+      </h3>
+      <p style="color: #1fb9b3" class="my-2">
+        {{ description }}
+      </p>
+    </div>
+  </div> -->
+  <!-- <div
+    style="
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      min-height: 250px;
+    "
+    class="d-flex gap-4 flex-column align-items-center shadow justify-content-center"
+  >
+    <div style="height: 164px; width: 100%" class="">
+      <router-link class="" v-if="to" :to="to">
+        <img
+          style="
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            height: 100%;
+            width: 100%;
+          "
+          class="img-fluid"
+          :src="img"
+          alt
+        />
+      </router-link>
+      <img
+        style="
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+          height: 100%;
+          width: 100%;
+        "
+        class="img-fluid"
+        v-else
+        @click="$emit('click-image')"
+        :src="img"
+        alt
+      />
+    </div>
+    <div style="min-height: 86px" class="text-center">
+      <h3>
+        {{ name }}
+      </h3>
+      <p style="color: #1fb9b3" class="my-2">
+        {{ description }}
+      </p>
+    </div>
+  </div> -->
+  <!-- <div
+    style="
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      min-height: 250px;
+    "
+    class="d-flex gap-4 flex-column align-items-center shadow justify-content-center"
+  >
+    <div style="height: 164px; width: 100%" class="">
+      <router-link class="" v-if="to" :to="to">
+        <img
+          style="
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            height: 100%;
+            width: 100%;
+          "
+          class="img-fluid"
+          :src="img"
+          alt
+        />
+      </router-link>
+      <img
+        style="
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+          height: 100%;
+          width: 100%;
+        "
+        class="img-fluid"
+        v-else
+        @click="$emit('click-image')"
+        :src="img"
+        alt
+      />
+    </div>
+    <div style="min-height: 86px" class="text-center">
+      <h3>
+        {{ name }}
+      </h3>
+      <p style="color: #1fb9b3" class="my-2">
+        {{ description }}
+      </p>
+    </div>
+  </div> -->
+  <!-- <div
+    style="
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      min-height: 250px;
+    "
+    class="d-flex gap-4 flex-column align-items-center shadow justify-content-center"
+  >
+    <div style="height: 164px; width: 100%" class="">
+      <router-link class="" v-if="to" :to="to">
+        <img
+          style="
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            height: 100%;
+            width: 100%;
+          "
+          class="img-fluid"
+          :src="img"
+          alt
+        />
+      </router-link>
+      <img
+        style="
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+          height: 100%;
+          width: 100%;
+        "
+        class="img-fluid"
+        v-else
+        @click="$emit('click-image')"
+        :src="img"
+        alt
+      />
+    </div>
+    <div style="min-height: 86px" class="text-center">
+      <h3>
+        {{ name }}
+      </h3>
+      <p style="color: #1fb9b3" class="my-2">
+        {{ description }}
+      </p>
+    </div>
+  </div> -->
+  <!-- <div
+    style="
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      min-height: 250px;
+    "
+    class="d-flex gap-4 flex-column align-items-center shadow justify-content-center"
+  >
+    <div style="height: 164px; width: 100%" class="">
+      <router-link class="" v-if="to" :to="to">
+        <img
+          style="
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            height: 100%;
+            width: 100%;
+          "
+          class="img-fluid"
+          :src="img"
+          alt
+        />
+      </router-link>
+      <img
+        style="
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+          height: 100%;
+          width: 100%;
+        "
+        class="img-fluid"
+        v-else
+        @click="$emit('click-image')"
+        :src="img"
+        alt
+      />
+    </div>
+    <div style="min-height: 86px" class="text-center">
+      <h3>
+        {{ name }}
+      </h3>
+      <p style="color: #1fb9b3" class="my-2">
+        {{ description }}
+      </p>
+    </div>
+  </div> -->
+  <!-- <div
+    style="
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      min-height: 250px;
+    "
+    class="d-flex gap-4 flex-column align-items-center shadow justify-content-center"
+  >
+    <div style="height: 164px; width: 100%" class="">
+      <router-link class="" v-if="to" :to="to">
+        <img
+          style="
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            height: 100%;
+            width: 100%;
+          "
+          class="img-fluid"
+          :src="img"
+          alt
+        />
+      </router-link>
+      <img
+        style="
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+          height: 100%;
+          width: 100%;
+        "
+        class="img-fluid"
+        v-else
+        @click="$emit('click-image')"
+        :src="img"
+        alt
+      />
+    </div>
+    <div style="min-height: 86px" class="text-center">
+      <h3>
+        {{ name }}
+      </h3>
+      <p style="color: #1fb9b3" class="my-2">
+        {{ description }}
+      </p>
+    </div>
+  </div> -->
+  <!-- <div
+    style="
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      min-height: 250px;
+    "
+    class="d-flex gap-4 flex-column align-items-center shadow justify-content-center"
+  >
+    <div style="height: 164px; width: 100%" class="">
+      <router-link class="" v-if="to" :to="to">
+        <img
+          style="
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            height: 100%;
+            width: 100%;
+          "
+          class="img-fluid"
+          :src="img"
+          alt
+        />
+      </router-link>
+      <img
+        style="
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+          height: 100%;
+          width: 100%;
+        "
+        class="img-fluid"
+        v-else
+        @click="$emit('click-image')"
+        :src="img"
+        alt
+      />
+    </div>
+    <div style="min-height: 86px" class="text-center">
+      <h3>
+        {{ name }}
+      </h3>
+      <p style="color: #1fb9b3" class="my-2">
+        {{ description }}
+      </p>
+    </div>
+  </div> -->
+  <!-- <div
+    style="
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      min-height: 250px;
+    "
+    class="d-flex gap-4 flex-column align-items-center shadow justify-content-center"
+  >
+    <div style="height: 164px; width: 100%" class="">
+      <router-link class="" v-if="to" :to="to">
+        <img
+          style="
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            height: 100%;
+            width: 100%;
+          "
+          class="img-fluid"
+          :src="img"
+          alt
+        />
+      </router-link>
+      <img
+        style="
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+          height: 100%;
+          width: 100%;
+        "
+        class="img-fluid"
+        v-else
+        @click="$emit('click-image')"
+        :src="img"
+        alt
+      />
+    </div>
+    <div style="min-height: 86px" class="text-center">
+      <h3>
+        {{ name }}
+      </h3>
+      <p style="color: #1fb9b3" class="my-2">
+        {{ description }}
+      </p>
+    </div>
+  </div> -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
