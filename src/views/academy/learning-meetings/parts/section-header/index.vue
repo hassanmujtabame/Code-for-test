8a054886f714bd2095e7170d46767e3f7ac81939
hamdy elgazar <aljzarh46@gmail.com>
2024-03-23 13:23:43 +0200

    temp

--- src/views/academy/learning-meetings/parts/section-header/index.vue
+++ src/views/academy/learning-meetings/parts/section-header/index.vue
@@ -1,25 +1,19 @@
 <template>
-   <div class="consulting">
+    <div class="">
         <div class="container">
           <div class="row align-items-center justify-content-between">
             <div class="col-md-6">
-              <h1 class="fs-1 text-white">   لقاءات تعليمية  </h1>
-              <p class="text-white">
-                كابشن بسيط عن اللقاءات واهميتها في سطرين ببساطة     
-                كابشن بسيط عن اللقاءات واهميتها في سطرين ببساطة
-            </p>
+              <img class="img-fluid position-absolute" style="width: 280px; top: 120px" src="@/assets/img/academy/Path 2212.png" alt="" />
+              <img class="img-fluid position-absolute" style="width: 280px; top: 160px" src="@/assets/img/academy/Path 2212.png" alt="" />
+
+              <h1 class="fs-1 text-gray">   لقاءات تعليمية  </h1>
+              <p class="text-gray">لقاءات وندوات إلكترونية وحضورية في مجالات مختلفة لتطوير</p>
             </div>
             <div class="col-md-6">
-              <div style="overflow: inherit" class="m-auto text-start ">
-                <img class="img-fluid" :src="`${publicPath}assets/img/Frame-learning-metting.png`" alt="" />
+              <div style="overflow: inherit" class="m-auto">
+                <img class="img-fluid" src="@/assets/img/academy/header-academy-meetings.png" alt="" />
               </div>
             </div>
-          </div>
-          <div class="star-cons">
-            <img :src="`${publicPath}assets/img/starrrr.png`" alt="" />
-          </div>
-          <div class="star-cons stars">
-            <img :src="`${publicPath}assets/img/starsss.png`" alt="" />
           </div>
         </div>
       </div>
