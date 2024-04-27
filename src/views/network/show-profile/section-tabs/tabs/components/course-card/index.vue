437a458e3a0e72b7ec4b6a7c47b4bb15255839d0
Moath Mushtaha <moathmushtaha@gmail.com>
2024-04-27 23:02:40 +0300

    RD-46 Edit card design

---
+++ src/views/network/show-profile/section-tabs/tabs/components/course-card/index.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="d-flex flex-wrap card-item">
+    <div class="card-img-contaner">
+      <div class="course-department">{{ departmentName ?? "department" }}</div>
+      <img
+        height="150px"
+        class="card-img"
+        :src="image"
+        @error="handleImageError"
+      />
+    </div>
+    <hr />
+    <div class="card-title-box px-3">
+      <p class="card-title my-1 p-0">
+        {{ title }}
+      </p>
+      <div class="d-flex mb-2 align-items-center">
+        <i class="fa-solid fa-star star-icon" v-for="i in rate" :key="i"></i>
+        <i
+          class="fa-regular fa-star star-icon"
+          v-for="i in 5 - rate"
+          :key="i"
+        ></i>
+        <!-- <span class="mx-1">(15)</span> -->
+      </div>
+    </div>
+    <div class="card-link-box">
+      <router-link :to="getRouteLocale('academy-course-show', { id: item_id })">
+        <!-- ,lang : "ar" -->
+        <div @click="linkToCourse" class="shadow-sm arrow-box">
+          <i class="fa-solid fa-arrow-up-right-from-square"></i>
+        </div>
+      </router-link>
+      <span class="fw-bolder price-text"> {{ price ?? "--.--" }}ر.س </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "course-card",
+  props: {
+    item: {},
+  },
+  computed: {
+    title() {
+      return this.item.title;
+    },
+    price() {
+      return this.item.price;
+    },
+    image() {
+      return this.item.image_path;
+    },
+    name() {
+      return this.item.price;
+    },
+    departmentName() {
+      return this.item.department_name;
+    },
+    rate() {
+      if (this.item.rates.length > 0) {
+        // TO-DO : calculate avg
+        return 0;
+      }
+      return 4;
+    },
+    item_id() {
+      return this.item.id;
+    },
+  },
+  methods: {
+    handleImageError(event) {
+      // Handle the image error by replacing the src attribute with the placeholder image URL
+      event.target.src = "/assets/img/person1.png"; // Replace with your placeholder image URL
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card-item {
+  min-height: 290px;
+  border-radius: 12px;
+  background-color: #f9fafc;
+}
+.card-img-contaner {
+  position: relative;
+  margin: 12px;
+  width: 100%;
+  height: 150px;
+  border-radius: 6px;
+  overflow: hidden;
+}
+.card-img {
+  object-fit: cover;
+}
+.course-department {
+  position: absolute;
+  font-size: 12px;
+  padding-inline: 10px;
+  top: 5px;
+  left: 6px;
+  z-index: 119;
+  background-color: rgba(255, 255, 255, 0.6);
+  border-radius: 4px;
+}
+.card-title-box {
+  width: 100%;
+}
+.card-title {
+  font-size: 14px;
+  display: block; /* or inline-block */
+  text-overflow: ellipsis;
+  word-wrap: break-word;
+  overflow: hidden;
+  height: 3.6em;
+  line-height: 1.8em;
+}
+.card-link-box {
+  border-top: solid 1px #e7e9eb;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 10px 15px;
+}
+.star-icon {
+  font-size: 12px;
+  color: #ffc27a;
+}
+.arrow-box {
+  border-radius: 5.5px;
+  background-color: #f9fafc;
+  width: 32px;
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #6d737a;
+}
+.arrow-box:hover {
+  cursor: pointer;
+}
+.price-text {
+  color: #1fb9b3;
+  font-size: 15px;
+  font-weight: 400;
+}
+</style>
--- src/views/network/show-profile/section-tabs/tabs/your-courses/index.vue
+++ src/views/network/show-profile/section-tabs/tabs/your-courses/index.vue
@@ -1,53 +1,94 @@
 <template>
-    <div class="position-relative container" style="min-height:200px">
-        <d-single-list
-        :call-list="initializing" 
-        :paginate="3"
-        showMore="أعرض المزيد من الدورات"
-        noMoreItems='لا يوجد المزيد من الدورات'
-        style="min-height:200px"
-        >
-            <template v-slot="{item}">
-    <CardItem  
-        :image="item.image_path"
-        :title="item.title"
-        :name="item.user_info.name"
-        :price="item.price"
-        :url="getRouteLocale('academy-course',{id:item.id})"
-    />
-</template>
-    </d-single-list>
+  <div class="container" style="min-height: 200px">
+    <div class="show-profile-grid-container">
+      <div v-for="(item, i) in courses" :key="i" class="grid-item">
+        <CardItem :item="item" />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import instructorAPI from '@/services/api/academy/instructor';
-import CardItem from './card-item.vue';
+import instructorAPI from "@/services/api/academy/instructor";
+import CardItem from "../components/course-card";
+
 export default {
- name:'d-tab-pane-your-course',
- components:{
+  name: "d-tab-pane-your-course",
+  components: {
     CardItem,
- },
- data:()=>({
-    loading:false,
- }),
- methods:{
-   async initializing(metaInfo){
-    this.loading = true;
-    try {
-
-       return await instructorAPI.getCourses(metaInfo)
-      
-    } catch (error) {
-        //
-    }
-    this.loading = false;
-
-   }
- }
-}
+  },
+  data: () => ({
+    loading: false,
+    courses: [],
+  }),
+  methods: {
+    async initializing(metaInfo) {
+      console.log(metaInfo);
+      this.loading = true;
+      try {
+        const response = await instructorAPI.getCourses(metaInfo);
+        console.log("response : ", response);
+        this.courses = response.data.data.slice(0, 8);
+      } catch (error) {}
+      this.loading = false;
+    },
+  },
+  mounted() {
+    this.initializing();
+  },
+};
 </script>
-
-<style>
-
-</style>
+<!-- {
+  course object 
+  {
+    "id": 302,
+    "title": "معسكر التجميل",
+    "type": "on-site",
+    "place_map": "https://www.google.com/maps/place/%D8%B4%D8%B1%D9%83%D8%A9+%D8%B1%D9%8A%D8%A7%D8%AF%D9%8A%D8%A7%D8%AA+%D8%A7%D9%84%D9%85%D8%AD%D8%AF%D9%88%D8%AF%D8%A9%E2%80%AD/@24.7605706,46.7157664,15z/data=!4m6!3m5!1s0x3e2efdb4a5756abf:0x2f5774a23cbe4344!8m2!3d24.7605706!4d46.7157664!16s%2Fg%2F11h1m7_3v8?entry=ttu",
+    "desc": "برنامج متكامل يقدم من قبل خبراء ريادة الأعمال وخبراء التجميل من خلال مجموعة من الخدمات الإرشادية والتعليمية تقدم من قبل فريق مؤهل مهنيًا في مجال ريادة الأعمال والتجميل؛ وذلك لدعم وتوجيه رائدات الأعمال والشغوفات في مجال التجميل بآلية متكاملة وبرامج تدريبية مكثفة وعقد جلسات إلهامية وإرشادية جماعية وجلسات استشارية، ليساهم  في الانطلاق نحو عالم ريادة الأعمال وتأسيس مشاريع في مجال التجميل.",
+    "image_path": "http://back-end-new.test/uploads/academy/courses/9cIxUVaAONtMEB08cSYKnPTNOQrlBJB354XHxIOc.png",
+    "type_training": "prive",
+    "price": 2500,
+    "short_description": "",
+    "place_id": null,
+    "place_name": null,
+    "learn": [strings.............],
+    "start_time": "17:00",
+    "end_time": "21:00",
+    "user_info": {
+        "id": 98,
+        "name": "أكاديمية رياديات",
+        "image": "http://back-end-new.test/uploads/users/uYVnBzRsFGYyaTXEoBRbMNLwswPieCZvQGpghVxw.png",
+        "description": null,
+        "graduate": null,
+        "note": null,
+        "job_title": null
+    },
+    "attachments": [],
+    "rates": [],
+    "department_name": "أكاديمية التجميل",
+    "department_id": 60,
+    "status": "published",
+    "views": 131,
+    "start_date": "2024-05-11",
+    "course_days": [
+        "sunday",
+        "tuesday",
+    ],
+    "number_day_course": "7",
+    "map_address": {
+        "lat": 24.782369593602784,
+        "lng": 46.74201922753906,
+        "address_name": "RFDA6696، 6696 علي المروزي، 3986، حي الشهداء، الرياض 13241، السعودية"
+    }
+}
+==================================================================
+    metaInfo object
+    "current_page": 1,
+    "total": 10,
+    "last_page": 0,
+    "paginate": 2,
+    "to": 2,
+    "total_page": 5
+} -->
+<style></style>
