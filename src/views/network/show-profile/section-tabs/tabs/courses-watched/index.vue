d68994e6ae58a9aa85ec2d586c75f1e560b561ca
Moath Mushtaha <moathmushtaha@gmail.com>
2024-04-27 18:09:56 +0300

    RD-46 edit grid design

--- src/views/network/show-profile/section-tabs/tabs/courses-watched/index.vue
+++ src/views/network/show-profile/section-tabs/tabs/courses-watched/index.vue
@@ -1,50 +1,66 @@
 <template>
-  <div class="position-relative container" style="min-height:200px">
-      <d-single-list
-      :call-list="initializing" 
-      :paginate="3"
-      showMore="أعرض المزيد من المعارض"
-      noMoreItems='لا يوجد المزيد من المعارض'
-      style="min-height:200px"
-      >
-          <template v-slot="{item}">
-  <CardItem  
-  :item="item"
-
-  />
-</template>
-  </d-single-list>
+  <div class="container" style="min-height: 200px">
+    <div class="grid-container">
+      <div v-for="(item, i) in courses" :key="i" class="grid-item">
+        <CardItem :item="item" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
- import userAPI from '@/services/api/user.js'
-import CardItem from './card.vue';
+import userAPI from "@/services/api/user.js";
+import CardItem from "./card.vue";
 export default {
-name:'d-tab-pane-your-blogs',
-components:{
-  CardItem,
-},
-data:()=>({
-  loading:false,
-}),
-methods:{
- async initializing(metaInfo){
-  this.loading = true;
-  try {
-
-     return await userAPI.getCoursesUser(this.$route.params.id,metaInfo)
-    
-  } catch (error) {
-      //
-  }
-  this.loading = false;
-
- }
-}
-}
+  name: "d-tab-pane-your-blogs",
+  components: {
+    CardItem,
+  },
+  data: () => ({
+    loading: false,
+    courses: [],
+  }),
+  methods: {
+    async initializing(metaInfo) {
+      this.loading = true;
+      try {
+        const a = await userAPI.getCoursesUser(this.$route.params.id, metaInfo);
+        console.log("a : ", a);
+        this.courses = a.data.data.slice(0, 8);
+      } catch (error) {
+        //
+      }
+      this.loading = false;
+    },
+  },
+  mounted() {
+    this.initializing();
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.grid-container {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px 14px;
+}
+@media screen and (max-width: 1024px) {
+  .grid-container {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+@media screen and (max-width: 670px) {
+  .grid-container {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
 
+@media screen and (max-width: 376px) {
+  .grid-container {
+    padding-inline: 10px;
+    grid-template-columns: 1fr;
+  }
+}
 </style>
