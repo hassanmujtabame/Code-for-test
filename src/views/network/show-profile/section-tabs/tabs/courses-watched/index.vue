9c26db7c3ef40dfefb13bc1e329594c265574864
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-04-30 21:43:46 +0300

    add pagination bar

--- src/views/network/show-profile/section-tabs/tabs/courses-watched/index.vue
+++ src/views/network/show-profile/section-tabs/tabs/courses-watched/index.vue
@@ -4,12 +4,20 @@
     seconderyText="لم يشاهد هذا العضو أي دورة حتى الآن"
     v-if="isEmptyObject"
   />
-  <div v-else class="container" style="min-height: 200px">
-    <div class="show-profile-grid-container">
-      <div v-for="(item, i) in courses" :key="i" class="grid-item">
-        <CardItem :item="item" />
+  <div v-else>
+    <div class="container" style="min-height: 200px">
+      <div class="show-profile-grid-container">
+        <div v-for="(item, i) in inPageCourses" :key="i" class="grid-item">
+          <CardItem :item="item" />
+        </div>
       </div>
     </div>
+    <PaginationBar
+      :links="links"
+      :currentPage="currentPage"
+      :lastPage="lastPage"
+      @navigate="navigate"
+    />
   </div>
 </template>
 
@@ -17,37 +25,78 @@
 import userAPI from "@/services/api/user.js";
 import CardItem from "./card.vue";
 import EmptyCard from "../components/empty-card.vue";
+import PaginationBar from "../../pagination-bar.vue";
 export default {
   name: "d-tab-pane-course-watched",
   components: {
     CardItem,
     EmptyCard,
+    PaginationBar,
   },
   data: () => ({
+    texta_: "",
     loading: false,
     isEmptyObject: true,
-    courses: [],
+    responseData: {},
   }),
+  computed: {
+    currentPage() {
+      return this.responseData.meta.current_page;
+    },
+    lastPage() {
+      return this.responseData.meta.last_page;
+    },
+    links() {
+      return this.responseData.meta.links;
+    },
+    inPageCourses() {
+      return this.responseData.data.slice(0, 8);
+    },
+  },
   methods: {
     async initializing(metaInfo) {
+      let params = {
+        page: metaInfo.current_page,
+      };
       this.loading = true;
       try {
         const response = await userAPI.getCoursesUser(
           this.$route.params.id,
-          metaInfo
+          params
         );
-        this.courses = response.data.data;
-        if (this.courses.length > 0) {
+        this.responseData = response.data;
+        if (this.responseData.data.length > 0) {
           this.isEmptyObject = false;
         }
       } catch (error) {
-        //
+        console.log(error);
       }
       this.loading = false;
     },
+    clear() {
+      this.responseData.data = [];
+      this.isEmptyObject = true;
+    },
+    navigate(link) {
+      console.log("link", link);
+      if (link.label == "pagination.previous") {
+        if (this.currentPage == 1) {
+          return;
+        }
+        this.initializing({ current_page: this.currentPage - 1 });
+      }
+      if (link.label == "pagination.next") {
+        if (this.currentPage == this.lastPage) {
+          return;
+        }
+        this.initializing({ current_page: this.currentPage + 1 });
+      } else {
+        this.initializing({ current_page: link.label });
+      }
+    },
   },
   mounted() {
-    this.initializing();
+    this.initializing({ current_page: 1 });
   },
 };
 </script>
