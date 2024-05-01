cdaa8f2e103ae2880f990991315a6e5d636ded96
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-05-01 08:22:41 +0300

    Add pagination to your-courses section

--- src/views/network/show-profile/section-tabs/tabs/your-courses/index.vue
+++ src/views/network/show-profile/section-tabs/tabs/your-courses/index.vue
@@ -4,12 +4,20 @@
     seconderyText="لم يضف هذا العضو أي درس حتى الأن"
     v-if="isEmptyObject"
   />
-  <div v-else class="container" style="min-height: 200px">
-    <div class="show-profile-grid-container">
-      <div v-for="(item, i) in courses" :key="i" class="grid-item">
-        <CardItem :item="item" />
+  <div v-else>
+    <div class="container" style="min-height: 200px">
+      <div class="show-profile-grid-container">
+        <div v-for="(item, i) in inPageCourses" :key="i" class="grid-item">
+          <CardItem :item="item" />
+        </div>
       </div>
     </div>
+    <PaginationBar
+      :links="links"
+      :currentPage="currentPage"
+      :lastPage="lastPage"
+      @navigate="navigate"
+    />
   </div>
 </template>
 
@@ -17,36 +25,70 @@
 import instructorAPI from "@/services/api/academy/instructor";
 import CardItem from "../components/course-card";
 import EmptyCard from "../components/empty-card.vue";
-import { isEmptyObject } from "jquery";
+import PaginationBar from "@/components/pagination-card/index.vue";
 
 export default {
   name: "d-tab-pane-your-course",
   components: {
     CardItem,
     EmptyCard,
+    PaginationBar,
   },
   data: () => ({
     loading: false,
     isEmptyObject: true,
-    courses: [],
+    responseData: {},
   }),
 
+  computed: {
+    currentPage() {
+      return this.responseData.meta.current_page;
+    },
+    lastPage() {
+      return this.responseData.meta.last_page;
+    },
+    links() {
+      return this.responseData.meta.links;
+    },
+    inPageCourses() {
+      return this.responseData.data.slice(0, 8);
+    },
+  },
   methods: {
     async initializing(metaInfo) {
+      let params = {
+        page: metaInfo.current_page,
+      };
       this.loading = true;
       try {
-        const response = await instructorAPI.getCourses(metaInfo);
+        const response = await instructorAPI.getCourses(params);
 
-        this.courses = response.data.data.slice(0, 8);
-        if (this.courses.length > 0) {
+        this.responseData = response.data;
+        if (this.responseData.data.length > 0) {
           this.isEmptyObject = false;
         }
       } catch (error) {}
       this.loading = false;
     },
+    navigate(link) {
+      if (link.label == "pagination.previous") {
+        if (this.currentPage == 1) {
+          return;
+        }
+        this.initializing({ current_page: this.currentPage - 1 });
+      }
+      if (link.label == "pagination.next") {
+        if (this.currentPage == this.lastPage) {
+          return;
+        }
+        this.initializing({ current_page: this.currentPage + 1 });
+      } else {
+        this.initializing({ current_page: link.label });
+      }
+    },
   },
   mounted() {
-    this.initializing();
+    this.initializing({ current_page: 1 });
   },
 };
 </script>
