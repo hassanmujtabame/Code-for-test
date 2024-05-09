60128327d8e217636502cb152c72dc6b69726570
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-05-09 23:28:09 +0300

    RD-142 Edit dashboard Main design

--- src/views/academy/dashboard/index.vue
+++ src/views/academy/dashboard/index.vue
@@ -1,87 +1,33 @@
 <template>
-  <!-- <div style="margin-top: 8rem !important" class="col-md-10">
-          <div
-            v-if="closeModal"
-            style="background-color: #e5fbff"
-            class="d-flex my-5 justify-content-between px-3 align-items-start"
-          >
-            <div>
-              <h1>تهانينا</h1>
-              <p style="color: #888">
-                انت الان مدرب من ضمن افضل المدربين الخاصين بأكاديمية رياديات ,
-                شويه مميزات و تحميس انت الان مدرب من ضمن افضل المدربين الخاصين
-                بأكاديمية رياديات , شويه مميزات و تحميس انت الان مدرب من ضمن
-                افضل المدربين الخاصين بأكاديمية رياديات , شويه مميزات و تحميس
-              </p>
-            </div>
-            <button
-              @click="close"
-              class=""
-              style="
-                border: none;
-                background-color: transparent;
-                margin-top: 18px;
-              "
-            >
-              <svg
-                width="24"
-                height="25"
-                viewBox="0 0 24 25"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M13.4099 12.5002L17.7099 8.21019C17.8982 8.02188 18.004 7.76649 18.004 7.50019C18.004 7.23388 17.8982 6.97849 17.7099 6.79019C17.5216 6.60188 17.2662 6.49609 16.9999 6.49609C16.7336 6.49609 16.4782 6.60188 16.2899 6.79019L11.9999 11.0902L7.70994 6.79019C7.52164 6.60188 7.26624 6.49609 6.99994 6.49609C6.73364 6.49609 6.47824 6.60188 6.28994 6.79019C6.10164 6.97849 5.99585 7.23388 5.99585 7.50019C5.99585 7.76649 6.10164 8.02188 6.28994 8.21019L10.5899 12.5002L6.28994 16.7902C6.19621 16.8831 6.12182 16.9937 6.07105 17.1156C6.02028 17.2375 5.99414 17.3682 5.99414 17.5002C5.99414 17.6322 6.02028 17.7629 6.07105 17.8848C6.12182 18.0066 6.19621 18.1172 6.28994 18.2102C6.3829 18.3039 6.4935 18.3783 6.61536 18.4291C6.73722 18.4798 6.86793 18.506 6.99994 18.506C7.13195 18.506 7.26266 18.4798 7.38452 18.4291C7.50638 18.3783 7.61698 18.3039 7.70994 18.2102L11.9999 13.9102L16.2899 18.2102C16.3829 18.3039 16.4935 18.3783 16.6154 18.4291C16.7372 18.4798 16.8679 18.506 16.9999 18.506C17.132 18.506 17.2627 18.4798 17.3845 18.4291C17.5064 18.3783 17.617 18.3039 17.7099 18.2102C17.8037 18.1172 17.8781 18.0066 17.9288 17.8848C17.9796 17.7629 18.0057 17.6322 18.0057 17.5002C18.0057 17.3682 17.9796 17.2375 17.9288 17.1156C17.8781 16.9937 17.8037 16.8831 17.7099 16.7902L13.4099 12.5002Z"
-                  fill="#646466"
-                />
-              </svg>
-            </button>
-          </div> -->
-
   <div class="mx-3">
+    <WarningTape v-if="hasWarnig"> {{ warningMsg }} </WarningTape>
     <SectionWidgets v-if="userAcademyRole == 'student'" />
     <SectionInstructorWidgets v-if="userAcademyRole == 'instructor'" />
   </div>
 </template>
 
 <script>
-import SectionHeader from "./parts/section-header/index.vue";
-import SectionBalance from "./parts/section-balance/index.vue";
+import WarningTape from "@/components/widgets/warning-tape.vue";
 import SectionWidgets from "./parts/section-widgets/index.vue";
-import SectionAsk from "@/components/ask-us.vue";
 import SectionInstructorWidgets from "./parts/section-instructor-widgets/index.vue";
-import userAPI from "../../../services/api/user";
 export default {
   name: "your-courses-page",
   components: {
-    SectionHeader,
-    SectionBalance,
-    SectionAsk,
+    WarningTape,
     SectionWidgets,
     SectionInstructorWidgets,
   },
   data() {
     return {
       currentUser: {},
-      closeModal: true,
+      hasWarnig: true,
+      warningMsg: this.$t(
+        "remember-that-you-cannot-create-courses-before-creating-at-least-3-meetings-to-activate-your-account"
+      ),
     };
   },
-  methods: {
-    close() {
-      this.closeModal = false;
-    },
-    async getUser() {
-      let { data } = await userAPI.me();
-      if (data.success) {
-        this.currentUser = data.data;
-      } else {
-        this.hasError = data.message;
-      }
-    },
-  },
-  mounted() {
-    this.getUser();
-  },
+  methods: {},
+  mounted() {},
 };
 </script>
 
