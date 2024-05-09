0b50f737652c037f31a6b665b8c34a840a602839
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-05-09 14:15:49 +0300

    RD-139 Show lectures in course page

--- src/views/academy/courses/show/guest/sections/section-lectures/index.vue
+++ src/views/academy/courses/show/guest/sections/section-lectures/index.vue
@@ -1,96 +1,109 @@
 <template>
   <div id="course-contents" class="mt-4">
-    <div>
-      <h1 class="course-guest-section__title">{{ $t('course-contents') }}</h1>
-      <d-expanded-panel :id="('accordionFlushExample')"  >
-        <d-expanded-panel-item :title="$t('lectures')" :opened='true' >
-          <div v-for="(lecture, c) in lectures" :key="('c' + c)" class="course-lectures__item">
+    <div class="w-100">
+      <h1 class="course-guest-section__title">{{ $t("course-contents") }}</h1>
+      <d-expanded-panel :id="'accordionFlushExample'">
+        <d-expanded-panel-item :title="$t('lectures')" :opened="true">
+          <div
+            v-for="(lecture, c) in lectures"
+            :key="'c' + c"
+            class="course-lectures__item"
+          >
             <div class="course-lectures__item-tile">{{ lecture.title }}</div>
-            <div v-if="lecture.is_free === true || c === 0" class="course-lectures__item-action">
-              <a href="#" @click="watchVideo(lecture, $event)" class="m-c"><i class="fa fa-circle-play"></i> شاهد مجانا</a>
+            <div
+              v-if="lecture.is_free === true || c === 0"
+              class="course-lectures__item-action"
+            >
+              <a href="#" @click="watchVideo(lecture, $event)" class="m-c"
+                ><i class="fa fa-circle-play"></i> شاهد مجانا</a
+              >
             </div>
           </div>
         </d-expanded-panel-item>
       </d-expanded-panel>
     </div>
     <WatchVideo />
-
   </div>
 </template>
 
 <script>
-import academyAPI from '@/services/api/academy'
-import WatchVideo from '../../dialogs/watch-video/index.vue'
+import academyAPI from "@/services/api/academy";
+import WatchVideo from "../../dialogs/watch-video/index.vue";
 export default {
-  name: 'section-lectures',
+  name: "section-lectures",
   props: {
-    itemPage: {}
+    itemPage: {},
   },
   components: {
-    WatchVideo
+    WatchVideo,
   },
   data: () => ({
     loading: false,
     lectures: [
-      { title: 'مقدمة', is_free: true },
-      { title: 'درس الاول' },
-      { title: 'درس الثاني' },
-    ]
+      // { title: "مقدمة", is_free: true },
+      // { title: "درس الاول" },
+      // { title: "درس الثاني" },
+    ],
   }),
   methods: {
     watchVideo(lecture, evt) {
       if (evt) evt.preventDefault();
-      console.log('tst')
+      console.log("tst");
 
-      this.fireOpenDialog('watch-lecture-course-dialog', { page: this.itemPage, item: lecture })
+      this.fireOpenDialog("watch-lecture-course-dialog", {
+        page: this.itemPage,
+        item: lecture,
+      });
     },
     async initializing() {
       this.loading = true;
       try {
-        let { data } = await academyAPI.lecturesAPI.getAll(this.itemPage.id)
+        let { data } = await academyAPI.lecturesAPI.getAll(this.itemPage.id);
         if (data.success) {
-          this.lectures = data.data.map(l => {
-            return { is_completed: false, ...l, uuid: this.generateRandomString(16) }
-
-          })
+          this.lectures = data.data.map((l) => {
+            return {
+              is_completed: false,
+              ...l,
+              uuid: this.generateRandomString(16),
+            };
+          });
         }
       } catch (error) {
-        console.mylog('error', error)
+        console.mylog("error", error);
       }
       this.loading = false;
-
-    }
+    },
   },
   mounted() {
-    this.initializing()
+    this.initializing();
   },
-}
+};
 </script>
 
-<style >
+<style>
 #course-contents {
   max-width: 628px;
 }
 
 #course-contents .accordion-header {
-  background: #FDFDFD;
+  background: #fdfdfd;
   /* Boarders */
 
-  border: 0.5px solid #D1D1D1;
+  border: 0.5px solid #d1d1d1;
   border-radius: 8px 8px 0px 0px;
 }
 
 #course-contents .accordion-body {
-  padding: 0
+  padding: 0;
 }
 
 #course-contents .course-lectures__item {
   /* Turquoise background */
   height: 46px;
-  background: #F6F8F9;
+  background: #f6f8f9;
   /* Boarders */
 
-  border: 0.5px solid #D1D1D1;
+  border: 0.5px solid #d1d1d1;
   border-radius: 8px;
   display: flex;
   align-content: center;
@@ -107,7 +120,6 @@ export default {
   font-size: 16px;
   line-height: 28px;
   /* identical to box height, or 175% */
-
 
   /* Medium gray */
 
