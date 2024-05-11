0ac2d378e182423d52764866c2d9d39d0e8a339e
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-05-11 16:43:36 +0300

    RD-157 Edit desgin

--- src/views/academy/my-schedules/index.vue
+++ src/views/academy/my-schedules/index.vue
@@ -1,25 +1,27 @@
 <template>
-  <div style="margin-top: 85px " class="consult max-width-100-hidden">
-      <div class="container">
+  <div style="margin-top: 95px">
+    <div class="container">
+      <div class="d-flex justify-content-between">
         <h1 class="home-section-title">جدول مواعيدي</h1>
-        <p  class="home-section-text">هنا جدول مواعيد الدورات والندوات الأونلاين وفي المقر</p>
-         <SectionFilterList />
+        <button class="btn btn-danger px-5 my-3">
+          <i class="fa-solid fa-plus"></i> {{ $t("add-schedule") }}
+        </button>
       </div>
+
+      <SectionFilterList />
+    </div>
   </div>
 </template>
 
 <script>
-import SectionFilterList from './parts/section-filter-list/index'
+import SectionFilterList from "./parts/section-filter-list/index";
 export default {
-name:'ready-services',
-components:{
-
-  SectionFilterList
-},
-data:()=>({})
-}
+  name: "ready-services",
+  components: {
+    SectionFilterList,
+  },
+  data: () => ({}),
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
