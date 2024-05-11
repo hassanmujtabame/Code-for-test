bd0f51458b5e2ba2275edbe8498b8ae736f20008
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-05-11 18:13:35 +0300

    RD-158 Add 'Add new schedule' dialog

--- src/views/academy/my-schedules/index.vue
+++ src/views/academy/my-schedules/index.vue
@@ -3,24 +3,38 @@
     <div class="container">
       <div class="d-flex justify-content-between">
         <h1 class="home-section-title">جدول مواعيدي</h1>
-        <button class="btn btn-danger px-5 my-3">
+        <button @click="addSchedule" class="btn btn-danger px-5 my-3">
           <i class="fa-solid fa-plus"></i> {{ $t("add-schedule") }}
         </button>
       </div>
 
       <SectionFilterList />
+      <DialogNewSchedule @success="updateEventSuccess" />
     </div>
   </div>
 </template>
 
 <script>
 import SectionFilterList from "./parts/section-filter-list/index";
+import DialogNewSchedule from "./dialogs/new-schedule/index.vue";
+
 export default {
   name: "ready-services",
   components: {
     SectionFilterList,
+    DialogNewSchedule,
   },
   data: () => ({}),
+  methods: {
+    addSchedule() {
+      this.fireOpenDialog("new-schedule", this.eventSelected);
+    },
+    updateEventSuccess(item) {
+      // let index = this.items.findIndex((x) => x.id == item.id);
+      // this.items[index] = Object.assign(this.items[index], item);
+      // this.updateAttributes();
+    },
+  },
 };
 </script>
 
