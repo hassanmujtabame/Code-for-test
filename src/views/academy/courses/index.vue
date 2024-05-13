a0ebd5007147d6a9673597a61872c7bd9faecffa
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-05-13 06:34:37 +0300

    Foramt code

--- src/views/academy/courses/index.vue
+++ src/views/academy/courses/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="margin-top: 85px " class="consult">
+  <div style="margin-top: 85px" class="consult">
     <!--header -->
     <SectionHeader />
     <div class="container mt-5">
@@ -9,17 +9,16 @@
 </template>
 
 <script>
-import SectionHeader from './parts/section-header/index';
-import SectionFilterList from './parts/section-filter-list/index';
+import SectionHeader from "./parts/section-header/index";
+import SectionFilterList from "./parts/section-filter-list/index";
 
 export default {
-  name: 'courses-page',
+  name: "courses-page",
   components: {
     SectionHeader,
     SectionFilterList,
-
-  }
-}
+  },
+};
 </script>
 
 <style></style>
--- src/views/academy/courses/show/on-site/index.vue
+++ src/views/academy/courses/show/on-site/index.vue
@@ -1,58 +1,57 @@
 <template>
-  <div style="margin-top: 85px " class="consult">
-     <div class="container mt-5">
-         <div class="course-show-page">
-          <SectionHeaderOwner  :itemPage="itemPage" v-if="isOwner && userAcademyRole == 'instructor'"/>
-     <SectionHeader  :itemPage="itemPage" v-else />
-     <SectionBody  :itemPage="itemPage" :isOwner="isOwner"/>
-     <updateCourseDialog group="update-course" :isOwner="isOwner" />
- </div>
- </div>
-   </div>
- </template>
- 
- <script>
- import SectionHeader from './sections/section-header/index.vue'
- import SectionHeaderOwner from './sections/section-header-owner/index.vue'
- import SectionBody from './sections/section-body/index.vue'
- import updateCourseDialog from '@/views/academy/instructor/your-courses/dialogs/add-course/on-site-course'
+  <div style="margin-top: 85px" class="consult">
+    <div class="container mt-5">
+      <div class="course-show-page">
+        <SectionHeaderOwner
+          :itemPage="itemPage"
+          v-if="isOwner && userAcademyRole == 'instructor'"
+        />
+        <SectionHeader :itemPage="itemPage" v-else />
+        <SectionBody :itemPage="itemPage" :isOwner="isOwner" />
+        <updateCourseDialog group="update-course" :isOwner="isOwner" />
+      </div>
+    </div>
+  </div>
+</template>
 
- //import coursesAPI from '@/services/api/academy/courses'
- export default {
-  name:'course-show-on-site',
-  props:{
-    itemPage:{},
-    isOwner:{
-      type:Boolean,
-      default:false,
-    }
-  },
-  components:{
-     SectionHeader,
-     SectionBody,
-     SectionHeaderOwner,
-     updateCourseDialog
-  },
-  data:()=>{
-     return {
-         loading:false,
-         hasError:false,
-     }
-  },
-  methods:{
-   
-   },
-   mounted(){
+<script>
+import SectionHeader from "./sections/section-header/index.vue";
+import SectionHeaderOwner from "./sections/section-header-owner/index.vue";
+import SectionBody from "./sections/section-body/index.vue";
+import updateCourseDialog from "@/views/academy/instructor/your-courses/dialogs/add-course/on-site-course";
 
-   }
- }
- </script>
- 
- <style scoped>
- .course-show-page{
-     width: 100%;
-     background: white;
-     padding: 5px;
-     border-radius: 10px 20px;
- }
- </style>
+//import coursesAPI from '@/services/api/academy/courses'
+export default {
+  name: "course-show-on-site",
+  props: {
+    itemPage: {},
+    isOwner: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  components: {
+    SectionHeader,
+    SectionBody,
+    SectionHeaderOwner,
+    updateCourseDialog,
+  },
+  data: () => {
+    return {
+      loading: false,
+      hasError: false,
+    };
+  },
+  methods: {},
+  mounted() {},
+};
+</script>
+
+<style scoped>
+.course-show-page {
+  width: 100%;
+  background: white;
+  padding: 5px;
+  border-radius: 10px 20px;
+}
+</style>
