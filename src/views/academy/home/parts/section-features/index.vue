9750cfdae8e41d02d330ec448a1d02b67fc0c48a
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-05-15 15:03:53 +0300

    Edit and refactor features section

--- src/views/academy/home/parts/section-features/index.vue
+++ src/views/academy/home/parts/section-features/index.vue
@@ -2,26 +2,17 @@
   <div class="container my-5">
     <h2 class="home-section-title text-center">مزايا أكاديمية رياديات</h2>
     <div class="row min-riadiat academy">
-      <div class="col-md-3 mt-2">
-        <CardItem :src="`${publicPath}assets/img/Group-academy.png`">
-          دورت و معسكرات تدريبة مميـــــــــزة
-        </CardItem>
-      </div>
-      <div class="col-md-3 mt-2">
-        <CardItem :src="`${publicPath}assets/img/Group-academy-2.png`">
-          جلسة ارشاديــــــــة جماعية عن بعـــــــد
-        </CardItem>
-      </div>
-      <div class="col-md-3 mt-2">
-        <CardItem :src="`${publicPath}assets/img/Group-aca-3.png`">
-          برامج التــــــــــــدريب يقدمها نخبة مــــــن رائدات
-          الاعمــــــــــال
-        </CardItem>
-      </div>
-      <div class="col-md-3 mt-2">
-        <CardItem :src="`${publicPath}assets/img/Group-aca-4.png`">
-          الانضمام لمجموعات متخصصة فــي مجال اهتمامـــــــــــــــــــك
-        </CardItem>
+      <div
+        v-for="(item, index) in itemsData"
+        :key="index"
+        class="col-md-3 my-2"
+      >
+        <CardItem
+          :src="item.src"
+          :title="item.title"
+          :description="item.description"
+          :isDark="(index % 2 === 0) ^ (index > 3)"
+        />
       </div>
     </div>
   </div>
@@ -30,8 +21,68 @@
 import CardItem from "./card-item";
 export default {
   name: "academy-features",
+  data(vm) {
+    return {};
+  },
   components: {
     CardItem,
+  },
+
+  mounted() {
+    console.log("vm : ", this);
+  },
+  computed: {
+    itemsData() {
+      return [
+        {
+          title: "أكسب المال",
+          description:
+            "اربح المال في كل دورة فرديه يقوم فيها أحد بالتسجيل في دورتك ، واحصل على مبالغ شهرية من خلال التحويل البنكي لك",
+          src: `${this.publicPath}assets/img/fi_584067.png`,
+        },
+        {
+          title: "إلهام الآخرين ",
+          description:
+            "ساعد المتدربين في تعلم مهارات جديدة ، وتطوير حياتهم المهنية، واكتشاف هوياتهم من خلال مشاركة خبراتك ومعارفك",
+          src: `${this.publicPath}assets/img/fi_1411586.png`,
+        },
+        {
+          title: "منصة إلكترونية",
+          description:
+            "تتضمن الاحتضان والتدريب والاستشارات في مجتمع مهتم بالمجال ",
+          src: `${this.publicPath}assets/img/fi_896530.png`,
+        },
+        {
+          title: "المرونة",
+          description:
+            "المرونة والتنوع في الدورات بين حضورية والمسجلة ودورات عن بعد",
+          src: `${this.publicPath}assets/img/fi_8718268.png`,
+        },
+        {
+          title: "التسويق",
+          description: "التسويق من منصة رياديات ومن قبلك للدورة التدريبية",
+          src: `${this.publicPath}assets/img/fi_1997928.png`,
+        },
+        {
+          title: "مجتمع متخصص",
+          description:
+            "برامج مباشرة ومسجلة وحضورية تقدم عن بعد  وفي المقرات من خلال التطبيق والموقع الإلكتروني",
+          src: `${this.publicPath}assets/img/fi_4350908.png`,
+        },
+        {
+          title: "برامج مختلفة",
+          description:
+            "ساعد المتدربين في تعلم مهارات جديدة ، وتطوير حياتهم المهنية، واكتشاف هوياتهم من خلال مشاركة خبراتك ومعارفك",
+
+          src: `${this.publicPath}assets/img/fi_2853364.png`,
+        },
+        {
+          title: "الاستشارات",
+          description: "فرص انضمامك كمستشارة في رياديات. ",
+          src: `${this.publicPath}assets/img/fi_2300422.png`,
+        },
+      ];
+    },
   },
 };
 </script>
