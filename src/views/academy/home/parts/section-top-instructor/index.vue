e952e939ff0a91f7fac76b0f84eb075e8bc6cb76
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-05-16 15:51:02 +0300

    Fix hime page bugs

--- src/views/academy/home/parts/section-top-instructor/index.vue
+++ src/views/academy/home/parts/section-top-instructor/index.vue
@@ -1,38 +1,40 @@
 <template>
   <div class="bg-1 px-5 position-relative mt-5">
-    <div class="row align-items-center justify-content-center m-auto px-3">
-      <div class="col-md-6 m-auto">
-        <h1 class="qustion">هل أنت مدرب مميز ؟</h1>
-        <p class="section-text">
-          أكاديمية رياديات تضم العديد من المدربين المميزين في الممكلة و الوطن
-          العربي يمكنك أن تنضم لهذه العائلة الآن !
-        </p>
+    <div class="container">
+      <div class="row align-items-center justify-content-center m-auto px-3">
+        <div class="col-md-6 m-auto">
+          <h1 class="qustion">هل أنت مدرب مميز ؟</h1>
+          <p class="section-text">
+            أكاديمية رياديات تضم العديد من المدربين المميزين في الممكلة و الوطن
+            العربي يمكنك أن تنضم لهذه العائلة الآن !
+          </p>
 
-        <router-link
-          :to="getRouteLocale('academy-speical')"
-          class="btn rounded-3 px-5 py-3 mt-4 text-white bg-transpernt btn-text"
-          style="background-color: #1fb9b3"
-        >
-          {{ $t("knows-more") }}
-        </router-link>
+          <router-link
+            :to="getRouteLocale('academy-speical')"
+            class="btn rounded-3 px-5 py-3 mt-4 text-white bg-transpernt btn-text"
+            style="background-color: #1fb9b3"
+          >
+            {{ $t("knows-more") }}
+          </router-link>
+        </div>
+        <div class="pt-5 col-md-6 position-relative">
+          <img
+            class="img-fluid"
+            src="../../../../../assets/img/academy/Col.png"
+            alt=""
+          />
+        </div>
       </div>
-      <div class="pt-5 col-md-6 position-relative">
+
+      <div class="star-big position-absolute">
         <img
-          class="img-fluid"
-          src="../../../../../assets/img/academy/Col.png"
+          height=" 100%"
+          width="auto"
+          class="landing"
+          :src="`${publicPath}assets/svg/star-big.svg`"
           alt=""
         />
       </div>
-    </div>
-
-    <div class="star-big position-absolute">
-      <img
-        height=" 100%"
-        width="auto"
-        class="landing"
-        :src="`${publicPath}assets/svg/star-big.svg`"
-        alt=""
-      />
     </div>
   </div>
 </template>
