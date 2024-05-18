dc062bb48b0667aef5e6a8b546d37a5dd3c98ebe
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-05-18 20:15:50 +0300

    Add getMembersHomeData request

--- src/services/api-v2/network/members.js
+++ src/services/api-v2/network/members.js
@@ -4,6 +4,10 @@ class MembersAPI_V2 extends BaseApi {
     let s = this.generateQueryUrl(params);
     return window.axios_v2.get(`network/members/search?${s}`);
   }
+
+  getMembersHomeData() {
+    return window.axios_v2.get("network/members");
+  }
 }
 
 export default new MembersAPI_V2();
