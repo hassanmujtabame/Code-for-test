28f43a6974a5259e0c1b8349ad6aa3620df1207a
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-05-22 17:44:35 +0300

    Foramt code in body section

--- src/views/academy/courses/show/recorded/joined/sections/section-body/add-lecture-btn.vue
+++ src/views/academy/courses/show/recorded/joined/sections/section-body/add-lecture-btn.vue
@@ -1,22 +1,38 @@
 <template>
   <div class="dropdown" @click="toggleDropdown">
-    <button class="btn m-c dropdown-toggle" type="button" :aria-expanded="isDropdownOpen">
+    <button
+      class="btn m-c dropdown-toggle"
+      type="button"
+      :aria-expanded="isDropdownOpen"
+    >
       <i class="fa-solid fa-square-plus"></i>
     </button>
-    <ul class="dropdown-menu" :class="{ 'show': isDropdownOpen }">
-      <li><a class="dropdown-item" href="#" @click="selectAction('lecture')">{{$t('add-lecture')}}</a></li>
-      <li><a class="dropdown-item" href="#" @click="selectAction('exam')">{{$t('add-exam')}}</a></li>
-      <li><a class="dropdown-item" href="#" @click="selectAction('project')">{{$t('add-project')}}</a></li>
+    <ul class="dropdown-menu" :class="{ show: isDropdownOpen }">
+      <li>
+        <a class="dropdown-item" href="#" @click="selectAction('lecture')">{{
+          $t("add-lecture")
+        }}</a>
+      </li>
+      <li>
+        <a class="dropdown-item" href="#" @click="selectAction('exam')">{{
+          $t("add-exam")
+        }}</a>
+      </li>
+      <li>
+        <a class="dropdown-item" href="#" @click="selectAction('project')">{{
+          $t("add-project")
+        }}</a>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-import $ from 'jquery'
+import $ from "jquery";
 export default {
   data() {
     return {
-      isDropdownOpen: false
+      isDropdownOpen: false,
     };
   },
   methods: {
@@ -26,15 +42,13 @@ export default {
     selectAction(type) {
       // Implement your logic here based on the selected action type
       // For instance, emitting an event to the parent component
-      console.log('sss')
-      this.$emit('add', type);
+      console.log("sss");
+      this.$emit("add", type);
       // Close the dropdown after an action is selected
       this.isDropdownOpen = false;
-    }
+    },
   },
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
