144f6e6a2bd603ee6ec5266d1fb19f6c562a00da
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-05-29 22:55:32 +0300

    Refactor : Edit feature section design

--- src/views/affiliate-marketing/home/parts/section-features/index.vue
+++ src/views/affiliate-marketing/home/parts/section-features/index.vue
@@ -1,48 +1,17 @@
 <template>
   <div class="mt-5 p-5">
-    <h2 class="text-center">
-      <!-- <span style="color: #ffcb00"> مزايا التسويق </span> -->
-      <!-- <span style=""> مزايا التسويق </span> -->
-      مزايا التسويق بالعمولة من خلال رياديات
-    </h2>
-    <div class="container-fluid">
-      <div class="row min-riadiat network">
-        <div class="col-md-3 mt-2">
-          <div class="box border d-flex gap-4 align-items-center rounded-2">
-            <div>
-              <img
-                :src="`${publicPath}assets/svg/ads-statics-chat.svg`"
-                alt=""
-              />
-            </div>
-            <div>التسويق بالروابط وتتابعها باكواد</div>
+    <h2 class="text-center">مزايا التسويق بالعمولة من خلال رياديات</h2>
+    <div class="container">
+      <div class="row features-row">
+        <div
+          class="col-md-3 mt-2 feature-item"
+          v-for="(item, i) in features"
+          :key="i"
+        >
+          <div class="feature-img">
+            <img width="100%" :src="item.img" alt="" />
           </div>
-        </div>
-        <div class="col-md-3 mt-2">
-          <div class="box border d-flex align-items-center gap-4 rounded-2">
-            <div>
-              <img :src="`${publicPath}assets/svg/money-bills.svg`" alt="" />
-            </div>
-            <div>أعلى نسبة عمولة تصــــــل الى 20%</div>
-          </div>
-        </div>
-        <div class="col-md-3 mt-2">
-          <div class="box border d-flex align-items-center gap-4 rounded-2">
-            <div>
-              <img :src="`${publicPath}assets/svg/watch-course.svg`" alt="" />
-            </div>
-            <div>
-              دورات متــــــخصصة ومجانية للمسوقين المعتمديـــــــــــــــن
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3 mt-2">
-          <div class="box border d-flex align-items-center gap-4 rounded-2">
-            <div>
-              <img :src="`${publicPath}assets/svg/earn-money.svg`" alt="" />
-            </div>
-            <div>سهولة الربــح وزيادة الدخــل</div>
-          </div>
+          <div class="feature-title mt-3">{{ item.title }}</div>
         </div>
       </div>
     </div>
@@ -52,7 +21,55 @@
 <script>
 export default {
   name: "section-3",
+  data(vm) {
+    return {
+      features: [
+        {
+          img: require("@/assets/img/Frame76467.png"),
+          title: "التسويق بالروابط وتتابعها باكواد",
+        },
+        {
+          img: require("@/assets/img/fi_879757.png"),
+          title: "أعلى نسبة عمولة تصــــــل الى 20%",
+        },
+        {
+          img: require("@/assets/img/fi_7781407.png"),
+          title: "دورات متــــــخصصة ومجانية للمسوقين المعتمدين",
+        },
+        {
+          img: require("@/assets/img/fi_1435715.png"),
+          title: "سهولة الربــح وزيادة الدخــل",
+        },
+      ],
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+h2 {
+  font-family: Cairo;
+  font-size: 48px;
+  font-weight: 700;
+  line-height: 89.95px;
+  color: #424143;
+}
+.features-row {
+  padding: 80px 56px 80px 56px;
+  border-radius: 24px;
+  background: linear-gradient(180deg, #def7f6 0%, rgba(255, 255, 255, 0) 100%);
+}
+.feature-item {
+  padding-inline-end: 30px;
+}
+.feature-img {
+  width: 32%;
+}
+.feature-title {
+  font-family: Cairo;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 26.4px;
+  color: #2cb7b3;
+}
+</style>
