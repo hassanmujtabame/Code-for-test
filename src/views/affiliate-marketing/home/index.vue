cf241107afaca5b1c8c0862cb7ee1344d40614a7
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-05-30 14:47:05 +0300

    Refactor : common section design temp data until backEnd changes

--- src/views/affiliate-marketing/home/index.vue
+++ src/views/affiliate-marketing/home/index.vue
@@ -14,7 +14,7 @@
     </div> -->
 
     <!-- <SectionDoubleWinings /> -->
-    <SectionGraduated />
+    <!-- <SectionGraduated /> -->
     <!-- <div class="my-5">
       <SectionWinning />
     </div> -->
@@ -23,7 +23,7 @@
     <SectionGraduated title=" المسوقين الاكثر ربحا " />
     </div> -->
     <!-- <SectionRecentCourses /> -->
-    <SectionCommonQuistions />
+    <CommonQuistions :questions="commonQuestions" />
     <SectionContinueLearning />
     <!-- <SectionHear :itemPage="itemPage" /> -->
   </div>
@@ -43,7 +43,7 @@ import SectionDoubleWinings from "./parts/section-double-winings/index.vue";
 import SectionRecognize from "./parts/section-recognize-our-marketing/index.vue";
 import SectionGraduated from "./parts/section-graduated/index.vue";
 import SectionRecentCourses from "./parts/section-recent-courses-online/index.vue";
-import SectionCommonQuistions from "./parts/section-common-quistions/index.vue";
+import CommonQuistions from "@/components/common-questions-section/index.vue";
 import SectionContinueLearning from "@/views/incubator/home/parts/section-continue-learning/index.vue";
 import SectionHear from "./parts/section-hear/index.vue";
 import incubatorAPI from "@/services/api/incubator";
@@ -54,6 +54,34 @@ export default {
     loading: true,
     hasError: false,
     itemPage: {},
+    commonQuestions: [
+      {
+        question: "كيف يمكنني البدء في العمل كمسوق بالعمولة على هذه المنصة؟",
+        answer:
+          "نص بديل | الإجابة تعتمد على العديد من العوامل المختلفة. للحصول على أفضل رد، يُفضل توضيح السؤال بشكل أكبر وتقديم المزيد من التفاصيل. من المهم أيضًا مراعاة السياق والظروف المحيطة بالموضوع. إذا كنت بحاجة إلى مساعدة إضافية أو معلومات محددة، فلا تتردد في توضيح المزيد",
+      },
+      {
+        question: "ما هي الخطوات الضرورية لتحقيق النجاح كمسوق بالعمولة؟",
+        answer:
+          "نص بديل | الإجابة تعتمد على العديد من العوامل المختلفة. للحصول على أفضل رد، يُفضل توضيح السؤال بشكل أكبر وتقديم المزيد من التفاصيل. من المهم أيضًا مراعاة السياق والظروف المحيطة بالموضوع. إذا كنت بحاجة إلى مساعدة إضافية أو معلومات محددة، فلا تتردد في توضيح المزيد",
+      },
+      {
+        question: "ما هي العروض المتاحة للترويج لها والحصول على عمولة؟",
+        answer:
+          "نص بديل | الإجابة تعتمد على العديد من العوامل المختلفة. للحصول على أفضل رد، يُفضل توضيح السؤال بشكل أكبر وتقديم المزيد من التفاصيل. من المهم أيضًا مراعاة السياق والظروف المحيطة بالموضوع. إذا كنت بحاجة إلى مساعدة إضافية أو معلومات محددة، فلا تتردد في توضيح المزيد",
+      },
+      {
+        question:
+          "هل هناك قيود على نوعية المنتجات أو الخدمات التي يمكنني الترويج لها؟",
+        answer:
+          "نص بديل | الإجابة تعتمد على العديد من العوامل المختلفة. للحصول على أفضل رد، يُفضل توضيح السؤال بشكل أكبر وتقديم المزيد من التفاصيل. من المهم أيضًا مراعاة السياق والظروف المحيطة بالموضوع. إذا كنت بحاجة إلى مساعدة إضافية أو معلومات محددة، فلا تتردد في توضيح المزيد",
+      },
+      {
+        question: "كيف يتم تتبع العمليات وحساب العمولات المكتسبة؟",
+        answer:
+          "نص بديل | الإجابة تعتمد على العديد من العوامل المختلفة. للحصول على أفضل رد، يُفضل توضيح السؤال بشكل أكبر وتقديم المزيد من التفاصيل. من المهم أيضًا مراعاة السياق والظروف المحيطة بالموضوع. إذا كنت بحاجة إلى مساعدة إضافية أو معلومات محددة، فلا تتردد في توضيح المزيد",
+      },
+    ],
   }),
   components: {
     SectionWhatWeProvide,
@@ -70,7 +98,7 @@ export default {
     SectionRecentCourses,
     SectionContinueLearning,
     SectionHear,
-    SectionCommonQuistions,
+    CommonQuistions,
     SectionWinning,
   },
   methods: {
