1f6fda74bcbf67eed2c3774bda6343d88e88f0b7
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-05-30 14:46:37 +0300

    Refactor : common section design create a new component

---
+++ src/components/common-questions-section/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container py-5">
+    <div class="title-text">{{ $t("common-questions") }}</div>
+    <div>
+      <QuestionTile
+        v-for="(question, index) in questions"
+        :key="index"
+        :question="question"
+        class="mt-3"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import QuestionTile from "./questionTile.vue";
+export default {
+  name: "common-questions-section",
+  components: {
+    QuestionTile,
+  },
+  props: {
+    questions: [],
+  },
+};
+</script>
+
+<style scoped>
+.title-text {
+  font-family: Cairo;
+  font-size: 56px;
+  font-weight: 600;
+  line-height: 61.6px;
+  text-align: center;
+  color: #2cb7b3;
+}
+</style>
