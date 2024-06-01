15dbb82ec6b33b055ae64410677da6f6771acae3
Ahmed AbuSamra <ahmed.w.abusamra@gmail.com>
2024-06-01 17:38:38 +0300

    Edit operation page

--- src/views/affiliate-marketing/dashboard/operations/index.vue
+++ src/views/affiliate-marketing/dashboard/operations/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>operations</div>
+  <div>
+    <div class="d-flex justify-content-between">
+      <div class="title-text">{{ $t("operations") }}</div>
+      <div>
+        <button class="btn btn-default">
+          استيراد البيانات
+          <i class="fa-solid fa-download" style="color: #1fb9b3"></i>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +18,19 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.title-text {
+  font-family: Cairo;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 36px;
+  color: #101828;
+}
+.btn-default {
+  font-family: Cairo;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 24px;
+  color: #344054;
+}
+</style>
